b18ef2959dfea1ae15a0c23f49f67d6ede05bb45
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-02 15:59:19 +0800

    removeData

--- v1/db_migration/V2015_06_01_14_41__remove_test_data.sql
+++ v1/db_migration/V2015_06_01_14_41__remove_test_data.sql
@@ -5,11 +5,3 @@ UPDATE `loan_repay` SET `status` = 'test';
 UPDATE `invest` SET `status` = 'test';
 
 UPDATE `invest_repay` SET `status` = 'test';
-
-TRUNCATE TABLE `user_bill`;
-
-TRUNCATE TABLE `system_bill`;
-
-TRUNCATE TABLE `recharge`;
-
-TRUNCATE TABLE `withdraw_cash`;
