ec36ece6783a02bfdf2e30bcd5b5444ddd93700d
Shi Lei <mleoshi@gmail.com>
2015-06-05 19:10:40 +0800

    fix status issue

--- v1/src/main/java/com/esoft/jdp2p/loan/service/impl/LoanCalculatorImpl.java
+++ v1/src/main/java/com/esoft/jdp2p/loan/service/impl/LoanCalculatorImpl.java
@@ -106,8 +106,8 @@ public class LoanCalculatorImpl implements LoanCalculator {
 		// 统计所有的此借款的投资信息，求和做减法，得出尚未募集到的金额。
 		// FIXME:记得，不用通过loan.invests取。为什么？
 		List investMoney = ht
-				.find("select sum(invest.investMoney) from Invest invest where invest.loan.id=? and invest.status = ?",
-						new String[]{loanId, InvestStatus.BID_SUCCESS});
+				.find("select sum(invest.investMoney) from Invest invest where invest.loan.id=? and invest.status not in (?,?)",
+						new String[]{loanId, InvestStatus.CANCEL, InvestStatus.WAIT_AFFIRM});
 
 		double sumMoney = investMoney.get(0) == null ? 0D
 				: (Double) investMoney.get(0);
