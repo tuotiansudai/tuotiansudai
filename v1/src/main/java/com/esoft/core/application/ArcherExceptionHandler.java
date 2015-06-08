d97549a2fa5e81342c533540c5dcedd08fd5100a
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-08 16:56:54 +0800

    modifyCode

--- v1/src/main/java/com/esoft/core/application/ArcherExceptionHandler.java
+++ v1/src/main/java/com/esoft/core/application/ArcherExceptionHandler.java
@@ -58,37 +58,29 @@ public class ArcherExceptionHandler extends ExceptionHandlerWrapper {
                             sbException.append("\n");
                         }
                     }
+                    sbException.append("******************************************************************************************************");
+                    sbException.append("\n");
                 }
-                sbException.append("******************************************************************************************************");
-                sbException.append("\n");
                 StackTraceElement[] stackTraceElements =  eqec.getException().getCause().getStackTrace();
                 sbException.append(eqec.getException().getCause().toString()+"\n");
                 for (StackTraceElement i: stackTraceElements){
                     sbException.append(i.toString());
                     sbException.append("\n");
                 }
-                if (eqec.getException().getCause().getCause()!=null) {
+                Throwable throwable = eqec.getException().getCause().getCause();
+                while (throwable!=null) {
                     sbException.append("******************************************************************************************************");
                     sbException.append("\n");
-                    sbException.append("Caused by:"+eqec.getException().getCause().getCause().toString()+"\n");
-                    StackTraceElement[] stackTraceElementsCause = eqec.getException().getCause().getCause().getStackTrace();
+                    sbException.append("Caused by:"+throwable.toString()+"\n");
+                    StackTraceElement[] stackTraceElementsCause = throwable.getStackTrace();
                     for (StackTraceElement i: stackTraceElementsCause){
                         sbException.append(i.toString());
                         sbException.append("\n");
                     }
-                }
-                if (eqec.getException().getCause().getCause().getCause()!=null) {
-                    sbException.append("******************************************************************************************************");
-                    sbException.append("\n");
-                    sbException.append("Caused by:"+eqec.getException().getCause().getCause().getCause().toString()+"\n");
-                    StackTraceElement[] stackTraceElementsCause = eqec.getException().getCause().getCause().getCause().getStackTrace();
-                    for (StackTraceElement i: stackTraceElementsCause){
-                        sbException.append(i.toString());
-                        sbException.append("\n");
-                    }
+                    throwable = throwable.getCause();
                 }
                 MailService mailService = new MailServiceImpl();
-                mailService.sendMail("all@tuotiansudai.com","系统异常报告:用户-"+userId+";"+request.getMethod()+"-"+RequestUrl,sbException.toString());
+                mailService.sendMail("zourenzheng@tuotiansudai.com","系统异常报告:用户-"+userId+";"+request.getMethod()+"-"+RequestUrl,sbException.toString());
                 throw new FacesException(sbException.toString());
             } finally {
                 it.remove();
