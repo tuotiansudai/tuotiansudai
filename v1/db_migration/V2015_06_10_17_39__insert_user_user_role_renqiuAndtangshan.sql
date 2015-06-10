7c54055b6ae4cb2685aa8c019061943037fa8236
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-10 18:24:31 +0800

    AddBeginCommit

--- v1/db_migration/V2015_06_10_17_39__insert_user_user_role_renqiuAndtangshan.sql
+++ v1/db_migration/V2015_06_10_17_39__insert_user_user_role_renqiuAndtangshan.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `user_temp` AS SELECT * FROM `user` WHERE 1=2;
 CREATE TABLE IF NOT EXISTS `user_role_temp` AS SELECT * FROM `user_role` WHERE 1=2;
-
+BEGIN;
 INSERT INTO `user_temp` VALUES ('tttsyanqiuju','tttsyanqiuju','281599211@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','闫秋菊','F','1984-09-22','河北省唐山市乐亭县闫各庄镇闫各庄村中心社区4条25号',null,null,'15030518192',null,'唐山市路南区仁泰里703-1-402',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
 INSERT INTO `user_temp` VALUES ('tttsluqingna','tttsluqingna','1032475110@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','卢庆娜','F','1988-04-11','河北省唐山市玉田县潮洛窝乡大赵官村兴官西七街38号',null,null,'15176707827',null,'唐山市新天地美域1404-1-1802',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
 INSERT INTO `user_temp` VALUES ('tttszhouna','tttszhouna','541325642@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','周娜','F','1982-05-11','河北省秦皇岛市海港区东华里40栋1单元3号',null,null,'13613155345',null,'唐山市路北区站前路铁路楼21-2-302',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
@@ -83,3 +83,4 @@ INSERT INTO `user_role` SELECT * FROM `user_role_temp` t WHERE EXISTS (SELECT '1
 DELETE FROM `user_temp` WHERE EXISTS (SELECT '1' FROM `user` WHERE `user`.`id` = `user_temp`.`id`);
 
 DELETE FROM `user_role_temp` WHERE EXISTS (SELECT '1' FROM `user` WHERE `user`.`id` = `user_role_temp`.`user_id`);
+COMMIT;
