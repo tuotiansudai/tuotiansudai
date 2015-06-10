e902601b65195c17c388504a90ef07192b5964eb
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-10 17:40:32 +0800

    insertRenQiuAndTangShan

---
+++ v1/db_migration/V2015_06_10_17_39__insert_user_user_role_renqiuAndtangshan.sql
@@ -0,0 +1,85 @@
+CREATE TABLE `user_temp` AS SELECT * FROM `user` WHERE 1=2;
+CREATE TABLE `user_role_temp` AS SELECT * FROM `user_role` WHERE 1=2;
+
+INSERT INTO `user_temp` VALUES ('tttsyanqiuju','tttsyanqiuju','281599211@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','闫秋菊','F','1984-09-22','河北省唐山市乐亭县闫各庄镇闫各庄村中心社区4条25号',null,null,'15030518192',null,'唐山市路南区仁泰里703-1-402',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttsluqingna','tttsluqingna','1032475110@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','卢庆娜','F','1988-04-11','河北省唐山市玉田县潮洛窝乡大赵官村兴官西七街38号',null,null,'15176707827',null,'唐山市新天地美域1404-1-1802',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttszhouna','tttszhouna','541325642@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','周娜','F','1982-05-11','河北省秦皇岛市海港区东华里40栋1单元3号',null,null,'13613155345',null,'唐山市路北区站前路铁路楼21-2-302',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttswuduan','tttswuduan','648351248@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','吴端','F','1990-07-14','河北省唐山市路北区西窑道前后村西小街平房8号',null,null,'15613597163',null,'唐山市路北区盛安金景2-1-1603',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttsyangliyuan','tttsyangliyuan','423459451@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','杨丽媛','F','1984-08-28','河北省唐山市古冶区林西林古里北新工房3条18号',null,null,'13832522794',null,'唐山市路南区春光楼109-1-501',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttsshiyao','tttsshiyao','550540395@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','石瑶','F','1988-03-19','河北省唐山市路南区文化北后街爱国里文南楼付17楼2门402号',null,null,'15081993371',null,'唐山市路南区文南楼11-3-301',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttstiannaiding','tttstiannaiding','624159291@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','田乃丁','M','1967-06-02','河北省唐山市路北区机场路北楼21楼3门202号',null,null,'15383351103',null,'唐山市路北区和平楼1-503',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttspengfeng','tttspengfeng','51517551@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','彭峰','M','1983-07-03','河北省唐山市路北区建华东道钓鱼台城山楼4楼2门8号',null,null,'13633157551',null,'唐山市路北区建华东道钓鱼台城山楼4-2-8',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttssunruixia','tttssunruixia','1401341858@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','孙瑞霞','F','1970-01-15','山东省鄄城县临濮镇杨楼行政村联合村163号',null,null,'13363217280',null,'唐山市机场路北楼20-1-201',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttschangyi','tttschangyi','447085990@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','常艺','F','1970-11-09','河北省唐山市路南区建工里卫国楼1楼2门4号',null,null,'18633134281',null,'唐山市路北区天元花园106-5-202',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttschenli','tttschenli','954522743@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','陈立','F','1959-11-24','河北省唐山市路北区兴源道坤源里天元花园106楼3门402号',null,null,'18630515287',null,'唐山市天元小区106-3-402',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttshanfengxia','tttshanfengxia','1353972583@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','韩凤霞','F','1964-05-19','河北省唐山市路北区高新王鄄子北新街2排7号',null,null,'13933340080',null,'唐山市路北区星河湾小区',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttslixiaomin','tttslixiaomin','2858488469@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','李晓敏','F','1957-04-04','河北省唐山市路北区兴源道勤源里幸福花园505楼5门502号',null,null,'15931537092',null,'唐山市路北区兴源道勤源里幸福花园505-5-502',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttsyangronghua','tttsyangronghua','821950374@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','杨荣华','F','1973-03-27','河北省唐山市路北区果园乡碑子院大街18号付3号',null,null,'13903156853',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttssunzheng','tttssunzheng','531850199@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','孙征','M','1977-10-05','河北省唐山市路南区大理路赵庄西里立新西楼109楼4门502号',null,null,'13930560899',null,'唐山市路南区尚湖名筑7-3-302',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('tttswulingling','tttswulingling','1049059803@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','吴玲玲','F','1984-12-10','河北省唐山市路南区尚湖名筑7楼3门302号',null,null,'15832536353',null,'唐山市路南区尚湖名筑7-3-302',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+
+INSERT INTO `user_temp` VALUES ('ttrqzhanghong','ttrqzhanghong','374159719@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','张红','F','1974-09-05','',null,null,'13643259428',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('ttrqliuguiqing','ttrqliuguiqing','693831005@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','刘桂清','F','1963-03-15','',null,null,'13932754010',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('ttrqlinyuling','ttrqlinyuling','2845988570@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','蔺玉玲','F','1958-04-28','',null,null,'13785482216',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('ttrqzhongweihua','ttrqzhongweihua','570688010@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','钟魏华','F','1978-01-01','',null,null,'13643373908',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('ttrqlisumei','ttrqlisumei','2716618252@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','李素梅','F','1968-09-12','',null,null,'13582677577',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('ttrqanqiaofen','ttrqanqiaofen','2234724261@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','安桥芬','F','1970-04-30','',null,null,'18931772989',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+INSERT INTO `user_temp` VALUES ('ttrqyanghuirong','ttrqyanghuirong','3284445749@qq.com','4be30d9814c6d4e9800e0d2ea9ec9fb00efa887b','1','杨辉荣','F','1973-09-16','',null,null,'15075771247',null,'',null,null,null,null,null,null,'2015-06-10 00:00:00',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,null,null);
+
+INSERT INTO `user_role_temp` VALUES ('tttsyanqiuju','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttsluqingna','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttszhouna','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttswuduan','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttsyangliyuan','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttsshiyao','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttstiannaiding','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttspengfeng','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttssunruixia','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttschangyi','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttschenli','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttshanfengxia','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttslixiaomin','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttsyangronghua','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttssunzheng','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('tttswulingling','MEMBER');
+
+INSERT INTO `user_role_temp` VALUES ('tttsyanqiuju','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttsluqingna','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttszhouna','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttswuduan','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttsyangliyuan','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttsshiyao','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttstiannaiding','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttspengfeng','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttssunruixia','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttschangyi','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttschenli','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttshanfengxia','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttslixiaomin','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttsyangronghua','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttssunzheng','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('tttswulingling','ROLE_MERCHANDISER');
+
+INSERT INTO `user_role_temp` VALUES ('ttrqzhanghong','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('ttrqliuguiqing','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('ttrqlinyuling','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('ttrqzhongweihua','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('ttrqlisumei','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('ttrqanqiaofen','MEMBER');
+INSERT INTO `user_role_temp` VALUES ('ttrqyanghuirong','MEMBER');
+
+INSERT INTO `user_role_temp` VALUES ('ttrqzhanghong','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('ttrqliuguiqing','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('ttrqlinyuling','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('ttrqzhongweihua','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('ttrqlisumei','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('ttrqanqiaofen','ROLE_MERCHANDISER');
+INSERT INTO `user_role_temp` VALUES ('ttrqyanghuirong','ROLE_MERCHANDISER');
+
+INSERT INTO `user` SELECT * FROM `user_temp` t WHERE NOT EXISTS (SELECT '1' FROM `user` m WHERE m.`mobile_number` = t.`mobile_number` OR m.`email` = t.`email`);
+
+INSERT INTO `user_role` SELECT * FROM `user_role_temp` t WHERE EXISTS (SELECT '1' FROM `user` m WHERE m.`id` = t.`user_id`);
+
+DELETE FROM `user_temp` WHERE EXISTS (SELECT '1' FROM `user` WHERE `user`.`id` = `user_temp`.`id`);
+
+DELETE FROM `user_role_temp` WHERE EXISTS (SELECT '1' FROM `user` WHERE `user`.`id` = `user_role_temp`.`user_id`);
