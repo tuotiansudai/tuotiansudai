78d4061d7ea27beaceb5d2ea4c68fe314966312c
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-12 11:44:53 +0800

    updateVersion

--- v1/src/main/webapp/site/themes/default/js/browserVersion.js
+++ v1/src/main/webapp/site/themes/default/js/browserVersion.js
@@ -1,19 +1,23 @@
-function IEVersion() {
-    var rv = -1;
-    if (navigator.appName == 'Microsoft Internet Explorer') {//IE浏览器
-        var ua = navigator.userAgent;
-        var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-        if (re.exec(ua) != null)
-            rv = parseFloat(RegExp.$1);
-        return rv;
-    }
-};
-
 var clientH = $(window).height();
 
-if (IEVersion() < 9) {
+if (IEVersion() < 9.0) {
     $(function(){
         $('.warning_mask').css('display', 'block').css('height', clientH);
         $('.Warning').css('display', 'block');
     });
 }
+
+function IEVersion() {
+    var agent = navigator.userAgent.toLowerCase() ;
+    var regStr_ie = /msie [\d.]+;/gi ;
+    var browser = '';
+    var versionInfo = '';
+    if(agent.indexOf("msie") > 0) {
+        browser = agent.match(regStr_ie) ;
+        versionInfo = (browser+"").replace(/[^0-9.]/ig,"");
+        return parseFloat(versionInfo);
+    } else {
+        return 9.0;
+    }
+}
+
