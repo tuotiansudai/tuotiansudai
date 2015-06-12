f00f437026087663af641f98ceb3b5671e5830a8
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-12 18:23:02 +0800

    modify

--- v1/src/main/webapp/site/themes/default/js/browserVersion.js
+++ v1/src/main/webapp/site/themes/default/js/browserVersion.js
@@ -1,26 +1,24 @@
 var clientH = $(window).height();
 
-if (IEVersion() < 9.0) {
+//if (IEVersion() < 9.0) {
+if(!document.createElement('canvas').getContext){
     $(function(){
         $('.warning_mask').css('display', 'block').css('height', clientH);
         $('.Warning').css('display', 'block');
     });
 }
 
-function IEVersion() {
-    var agent = navigator.userAgent.toLowerCase() ;
-    alert(agent);
-    var regStr_ie = /msie [\d.]+;/gi ;
-    var browser = '';
-    var versionInfo = '';
-    if(agent.indexOf("msie") > 0 && agent.indexOf("firefox") == -1 && agent.indexOf("safari") == -1 && agent.indexOf("chrome") == -1) {
-        browser = agent.match(regStr_ie) ;
-        alert(browser);
-        versionInfo = (browser+"").replace(/[^0-9.]/ig,"");
-        alert(versionInfo);
-        return parseFloat(versionInfo);
-    } else {
-        return 9.0;
-    }
-}
+//function IEVersion() {
+//    var agent = navigator.userAgent.toLowerCase() ;
+//    var regStr_ie = /msie [\d.]+;/gi ;
+//    var browser = '';
+//    var versionInfo = '';
+//    if(agent.indexOf("msie") > 0 && agent.indexOf("firefox") == -1 && agent.indexOf("safari") == -1 && agent.indexOf("chrome") == -1) {
+//        browser = agent.match(regStr_ie) ;
+//        versionInfo = (browser+"").replace(/[^0-9.]/ig,"");
+//        return parseFloat(versionInfo);
+//    } else {
+//        return 9.0;
+//    }
+//}
 
