d340ee5009f6ddc3c861b0025176ab0604a91975
zourenzheng <zourenzheng@tuotiansudai.com>
2015-06-12 18:52:29 +0800

    judgeURL

--- v1/src/main/webapp/site/themes/default/js/browserVersion.js
+++ v1/src/main/webapp/site/themes/default/js/browserVersion.js
@@ -1,24 +1,8 @@
 var clientH = $(window).height();
-
-//if (IEVersion() < 9.0) {
-if(!document.createElement('canvas').getContext){
+var url = '/user/center';
+if(!document.createElement('canvas').getContext && window.location.href.indexOf(url) == -1){
     $(function(){
         $('.warning_mask').css('display', 'block').css('height', clientH);
         $('.Warning').css('display', 'block');
     });
 }
-
-//function IEVersion() {
-//    var agent = navigator.userAgent.toLowerCase() ;
-//    var regStr_ie = /msie [\d.]+;/gi ;
-//    var browser = '';
-//    var versionInfo = '';
-//    if(agent.indexOf("msie") > 0 && agent.indexOf("firefox") == -1 && agent.indexOf("safari") == -1 && agent.indexOf("chrome") == -1) {
-//        browser = agent.match(regStr_ie) ;
-//        versionInfo = (browser+"").replace(/[^0-9.]/ig,"");
-//        return parseFloat(versionInfo);
-//    } else {
-//        return 9.0;
-//    }
-//}
-
