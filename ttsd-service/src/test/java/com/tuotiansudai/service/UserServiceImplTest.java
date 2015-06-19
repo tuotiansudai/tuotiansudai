a9f222b460206bb04d0fb765022a98a7b06d3311
zhanglong888 <zhanglong@tuotiansudai.com>
2015-06-19 16:27:32 +0800

    update userservicetest

--- ttsd-service/src/test/java/com/tuotiansudai/service/UserServiceImplTest.java
+++ ttsd-service/src/test/java/com/tuotiansudai/service/UserServiceImplTest.java
@@ -1,14 +1,18 @@
 package com.tuotiansudai.service;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.tuotiansudai.repository.mapper.UserMapper;
 import com.tuotiansudai.repository.model.UserModel;
 import com.tuotiansudai.service.impl.UserServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,12 +22,13 @@ import static org.mockito.Mockito.when;
 /**
  * Created by hourglasskoala on 15/6/19.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userServiceImpl;
     @Mock
     private UserMapper userMapper;
-
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
@@ -32,12 +37,30 @@ public class UserServiceImplTest {
     @Test
     public void testIsExistEmail() throws Exception{
         UserModel userModel = new UserModel();
-        String email = "123123124@abc.com";
-        userModel.setEmail(email);
+        userModel.setEmail("123@abc.com");
         when(userMapper.findUserByEmail(anyString())).thenReturn(userModel);
-        boolean isExistEmailFlag = userServiceImpl.isExistEmail(email);
-        System.out.println(isExistEmailFlag);
+        boolean isExistEmail = userServiceImpl.isExistEmail(anyString());
+        boolean testResult = true;
+        assertThat(isExistEmail, is(testResult));
+    }
+    @Test
+    public void testIsExistMobileNumber() throws Exception{
+        UserModel userModel = new UserModel();
+        userModel.setMobile_Number("18610361804");
+        when(userMapper.findUserByMobileNumber(anyString())).thenReturn(userModel);
+        boolean isExistMobileNumber = userServiceImpl.isExistMobileNumber(anyString());
+        boolean testResult = true;
+        assertThat(isExistMobileNumber, is(testResult));
 
+    }
+    @Test
+    public void testIsExistReferrer() throws Exception{
+        UserModel userModel = new UserModel();
+        userModel.setLogin_Name("hourglass");
+        when(userMapper.findReferrerByLoginName(anyString())).thenReturn(userModel);
+        boolean isExistReferrer = userServiceImpl.isExistReferrer(anyString());
+        boolean testResult = true;
+        assertThat(isExistReferrer, is(testResult));
 
     }
 }
