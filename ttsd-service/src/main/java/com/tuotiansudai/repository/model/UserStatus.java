8bfb2bc31b3f8d854470cc657dca000ae8e9dc83
Xiduan Gao <gaoxiduan@gmail.com>
2015-06-26 18:42:53 +0800

    add UserStatus

--- ttsd-service/src/main/java/com/tuotiansudai/repository/model/UserStatus.java
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/model/UserStatus.java
@@ -1,12 +1,6 @@
 package com.tuotiansudai.repository.model;
 
 public enum UserStatus {
-    INACTIVE("inactive"),
-    ACTIVE("active");
-
-    private String status;
-
-    UserStatus(String status) {
-        this.status = status;
-    }
+    INACTIVE,
+    ACTIVE
 }
