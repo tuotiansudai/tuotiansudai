225c8933a68d196504ce43d6fea75db2ba37d884
Xiduan Gao <gaoxiduan@gmail.com>
2015-07-08 14:48:09 +0800

    set up mobile dev

---
+++ v1/src/main/java/com/ttsd/mobile/controller/RegisterController.java
@@ -0,0 +1,16 @@
+package com.ttsd.mobile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/register")
+public class RegisterController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView register() {
+        return new ModelAndView("/register");
+    }
+}
