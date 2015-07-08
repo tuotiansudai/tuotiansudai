628559df4247e36f80f15113a9935d473248d699
Xiduan Gao <gaoxiduan@gmail.com>
2015-07-08 18:11:48 +0800

    integrate with html page

--- v1/src/main/java/com/ttsd/mobile/controller/CertificationController.java
+++ v1/src/main/java/com/ttsd/mobile/controller/CertificationController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/register")
-public class RegisterController {
+@RequestMapping("/certification")
+public class CertificationController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView register() {
-        return new ModelAndView("/register");
+    public ModelAndView certification() {
+        return new ModelAndView("/certification");
     }
 }
