0d29b7680893cc4054c70221fd607164f9e20a25
zhaoshuai <zhaoshuai@tuotiansudai.com>
2015-07-10 10:49:04 +0000

    newmobilandpccode

--- ttsd-web/mobile/js/logUp.js
+++ ttsd-web/mobile/js/logUp.js
@@ -5,76 +5,122 @@ require.config({
     baseUrl: 'js',
     paths: {
         'jquery': 'libs/jquery-1.10.1.min',
-        'validate': 'libs/jquery.validate.min'
+        'validate': 'libs/jquery.validate.min',
+        'validate-ex':'validate-ex'
     }
 });
-require(['jquery', 'validate'], function ($) {
-    $('.send_vCode').on('click', function () {
-        var Num = 5;
-        var Down = setInterval(countDown, 1000);
-
-        function countDown() {
-            $('.send_vCode').html(Num + '秒后重新发送').css({'background': '#666', 'color': '#fff', 'width': '100','pointer-events': 'none'});
-            if (Num == 0) {
-                clearInterval(Down);
-                $('.send_vCode').html('重新获取验证码').css({'background': '#e9a922','pointer-events': 'auto'});
-            }
-            Num--;
-        }
-    });
-    var oUserName=$('.userName');
-    var oPassWord=$('.passWord');
-    var oPhoneNumber=$('.phoneNumber');
-    var obj=['oUserName','oPassWord','oPhoneNumber'];
-    for(var i=0;i<obj.length;i++){
-        obj[i].index=i;
-        obj[i].onblur=function(){
-            alert(this.index);
-        }
-    }
-
+require(['jquery', 'validate','validate-ex'], function ($) {
     $('.cmxForm').validate({
-        onkeyup:false,
+        //focusInvalid: false,
         rules: {
             username: {
                 required: true,
-                regex: "^[a-zA-Z0-9]+$",
-                rangeLength: [5, 25]
+                rangelength: [5,25],
+                remote: {
+                    url: 'index.json',
+                    type: 'GET',
+                    dataType:'json',
+                    data: {
+                        username: function () {
+                            return $('.userName').val();
+                        }
+                    }
+                }
             },
-            passWord: {
+            password: {
                 required: true,
-                minLength: 5
+                minLength: 6
             },
             phoneNumber: {
+                isMobile:true,
                 required: true,
                 digits: true,
-                rangeLength: [11, 11]
+                remote: {
+                    url: 'index.json',
+                    type: 'GET',
+                    dataType:'json',
+                    data: {
+                        phoneNumber: function () {
+                            return $('.phoneNumber').val();
+                        }
+                    }
+                }
             },
             vCode: {
                 required: true,
-                regex: "/^[0-9]+$/",
-                digits: true
-            }
-        },
-        remote: {
-            url: "check-email.php",     //后台处理程序
-            type: "GET",               //数据发送方式
-            dataType: "json",           //接受数据格式
-            data: {                     //要传递的数据
-                username: function() {
-                    return $("#username").val();
+                remote: {
+                    url: 'index.json',
+                    type: 'GET',
+                    dataType:'json',
+                    data: {
+                        vCode: function () {
+                            return $('.vCode').val();
+                        }
+                    }
                 }
             }
         },
+        errorElement: 'span',
         messages: {
-            userName: "5-25位非手机号用户名",
+            username: "5-25位非手机号用户名",
             passWord: "请输入6位以上字母混合密码",
             phoneNumber: "手机号码不正确",
-            vCode: "验证码不正确"
+            vCode: "验证码错误"
         },
-        success:function(label){
-            label.text(" ").addClass('success');
+        submitHandler:function(form){
+            alert("submitted");
+            form.submit();
         }
     });
+    //var namevalue=$('.username').val();
+    //$('.logUp').on('click',function(){
+    //    alert('1');
+    //    $(this).ajaxSubmit({
+    //        url:'index.php'+'namevalue',
+    //        type:'GET',
+    //        success:function(data){
+    //            alert('提交成功');
+    //        }
+    //       //$(this).resetForm(); // 提交后重置表单
+    //    });
+    //    return false; // 阻止表单自动提交事件
+    //});
+    var isName=$('.userName');
+    var isPhoneNumber=$('.phoneNumber');
+    $('.send_vCode').on('click', function () {
+        if(isName.val()!==""&& isPhoneNumber.val()!==""){
+            $.ajax({
+                url:'index.php?operationType=',
+                type:'GET',
+                dataType:'json',
+                success:function(response){
+                    if(response.operation===1){
+                        alert('1');
+                        return true;
+                    }else if(response.operation===0){
+                        alert('0');
+                        return false;
+                    }
+                }
+            });
+            var Num = 5;
+            var Down = setInterval(countDown, 1000);
 
+            function countDown() {
+                $('.send_vCode').html(Num + '秒后重新发送').css({
+                    'background': '#666',
+                    'color': '#fff',
+                    'width': '100',
+                    'pointer-events': 'none'
+                });
+                if (Num == 0) {
+                    clearInterval(Down);
+                    $('.send_vCode').html('重新获取验证码').css({'background': '#e9a922', 'pointer-events': 'auto'});
+                }
+                Num--;
+            }
+        }else{
+            alert('请检查您的输入!');
+        }
+    });
 });
---
+++ ttsd-web/mobile/js/realName.js
@@ -0,0 +1,32 @@
+/**
+ * Created by zhaoshuai on 2015/7/9.
+ */
+require.config({
+    baseUrl: 'js',
+    paths: {
+        'jquery': 'libs/jquery-1.10.1.min',
+        'validate': 'libs/jquery.validate.min',
+        'validate-ex':'validate-ex'
+    }
+});
+require(['jquery', 'validate','validate-ex'], function ($) {
+        $('.realName').validate({
+            rules:{
+                yourName:{
+                    required:true
+                },
+                yourId:{
+                    required:true,
+                    isIdCardNo:true
+                }
+            },
+            errorElement: 'small',
+            messages:{
+                yourName:'请检查您的输入',
+                yourId:'请输入正确的身份证号码'
+            },
+            submitHandler:function(form){
+                form.submit();
+            }
+        });
+});
