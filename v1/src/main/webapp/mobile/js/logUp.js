55925a95435d09b55b0704e0e0942e15f3fee85d
tuotian <tuotian@tuotiandeMacBook-Pro.local>
2015-07-16 11:21:06 +0800

    测试

--- v1/src/main/webapp/mobile/js/logUp.js
+++ v1/src/main/webapp/mobile/js/logUp.js
@@ -112,7 +112,7 @@ require(['jquery', 'validate', 'validate-ex'], function ($) {
                 dataType: 'json',
                 success: function (result) {
                     if (result) {
-                        window.location.href='/mobile/templates/certification.ftl?tempData='+new Date().getTime();
+                        window.location.href='/mobile/certification';
                     }
                 }
             });
