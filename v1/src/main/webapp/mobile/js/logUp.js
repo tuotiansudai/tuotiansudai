df27d9530756b0bf5ce632520a21cc1d17bc5a6d
zhaoshuai <zhaoshuai@tuotiansudai.com>
2015-07-23 11:08:20 +0000

    remove onkeyup false

--- v1/src/main/webapp/mobile/js/logUp.js
+++ v1/src/main/webapp/mobile/js/logUp.js
@@ -4,7 +4,6 @@
 require(['jquery', 'jquery.validate', 'validate-ex'], function ($) {
 
     $('.cmxForm').validate({
-        onkeyup:false,
         rules: {
             username: {
                 required: true,
