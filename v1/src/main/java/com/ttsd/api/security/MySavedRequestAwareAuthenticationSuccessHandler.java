f1087897c19a82be49e5fccf7f2dbe6cec4878f2
Xiduan Gao <gaoxiduan@gmail.com>
2015-07-31 16:19:03 +0800

    remove unused code

--- v1/src/main/java/com/ttsd/api/security/MySavedRequestAwareAuthenticationSuccessHandler.java
+++ v1/src/main/java/com/ttsd/api/security/MySavedRequestAwareAuthenticationSuccessHandler.java
@@ -1,15 +1,9 @@
 package com.ttsd.api.security;
 
-import com.google.common.base.Strings;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,37 +12,16 @@ import java.io.IOException;
 
 public class MySavedRequestAwareAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
     protected final Log logger = LogFactory.getLog(this.getClass());
-    private RequestCache requestCache = new HttpSessionRequestCache();
-    private String source;
+    private String source = "web";
 
-    public MySavedRequestAwareAuthenticationSuccessHandler() {
-    }
-
+    @Override
     protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         String source = request.getParameter("source");
         String targetUrl = this.determineTargetUrl(request, response);
         if(response.isCommitted()) {
             this.logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
-        } else if (!Strings.isNullOrEmpty(source) && source.equalsIgnoreCase(this.source)) {
+        } else if (this.source.equalsIgnoreCase(source)) {
             this.getRedirectStrategy().sendRedirect(request, response, targetUrl);
-        } else {
-            this.generateToken(request, response);
         }
-    }
-
-    private String generateToken(HttpServletRequest request, HttpServletResponse response) {
-        return "test";
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public void setRequestCache(RequestCache requestCache) {
-        this.requestCache = requestCache;
     }
 }
