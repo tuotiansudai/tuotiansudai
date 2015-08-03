69ddaf0d1b917dbec73bcc7642dcdfc208a7fe6f
zourenzheng <zourenzheng@tuotiansudai.com>
2015-08-03 11:42:51 +0800

    test

--- v1/src/main/java/com/esoft/jdp2p/schedule/job/RepayingLoanReferrerRewardJob.java
+++ v1/src/main/java/com/esoft/jdp2p/schedule/job/RepayingLoanReferrerRewardJob.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -36,9 +35,9 @@ public class RepayingLoanReferrerRewardJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
-            String hql = "from Loan t where t.status = ? and t.giveMoneyTime < ?";
+            String hql = "from Loan t where t.status in (?,?,?) and t.giveMoneyTime < ?";
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            List<Loan> listLoan= hibernateTemplate.find(hql, new Object[]{LoanConstants.LoanStatus.REPAYING, simpleDateFormat.parse("2015-08-02 19:42:00")});
+            List<Loan> listLoan= hibernateTemplate.find(hql, new Object[]{LoanConstants.LoanStatus.REPAYING,LoanConstants.LoanStatus.COMPLETE, simpleDateFormat.parse("2015-04-27 00:00:00")});
             log.info("四级奖励补发共需要处理" + listLoan.size() + "个标的！");
             for (int i = 0; i < listLoan.size(); i++) {
                 try {
