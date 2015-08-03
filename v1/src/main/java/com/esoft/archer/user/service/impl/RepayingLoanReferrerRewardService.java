25a6a46eabb1686ee7c242e65d0c58ff1be1d5f4
zourenzheng <zourenzheng@tuotiansudai.com>
2015-08-03 17:50:11 +0800

    add log and sure profile

---
+++ v1/src/main/java/com/esoft/archer/user/service/impl/RepayingLoanReferrerRewardService.java
@@ -0,0 +1,55 @@
+package com.esoft.archer.user.service.impl;
+
+import com.esoft.core.annotations.Logger;
+import com.esoft.jdp2p.loan.LoanConstants;
+import com.esoft.jdp2p.loan.model.Loan;
+import com.esoft.umpay.repay.service.impl.UmPayNormalRepayOperation;
+import com.umpay.api.exception.ReqDataException;
+import com.umpay.api.exception.RetDataException;
+import org.apache.commons.logging.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/8/3.
+ */
+@Service
+public class RepayingLoanReferrerRewardService {
+
+    @Autowired
+    private UmPayNormalRepayOperation umPayNormalRepayOperation;
+
+    @Autowired
+    private HibernateTemplate hibernateTemplate;
+
+    @Logger
+    Log log;
+
+    public void reward() {
+        try {
+//            String hql = "from Loan t where t.status in (?,?) and t.giveMoneyTime < ?";
+//            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            List<Loan> listLoan= hibernateTemplate.find(hql, new Object[]{LoanConstants.LoanStatus.REPAYING,LoanConstants.LoanStatus.COMPLETE, simpleDateFormat.parse("2015-04-27 00:00:00")});
+            String hql = "from Loan t where t.id = ?";
+            List<Loan> listLoan= hibernateTemplate.find(hql, new Object[]{"20150724000146"});
+            log.info("四级奖励补发共需要处理" + listLoan.size() + "个标的！");
+            for (int i = 0; i < listLoan.size(); i++) {
+                try {
+                    this.umPayNormalRepayOperation.recommendedIncomeReward(listLoan.get(i));
+                    log.info("四级奖励标的" + listLoan.get(i).getId() + "处理完毕！");
+                } catch (IOException | ReqDataException | RetDataException e) {
+                    e.printStackTrace();
+                    log.error(e.getLocalizedMessage(), e);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getLocalizedMessage(), e);
+        }
+    }
+}
--- v1/src/main/java/com/esoft/jdp2p/schedule/job/RepayingLoanReferrerRewardJob.java
+++ v1/src/main/java/com/esoft/jdp2p/schedule/job/RepayingLoanReferrerRewardJob.java
@@ -1,22 +1,13 @@
 package com.esoft.jdp2p.schedule.job;
 
-import com.esoft.jdp2p.loan.LoanConstants;
-import com.esoft.jdp2p.loan.model.Loan;
-import com.esoft.umpay.repay.service.impl.UmPayNormalRepayOperation;
-import com.umpay.api.exception.ReqDataException;
-import com.umpay.api.exception.RetDataException;
+import com.esoft.archer.user.service.impl.RepayingLoanReferrerRewardService;
+import com.esoft.core.annotations.Logger;
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 /**
  * Created by Administrator on 2015/8/2.
@@ -25,34 +16,19 @@ import java.util.List;
 public class RepayingLoanReferrerRewardJob implements Job {
 
     @Autowired
-    private UmPayNormalRepayOperation umPayNormalRepayOperation;
+    private RepayingLoanReferrerRewardService repayingLoanReferrerRewardService;
 
-    @Autowired
-    private HibernateTemplate hibernateTemplate;
-
-    Log log = LogFactory.getLog(RepayingLoanReferrerRewardJob.class);
+    @Logger
+    Log log;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
-//            String hql = "from Loan t where t.status in (?,?) and t.giveMoneyTime < ?";
-//            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//            List<Loan> listLoan= hibernateTemplate.find(hql, new Object[]{LoanConstants.LoanStatus.REPAYING,LoanConstants.LoanStatus.COMPLETE, simpleDateFormat.parse("2015-04-27 00:00:00")});
-            String hql = "from Loan t where t.id = ?";
-            List<Loan> listLoan= hibernateTemplate.find(hql, new Object[]{"20150724000146"});
-            log.info("四级奖励补发共需要处理" + listLoan.size() + "个标的！");
-            for (int i = 0; i < listLoan.size(); i++) {
-                try {
-                    this.umPayNormalRepayOperation.recommendedIncome(listLoan.get(i));
-                    log.info("四级奖励标的" + listLoan.get(i).getId() + "处理完毕！");
-                } catch (IOException | ReqDataException | RetDataException e) {
-                    e.printStackTrace();
-                    log.error(e.getLocalizedMessage(), e);
-                }
-            }
+            repayingLoanReferrerRewardService.reward();
         } catch (Exception e) {
+            //log.error(e);
+            //throw e;
             e.printStackTrace();
-            log.error(e.getLocalizedMessage(), e);
         }
     }
 }
