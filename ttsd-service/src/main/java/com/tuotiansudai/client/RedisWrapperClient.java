c020f339130b7a745eeb1cda1ff0efd3e26bd95f
zourenzheng <zourenzheng@tuotiansudai.com>
2015-08-08 15:41:14 +0800

    redis client

---
+++ ttsd-service/src/main/java/com/tuotiansudai/client/RedisWrapperClient.java
@@ -0,0 +1,16 @@
+package com.tuotiansudai.client;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2015/8/7.
+ */
+@Component
+public class RedisWrapperClient {
+
+    static Logger logger = Logger.getLogger(SmsWrapperClient.class);
+
+
+
+}
