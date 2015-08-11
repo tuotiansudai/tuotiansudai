d60bfbafb3e0a5728d76d13ef31e04034fd1dba3
zourenzheng <zourenzheng@tuotiansudai.com>
2015-08-11 14:16:04 +0800

    redis service

--- ttsd-service/src/main/java/com/tuotiansudai/client/RedisWrapperClient.java
+++ ttsd-service/src/main/java/com/tuotiansudai/client/RedisWrapperClient.java
@@ -1,7 +1,13 @@
 package com.tuotiansudai.client;
 
+import com.tuotiansudai.service.impl.JedisServiceImpl;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Administrator on 2015/8/7.
@@ -9,8 +15,70 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisWrapperClient {
 
-    static Logger logger = Logger.getLogger(SmsWrapperClient.class);
+    static Logger logger = Logger.getLogger(RedisWrapperClient.class);
 
+    @Value("${redis.ip}")
+    private String redisIp;
 
+    @Value("${redis.port}")
+    private Integer redisPort;
+
+    @Resource
+    private JedisServiceImpl jedisService;
+
+    private static JedisPool pool = null;
+
+    public JedisPool getPool() {
+        if (pool == null) {
+            JedisPoolConfig config = new JedisPoolConfig();
+            pool = new JedisPool(config, redisIp, redisPort);
+        }
+        return pool;
+    }
+
+    public String getRedisIp() {
+        return redisIp;
+    }
+
+    public void setRedisIp(String redisIp) {
+        this.redisIp = redisIp;
+    }
+
+    public Integer getRedisPort() {
+        return redisPort;
+    }
+
+    public void setRedisPort(Integer redisPort) {
+        this.redisPort = redisPort;
+    }
+
+    public String get(String key) {
+        jedisService.setJedisPool(getPool());
+        return jedisService.get(key);
+    }
+
+    public boolean exists(String key) {
+        jedisService.setJedisPool(getPool());
+        return jedisService.exists(key);
+    }
+
+    public void setex(String key, int seconds, String value) {
+        jedisService.setJedisPool(getPool());
+        jedisService.setex(key, value, seconds);
+    }
+
+    public void set(String key, String value) {
+        jedisService.setJedisPool(getPool());
+        jedisService.set(key, value);
+    }
+
+    public boolean del(String key) {
+        jedisService.setJedisPool(getPool());
+        return jedisService.del(key);
+    }
+    public Long lpush(final String key, final String... values) {
+        jedisService.setJedisPool(getPool());
+        return jedisService.lpush(key,values);
+    }
 
 }
---
+++ ttsd-service/src/main/java/com/tuotiansudai/service/impl/JedisServiceImpl.java
@@ -0,0 +1,180 @@
+package com.tuotiansudai.service.impl;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+import redis.clients.jedis.exceptions.JedisDataException;
+import redis.clients.jedis.exceptions.JedisException;
+
+/**
+ * Created by Administrator on 2015/8/11.
+ */
+@Service
+public class JedisServiceImpl {
+
+    static Logger log = Logger.getLogger(JedisServiceImpl.class);
+
+    private JedisPool jedisPool;
+
+    @Value("${redis.password}")
+    private String redisPassword;
+
+    @Value("${redis.db}")
+    private Integer redisDb;
+
+    public void setJedisPool(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+
+    public JedisPool getJedisPool() {
+
+        return jedisPool;
+    }
+
+    public interface JedisAction<T> {
+        T action(Jedis jedis);
+    }
+
+    public interface JedisActionNoResult {
+        void action(Jedis jedis);
+    }
+
+    public <T> T execute(JedisAction<T> jedisAction) throws JedisException {
+        Jedis jedis = null;
+        boolean broken = false;
+        try {
+            jedis = jedisPool.getResource();
+            if(StringUtils.isNotEmpty(redisPassword)){
+                jedis.auth(redisPassword);
+            }
+            jedis.select(redisDb);
+            return jedisAction.action(jedis);
+        } catch (JedisException e) {
+            broken = handleJedisException(e);
+            throw e;
+        } finally {
+            closeResource(jedis, broken);
+        }
+    }
+
+    public void execute(JedisActionNoResult jedisAction) throws JedisException {
+        Jedis jedis = null;
+        boolean broken = false;
+        try {
+            jedis = jedisPool.getResource();
+            if(StringUtils.isNotEmpty(redisPassword)){
+                jedis.auth(redisPassword);
+            }
+            jedis.select(redisDb);
+            jedisAction.action(jedis);
+        } catch (JedisException e) {
+            broken = handleJedisException(e);
+            throw e;
+        } finally {
+            closeResource(jedis, broken);
+        }
+    }
+
+    protected boolean handleJedisException(JedisException jedisException) {
+        if (jedisException instanceof JedisConnectionException) {
+            log.error(jedisException.getLocalizedMessage(), jedisException);
+        } else if (jedisException instanceof JedisDataException) {
+            if ((jedisException.getMessage() != null) && (jedisException.getMessage().indexOf("READONLY") != -1)) {
+                log.error(jedisException.getLocalizedMessage(), jedisException);
+            } else {
+                return false;
+            }
+        } else {
+            log.error(jedisException.getLocalizedMessage(), jedisException);
+        }
+        return true;
+    }
+
+    protected void closeResource(Jedis jedis, boolean conectionBroken) {
+        try {
+            if (conectionBroken) {
+                jedisPool.returnBrokenResource(jedis);
+            } else {
+                jedisPool.returnResource(jedis);
+            }
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage(), e);
+            destroyJedis(jedis);
+        }
+    }
+
+    public static void destroyJedis(Jedis jedis) {
+        if ((jedis != null) && jedis.isConnected()) {
+            try {
+                try {
+                    jedis.quit();
+                } catch (Exception e) {
+                    log.error(e.getLocalizedMessage(), e);
+                }
+                jedis.disconnect();
+            } catch (Exception e) {
+                log.error(e.getLocalizedMessage(), e);
+            }
+        }
+    }
+
+    public Boolean del(final String... keys) {
+        return execute(new JedisAction<Boolean>() {
+            @Override
+            public Boolean action(Jedis jedis) {
+                return jedis.del(keys) == keys.length ? true : false;
+            }
+        });
+    }
+
+    public String get(final String key) {
+        return execute(new JedisAction<String>() {
+            @Override
+            public String action(Jedis jedis) {
+                return jedis.get(key);
+            }
+        });
+    }
+
+    public boolean exists(final String key) {
+        return execute(new JedisAction<Boolean>() {
+            @Override
+            public Boolean action(Jedis jedis) {
+                return jedis.exists(key);
+            }
+        });
+    }
+
+    public void set(final String key, final String value) {
+        execute(new JedisActionNoResult() {
+            @Override
+            public void action(Jedis jedis) {
+                jedis.set(key, value);
+            }
+        });
+    }
+
+    public void setex(final String key, final String value, final int seconds) {
+        execute(new JedisActionNoResult() {
+            @Override
+            public void action(Jedis jedis) {
+                jedis.setex(key, seconds, value);
+            }
+        });
+    }
+
+    public Long lpush(final String key, final String... values) {
+        return execute(new JedisAction<Long>() {
+            @Override
+            public Long action(Jedis jedis) {
+                return jedis.lpush(key, values);
+            }
+        });
+    }
+
+
+}
