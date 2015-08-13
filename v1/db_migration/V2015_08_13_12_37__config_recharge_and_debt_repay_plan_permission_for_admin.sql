1af0b2f9e7a2d8278e2a4f8487f6607ca4ec49d0
xiangjie <xiangjie@tuotiansudai.com>
2015-08-13 12:41:33 +0800

    config permission for admin

---
+++ v1/db_migration/V2015_08_13_12_37__config_recharge_and_debt_repay_plan_permission_for_admin.sql
@@ -0,0 +1,6 @@
+BEGIN ;
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','creditor_repay_plan_list');
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','recharge');
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','manual_recharge');
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','batch_recharge');
+COMMIT ;
