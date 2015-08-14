235b5c31703ce3d462a4de8280f94b0802561c8e
zourenzheng <zourenzheng@tuotiansudai.com>
2015-08-14 18:00:59 +0800

    console login

--- ttsd-console/src/main/java/com/tuotiansudai/console/controller/LoginController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/LoginController.java
@@ -3,13 +3,23 @@ package com.tuotiansudai.console.controller;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Administrator on 2015/8/14.
  */
 @Controller
-@RequestMapping(value = "/admin/login")
+@RequestMapping(value = "/login")
 public class LoginController {
 
     static Logger logger = Logger.getLogger(LoginController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView login() {
+        return new ModelAndView("/login");
+    }
+
+
+
 }
