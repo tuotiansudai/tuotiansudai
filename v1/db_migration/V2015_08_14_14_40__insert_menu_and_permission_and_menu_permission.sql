e8bb0ca372b035fe59d8589d5bd0e12674cbe4db
xiangjie <xiangjie@tuotiansudai.com>
2015-08-14 15:27:24 +0800

    modify migrate script

--- v1/db_migration/V2015_08_14_14_40__insert_menu_and_permission_and_menu_permission.sql
+++ v1/db_migration/V2015_08_14_14_40__insert_menu_and_permission_and_menu_permission.sql
@@ -21,7 +21,7 @@ INSERT INTO `permission` VALUES ('batch_recharge','批量充值','批量充值')
 INSERT INTO `permission` VALUES ('creditor_repay_plan_list','债权还款计划','债权还款计划') on duplicate key update `name`=values(`name`),description=values(description);
 
 /******给“债权还款计划”菜单绑定“债权还款计划”权限******/
-INSERT INTO `menu_permission` VALUES ('creditor_repay_plan_list','creditor_repay_plan_list') on duplicate key update `name`=values(`name`),description=values(description);
+INSERT INTO `menu_permission` VALUES ('creditor_repay_plan_list','creditor_repay_plan_list') on duplicate key update menu_id=values(menu_id),permission_id=values(permission_id);
 
 /******给“充值”菜单绑定“充值”权限******/
 INSERT INTO `menu_permission` VALUES ('recharge','recharge') on duplicate key update menu_id=values(menu_id),permission_id=values(permission_id);
