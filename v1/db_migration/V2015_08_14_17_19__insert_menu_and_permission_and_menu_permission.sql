78e56800f0d860f84fc6b6f2041dd57f2711b3d3
xiangjie <xiangjie@tuotiansudai.com>
2015-08-14 18:20:04 +0800

    modify migrate script

--- v1/db_migration/V2015_08_14_17_19__insert_menu_and_permission_and_menu_permission.sql
+++ v1/db_migration/V2015_08_14_17_19__insert_menu_and_permission_and_menu_permission.sql
@@ -31,4 +31,16 @@ INSERT INTO `menu_permission` VALUES ('manual_recharge','manual_recharge') on du
 
 /******给“批量充值”菜单绑定“批量充值”权限******/
 INSERT INTO `menu_permission` VALUES ('batch_recharge','batch_recharge') on duplicate key update menu_id=values(menu_id),permission_id=values(permission_id);
+
+/****给(系统)管理员添加“债权还款计划”权限****/
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','creditor_repay_plan_list') on duplicate key update role_id=values(role_id),permission_id=values(permission_id);
+
+/****给(系统)管理员添加“充值”权限****/
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','recharge') on duplicate key update role_id=values(role_id),permission_id=values(permission_id);
+
+/****给(系统)管理员添加“手动充值”权限****/
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','manual_recharge') on duplicate key update role_id=values(role_id),permission_id=values(permission_id);
+
+/****给(系统)管理员添加“批量充值”权限****/
+INSERT INTO `role_permission` VALUES ('ADMINISTRATOR','batch_recharge') on duplicate key update role_id=values(role_id),permission_id=values(permission_id);
 COMMIT;
