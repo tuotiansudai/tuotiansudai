15377e303076d6d557c08bf8d443a6aaff319c14
xiangjie <xiangjie@tuotiansudai.com>
2015-08-17 10:23:52 +0800

    repair request data

--- v1/src/main/java/com/esoft/umpay/bankcard/service/impl/UmPayBindingBankCardOperation.java
+++ v1/src/main/java/com/esoft/umpay/bankcard/service/impl/UmPayBindingBankCardOperation.java
@@ -167,7 +167,7 @@ public class UmPayBindingBankCardOperation extends
 			baseResponseDto.setMessage(ReturnMessage.SUCCESS.getMsg());
 			BankCardResponseDto bankCardResponseDto = new BankCardResponseDto();
 			bankCardResponseDto.setUrl(reqData.getUrl());
-			bankCardResponseDto.setRequestData(CommonUtils.mapToFormData(reqData.getField(),false));
+			bankCardResponseDto.setRequestData(CommonUtils.mapToFormData(reqData.getField(),true));
 			baseResponseDto.setData(bankCardResponseDto);
 		} catch (UnsupportedEncodingException e){
 			log.error(e.getLocalizedMessage(),e);
