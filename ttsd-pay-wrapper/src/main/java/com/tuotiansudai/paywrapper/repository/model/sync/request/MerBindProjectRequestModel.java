983bc37c9b8d6bed0e50d6cabf97707a795cdbb1
xiangjie <xiangjie@tuotiansudai.com>
2015-08-19 14:34:40 +0800

    modify umpay interface code

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/request/MerBindProjectRequestModel.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/request/MerBindProjectRequestModel.java
@@ -1,23 +1,31 @@
 package com.tuotiansudai.paywrapper.repository.model.sync.request;
 
+import com.google.common.collect.Maps;
+
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by tuotian on 15/8/17.
  */
 public class MerBindProjectRequestModel extends BaseSyncRequestModel{
-    private String sign;
     private String projectId;
     private String projectName;
     private String projectAmount;
     private String loanUserId;
 
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
+    public Map<String, String> generatePayRequestData() {
+        Map<String, String> payRequestData = Maps.newHashMap();
+        payRequestData.put("service", this.service);
+        payRequestData.put("sign_type", this.getSignType());
+        payRequestData.put("charset", this.getCharset());
+        payRequestData.put("mer_id", this.getMerId());
+        payRequestData.put("version", this.getVersion());
+        payRequestData.put("project_id", this.projectId);
+        payRequestData.put("project_name", this.projectName);
+        payRequestData.put("project_amount", this.projectAmount);
+        payRequestData.put("loan_user_id", this.loanUserId);
+        return payRequestData;
     }
 
     public String getProjectId() {
--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/response/MerBindProjectResponseModel.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/response/MerBindProjectResponseModel.java
@@ -14,18 +14,18 @@ public class MerBindProjectResponseModel extends BaseSyncResponseModel{
     private String projectState;
     private String merCheckDate;
 
-//    public void initializeModel(Map<String, String> resData) {
-//        this.setResponseData(resData.toString());
-//        this.setReturnCode(resData.get("ret_code"));
-//        this.setReturnMessage(resData.get("ret_msg"));
-//        this.setSignType(resData.get("sign_type"));
-//        this.setSign(resData.get("sign"));
-//        this.setMerchantId(resData.get("mer_id"));
-//        this.setVersion(resData.get("version"));
-//        this.setProjectAccountId(resData.get("project_account_id"));
-//        this.setProjectState(resData.get("project_state"));
-//        this.setMerCheckDate(resData.get("mer_check_date"));
-//    }
+    public void initializeModel(Map<String, String> resData) {
+        this.setResponseData(resData.toString());
+        this.setRetCode(resData.get("ret_code"));
+        this.setRetMsg(resData.get("ret_msg"));
+        this.setSignType(resData.get("sign_type"));
+        this.setSign(resData.get("sign"));
+        this.setMerchantId(resData.get("mer_id"));
+        this.setVersion(resData.get("version"));
+        this.setProjectAccountId(resData.get("project_account_id"));
+        this.setProjectState(resData.get("project_state"));
+        this.setMerCheckDate(resData.get("mer_check_date"));
+    }
 
     public String getSignType() {
         return signType;
