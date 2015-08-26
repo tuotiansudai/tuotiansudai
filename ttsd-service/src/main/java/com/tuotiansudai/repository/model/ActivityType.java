aa8101ac89a4ee2f7c02f4a3334fb44e31c601d5
xiangjie <xiangjie@tuotiansudai.com>
2015-08-26 11:19:58 +0800

    modify ActivityType.java file

--- ttsd-service/src/main/java/com/tuotiansudai/repository/model/ActivityType.java
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/model/ActivityType.java
@@ -8,10 +8,10 @@ import java.util.Map;
  */
 public enum ActivityType {
     /***默认值***/
-    NORMAL_INVEST("normalInvest","普通投资"),
-    NOVICE_INVEST("noviceInvest","新手投资"),
-    DIRECTIONAL_INVEST("directionalInvest","定向投资"),
-    INCREASE_INTEREST_INVEST("increaseInterestInvest","加息投资");
+    NORMAL_INVEST("NORMAL","普通投资"),
+    NOVICE_INVEST("NOVICE","新手投资"),
+    DIRECTIONAL_INVEST("EXCLUSIVE","定向投资"),
+    INCREASE_INTEREST_INVEST("PROMOTION","加息投资");
 
     private String activityTypeCode;
     private String activityTypeName;
