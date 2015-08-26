eb9166dc7f6c59c4aa83e3104332da6f740a8bf3
xiangjie <xiangjie@tuotiansudai.com>
2015-08-26 11:40:10 +0800

    add //TODO

--- ttsd-console/src/main/java/com/tuotiansudai/console/controller/LoanController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/LoanController.java
@@ -23,8 +23,8 @@ public class LoanController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView createLoan(HttpServletRequest request){
-        List contracts = new ArrayList();
         //TODO 合同需要从数据库中获取
+        List contracts = new ArrayList();
         Map<String,String> contract = new HashMap<>();
         contract.put("id", "789098123");
         contract.put("contractName", "四方合同");
