8fab37d066de55a6fcafa653cef1daabdf031947
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-08-31 15:10:35 +0800

    remove test code

--- ttsd-service/src/main/java/com/tuotiansudai/exception/InsufficientBalanceException.java
+++ ttsd-service/src/main/java/com/tuotiansudai/exception/InsufficientBalanceException.java
@@ -14,12 +14,4 @@ public class InsufficientBalanceException extends RuntimeException {
     public InsufficientBalanceException(String message, Throwable cause){
         super(message, cause);
     }
-
-
-    public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat(".##");
-        long amount = 23210330100L;
-        String s = df.format(amount / 1000);
-        System.out.println(s);
-    }
 }
