e6aae9987b72f48c153073963624e1db6c5747e7
xiangjie <xiangjie@tuotiansudai.com>
2015-09-01 18:08:12 +0800

    repair code

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/request/MerUpdateProjectRequestModel.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/request/MerUpdateProjectRequestModel.java
@@ -1,6 +1,5 @@
 package com.tuotiansudai.paywrapper.repository.model.sync.request;
 
-import java.util.Date;
 import java.util.Map;
 
 public class MerUpdateProjectRequestModel extends BaseSyncRequestModel {
@@ -12,13 +11,20 @@ public class MerUpdateProjectRequestModel extends BaseSyncRequestModel {
     private String projectExpireDate;
 
 
-    public MerUpdateProjectRequestModel(long loanAmount, long projectId, String projectName,String changeType) {
+    public MerUpdateProjectRequestModel(long loanAmount,
+                                        long projectId,
+                                        String projectName,
+                                        String changeType,
+                                        String projectState,
+                                        String projectExpireDate) {
         super();
-        this.service = "mer_bind_project";
+        this.service = "mer_update_project";
         this.projectId = projectId;
         this.projectName = projectName;
         this.projectAmount = loanAmount;
         this.changeType = changeType;
+        this.projectState = projectState;
+        this.projectExpireDate = projectExpireDate;
     }
 
     public Map<String, String> generatePayRequestData() {
@@ -27,8 +33,57 @@ public class MerUpdateProjectRequestModel extends BaseSyncRequestModel {
         payRequestData.put("project_id", String.valueOf(this.projectId));
         payRequestData.put("project_name", this.projectName);
         payRequestData.put("project_amount", String.valueOf(this.projectAmount));
-//        payRequestData.put("loan_user_id", String.valueOf(this.loanUserId));
+        payRequestData.put("change_type", this.changeType);
+        payRequestData.put("project_state", this.projectState);
+        payRequestData.put("project_expire_date", this.projectExpireDate);
         return payRequestData;
     }
 
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public long getProjectAmount() {
+        return projectAmount;
+    }
+
+    public void setProjectAmount(long projectAmount) {
+        this.projectAmount = projectAmount;
+    }
+
+    public String getChangeType() {
+        return changeType;
+    }
+
+    public void setChangeType(String changeType) {
+        this.changeType = changeType;
+    }
+
+    public String getProjectState() {
+        return projectState;
+    }
+
+    public void setProjectState(String projectState) {
+        this.projectState = projectState;
+    }
+
+    public String getProjectExpireDate() {
+        return projectExpireDate;
+    }
+
+    public void setProjectExpireDate(String projectExpireDate) {
+        this.projectExpireDate = projectExpireDate;
+    }
 }
--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/response/MerUpdateProjectResponseModel.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/response/MerUpdateProjectResponseModel.java
@@ -3,24 +3,13 @@ package com.tuotiansudai.paywrapper.repository.model.sync.response;
 import java.util.Map;
 
 public class MerUpdateProjectResponseModel extends BaseSyncResponseModel{
-    private String projectAccountId;
     private String projectState;
     private String merCheckDate;
 
     public void initializeModel(Map<String, String> resData) {
         super.initializeModel(resData);
-        this.setProjectAccountId(resData.get("project_account_id"));
         this.setProjectState(resData.get("project_state"));
         this.setMerCheckDate(resData.get("mer_check_date"));
-    }
-
-
-    public String getProjectAccountId() {
-        return projectAccountId;
-    }
-
-    public void setProjectAccountId(String projectAccountId) {
-        this.projectAccountId = projectAccountId;
     }
 
     public String getProjectState() {
