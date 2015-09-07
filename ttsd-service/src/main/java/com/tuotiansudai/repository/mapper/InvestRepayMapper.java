bd7ddce77527d2a01e7b4bd02f65aea186659d11
zourenzheng <zourenzheng@tuotiansudai.com>
2015-09-07 16:01:29 +0800

    temp

---
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/mapper/InvestRepayMapper.java
@@ -0,0 +1,14 @@
+package com.tuotiansudai.repository.mapper;
+
+import com.tuotiansudai.repository.model.InvestRepayModel;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/9/7.
+ */
+public interface InvestRepayMapper {
+
+    public void insertInvestRepay(List<InvestRepayModel> investRepayModels);
+
+}
