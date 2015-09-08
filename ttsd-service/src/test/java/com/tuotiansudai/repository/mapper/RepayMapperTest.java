cc4d29254524f974f8a5e7103c976d8ebc269bc5
zourenzheng <zourenzheng@tuotiansudai.com>
2015-09-08 19:48:07 +0800

    test

--- ttsd-service/src/test/java/com/tuotiansudai/repository/mapper/RepayMapperTest.java
+++ ttsd-service/src/test/java/com/tuotiansudai/repository/mapper/RepayMapperTest.java
@@ -1,9 +1,8 @@
 package com.tuotiansudai.repository.mapper;
 
 import com.google.common.collect.Lists;
-import com.tuotiansudai.repository.model.InvestRepayModel;
-import com.tuotiansudai.repository.model.LoanRepayModel;
-import com.tuotiansudai.repository.model.RepayStatus;
+import com.tuotiansudai.dto.LoanDto;
+import com.tuotiansudai.repository.model.*;
 import com.tuotiansudai.utils.DateUtil;
 import com.tuotiansudai.utils.IdGenerator;
 import org.junit.Test;
@@ -13,8 +12,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Administrator on 2015/9/8.
@@ -39,28 +40,16 @@ public class RepayMapperTest {
     @Autowired
     private LoanMapper loanMapper;
 
-    @Test
-    public void shouldCreateInvestRepayModel() throws Exception {
-        List<InvestRepayModel> investRepayModels = Lists.newArrayList();
-        InvestRepayModel investRepayModel = new InvestRepayModel();
-        investRepayModel.setId(idGenerator.generate());
-        investRepayModel.setCorpus(0);
-        investRepayModel.setDefaultInterest(0);
-        investRepayModel.setExpectInterest(0);
-        investRepayModel.setActualInterest(0);
-        investRepayModel.setExpectFee(0);
-        investRepayModel.setActualFee(0);
-        investRepayModel.setInvestId(investModel.getId());
-        investRepayModel.setPeriod(1);
-        investRepayModel.setRepayDate(new Date());
-        investRepayModel.setStatus(RepayStatus.REPAYING);
-        investRepayModels.add(investRepayModel);
-        investRepayMapper.insertInvestRepay(investRepayModels);
-
-    }
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     public void shouldCreateLoanRepayModel() throws Exception {
+        UserModel userModel = this.getUserModelTest();
+        userMapper.create(userModel);
+        LoanDto loanDto = this.getLoanModel();
+        LoanModel loanModel = new LoanModel(loanDto);
+        loanMapper.create(loanModel);
         List<LoanRepayModel> loanRepayModels = Lists.newArrayList();
         LoanRepayModel loanRepayModel = new LoanRepayModel();
         loanRepayModel.setId(idGenerator.generate());
@@ -76,5 +65,46 @@ public class RepayMapperTest {
         loanRepayMapper.insertLoanRepay(loanRepayModels);
     }
 
+    private LoanDto getLoanModel(){
+        LoanDto loanDto = new LoanDto();
+        loanDto.setLoanerLoginName("helloworld");
+        loanDto.setAgentLoginName("helloworld");
+        loanDto.setBasicRate("16.00");
+        long id = idGenerator.generate();
+        loanDto.setId(id);
+        loanDto.setProjectName("店铺资金周转");
+        loanDto.setActivityRate("12");
+        loanDto.setShowOnHome(true);
+        loanDto.setPeriods(30);
+        loanDto.setActivityType(ActivityType.NORMAL);
+        loanDto.setContractId(123);
+        loanDto.setDescriptionHtml("asdfasdf");
+        loanDto.setDescriptionText("asdfasd");
+        loanDto.setFundraisingEndTime(new Date());
+        loanDto.setFundraisingStartTime(new Date());
+        loanDto.setInvestFeeRate("15");
+        loanDto.setInvestIncreasingAmount("1");
+        loanDto.setLoanAmount("10000");
+        loanDto.setType(LoanType.LOAN_TYPE_1);
+        loanDto.setMaxInvestAmount("100000000000");
+        loanDto.setMinInvestAmount("0");
+        loanDto.setCreatedTime(new Date());
+        loanDto.setLoanStatus(LoanStatus.WAITING_VERIFY);
+        List<LoanTitleRelationModel> loanTitleRelationModelList = new ArrayList<LoanTitleRelationModel>();
+        loanDto.setLoanTitles(loanTitleRelationModelList);
+        return loanDto;
+    }
+
+    private UserModel getUserModelTest() {
+        UserModel userModelTest = new UserModel();
+        userModelTest.setLoginName("helloworld");
+        userModelTest.setPassword("123abc");
+        userModelTest.setEmail("12345@abc.com");
+        userModelTest.setMobile("13900000000");
+        userModelTest.setRegisterTime(new Date());
+        userModelTest.setStatus(UserStatus.ACTIVE);
+        userModelTest.setSalt(UUID.randomUUID().toString().replaceAll("-", ""));
+        return userModelTest;
+    }
 
 }
