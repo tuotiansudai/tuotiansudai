3dfad27adc0f2533f8d965b3e126e0f570cac5d2
Xiduan Gao <gaoxiduan@gmail.com>
2015-09-09 19:12:13 +0800

    finished new version paver script

--- pavement.py
+++ pavement.py
@@ -139,16 +139,10 @@ def deploy_to_docker(options):
 
 
 @task
-@cmdopts([
-    ('webport=', 'w', 'Web Local port which is mapped to containers 8080'),
-    ('smsport=', 's', 'SMS Local port which is mapped to containers 8080'),
-    ('payport=', 'p', 'Payment Local port which is mapped to containers 8080'),
-    ('consoleport=', 'c', 'Local port which is mapped to containers 8080'),
-])
-def v2deploy(options):
+def v2deploy():
     from scripts.deployment import NewVersionDeployment
 
-    v2 = NewVersionDeployment(options.webport, options.smsport, options.payport, options.consoleport)
+    v2 = NewVersionDeployment()
     v2.deploy()
 
 
