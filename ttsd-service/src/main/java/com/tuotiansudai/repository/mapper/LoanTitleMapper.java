3ed7235f22244e1cd3cf08557d4c3057b587d3ea
zourenzheng <zourenzheng@tuotiansudai.com>
2015-09-16 17:01:05 +0800

    redis test

--- ttsd-service/src/main/java/com/tuotiansudai/repository/mapper/LoanTitleMapper.java
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/mapper/LoanTitleMapper.java
@@ -2,9 +2,10 @@ package com.tuotiansudai.repository.mapper;
 
 import com.tuotiansudai.repository.model.LoanTitleModel;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface LoanTitleMapper {
     void create(LoanTitleModel title);
     List<LoanTitleModel> findAll();
