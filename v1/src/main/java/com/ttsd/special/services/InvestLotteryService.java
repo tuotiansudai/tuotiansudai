0e139954b23fdeb9c2398af3755850e3854f2433
zourenzheng <zourenzheng@tuotiansudai.com>
2015-09-17 19:30:59 +0800

    add send money

--- v1/src/main/java/com/ttsd/special/services/InvestLotteryService.java
+++ v1/src/main/java/com/ttsd/special/services/InvestLotteryService.java
@@ -1,12 +1,15 @@
 package com.ttsd.special.services;
 
+import com.esoft.archer.user.model.User;
+
 /**
  * Created by Administrator on 2015/9/9.
  */
 public interface InvestLotteryService {
 
-    public void insertIntoInvestLottery(String investId);
+    void insertIntoInvestLottery(String investId);
 
     void updateInvestLotteryGranted(long id,boolean granted);
 
+    boolean WinningPersonIncome(String orderId, double bonus, String particUserId, User user);
 }
--- v1/src/main/java/com/ttsd/special/services/impl/InvestLotteryServiceImpl.java
+++ v1/src/main/java/com/ttsd/special/services/impl/InvestLotteryServiceImpl.java
@@ -1,24 +1,38 @@
 package com.ttsd.special.services.impl;
 
+import com.esoft.archer.user.model.User;
+import com.esoft.archer.user.model.UserBill;
+import com.esoft.archer.user.service.impl.UserBillBO;
+import com.esoft.core.annotations.Logger;
 import com.esoft.core.util.ArithUtil;
 import com.esoft.core.util.IdGenerator;
 import com.esoft.jdp2p.invest.model.Invest;
+import com.esoft.jdp2p.invest.model.InvestUserReferrer;
+import com.esoft.jdp2p.loan.LoanConstants;
 import com.esoft.jdp2p.loan.model.Loan;
+import com.esoft.umpay.loan.service.impl.UmPayLoanMoneyService;
+import com.esoft.umpay.repay.service.impl.UmPayNormalRepayOperation;
+import com.esoft.umpay.trusteeship.UmPayConstants;
 import com.google.common.collect.Lists;
 import com.ttsd.special.model.InvestLottery;
 import com.ttsd.special.model.InvestLotteryPrizeType;
 import com.ttsd.special.model.InvestLotteryProbabilityType;
 import com.ttsd.special.model.InvestLotteryType;
 import com.ttsd.special.services.InvestLotteryService;
+import com.umpay.api.exception.ReqDataException;
+import com.umpay.api.exception.RetDataException;
+import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by Administrator on 2015/9/9.
@@ -29,6 +43,17 @@ public class InvestLotteryServiceImpl implements InvestLotteryService{
     @Autowired
     private HibernateTemplate hibernateTemplate;
 
+    @Autowired
+    private UmPayLoanMoneyService umPayLoanMoneyService;
+
+    @Autowired
+    private UserBillBO userBillBO;
+
+    @Autowired
+    private HibernateTemplate ht;
+
+    @Logger
+    Log log;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
@@ -36,8 +61,8 @@ public class InvestLotteryServiceImpl implements InvestLotteryService{
         Invest invest = hibernateTemplate.get(Invest.class,investId);
         Loan loan = invest.getLoan();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        List<InvestLottery> investLotteryList = hibernateTemplate.find("from InvestLottery t where t.user.id = ? and t.type = ? and t.createdTime like ?",
-                new Object[]{invest.getUser().getId(),InvestLotteryType.NORMAL,simpleDateFormat.format(new Date())});
+        List<InvestLottery> investLotteryList = hibernateTemplate.find("from InvestLottery t where t.user.id = ? and t.type = ? and t.createdTime = ?",
+                new Object[]{invest.getUser().getId(),InvestLotteryType.NORMAL.name(),simpleDateFormat.format(new Date())});
         int total = 0;
         if (invest.getInvestMoney() >= 500 && invest.getInvestMoney() < 1500) {
             total = 1;
@@ -46,10 +71,10 @@ public class InvestLotteryServiceImpl implements InvestLotteryService{
         }
         int nowTotal = investLotteryList.size();
         List<InvestLottery> investLotterys = Lists.newArrayList();
-        if (("xs").equals(loan.getLoanActivityType())){
+        if ((LoanConstants.LoanActivityType.XS).equals(loan.getLoanActivityType())){
             InvestLottery investLottery = luckyDrawRules(invest.getInvestMoney(), this.getInvestLottery(invest, InvestLotteryType.NOVICE));
             investLotterys.add(investLottery);
-        } else if (("pt").equals(loan.getLoanActivityType())) {
+        } else if ((LoanConstants.LoanActivityType.PT).equals(loan.getLoanActivityType())) {
             for (int i=0;i < total - nowTotal;i++) {
                 InvestLottery investLottery = luckyDrawRules(invest.getInvestMoney(),this.getInvestLottery(invest, InvestLotteryType.NORMAL));
                 investLotterys.add(investLottery);
@@ -82,6 +107,7 @@ public class InvestLotteryServiceImpl implements InvestLotteryService{
         investLottery.setType(investLotteryType);
         investLottery.setCreatedTime(new Date());
         investLottery.setValid(false);
+        investLottery.setGranted(false);
         return investLottery;
     }
 
@@ -104,18 +130,23 @@ public class InvestLotteryServiceImpl implements InvestLotteryService{
             case 0:
                 investLottery.setPrizeType(InvestLotteryPrizeType.G);
                 investLottery.setAmount(calculateMoney(investMoney, investLottery));
+                break;
             case 1:
                 investLottery.setPrizeType(InvestLotteryPrizeType.F);
                 investLottery.setAmount(0L);
+                break;
             case 2:
                 investLottery.setPrizeType(InvestLotteryPrizeType.E);
                 investLottery.setAmount(0L);
+                break;
             case 3:
                 investLottery.setPrizeType(InvestLotteryPrizeType.D);
                 investLottery.setAmount(0L);
+                break;
             case 4:
                 investLottery.setPrizeType(InvestLotteryPrizeType.C);
                 investLottery.setAmount(0L);
+                break;
         }
         return investLottery;
     }
@@ -143,6 +174,45 @@ public class InvestLotteryServiceImpl implements InvestLotteryService{
         }
         double money = ArithUtil.div(ArithUtil.mul(ArithUtil.mul(investMoney,0.01), deadLine), repayWay, 2);
         return (long) (money*100);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public boolean WinningPersonIncome(String lotteryId, double bonus, String particUserId, User user) {
+        String orderId = lotteryId + System.currentTimeMillis();
+        String particAccType = UmPayConstants.TransferProjectStatus.PARTIC_ACC_TYPE_PERSON;
+        String transAction = UmPayConstants.TransferProjectStatus.TRANS_ACTION_OUT;
+        String transferOutDetailFormat = "投资人中奖金额发放, 中奖纪录号:{0}, 投资人:{1}, 订单:{2}";
+        String transferOutDetail = MessageFormat.format(transferOutDetailFormat, lotteryId, user.getId(), orderId);
+        String returnMsg = null;
+        try {
+            returnMsg = umPayLoanMoneyService.giveMoney2ParticUserId(orderId, bonus,particAccType,transAction,particUserId,transferOutDetail,"invest_lottery");
+        } catch (ReqDataException | RetDataException e) {
+            log.error(e.getLocalizedMessage(), e);
+            return false;
+        }
+        if(returnMsg.split("\\|")[0].equals("0000")){
+            insertIntoUserBill(user,bonus,transferOutDetail);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private void insertIntoUserBill(User user, double bonus, String detail) {
+        double balance = userBillBO.getBalance(user.getId());
+        double frozenMoney = userBillBO.getFrozenMoney(user.getId());
+        UserBill ub = new UserBill();
+        ub.setBalance(ArithUtil.add(balance, bonus));
+        ub.setFrozenMoney(frozenMoney);
+        ub.setTime(new Date());
+        ub.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+        ub.setType("ti_balance");
+        ub.setTypeInfo("invest_lottery");
+        ub.setMoney(bonus);
+        ub.setSeqNum((userBillBO.getLastestBill(user.getId())!=null?userBillBO.getLastestBill(user.getId()).getSeqNum():0) + 1);
+        ub.setUser(user);
+        ub.setDetail(detail);
+        ht.save(ub);
     }
 
 }
