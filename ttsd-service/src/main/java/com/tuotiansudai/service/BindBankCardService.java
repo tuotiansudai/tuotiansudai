eb3eb2b5954cec9771bab3234e06c3ceb2b4a3b6
zhanglong888 <zhanglong@tuotiansudai.com>
2015-09-28 18:58:25 +0800

    features/zl/bind_bank_success_to_mycard

--- ttsd-service/src/main/java/com/tuotiansudai/service/BindBankCardService.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/BindBankCardService.java
@@ -3,10 +3,13 @@ package com.tuotiansudai.service;
 import com.tuotiansudai.dto.BaseDto;
 import com.tuotiansudai.dto.BindBankCardDto;
 import com.tuotiansudai.dto.PayFormDataDto;
+import com.tuotiansudai.repository.model.BankCardModel;
 
 public interface BindBankCardService {
 
     BaseDto<PayFormDataDto> bindBankCard(BindBankCardDto dto);
 
     String getUserName();
+
+    BankCardModel getPassedBankCard();
 }
