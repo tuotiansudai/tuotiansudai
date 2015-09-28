fac7806a2b4c312a4baf56c558bafecfbde2400f
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-09-28 19:00:33 +0800

    fix custom id, to match with production

--- v1/db_migration/V2015_09_28_12_06__add_user_invest_manage_menu.sql
+++ v1/db_migration/V2015_09_28_12_06__add_user_invest_manage_menu.sql
@@ -8,14 +8,14 @@ COMMIT;
 BEGIN ;
 
 /**增加客服角色**/
-INSERT INTO `role` VALUES ('custom_service','客服','客服') ON duplicate KEY UPDATE `name`=values(`name`),`description`=values(`description`);
+INSERT INTO `role` VALUES ('custorm_service','客服','客服') ON duplicate KEY UPDATE `name`=values(`name`),`description`=values(`description`);
 
 /**增加用户投资管理权限**/
 INSERT INTO `permission` VALUES ('user_invest', '用户投资管理', '用户投资管理') ON duplicate KEY UPDATE `name`=values(`name`),`description`=values(`description`);
 
 /**给管理员和客服角色增加新权限**/
 INSERT INTO `role_permission` VALUES ('ADMINISTRATOR', 'user_invest') ON duplicate KEY update role_id=values(role_id),permission_id=values(permission_id);
-INSERT INTO `role_permission` VALUES ('custom_service', 'user_invest') ON duplicate KEY update role_id=values(role_id),permission_id=values(permission_id);
+INSERT INTO `role_permission` VALUES ('custorm_service', 'user_invest') ON duplicate KEY update role_id=values(role_id),permission_id=values(permission_id);
 
 /**增加用户投资管理菜单**/
 INSERT INTO `menu` VALUES ('user_invest','Management','用户投资管理','/admin/user/userInvest.htm','user_mgr_2','1','1','用户投资管理','1','*','_self','') on duplicate key update type=values(type),label=values(label),url=values(url),pid=values(pid),enable=values(enable),seq_num=values(seq_num),expanded=values(expanded),language=values(language),target=values(target);
