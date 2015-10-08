71fc0216b03892484d2d2586aba0e505715c95b2
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-10-08 15:49:35 +0800

    add app release & download url mapping

---
+++ v1/db_migration/V2015_10_08_15_40__add_app_download_page.sql
@@ -0,0 +1,4 @@
+BEGIN;
+INSERT INTO `url_mapping` VALUES ('appRelease', '/app/release', 'themepath:website/appRelease.xhtml', 'App上线活动页');
+INSERT INTO `url_mapping` VALUES ('appDownload', '/app/download', 'themepath:website/appDownload.xhtml', 'App下载');
+COMMIT;
