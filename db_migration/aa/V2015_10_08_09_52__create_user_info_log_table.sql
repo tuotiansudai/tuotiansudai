61f4216160cb93561eb1ffafc04a8191d238229d
zhanglong888 <zhanglong@tuotiansudai.com>
2015-10-08 09:53:33 +0800

    features/zl/edit_user_info_new

---
+++ db_migration/aa/V2015_10_08_09_52__create_user_info_log_table.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE `aa`.`user_info_log` (
+  `id` varchar(32) NOT NULL,
+  `user_id` varchar(32) NOT NULL,
+  `operate_time` datetime DEFAULT NULL,
+  `ip` varchar(70) DEFAULT NULL,
+  `obj_id` varchar(100) DEFAULT NULL,
+  `description` text,
+  `is_success` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_info_log_id` (`id`) USING BTREE,
+  INDEX `user_info_log_user_id` (`user_id`) USING BTREE,
+  INDEX `user_info_log_obj_id` (`obj_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
