2a812252d57a432226e84dcfa710c588262e4476
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-10-08 16:17:41 +0800

    fix url_mapping template error

--- v1/db_migration/V2015_10_08_15_40__add_app_download_page.sql
+++ v1/db_migration/V2015_10_08_15_40__add_app_download_page.sql
@@ -1,4 +1,4 @@
 BEGIN;
-INSERT INTO `url_mapping` VALUES ('appRelease', '/app/release', 'themepath:website/appRelease.xhtml', 'App上线活动页');
-INSERT INTO `url_mapping` VALUES ('appDownload', '/app/download', 'themepath:website/appDownload.xhtml', 'App下载');
+INSERT INTO `url_mapping` VALUES ('appRelease', '/app/release', 'themepath:website/appRelease.htm', 'App上线活动页');
+INSERT INTO `url_mapping` VALUES ('appDownload', '/app/download', 'themepath:website/appDownload.htm', 'App下载');
 COMMIT;
