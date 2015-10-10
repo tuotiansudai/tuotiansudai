86e9b99acb73109dd4d7a7b3ea0092e0d2d071a5
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-10-10 13:44:12 +0800

    add common auto-invest controller

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/InvestController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/InvestController.java
@@ -83,6 +83,11 @@ public class InvestController {
         return dto;
     }
 
+    @RequestMapping(value = "/investor/auto-invest", method = RequestMethod.GET)
+    public ModelAndView autoInvest(){
+        return autoInvestAuthorize();
+    }
+
     private InvestDetailQueryDto buildInvestDetailQueryDto(Long loanId, @DateTimeFormat(pattern = "yyyy-MM-dd") Date startTime, @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime, LoanStatus loanStatus, InvestStatus investStatus, Integer index, Integer pageSize, String loginName) {
         InvestDetailQueryDto queryDto = new InvestDetailQueryDto();
         queryDto.setLoanId(loanId);
@@ -108,5 +113,9 @@ public class InvestController {
             queryDto.setPageSize(pageSize);
         }
         return queryDto;
+    }
+
+    private ModelAndView autoInvestAuthorize(){
+        return null;
     }
 }
