644c52329f43b40ba8d605bd5c2e3b674b85e445
zhanglong888 <zhanglong@tuotiansudai.com>
2015-10-13 05:39:02 +0800

    hotfixs/zl/add_update_password_regex

--- ttsd-web/src/main/webapp/js/input_password.js
+++ ttsd-web/src/main/webapp/js/input_password.js
@@ -2,7 +2,17 @@ require(['jquery', 'csrf'], function ($) {
     $(function () {
         $('.jq-ps-1').blur(function () {
             if ($(this).val() != '') {
-                if ($('.jq-ps-2').val() != '') {
+                if ($('.jq-ps-1').val() != '') {
+                    var regex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+                    if($(this).val().length < 6 || $(this).val().length > 20){
+                        $('.error').html('长度6至20位');
+                        return;
+                    }
+                    if(!regex.test($(this).val())){
+                        $('.error').html('只能字母和数字组合');
+                        return;
+                    }
+
                     if ($(this).val() != $('.jq-ps-2').val()) {
                         $('.error').html('密码输入不一致');
                         $('.btn-send-form').addClass('grey').attr('disabled', 'disabled');
@@ -35,6 +45,10 @@ require(['jquery', 'csrf'], function ($) {
                 $('.btn-send-form').addClass('grey').attr('disabled', 'disabled');
             }
         })
+
+        function a(sdfsd){
+
+        }
 
     });
 });
