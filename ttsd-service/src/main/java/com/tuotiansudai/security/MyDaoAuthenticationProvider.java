07f6c9c8d239b8e9417702232c7bd248bac389a8
zourenzheng <zourenzheng@tuotiansudai.com>
2015-10-13 18:06:12 +0800

    bug

--- ttsd-service/src/main/java/com/tuotiansudai/security/MyDaoAuthenticationProvider.java
+++ ttsd-service/src/main/java/com/tuotiansudai/security/MyDaoAuthenticationProvider.java
@@ -1,16 +1,18 @@
 package com.tuotiansudai.security;
 
-import com.tuotiansudai.exception.LoginFailedMaxTimesException;
+import com.tuotiansudai.repository.mapper.UserMapper;
+import com.tuotiansudai.repository.model.UserModel;
 import com.tuotiansudai.utils.CaptchaVerifier;
-import com.tuotiansudai.utils.LoginFailedTimesVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.MessageFormat;
 
 public class MyDaoAuthenticationProvider extends DaoAuthenticationProvider {
 
@@ -19,15 +21,20 @@ public class MyDaoAuthenticationProvider extends DaoAuthenticationProvider {
     @Autowired
     private CaptchaVerifier captchaVerifier;
     @Autowired
-    private LoginFailedTimesVerifier loginFailedTimesVerifier;
+    private UserMapper userMapper;
 
     @Override
     protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
         super.additionalAuthenticationChecks(userDetails, authentication);
 
-        String captcha = httpServletRequest.getParameter("captcha");
-        String loginName = httpServletRequest.getParameter("username");
+        UserModel userModel = userMapper.findByLoginNameOrMobile(httpServletRequest.getParameter("username"));
+        boolean enabled = userModel.isActive();
+        if (!enabled) {
+            String errorMessage = MessageFormat.format("Login Error: {0} is locked!", httpServletRequest.getParameter("username"));
+            throw new DisabledException(errorMessage);
+        }
 
+        String captcha = httpServletRequest.getParameter("captcha");
         boolean result = this.captchaVerifier.loginCaptchaVerify(captcha);
 
         if (!result) {
@@ -36,19 +43,10 @@ public class MyDaoAuthenticationProvider extends DaoAuthenticationProvider {
                     "AbstractUserDetailsAuthenticationProvider.badCredentials",
                     "Bad credentials"));
         }
-
-        boolean resultLoginFailedTimes = loginFailedTimesVerifier.verifyLoginFailedMaxTimes(loginName);
-        if (!resultLoginFailedTimes) {
-            logger.debug("Authentication failed: failed times is reach the ceiling");
-            throw new LoginFailedMaxTimesException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials","Bad credentials"));
-        }
     }
 
     public void setCaptchaVerifier(CaptchaVerifier captchaVerifier) {
         this.captchaVerifier = captchaVerifier;
     }
 
-    public void setLoginFailedTimesVerifier(LoginFailedTimesVerifier loginFailedTimesVerifier) {
-        this.loginFailedTimesVerifier = loginFailedTimesVerifier;
-    }
 }
