afa3a30a8fee49c418ca46becd70ada2ce67dea2
zhanglong888 <zhanglong@tuotiansudai.com>
2015-10-27 10:29:50 +0800

    features/zl/send_repay_sms_remind_job

--- v1/db_migration/V2015_10_23_11_40__update_repay_job_config.sql
+++ v1/db_migration/V2015_10_23_11_40__update_repay_job_config.sql
@@ -3,7 +3,7 @@
 BEGIN ;
 
 update config set value = '1' where id = 'schedule.enable_repay_alert';
-update user_message_template set templete = '今天有#{loanRepayAmount}元尚未还款，请及时登录系统还款。【拓天速贷】';
+UPDATE `user_message_template` SET `template`='今天有#{loanRepayAmount}元尚未还款，请及时登录系统还款。【拓天速贷】' WHERE `id`='repay_alert_sms';
 update config set value = '0' where id = 'repay_alert.days_before';
 
 COMMIT ;
