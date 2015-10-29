b941557f84870d0ede05ad2822386f56ff71fba0
zhanglong888 <zhanglong@tuotiansudai.com>
2015-10-29 12:11:59 +0800

    checkout mobile number

--- ttsd-console/src/main/webapp/js/edit-user.js
+++ ttsd-console/src/main/webapp/js/edit-user.js
@@ -1,5 +1,19 @@
 require(['jquery', 'csrf', 'jquery-ui'], function ($) {
 
+    $('.btn-submit').click(function(){
+        var mobile = $('#mobile').val();
+        var reg = /^\d{11}$/;
+
+        if(!reg.test(mobile)){
+            $('.web-error-message').show();
+            $('.console-error-message').hide();
+            $('.message').html('手机号码应为11位数字！');
+            return false;
+        }else{
+            $('.web-error-message').hide();
+        }
+    });
+
     $('#referrer').autocomplete({
         minLength: 0,
         source: function (query, process) {
