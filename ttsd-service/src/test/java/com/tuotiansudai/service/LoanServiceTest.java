07e22f3375c1f8e3936d1647c7e69ac937996eef
zourenzheng <zourenzheng@tuotiansudai.com>
2015-11-03 15:51:39 +0800

    ut

--- ttsd-service/src/test/java/com/tuotiansudai/service/LoanServiceTest.java
+++ ttsd-service/src/test/java/com/tuotiansudai/service/LoanServiceTest.java
@@ -110,7 +110,7 @@ public class LoanServiceTest {
         loanDto.setFundraisingStartTime(date);
         return loanDto;
     }
-    
+
     private List<UserRoleModel> getFakeUserRole(UserModel userModel,Role role) {
         List<UserRoleModel> userRoleModels = Lists.newArrayList();
         UserRoleModel userRoleModel = new UserRoleModel(userModel.getLoginName(),role);
