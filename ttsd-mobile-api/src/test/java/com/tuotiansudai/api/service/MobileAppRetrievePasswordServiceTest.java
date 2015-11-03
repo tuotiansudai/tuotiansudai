197d098f6f9d1ce70936924b5200b852db803b31
zhanglong888 <zhanglong@tuotiansudai.com>
2015-11-03 19:52:17 +0800

    features/zl/new_mobile_api_retrievepassword

--- ttsd-mobile-api/src/test/java/com/tuotiansudai/api/service/MobileAppRetrievePasswordServiceTest.java
+++ ttsd-mobile-api/src/test/java/com/tuotiansudai/api/service/MobileAppRetrievePasswordServiceTest.java
@@ -2,16 +2,28 @@ package com.tuotiansudai.api.service;
 
 import com.tuotiansudai.api.dto.BaseResponseDto;
 import com.tuotiansudai.api.dto.RetrievePasswordRequestDto;
+import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.service.impl.MobileAppRetrievePasswordServiceImpl;
 import com.tuotiansudai.dto.BaseDto;
 import com.tuotiansudai.dto.SmsDataDto;
+import com.tuotiansudai.repository.mapper.UserMapper;
 import com.tuotiansudai.repository.model.CaptchaType;
+import com.tuotiansudai.repository.model.UserModel;
+import com.tuotiansudai.repository.model.UserStatus;
 import com.tuotiansudai.service.SmsCaptchaService;
 import com.tuotiansudai.service.UserService;
+import com.tuotiansudai.utils.IdGenerator;
+import com.tuotiansudai.utils.MyShaPasswordEncoder;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
@@ -25,6 +37,40 @@ public class MobileAppRetrievePasswordServiceTest extends ServiceTestBase {
 
     @Mock
     private SmsCaptchaService smsCaptchaService;
+    @Mock
+    private UserMapper userMapper;
+
+    @Autowired
+    private IdGenerator idGenerator;
+    @Mock
+    private MyShaPasswordEncoder myShaPasswordEncoder;
+
+    @Test
+    public void shouldRetrievePasswordIsOk(){
+        UserModel userModel = getFakeUser();
+        RetrievePasswordRequestDto retrievePasswordRequestDto = new RetrievePasswordRequestDto();
+        retrievePasswordRequestDto.setPassword("123abc");
+        retrievePasswordRequestDto.setPhoneNum("13900000000");
+        retrievePasswordRequestDto.setValidateCode("123456");
+        when(smsCaptchaService.verifyMobileCaptcha(anyString(), anyString(), any(CaptchaType.class))).thenReturn(true);
+        when(userMapper.findByMobile(anyString())).thenReturn(userModel);
+        when(myShaPasswordEncoder.encodePassword(anyString(),anyString())).thenReturn("123456789");
+        BaseResponseDto baseResponseDto = mobileAppRetrievePasswordService.retrievePassword(retrievePasswordRequestDto);
+        assertEquals(ReturnMessage.SUCCESS.getCode(),baseResponseDto.getCode());
+    }
+    @Test
+    public void shouldRetrievePasswordCaptchaIsValid(){
+        UserModel userModel = getFakeUser();
+        RetrievePasswordRequestDto retrievePasswordRequestDto = new RetrievePasswordRequestDto();
+        retrievePasswordRequestDto.setPassword("123abc");
+        retrievePasswordRequestDto.setPhoneNum("13900000000");
+        retrievePasswordRequestDto.setValidateCode("123456");
+        when(smsCaptchaService.verifyMobileCaptcha(anyString(), anyString(), any(CaptchaType.class))).thenReturn(false);
+        when(userMapper.findByMobile(anyString())).thenReturn(userModel);
+        when(myShaPasswordEncoder.encodePassword(anyString(),anyString())).thenReturn("123456789");
+        BaseResponseDto baseResponseDto = mobileAppRetrievePasswordService.retrievePassword(retrievePasswordRequestDto);
+        assertEquals(ReturnMessage.SMS_CAPTCHA_ERROR.getCode(),baseResponseDto.getCode());
+    }
 
     @Test
     public void shouldSendSMS() {
@@ -55,5 +101,19 @@ public class MobileAppRetrievePasswordServiceTest extends ServiceTestBase {
         when(smsCaptchaService.verifyMobileCaptcha(anyString(), anyString(), eq(CaptchaType.RETRIEVE_PASSWORD_CAPTCHA))).thenReturn(true);
         BaseResponseDto responseDto = mobileAppRetrievePasswordService.validateAuthCode(requestDto);
         assert responseDto.isSuccess();
+    }
+
+    private UserModel getFakeUser(){
+        UserModel user1 = new UserModel();
+        user1.setId(idGenerator.generate());
+        user1.setLoginName("test1");
+        user1.setPassword("123");
+        user1.setMobile("13900000000");
+        user1.setRegisterTime(new Date());
+        user1.setLastLoginTime(new Date());
+        user1.setLastModifiedTime(new Date());
+        user1.setStatus(UserStatus.ACTIVE);
+        user1.setSalt("123");
+        return user1;
     }
 }
