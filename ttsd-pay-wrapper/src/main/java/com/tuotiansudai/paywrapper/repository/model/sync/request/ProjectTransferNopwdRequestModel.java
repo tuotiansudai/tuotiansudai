fb1ee7d0f531aa736df2b2a3f916a56e8b17e2e6
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-11-04 19:20:11 +0800

    create a new branch(_B) because the old branch has too many conflict with master

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/request/ProjectTransferNopwdRequestModel.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/sync/request/ProjectTransferNopwdRequestModel.java
@@ -1,12 +1,14 @@
 package com.tuotiansudai.paywrapper.repository.model.sync.request;
 
 import com.tuotiansudai.paywrapper.repository.model.*;
+import com.tuotiansudai.paywrapper.repository.model.async.request.BaseAsyncRequestModel;
 
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
-public class ProjectTransferNopwdRequestModel extends BaseSyncRequestModel{
+public class ProjectTransferNopwdRequestModel extends BaseAsyncRequestModel {
     private String orderId;
     private String merDate;
     private String projectId;
@@ -38,6 +40,7 @@ public class ProjectTransferNopwdRequestModel extends BaseSyncRequestModel{
         this.merDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
         this.particAccType = UmPayParticAccType.INDIVIDUAL.getCode();
         this.particType = UmPayParticType.INVESTOR.getCode();
+        this.notifyUrl = MessageFormat.format("{0}/callback/{1}", CALLBACK_HOST_PROPS.get("ump.callback.back.host"), "invest_notify");
     }
 
     public Map<String, String> generatePayRequestData() {
