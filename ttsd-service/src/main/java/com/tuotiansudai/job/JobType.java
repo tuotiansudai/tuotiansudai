e59cf15e17335385bbf81d4cc609877e28f29909
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-11-04 12:44:56 +0800

    add parameter to distZip task

--- ttsd-service/src/main/java/com/tuotiansudai/job/JobType.java
+++ ttsd-service/src/main/java/com/tuotiansudai/job/JobType.java
@@ -1,9 +1,29 @@
 package com.tuotiansudai.job;
 
+/**
+ *
+ * TODO: 所有人请注意
+ * TODO: 所有人请注意
+ * TODO: 所有人请注意
+ *
+ *
+ *
+ *
+ * 在此枚举中添加字段时，请同步添加到 ttsd-job-worker/src/main/resources/job-worker.properties.deploy/job-worker.properties 里
+ *
+ * 请不要将 OverInvestPayBack 添加到上述文件中
+ *
+ *
+ * TODO: 所有人请注意
+ * TODO: 所有人请注意
+ * TODO: 所有人请注意
+ */
 public enum JobType {
     Default("默认类别"),
-    LoanStatue("标的状态从预热转为可投资"),
-    LoanOut("放款后续处理");
+    LoanStatusToRaising("标的状态从预热转为可投资"),
+    LoanOut("放款后续处理"),
+    AutoInvest("自动投资"),
+    OverInvestPayBack("超投还款");
 
     private final String description;
 
