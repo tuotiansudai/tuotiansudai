1248dfce169362575f130f9175e1bdd251019319
zourenzheng <zourenzheng@tuotiansudai.com>
2015-11-05 20:47:52 +0800

    funds user download

--- ttsd-console/src/main/webapp/js/user-funds.js
+++ ttsd-console/src/main/webapp/js/user-funds.js
@@ -1,11 +1,20 @@
 /**
  * Created by CBJ on 2015/11/2.
  */
-require(['jquery', 'bootstrap','bootstrapSelect','bootstrapDatetimepicker'], function ($) {
+require(['jquery', 'bootstrap','bootstrapSelect','bootstrapDatetimepicker','jquery-ui'], function ($) {
     $(function () {
 
-        $('#investDate .date').datetimepicker({
+        $('#investDateBegin,#investDateEnd').datetimepicker({
             format: 'YYYY-MM-DD'
+        });
+        $('#investDateEnd').datetimepicker({
+            useCurrent: false
+        });
+        $("#investDateBegin").on("dp.change", function (e) {
+            $('#investDateEnd').data("DateTimePicker").minDate(e.date);
+        });
+        $("#investDateEnd").on("dp.change", function (e) {
+            $('#investDateBegin').data("DateTimePicker").maxDate(e.date);
         });
         $('.selectpicker').selectpicker();
 
@@ -20,6 +29,15 @@ require(['jquery', 'bootstrap','bootstrapSelect','bootstrapDatetimepicker'], fun
             var operationType = $('.operationType').val();
             var businessType = $('.businessType').val();
             window.location.href = "/userFunds?loginName="+loginName+"&startTime="+startTime+"&endTime="+endTime+"&userBillOperationType="+operationType+"&userBillBusinessType="+businessType+"&currentPageNo=1&pageSize=10";
+        });
+
+        $('.down-load').click(function(){
+            var loginName = $('.jq-loginName').val();
+            var startTime = $('.jq-startTime').val();
+            var endTime = $('.jq-endTime').val();
+            var operationType = $('.operationType').val();
+            var businessType = $('.businessType').val();
+            window.location.href = "/userFundsDownload?loginName="+loginName+"&startTime="+startTime+"&endTime="+endTime+"&userBillOperationType="+operationType+"&userBillBusinessType="+businessType;
         });
     });
 })
