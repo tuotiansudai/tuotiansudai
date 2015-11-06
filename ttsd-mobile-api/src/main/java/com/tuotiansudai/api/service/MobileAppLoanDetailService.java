f69c2f1c66438b69a92afcb574a5e3ed1e8db9e3
zhanglong888 <zhanglong@tuotiansudai.com>
2015-11-06 11:56:02 +0800

    features/zl/new_mobile_api_loan_detail

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/MobileAppLoanDetailService.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/MobileAppLoanDetailService.java
@@ -11,11 +11,5 @@ public interface MobileAppLoanDetailService {
 
     BaseResponseDto generateLoanDetail(LoanDetailRequestDto loanDetailRequestDto);
 
-    LoanDetailResponseDataDto convertLoanDetailFromLoan(LoanModel loan, List<EvidenceResponseDataDto> evidence);
-
-    List<InvestRecordResponseDataDto> convertInvestRecordDtoFromInvest(List<InvestModel> invests);
-
-    List<String> getImageUrl(String guaranteeCompanyDescription);
-
 
 }
