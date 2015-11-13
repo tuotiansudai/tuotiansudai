73bb2620ca0dc5042ee8dd986a64fafb2fb2b85a
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-11-14 02:17:18 +0800

    add unit test

--- ttsd-mobile-api/src/test/java/com/tuotiansudai/api/controller/MobileAppBankCardControllerTest.java
+++ ttsd-mobile-api/src/test/java/com/tuotiansudai/api/controller/MobileAppBankCardControllerTest.java
@@ -1,7 +1,9 @@
 package com.tuotiansudai.api.controller;
 
-import com.tuotiansudai.api.dto.BankCardReplaceRequestDto;
 import com.tuotiansudai.api.dto.BankCardRequestDto;
+import com.tuotiansudai.api.dto.BankCardResponseDto;
+import com.tuotiansudai.api.dto.BaseResponseDto;
+import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.service.MobileAppBankCardService;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -9,6 +11,7 @@ import org.mockito.Mock;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 public class MobileAppBankCardControllerTest extends ControllerTestBase {
     @InjectMocks
@@ -24,10 +27,20 @@ public class MobileAppBankCardControllerTest extends ControllerTestBase {
 
     @Test
     public void shouldBankCardBind() throws Exception {
-        /*
+        BankCardResponseDto dataDto = new BankCardResponseDto();
+        dataDto.setUrl("url");
+        dataDto.setRequestData("requestData");
+
+        BaseResponseDto baseDto = new BaseResponseDto();
+        baseDto.setCode(ReturnMessage.SUCCESS.getCode());
+        baseDto.setMessage(ReturnMessage.SUCCESS.getMsg());
+        baseDto.setData(dataDto);
+
+        when(service.bindBankCard(any(BankCardRequestDto.class))).thenReturn(baseDto);
         doRequestWithServiceMockedTest("/bankcard/bind",
-                new BankCardRequestDto());
-        */
+                new BankCardRequestDto())
+                .andExpect(jsonPath("$.data.url").value("url"))
+                .andExpect(jsonPath("$.data.requestData").value("requestData"));
     }
 
     @Test
@@ -43,17 +56,15 @@ public class MobileAppBankCardControllerTest extends ControllerTestBase {
 
     @Test
     public void shouldBankCardSign() throws Exception {
-        /*
+        when(service.openFastPay(any(BankCardRequestDto.class))).thenReturn(successResponseDto);
         doRequestWithServiceMockedTest("/bankcard/sign",
-                new BankCardReplaceRequestDto());
-        */
+                new BankCardRequestDto());
     }
 
     @Test
     public void queryBindAndSginStatus() throws Exception {
-        /*
+        when(service.queryStatus(any(BankCardRequestDto.class))).thenReturn(successResponseDto);
         doRequestWithServiceMockedTest("/bankcard/query",
                 new BankCardRequestDto());
-        */
     }
 }
