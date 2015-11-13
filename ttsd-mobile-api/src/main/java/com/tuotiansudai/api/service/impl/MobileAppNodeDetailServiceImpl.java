2844b93ece7202eeadbbdef7c6fe7c498cb702b6
zhanglong888 <zhanglong@tuotiansudai.com>
2015-11-13 19:45:13 +0800

    features/zl/new_mobile_api_node_list_detail

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppNodeDetailServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppNodeDetailServiceImpl.java
@@ -3,14 +3,40 @@ package com.tuotiansudai.api.service.impl;
 
 import com.tuotiansudai.api.dto.BaseResponseDto;
 import com.tuotiansudai.api.dto.NodeDetailRequestDto;
+import com.tuotiansudai.api.dto.NodeDetailResponseDataDto;
+import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.service.MobileAppNodeDetailService;
-import org.apache.commons.lang3.NotImplementedException;
+import com.tuotiansudai.repository.mapper.AnnouncementManagementMapper;
+import com.tuotiansudai.repository.model.AnnouncementManagementModel;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MobileAppNodeDetailServiceImpl implements MobileAppNodeDetailService {
+    @Value("${mobile.app.imageUrl.pattern}")
+    private String urlPattern;
+    @Value("${announcement.image.domain}")
+    private String domainName;
+    @Autowired
+    private AnnouncementManagementMapper announcementManagementMapper;
     @Override
     public BaseResponseDto generateNodeDetail(NodeDetailRequestDto requestDto) {
-        throw new NotImplementedException(getClass().getName());
+        BaseResponseDto<NodeDetailResponseDataDto> dto = new BaseResponseDto<>();
+        String nodeId = requestDto.getNodeId();
+        if(StringUtils.isEmpty(nodeId)){
+            return new BaseResponseDto(ReturnMessage.NODE_ID_IS_NOT_EXIST.getCode(),ReturnMessage.NODE_ID_IS_NOT_EXIST.getMsg());
+        }
+        AnnouncementManagementModel announcementManagementModel = announcementManagementMapper.findById(Long.parseLong(nodeId));
+        if(announcementManagementModel == null){
+            return new BaseResponseDto(ReturnMessage.NODE_ID_IS_NOT_EXIST.getCode(),ReturnMessage.NODE_ID_IS_NOT_EXIST.getMsg());
+        }
+        dto.setCode(ReturnMessage.SUCCESS.getCode());
+        dto.setMessage(ReturnMessage.SUCCESS.getMsg());
+        NodeDetailResponseDataDto dataDto = new NodeDetailResponseDataDto(announcementManagementModel);
+        dataDto.addDomainNameToImageUrl(urlPattern, domainName);
+        dto.setData(dataDto);
+        return dto;
     }
 }
