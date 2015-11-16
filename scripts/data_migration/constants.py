d70523a765296508586a497919ee51d0473308e9
Shi Lei <mleoshi@gmail.com>
2015-11-17 00:03:57 +0800

    migrate user data

---
+++ scripts/data_migration/constants.py
@@ -0,0 +1,6 @@
+HOST = '192.168.33.10'
+AA_DB = 'aa'
+ORIGINAL_DB = 'edxapp'
+
+USERNAME = 'root'
+PASSWORD = ''
---
+++ scripts/data_migration/migrate.py
@@ -0,0 +1,15 @@
+import argparse
+import importlib
+from scripts.data_migration.base import DBWrapper
+from scripts.data_migration.constants import HOST, USERNAME, PASSWORD, ORIGINAL_DB, AA_DB
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Data migration tool')
+    parser.add_argument('-t', '--table', required=True, help='New table name')
+    args = parser.parse_args()
+
+    old_db = DBWrapper(host=HOST, user=USERNAME, password=PASSWORD, db_name=ORIGINAL_DB)
+    new_db = DBWrapper(host=HOST, user=USERNAME, password=PASSWORD, db_name=AA_DB)
+    cls = getattr(importlib.import_module(args.table), '{0}Migrate'.format(args.table.title()))
+    cls(old_db, new_db).migrate()
+
