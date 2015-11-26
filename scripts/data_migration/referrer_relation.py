0ebf0f58673a62f281e4537dc85edc045028152f
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-11-26 15:02:12 +0800

    add level<=4 limitation for referrer_relation table

--- scripts/data_migration/referrer_relation.py
+++ scripts/data_migration/referrer_relation.py
@@ -6,7 +6,7 @@ class ReferrerRelationMigrate(BaseMigrate):
     Class Naming Convention: `NewTableNameMigrate(BaseMigrate)`
     """
     # select sql which is executed on original db (edxapp, tuotiansudai etc)
-    SELECT_SQL = "SELECT referrer_id, user_id, level FROM referrer_relation"
+    SELECT_SQL = "SELECT referrer_id, user_id, level FROM referrer_relation where level <= 4"
     # insert sql which is executed on aa db
     INSERT_SQL = "INSERT INTO referrer_relation(`referrer_login_name`, `login_name`, `level`) VALUES(%s, %s, %s)"
 
