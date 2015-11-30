fdcdaca866d24af32ccde2418e38130a48673866
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-11-30 18:19:46 +0800

    fix bug of db migrate

--- v1/db_migration/V2015_11_30_15_21__insert_into_dict_activity_reward.sql
+++ v1/db_migration/V2015_11_30_15_21__insert_into_dict_activity_reward.sql
@@ -1,3 +1,3 @@
 BEGIN ;
-INSERT INTO `dict` VALUES ('activity_reward','activity_reward',NULL,0,NULL,'利息管理费','ecb1e59125f64f9b9765901cc6a9a222');
+INSERT INTO `dict` VALUES ('activity_reward','activity_reward',NULL,0,NULL,'活动奖励','ecb1e59125f64f9b9765901cc6a9a222');
 COMMIT ;
