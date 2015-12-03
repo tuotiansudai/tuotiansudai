7ae14ca4adec2073ec4211330172a17a44c86bd7
Xiduan Gao <gaoxiduan@gmail.com>
2015-12-03 15:08:16 +0800

    invalid url nav to 404

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/ActivitiesController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/ActivitiesController.java
@@ -1,4 +1,5 @@
 package com.tuotiansudai.web.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(path = "/activity")
 public class ActivitiesController {
-    @RequestMapping(path = "/{item}", method = RequestMethod.GET)
-    public ModelAndView account(@PathVariable String item){
-        return new ModelAndView("/activity/" + item);
+
+    @RequestMapping(path = "/{item:^grand|ranking|recruit$}", method = RequestMethod.GET)
+    public ModelAndView activities(@PathVariable String item) {
+        return new ModelAndView("/activities/" + item, "responsive", true);
     }
 }
