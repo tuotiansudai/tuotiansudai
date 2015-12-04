979a2469af9cf1873ff8769be9259a59dd76d893
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-12-04 10:57:26 +0800

    fix unit test

--- ttsd-mobile-api/src/test/java/com/tuotiansudai/api/controller/MobileAppBannerControllerTest.java
+++ ttsd-mobile-api/src/test/java/com/tuotiansudai/api/controller/MobileAppBannerControllerTest.java
@@ -1,11 +1,18 @@
 package com.tuotiansudai.api.controller;
 
+import com.tuotiansudai.api.service.MobileAppBannerService;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
 
 public class MobileAppBannerControllerTest extends ControllerTestBase {
     @InjectMocks
     private MobileAppBannerController controller;
+
+    @Mock
+    private MobileAppBannerService service;
 
     @Override
     protected Object getControllerObject() {
@@ -14,6 +21,7 @@ public class MobileAppBannerControllerTest extends ControllerTestBase {
 
     @Test
     public void getBannerTest() throws Exception {
+        when(service.generateBannerList()).thenReturn(successResponseDto);
         doRequestWithServiceMockedTest("/get/banner");
     }
 }
