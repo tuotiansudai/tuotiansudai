7d5bed89948bc7d557099e490253f8757131895f
zourenzheng <zourenzheng@tuotiansudai.com>
2015-12-07 16:36:43 +0800

    recharge and withdraw add others

--- ttsd-console/src/main/java/com/tuotiansudai/console/controller/BusinessIntelligenceController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/BusinessIntelligenceController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -19,6 +20,9 @@ import java.util.List;
 public class BusinessIntelligenceController {
     @Autowired
     private BusinessIntelligenceService businessIntelligenceService;
+
+    private static final List<String> PROVINCES = Arrays.asList("北京","上海","天津","重庆","辽宁","吉林","黑龙江","河北","山西","陕西","甘肃","青海","山东","安徽","江苏","浙江","河南",
+            "湖北","湖南","江西","福建","云南","海南","四川","贵州","广东","内蒙古","新疆","广西","西藏","宁夏","香港","澳门","台湾");
 
     @ResponseBody
     @RequestMapping(value = "/user-register-trend", method = RequestMethod.GET)
@@ -34,8 +38,29 @@ public class BusinessIntelligenceController {
     public List<KeyValueModel> queryUserRechargeTrend(
             @RequestParam(name = "granularity") Granularity granularity,
             @RequestParam(name = "startTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startTime,
-            @RequestParam(name = "endTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime){
-        return businessIntelligenceService.queryUserRechargeTrend(granularity, startTime, endTime);
+            @RequestParam(name = "endTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime,
+            @RequestParam(name = "province",required = false) String province){
+        return businessIntelligenceService.queryUserRechargeTrend(granularity, startTime, endTime, province);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "user-withdraw-trend", method = RequestMethod.GET)
+    public List<KeyValueModel> queryUserWithdrawTrend(
+            @RequestParam(name = "granularity") Granularity granularity,
+            @RequestParam(name = "startTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startTime,
+            @RequestParam(name = "endTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime,
+            @RequestParam(name = "province",required = false) String province){
+        return businessIntelligenceService.queryUserWithdrawTrend(granularity, startTime, endTime, province);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "user-account-trend", method = RequestMethod.GET)
+    public List<KeyValueModel> queryUserAccountTrend(
+            @RequestParam(name = "granularity") Granularity granularity,
+            @RequestParam(name = "startTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startTime,
+            @RequestParam(name = "endTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime,
+            @RequestParam(name = "province",required = false) String province){
+        return businessIntelligenceService.queryUserAccountTrend(granularity, startTime, endTime, province);
     }
 
 }
