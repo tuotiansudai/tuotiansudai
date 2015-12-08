dfea4ab58a49ecffba2695dcf7e8cda6a2d34d20
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-12-08 19:59:53 +0800

    bi sql refactor

--- ttsd-service/src/main/java/com/tuotiansudai/service/BusinessIntelligenceService.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/BusinessIntelligenceService.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 public interface BusinessIntelligenceService {
-    List<KeyValueModel> queryUserRegisterTrend(Granularity granularity, Date startTime, Date endTime);
+    List<KeyValueModel> queryUserRegisterTrend(Granularity granularity, Date startTime, Date endTime, String province);
 
     List<KeyValueModel> queryUserRechargeTrend(Granularity granularity, Date startTime, Date endTime, String province);
 
