d40f2d960eb909311561c5022da3e45694213e89
zhanglong888 <zhanglong@tuotiansudai.com>
2015-12-10 18:14:14 +0800

    hotfixs/zl/contract_investor_loaner

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/ContractController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/ContractController.java
@@ -3,6 +3,7 @@ package com.tuotiansudai.web.controller;
 import com.tuotiansudai.repository.model.ContractType;
 import com.tuotiansudai.service.ContractService;
 import com.tuotiansudai.web.util.LoginUserInfo;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -23,32 +26,34 @@ public class ContractController {
     private ContractService contractService;
 
     @RequestMapping(value = "/investor/loanId/{loanId}", method = RequestMethod.GET)
-    public void generateInvestorContract(@PathVariable long loanId, HttpServletResponse response) throws IOException {
+    public void generateInvestorContract(@PathVariable long loanId, HttpServletRequest httpServletRequest , HttpServletResponse response) throws ServletException, IOException {
         String loginName = LoginUserInfo.getLoginName();
         try {
-            String pdfString = contractService.generateInvestorContract(loginName,loanId, ContractType.INVEST);
+            String pdfString = contractService.generateInvestorContract(loginName, loanId, ContractType.INVEST);
+            if(StringUtils.isEmpty(pdfString)){
+                httpServletRequest.getRequestDispatcher("/error/404").forward(httpServletRequest, response);
+                return;
+            }
             response.setContentType("application/pdf;charset=UTF-8");
-            contractService.generateContractPdf(pdfString,response.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }catch (Exception e){
-            logger.error(e.getLocalizedMessage(),e);
-            response.sendRedirect("/error/404");
+            contractService.generateContractPdf(pdfString, response.getOutputStream());
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
         }
     }
 
     @RequestMapping(value = "/loaner/loanId/{loanId}", method = RequestMethod.GET)
-    public void generateLoanerContract(@PathVariable long loanId, HttpServletResponse response) throws IOException {
+    public void generateLoanerContract(@PathVariable long loanId,HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
         String loginName = LoginUserInfo.getLoginName();
-        String pdfString = contractService.generateInvestorContract(loginName,loanId, ContractType.LOAN);
-        response.setContentType("application/pdf;charset=UTF-8");
         try {
-            contractService.generateContractPdf(pdfString,response.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }catch (Exception e){
-            logger.error(e.getLocalizedMessage(),e);
-            response.sendRedirect("/error/404");
+            String pdfString = contractService.generateInvestorContract(loginName, loanId, ContractType.LOAN);
+            if(StringUtils.isEmpty(pdfString)){
+                httpServletRequest.getRequestDispatcher("/error/404").forward(httpServletRequest, response);
+                return;
+            }
+            response.setContentType("application/pdf;charset=UTF-8");
+            contractService.generateContractPdf(pdfString, response.getOutputStream());
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
         }
     }
 
