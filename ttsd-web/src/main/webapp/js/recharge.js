67c233815f23cef8151b6f04bafc50bea3733852
fulanyan <fly39244080@163.com>
2015-12-14 16:46:50 +0800

    remove note

--- ttsd-web/src/main/webapp/js/recharge.js
+++ ttsd-web/src/main/webapp/js/recharge.js
@@ -19,10 +19,13 @@ require(['jquery', 'layerWrapper', 'csrf', 'autoNumeric', 'commonFun'], function
         if (rechargeInputAmountElement) {
             rechargeInputAmountElement.autoNumeric("init");
             rechargeInputAmountElement.keyup(function () {
-                var amount = parseFloat(rechargeInputAmountElement.autoNumeric("get"));
-                if (isNaN(amount) || amount < 0.01) {
+                var $this=$(this),
+                 amount = parseFloat(rechargeInputAmountElement.autoNumeric("get"));
+                if (isNaN(amount) || amount < 1) {
+                    $this.parents('form').find('.error').show();
                     rechargeSubmitElement.prop('disabled', true).removeClass('btn-normal');
                 } else {
+                    $this.parents('form').find('.error').hide();
                     rechargeSubmitElement.prop('disabled', false).addClass('btn-normal');
                 }
             });
@@ -44,8 +47,8 @@ require(['jquery', 'layerWrapper', 'csrf', 'autoNumeric', 'commonFun'], function
         if (fastRechargeInputAmountElement) {
             fastRechargeInputAmountElement.autoNumeric("init");
             fastRechargeInputAmountElement.keyup(function () {
-                var $this=$(this);
-                var amount = parseFloat($this.autoNumeric("get"));
+                var $this=$(this),
+                 amount = parseFloat($this.autoNumeric("get"));
                 if (isNaN(amount) || amount < 1) {
                     $this.parents('form').find('.error').show();
                     fastRechargeSubmitElement.prop('disabled', true).removeClass('btn-normal');
