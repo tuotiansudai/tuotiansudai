9b2911b1d54ac87b5f9d36f2b61b8af384142cb7
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-12-15 19:55:58 +0800

    add change password api

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppRetrievePasswordController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppRetrievePasswordController.java
@@ -2,9 +2,11 @@ package com.tuotiansudai.api.controller;
 
 import com.tuotiansudai.api.dto.BaseResponseDto;
 import com.tuotiansudai.api.dto.RetrievePasswordRequestDto;
+import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.service.MobileAppRetrievePasswordService;
 import com.tuotiansudai.api.util.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,8 +27,14 @@ public class MobileAppRetrievePasswordController extends MobileAppBaseController
      * @function 找回密码
      */
     @RequestMapping(value = "/retrievepassword", method = RequestMethod.POST)
-    public BaseResponseDto retrievePassword(@Valid @RequestBody RetrievePasswordRequestDto retrievePasswordRequestDto) {
-        return retrievePasswordService.retrievePassword(retrievePasswordRequestDto);
+    public BaseResponseDto retrievePassword(@Valid @RequestBody RetrievePasswordRequestDto retrievePasswordRequestDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            String errorCode = bindingResult.getFieldError().getDefaultMessage();
+            String errorMessage = ReturnMessage.getErrorMsgByCode(errorCode);
+            return new BaseResponseDto(errorCode,errorMessage);
+        } else {
+            return retrievePasswordService.retrievePassword(retrievePasswordRequestDto);
+        }
     }
 
     /**
