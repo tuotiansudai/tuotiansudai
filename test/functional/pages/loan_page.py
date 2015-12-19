11ca8edebe2d18691c63890685df522f0d59b8af
zhanglong888 <zhanglong@tuotiansudai.com>
2015-12-19 18:16:42 +0800

    features/zl/function_test_invest

--- test/functional/pages/loan_page.py
+++ test/functional/pages/loan_page.py
@@ -3,10 +3,10 @@ from test.functional.pages.base_page import BasePage
 
 
 class LoanPage(BasePage):
-    url = "/loan"
+    url = "/loan/313502507433984"
 
     def click_invest(self):
-        self.find_element_by_link_text("立即投资").click();
+        self.find_element_by_css(".btn-pay").click();
         return self
 
 
--- test/functional/test_invest.py
+++ test/functional/test_invest.py
@@ -9,12 +9,15 @@ class TestInvest(BaseSeleniumTestCase):
     def test_should_navigate_to_index_page_after_login(self):
         LoginPage(self.selenium).login('shenjiaojiao', '123abc')
         username = IndexPage(self.selenium).get_username()
+
         self.assertIn("shenjiaojiao", username)
 
         IndexPage(self.selenium).click_prepare_invest()
 
-        LoanPage(self.selenium).click_invest()
-        time.sleep(50)
+        ump_url = LoanPage(self.selenium).click_invest().get_current_page_url()
+
+        self.assertEqual("http://pay.soopay.net/spay/pay/payservice.do",ump_url)
+
 
 
 
