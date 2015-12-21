c28f1ada2b3ee98fee98185aeae4659fb22b0819
zourenzheng <zourenzheng@tuotiansudai.com>
2015-12-21 12:11:45 +0800

    format code

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/async/callback/BankCardNotifyRequestModel.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/repository/model/async/callback/BankCardNotifyRequestModel.java
@@ -1,5 +1,7 @@
 package com.tuotiansudai.paywrapper.repository.model.async.callback;
 
+import com.tuotiansudai.repository.model.AgreementType;
+
 public class BankCardNotifyRequestModel extends BaseCallbackRequestModel {
     private String userId;
 
@@ -8,6 +10,12 @@ public class BankCardNotifyRequestModel extends BaseCallbackRequestModel {
     private String userBindAgreementList;
 
     private String gateId;
+
+    private static String SUCCESS_CODE = "0000";
+
+    public boolean isOpenPay() {
+        return userBindAgreementList != null && userBindAgreementList.contains(AgreementType.ZKJP0700.name()) && userBindAgreementList.lastIndexOf(SUCCESS_CODE) != -1;
+    }
 
     public String getUserId() {
         return userId;
