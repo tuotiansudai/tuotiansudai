6c5e958c7d6b793feb25197eb09d35c9aad568a2
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-12-22 12:33:36 +0800

    delete logger in __init__.py

--- test/functional/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-hdlr = logging.FileHandler('function_test.log')
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.DEBUG)
