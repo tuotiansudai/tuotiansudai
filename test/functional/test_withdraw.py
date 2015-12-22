587c73dd33acf6e5b87f2f84687d89fa14f319d1
zhoubx <zhoubaoxin@tuotiansudai.com>
2015-12-22 12:32:00 +0800

    add test_should_navigate_to_umpay_page_after_submit method

--- test/functional/test_withdraw.py
+++ test/functional/test_withdraw.py
@@ -1,3 +1,4 @@
+import test.functional.settings as settings
 from test.functional.base import BaseSeleniumTestCase
 from test.functional.pages.withdraw_page import WithdrawPage
 from test.functional.pages.login_page import LoginPage
@@ -20,3 +21,18 @@ class TestWithdraw(BaseSeleniumTestCase):
         new_balance = AccountPage(self.selenium).get_balance()
 
         self.assertEqual(old_balance, new_balance + withdraw_money)
+
+
+    def test_should_navigate_to_umpay_page_after_submit(self):
+
+        LoginPage(self.selenium).login('shenjiaojiao', '123abc')
+
+        WithdrawPage(self.selenium).withdraw(3)
+
+        new_window_handle = self.selenium.window_handles[1]
+
+        self.selenium.switch_to_window(new_window_handle)
+
+        new_window_url = self.selenium.current_url
+
+        self.assertIn(settings.UMPAY_BASE_URL, new_window_url)
