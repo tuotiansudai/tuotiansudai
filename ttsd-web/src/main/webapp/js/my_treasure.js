2ddf2dc6a2fb5853617c3ace00ae39d35d798672
xvqiang <xuqiang@tuotiansudai.com>
2015-12-23 18:50:34 +0800

    make my-treasure

--- ttsd-web/src/main/webapp/js/my_treasure.js
+++ ttsd-web/src/main/webapp/js/my_treasure.js
@@ -1,30 +1,29 @@
-require(['jquery','layerWrapper', 'moment', 'pagination', 'mustache', 'text!/tpl/my-treasure-table.mustache'],
- function ($, layer, Mustache, moment, pagination, treasureListTemplate) {
+require(['jquery', 'layerWrapper', 'moment', 'pagination', 'mustache', 'text!/tpl/my-treasure-table.mustache','csrf'],
+function($, layer, moment, pagination, Mustache, treasureListTemplate) {
     $(function() {
-        var $body=$('body'),
-            $navLi=$('.column-title .title-navli'),
-            $listTab=$('.list-tab');
+        var $body = $('body'),
+            $navLi = $('.column-title .title-navli'),
+            $listTab = $('.list-tab');
 
-        $navLi.on('click',function(event) {
+        $navLi.on('click', function(event) {
             event.preventDefault();
-            var $self=$(this),
-                index=$self.index();
+            var $self = $(this),
+                index = $self.index();
             $navLi.removeClass('active');
             $self.addClass('active');
             $listTab.removeClass('tab-show');
-            $('.list-tab:eq('+index+')').addClass('tab-show');
+            $('.list-tab:eq(' + index + ')').addClass('tab-show');
         });
-        $body.on('click','.select-item',function(event) {
+        $body.on('click', '.select-item', function(event) {
             event.preventDefault();
-            var $self=$(this),
-                dataStatus=$self.attr('data-status');
+            var $self = $(this);
             $self.addClass('current').siblings('.select-item').removeClass('current');
-            templateData(dataStatus);
+            loadLoanData();
         });
 
-        $('.invest-list').on('mouseenter','.project-name',function() {
+        $('.invest-list').on('mouseenter', '.project-name', function() {
             layer.closeAll('tips');
-            if($(this).text().length>15){
+            if ($(this).text().length > 15) {
                 layer.tips($(this).text(), $(this), {
                     tips: [1, '#efbf5c'],
                     time: 2000,
@@ -35,23 +34,25 @@ require(['jquery','layerWrapper', 'moment', 'pagination', 'mustache', 'text!/tpl
             }
         });
 
-        function templateData(data){
-            $.ajax({
-                url: 'http://192.168.100.70:8080/coupon/use-record?loginName=shenjiaojiao&index=1&pageSize=10',
-                type: 'POST',
-                dataType: 'json',
-                data: {param1: data}
-            })
-            .done(function(json) {
-                var html = Mustache.render(treasureListTemplate, json);
-                console.log(json);
+        function loadLoanData() {
+            $('.pagination').loadPagination({}, function(data) {
+                var html = Mustache.render(treasureListTemplate, data);
                 $('.invest-list').html(html);
-            })
-            .fail(function() {
-                console.log("error");
             });
-        }
-        
-        templateData($('.select-item.current').attr('data-status'));
+        };
+
+        $('.invest-list').on('mouseenter', '.project-name', function() {
+            layer.closeAll('tips');
+            if ($(this).text().length > 15) {
+                layer.tips($(this).text(), $(this), {
+                    tips: [1, '#efbf5c'],
+                    time: 2000,
+                    tipsMore: true,
+                    area: 'auto',
+                    maxWidth: '500'
+                });
+            }
+        });
+        loadLoanData();
     });
 });
--- ttsd-web/src/main/webapp/templates/my-treasure.ftl
+++ ttsd-web/src/main/webapp/templates/my-treasure.ftl
@@ -57,7 +57,7 @@
         <div class="invest-list">
 
         </div>
-        <div class="pagination" data-url="/investor/invest-list-data" data-page-size="10">
+        <div class="pagination" data-url="/coupon/use-record" data-page-size="10">
         </div>
     </div>
 </div>
