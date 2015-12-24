3869a653b838f66faedb7bf7ce963b7b43f52971
fulanyan <fly39244080@163.com>
2015-12-24 19:59:11 +0800

    change home page

--- ttsd-console/src/main/webapp/js/index.js
+++ ttsd-console/src/main/webapp/js/index.js
@@ -60,7 +60,7 @@ require(['jquery','loadEcharts','bootstrapDatetimepicker'],function($,loadEchart
     $('.start-date').val(initStartDate);
     $('.end-date').val(initEndDate);
 
-    function showReport(form,url,reportbox,name,category) {
+    function showReport(form,url,reportbox,name,category,xAxisName) {
         var Btn=$(form).find(':button');
         Btn.click(function() {
             var dataFormat=$(form).serialize();
@@ -81,7 +81,7 @@ require(['jquery','loadEcharts','bootstrapDatetimepicker'],function($,loadEchart
                          option = loadEcharts.ChartOptionTemplates.Lines(data, name);
                         break;
                     case 'bar':
-                         option = loadEcharts.ChartOptionTemplates.Bar(data, name);
+                         option = loadEcharts.ChartOptionTemplates.Bar(data, name,xAxisName);
                         break;
                     case 'pie':
                         option = loadEcharts.ChartOptionTemplates.Pie(data, name);
@@ -107,7 +107,7 @@ require(['jquery','loadEcharts','bootstrapDatetimepicker'],function($,loadEchart
     showReport('#formWithdrawReport','/bi/user-withdraw-trend','userWithdrawDistribution','用户提现(元)','Lines');
 
     /*用户续投情况*/
-    showReport('#formUserInvestViscosityReport','/bi/user-invest-viscosity','userInvestViscosity','投资人数(人)','bar');
+    showReport('#formUserInvestViscosityReport','/bi/user-invest-viscosity','userInvestViscosity','投资人数(人)','bar','投资次数');
 
     /*用户投资金额时间分布*/
     showReport('#formUserInvestAmountReport','/bi/user-invest-amount-trend','userInvestAmountDistribution','用户投资金额(元)','Lines');
@@ -122,6 +122,6 @@ require(['jquery','loadEcharts','bootstrapDatetimepicker'],function($,loadEchart
     showReport('#formInvestorUserAgeReport','/bi/investor-user-age-trend','investorUserAgeDistribution','投资用户(人)','pie');
 
     /*标的资金分布*/
-    showReport('#formLoanAmountReport','/bi/loan-amount-distribution','loanAmountDistribution','标的金额(元)','bar');
+    showReport('#formLoanAmountReport','/bi/loan-amount-distribution','loanAmountDistribution','标的金额(元)','bar','标的期数');
 
 });
