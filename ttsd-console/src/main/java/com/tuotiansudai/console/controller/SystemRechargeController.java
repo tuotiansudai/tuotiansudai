e02d7780624d887f87bee6066af3914ab56ad5ee
zhanglong888 <zhanglong@tuotiansudai.com>
2015-12-25 15:29:15 +0800

    add log

--- ttsd-console/src/main/java/com/tuotiansudai/console/controller/SystemRechargeController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/SystemRechargeController.java
@@ -6,6 +6,7 @@ import com.tuotiansudai.dto.BaseDto;
 import com.tuotiansudai.dto.PayFormDataDto;
 import com.tuotiansudai.dto.SystemRechargeDto;
 import com.tuotiansudai.service.SystemRechargeService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +19,7 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping(value = "/finance-manage")
 public class SystemRechargeController {
+    static Logger logger = Logger.getLogger(SystemRechargeController.class);
     @Autowired
     private SystemRechargeService systemRechargeService;
 
@@ -29,9 +31,14 @@ public class SystemRechargeController {
 
     @RequestMapping(value = "/system-recharge",method = RequestMethod.POST)
     public ModelAndView systemRecharge(@Valid @ModelAttribute SystemRechargeDto systemRechargeDto) {
+        logger.debug("=======================BEGIN1=========" + systemRechargeDto.getLoginName());
+        logger.debug("=======================BEGIN2=========" + systemRechargeDto.getAmount());
+        logger.debug("=======================BEGIN3=========" + systemRechargeDto.getOperatorLoginName());
         String operatorLoginName = LoginUserInfo.getLoginName();
         systemRechargeDto.setOperatorLoginName(operatorLoginName);
+        logger.debug("=======================BEGIN4=========");
         BaseDto<PayFormDataDto> baseDto = systemRechargeService.systemRecharge(systemRechargeDto);
+        logger.debug("=======================BEGIN5========="+ baseDto.getData().getUrl());
         return new ModelAndView("/pay", "pay", baseDto);
     }
 }
