62d200c7738f29a25bda6d6c933d2acbc6101636
zhanglong888 <zhanglong@tuotiansudai.com>
2015-12-30 20:47:15 +0800

    features/zl/coupon_2.0_create_new_coupon

--- ttsd-service/src/test/java/com/tuotiansudai/repository/mapper/UserCouponMapperTest.java
+++ ttsd-service/src/test/java/com/tuotiansudai/repository/mapper/UserCouponMapperTest.java
@@ -4,6 +4,8 @@ import com.tuotiansudai.coupon.repository.mapper.CouponMapper;
 import com.tuotiansudai.coupon.repository.mapper.UserCouponMapper;
 import com.tuotiansudai.coupon.repository.model.CouponModel;
 import com.tuotiansudai.coupon.repository.model.UserCouponModel;
+import com.tuotiansudai.repository.model.CouponType;
+import com.tuotiansudai.repository.model.ProductType;
 import com.tuotiansudai.repository.model.UserModel;
 import com.tuotiansudai.repository.model.UserStatus;
 import org.junit.Test;
@@ -72,6 +74,8 @@ public class UserCouponMapperTest {
         couponModel.setCreatedBy("couponTest");
         couponModel.setTotalCount(1000l);
         couponModel.setUsedCount(500l);
+        couponModel.setCouponType(CouponType.INVEST_COUPON.name());
+        couponModel.setProductType(ProductType.JYF.name());
 
         return couponModel;
     }
