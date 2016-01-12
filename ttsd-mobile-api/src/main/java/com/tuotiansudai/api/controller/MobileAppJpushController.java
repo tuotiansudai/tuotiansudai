92cb8cc7caa14cf223663139514b46e3b2f2dfb0
zhanglong888 <zhanglong@tuotiansudai.com>
2016-01-12 16:10:18 +0800

    features/zl/mobile_app_storage_jpush_in_redis

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppJpushController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppJpushController.java
@@ -4,6 +4,7 @@ import com.tuotiansudai.api.dto.BaseResponseDto;
 import com.tuotiansudai.api.dto.JpushRequestDto;
 import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.service.MobileAppJpushService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.text.MessageFormat;
 
 @RestController
 public class MobileAppJpushController extends MobileAppBaseController {
+    static Logger log = Logger.getLogger(MobileAppJpushController.class);
 
     @Autowired
     private MobileAppJpushService mobileAppJpushService;
@@ -24,8 +27,10 @@ public class MobileAppJpushController extends MobileAppBaseController {
         if (bindingResult.hasErrors()) {
             String errorCode = bindingResult.getFieldError().getDefaultMessage();
             String errorMessage = ReturnMessage.getErrorMsgByCode(errorCode);
+            log.debug(MessageFormat.format("{0}:{1}",errorCode,errorMessage));
             return new BaseResponseDto(errorCode, errorMessage);
         } else {
+            jpushRequestDto.getBaseParam().setUserId(getLoginName());
             return mobileAppJpushService.storeJpushId(jpushRequestDto);
         }
 
