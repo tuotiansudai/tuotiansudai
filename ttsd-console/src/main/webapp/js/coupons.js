00f402fa77d0e5287a94a36a548cd0f28c1eb72e
zourenzheng <zourenzheng@tuotiansudai.com>
2016-01-12 16:10:22 +0800

    inactive

--- ttsd-console/src/main/webapp/js/coupons.js
+++ ttsd-console/src/main/webapp/js/coupons.js
@@ -2,6 +2,7 @@ require(['jquery','bootstrap', 'bootstrapDatetimepicker','csrf'], function($) {
     $(function() {
         var $body=$('body'),
             $confirmBtn=$('.confirm-btn'),//conirm button
+            $inactiveBtn = $('.inactive-btn'),
             $tooltip = $('.add-tooltip'),
             $couponDelete = $('.coupon-delete'),
             $tipCom=$('.tip-container');
@@ -32,12 +33,41 @@ require(['jquery','bootstrap', 'bootstrapDatetimepicker','csrf'], function($) {
             }
         });
 
+        $inactiveBtn.on('click',function(e){
+            e.preventDefault();
+            var $self = $(this),
+                $parentTd = $self.parents('td'),
+                thisId = $self.attr('data-id');//data id
+            if (!confirm("是否确认执行此操作?")) {
+                return;
+            } else {
+                $.ajax({
+                    url:'/activity-manage/coupon/'+thisId+'/inactive',
+                    type:'POST',
+                    dataType:'json'
+                })
+                .done(function(res) {
+                    if (res.data.status) {
+                        $parentTd.html('<i class="check-btn"></i><a class="loan_repay confirm-btn" href="javascript:void(0)" data-id="thisId">确认生效</a>');
+                        $parentTd.prev().html('<a href="/activity-manage/coupon/'+thisId+'/edit" class="btn-link">编辑</a> / <button class="btn-link coupon-delete" data-link="/activity-manage/coupon/'+thisId+'" >删除</button>');
+                    } else {
+                        $tipCom.show().find('.txt').text('操作失败！');
+                    }
+                })
+                .fail(function(res) {
+                    $self.addClass('confirm-btn').text('操作失败');
+                    $tipCom.show().find('.txt').text('请求发送失败，请刷新重试！');
+                });
+            }
+        });
+
         //confirm event
         $confirmBtn.on('click',function(e) {
             e.preventDefault();
             var $self=$(this),
                 $parentTd = $self.parents('td'),
                 thisId=$self.attr('data-id');//data id
+            var couponType = $self.attr('data-type');
             if (!confirm("是否确认执行此操作?")) {
                 return;
             }else{
@@ -48,7 +78,10 @@ require(['jquery','bootstrap', 'bootstrapDatetimepicker','csrf'], function($) {
                 })
                 .done(function(res) {
                     if(res.data.status){
-                        $parentTd.html('<i class="check-btn add-check"></i><button class="loan_repay already-btn btn-link" disabled>已生效</button>');
+                        $parentTd.html('<i class="check-btn add-check"></i><button class="loan_repay already-btn btn-link" data-id="thisId">已生效</button>');
+                        if (couponType != 'NEWBIE_COUPON') {
+                            $parentTd.find('button').attr('disabled');
+                        }
                         $parentTd.prev().html('<a href="/activity-manage/coupon/'+thisId+'/detail" class="btn-link">查看详情</a>');
                     }else{
                         $tipCom.show().find('.txt').text('操作失败！');
--- ttsd-service/src/main/java/com/tuotiansudai/coupon/service/CouponActivationService.java
+++ ttsd-service/src/main/java/com/tuotiansudai/coupon/service/CouponActivationService.java
@@ -4,5 +4,7 @@ public interface CouponActivationService {
 
     void active(String loginNameLoginName, long couponId);
 
+    void inactive(String loginNameLoginName, long couponId);
+
     void sendSms(long couponId);
 }
