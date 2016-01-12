0f3a835f5e00da988d433cf5c4bb12ddbd502ddb
zourenzheng <zourenzheng@tuotiansudai.com>
2016-01-12 18:46:30 +0800

    ios update

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppCheckVersionController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppCheckVersionController.java
@@ -8,6 +8,7 @@ import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.util.GsonUtil;
 import com.tuotiansudai.api.util.HttpClientUtil;
 import com.tuotiansudai.client.RedisWrapperClient;
+import com.tuotiansudai.repository.model.Source;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MobileAppCheckVersionController extends MobileAppBaseController {
 
-    private static final String APP_VERSION_CHECK_URL = "https://tuotiansudai.com/app/version.json";
-    private static final String APP_VERSION_INFO_REDIS_KEY = "app:version:info";
+    private static final String APP_ANDROID_VERSION_CHECK_URL = "https://tuotiansudai.com/app/android_version.json";
+    private static final String APP_IOS_VERSION_CHECK_URL = "https://tuotiansudai.com/app/ios_version.json";
+    private static final String APP_ANDROID_VERSION_INFO_REDIS_KEY = "app:android_version:info";
+    private static final String APP_IOS_VERSION_INFO_REDIS_KEY = "app:ios_version:info";
     private static final int APP_VERSION_INFO_EXPIRE_SECONDS = 60 * 60;
     static Logger log = Logger.getLogger(MobileAppCheckVersionController.class);
 
@@ -32,7 +35,7 @@ public class MobileAppCheckVersionController extends MobileAppBaseController {
     @ResponseBody
     public BaseResponseDto getAndroidVersion(@RequestBody BaseParamDto dto) {
         BaseResponseDto<AppVersionResponseDataDto> baseResponseDto = new BaseResponseDto<>();
-        AppVersionResponseDataDto dataDto = getLastestVersionInfo();
+        AppVersionResponseDataDto dataDto = getLastestVersionInfo(dto.getBaseParam().getPlatform());
         if (dataDto != null) {
             baseResponseDto.setData(dataDto);
             baseResponseDto.setCode(ReturnMessage.SUCCESS.getCode());
@@ -45,20 +48,31 @@ public class MobileAppCheckVersionController extends MobileAppBaseController {
         return baseResponseDto;
     }
 
-    private AppVersionResponseDataDto getLastestVersionInfo() {
+    private AppVersionResponseDataDto getLastestVersionInfo(String platform) {
         try {
-            String jsonString = redisWrapperClient.get(APP_VERSION_INFO_REDIS_KEY);
+            String redisKey = "";
+            String url = "";
+            if (platform.equalsIgnoreCase(Source.ANDROID.name())) {
+                redisKey = APP_ANDROID_VERSION_INFO_REDIS_KEY;
+                url = APP_ANDROID_VERSION_CHECK_URL;
+            } else if (platform.equalsIgnoreCase(Source.IOS.name())) {
+                redisKey = APP_IOS_VERSION_INFO_REDIS_KEY;
+                url = APP_IOS_VERSION_CHECK_URL;
+            }
+            String jsonString = redisWrapperClient.get(redisKey);
             if (StringUtils.isBlank(jsonString)) {
-                jsonString = HttpClientUtil.getResponseBodyAsString(APP_VERSION_CHECK_URL, "UTF-8");
-                redisWrapperClient.setex(APP_VERSION_INFO_REDIS_KEY, APP_VERSION_INFO_EXPIRE_SECONDS, jsonString);
+                jsonString = HttpClientUtil.getResponseBodyAsString(url, "UTF-8");
+                redisWrapperClient.setex(redisKey, APP_VERSION_INFO_EXPIRE_SECONDS, jsonString);
             }
             JsonObject json = GsonUtil.stringToJsonObject(jsonString);
             AppVersionResponseDataDto dto = new AppVersionResponseDataDto();
             dto.setForceUpgrade(json.get("forceUpgrade").getAsBoolean());
             dto.setMessage(json.get("message").getAsString());
-            dto.setUrl(json.get("url").getAsString());
             dto.setVersion(json.get("version").getAsString());
-            dto.setVersionCode(json.get("versionCode").getAsInt());
+            if (platform.equalsIgnoreCase(Source.ANDROID.name())){
+                dto.setVersionCode(json.get("versionCode").getAsInt());
+                dto.setUrl(json.get("url").getAsString());
+            }
             return dto;
         } catch (Exception e) {
             log.error(e);
--- ttsd-mobile-api/src/test/java/com/tuotiansudai/api/controller/MobileAppCheckVersionControllerTest.java
+++ ttsd-mobile-api/src/test/java/com/tuotiansudai/api/controller/MobileAppCheckVersionControllerTest.java
@@ -1,24 +1,16 @@
 package com.tuotiansudai.api.controller;
 
 import com.tuotiansudai.api.dto.BaseParamDto;
-import com.tuotiansudai.api.dto.RegisterRequestDto;
-import com.tuotiansudai.api.dto.SendSmsRequestDto;
-import com.tuotiansudai.api.service.MobileAppRegisterService;
 import com.tuotiansudai.client.RedisWrapperClient;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class MobileAppCheckVersionControllerTest extends ControllerTestBase {
     @InjectMocks
@@ -36,7 +28,7 @@ public class MobileAppCheckVersionControllerTest extends ControllerTestBase {
     public void shouldGetAndroidVersionFromRedis() throws Exception {
 
         String requestJson = generateRequestJson(new BaseParamDto());
-        when(redisWrapperClient.get(eq("app:version:info"))).thenReturn("{" +
+        when(redisWrapperClient.get(eq("app:android_version:info"))).thenReturn("{" +
                 "  \"version\": \"1.0\"," +
                 "  \"versionCode\": 1," +
                 "  \"forceUpgrade\": false," +
@@ -58,7 +50,7 @@ public class MobileAppCheckVersionControllerTest extends ControllerTestBase {
     public void shouldGetAndroidVersionFromOSS() throws Exception {
 
         String requestJson = generateRequestJson(new BaseParamDto());
-        when(redisWrapperClient.get("app:version:info")).thenReturn(null);
+        when(redisWrapperClient.get("app:android_version:info")).thenReturn(null);
 
         mockMvc.perform(post("/v1.0/get/version").
                 contentType(MediaType.APPLICATION_JSON_VALUE)
