c2ce83a7ca633956b6cc1eb1d920b690b35813ec
zourenzheng <zourenzheng@tuotiansudai.com>
2016-01-12 16:31:35 +0800

    bug

--- ttsd-console/src/main/webapp/js/coupons.js
+++ ttsd-console/src/main/webapp/js/coupons.js
@@ -48,7 +48,7 @@ require(['jquery','bootstrap', 'bootstrapDatetimepicker','csrf'], function($) {
                 })
                 .done(function(res) {
                     if (res.data.status) {
-                        $parentTd.html('<i class="check-btn"></i><a class="loan_repay confirm-btn" href="javascript:void(0)" data-id="thisId">确认生效</a>');
+                        $parentTd.html('<i class="check-btn"></i><a class="loan_repay confirm-btn" href="javascript:void(0)" data-id="'+thisId+'">确认生效</a>');
                         $parentTd.prev().html('<a href="/activity-manage/coupon/'+thisId+'/edit" class="btn-link">编辑</a> / <button class="btn-link coupon-delete" data-link="/activity-manage/coupon/'+thisId+'" >删除</button>');
                     } else {
                         $tipCom.show().find('.txt').text('操作失败！');
@@ -78,7 +78,7 @@ require(['jquery','bootstrap', 'bootstrapDatetimepicker','csrf'], function($) {
                 })
                 .done(function(res) {
                     if(res.data.status){
-                        $parentTd.html('<i class="check-btn add-check"></i><button class="loan_repay already-btn btn-link" data-id="thisId">已生效</button>');
+                        $parentTd.html('<i class="check-btn add-check"></i><button class="loan_repay already-btn btn-link" data-id="'+thisId+'">已生效</button>');
                         if (couponType != 'NEWBIE_COUPON') {
                             $parentTd.find('button').attr('disabled');
                         }
