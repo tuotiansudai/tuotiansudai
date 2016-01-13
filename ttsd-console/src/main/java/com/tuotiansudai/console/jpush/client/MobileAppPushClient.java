c56e0174da9aab6db675f50a4246334fbeafd834
zhanglong888 <zhanglong@tuotiansudai.com>
2016-01-13 16:20:32 +0800

    features/zl/mobile_app_alert_push

---
+++ ttsd-console/src/main/java/com/tuotiansudai/console/jpush/client/MobileAppPushClient.java
@@ -0,0 +1,53 @@
+package com.tuotiansudai.console.jpush.client;
+
+import cn.jpush.api.JPushClient;
+import cn.jpush.api.push.model.Platform;
+import cn.jpush.api.push.model.PushPayload;
+import cn.jpush.api.push.model.audience.Audience;
+import cn.jpush.api.push.model.notification.Notification;
+import com.google.common.base.Joiner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MobileAppPushClient {
+    @Value("${common.jpush.masterSecret}")
+    private String masterSecret;
+
+    @Value("${common.jpush.appKey}")
+    private String appKey;
+
+    private static JPushClient jPushClient;
+
+    public JPushClient getjPushClient(){
+        if(jPushClient == null){
+            jPushClient = new JPushClient(masterSecret,appKey,3);
+        }
+        return jPushClient;
+    }
+
+    public static PushPayload buildPushObject_all_all_alert(String alert) {
+        return PushPayload.alertAll(alert);
+    }
+
+    public static PushPayload buildPushObject_all_aliases_alert(List<String> alias,String alert) {
+
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.alias(alias))
+                .setNotification(Notification.alert(alert))
+                .build();
+    }
+
+    public static PushPayload buildPushObject_all_alias_alert(String alias,String alert) {
+
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.alias(alias))
+                .setNotification(Notification.alert(alert))
+                .build();
+    }
+
+}
---
+++ ttsd-console/src/test/java/com/tuotiansudai/console/controller/jpush/client/MobileAppPushClientTest.java
@@ -0,0 +1,38 @@
+package com.tuotiansudai.console.controller.jpush.client;
+
+import cn.jpush.api.JPushClient;
+import cn.jpush.api.common.APIConnectionException;
+import cn.jpush.api.common.APIRequestException;
+import cn.jpush.api.push.PushResult;
+import cn.jpush.api.push.model.PushPayload;
+
+public class MobileAppPushClientTest {
+
+    public static void main(String[] args){
+        String masterSecret = "7e85cd2928f860b70e8b3907";
+        String appKey = "78d62eae2bba85e7c08ef7f5";
+        JPushClient jPushClient = new JPushClient(masterSecret,appKey,3);
+
+        // For push, all you need do is to build PushPayload object.
+        PushPayload payload = buildPushObject_all_all_alert();
+
+        try {
+            PushResult result = jPushClient.sendPush(payload);
+
+            System.out.println("========" + result.msg_id);
+            System.out.println("===========" + result.sendno);
+
+        } catch (APIConnectionException e) {
+            // Connection error, should retry later
+
+        } catch (APIRequestException e) {
+            // Should review the error, and fix the request
+
+        }
+
+    }
+
+    public static PushPayload buildPushObject_all_all_alert() {
+        return PushPayload.alertAll("mobile app test");
+    }
+}
