876d8a17dfaf60a43ff8f43e09c163e53b65e84e
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-01-13 19:09:19 +0800

    redirect to 404 page if impersonate login fail

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/ImpersonateController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/ImpersonateController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Component
 @RequestMapping(value = "/impersonate")
@@ -16,11 +17,14 @@ public class ImpersonateController {
     private ImpersonateService impersonateService;
 
     @RequestMapping(params = "securityCode")
-    public ModelAndView impersonate(String securityCode, HttpServletRequest request) {
+    public ModelAndView impersonate(String securityCode, HttpServletRequest request, HttpServletResponse response) {
 
         if (impersonateService.impersonateLogin(securityCode)) {
             request.getSession().setAttribute("impersonate", "1");
+            return new ModelAndView("redirect:/");
+        } else {
+            response.setStatus(404);
+            return new ModelAndView("/error/404");
         }
-        return new ModelAndView("redirect:/");
     }
 }
