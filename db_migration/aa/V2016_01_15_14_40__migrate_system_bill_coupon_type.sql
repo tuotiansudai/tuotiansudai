a7e480eaf1c4341ba969c7a29a07d787f40a851c
Xiduan Gao <gaoxiduan@gmail.com>
2016-01-15 15:43:06 +0800

    fixed migrate sql

--- db_migration/aa/V2016_01_15_14_40__migrate_system_bill_coupon_type.sql
+++ db_migration/aa/V2016_01_15_14_40__migrate_system_bill_coupon_type.sql
@@ -4,9 +4,10 @@ SET business_type = 'COUPON'
 WHERE business_type = 'NEWBIE_COUPON';
 
 UPDATE `aa`.user_bill
-SET `business_type` = (SELECT `coupon_type`
-                       FROM `coupon`
-                       WHERE id = `user_bill`.`order_id`)
+SET `business_type` = (SELECT `coupon`.`coupon_type`
+                       FROM `user_coupon`
+                         JOIN `coupon` ON `user_coupon`.`coupon_id` = `coupon`.`id`
+                       WHERE `user_coupon`.`id` = `user_bill`.`order_id`)
 WHERE `business_type` = 'NEWBIE_COUPON';
 
 COMMIT;
