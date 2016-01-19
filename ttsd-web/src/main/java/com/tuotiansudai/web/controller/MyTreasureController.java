a10b2e06290591e3fec852a2437a6edf7c709dbb
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-01-19 16:42:50 +0800

    refactor getUserCoupons method

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/MyTreasureController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/MyTreasureController.java
@@ -1,7 +1,9 @@
 package com.tuotiansudai.web.controller;
 
+import com.google.common.collect.Lists;
 import com.tuotiansudai.coupon.dto.UserCouponDto;
 import com.tuotiansudai.coupon.service.UserCouponService;
+import com.tuotiansudai.repository.model.CouponType;
 import com.tuotiansudai.web.util.LoginUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,8 +24,9 @@ public class MyTreasureController {
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView getUserCoupon() {
         String loginName = LoginUserInfo.getLoginName();
-        List<UserCouponDto> moneyCoupons = userCouponService.getUserMoneyCoupons(loginName);
-        List<UserCouponDto> interestCoupons = userCouponService.getUserInterestCoupons(loginName);
+
+        List<UserCouponDto> moneyCoupons = userCouponService.getUserCoupons(loginName, Lists.newArrayList(CouponType.NEWBIE_COUPON, CouponType.INVEST_COUPON));
+        List<UserCouponDto> interestCoupons = userCouponService.getUserCoupons(loginName, Lists.newArrayList(CouponType.INTEREST_COUPON));
 
         ModelAndView modelAndView = new ModelAndView("/my-treasure");
         modelAndView.addObject("moneyCoupons", moneyCoupons);
