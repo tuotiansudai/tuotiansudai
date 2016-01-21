28648410d6fd8352c7afdb23c4c7fc2c660b665a
zhanglong888 <zhanglong@tuotiansudai.com>
2016-01-21 12:11:48 +0800

    features/zl/mobile_app_invest_user_coupon

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppInvestServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppInvestServiceImpl.java
@@ -68,7 +68,7 @@ public class MobileAppInvestServiceImpl implements MobileAppInvestService {
         investDto.setLoanId(investRequestDto.getLoanId());
         investDto.setLoginName(investRequestDto.getUserId());
         investDto.setChannel(mobileAppChannelService.obtainChannelBySource(investRequestDto.getBaseParam()));
-        investDto.setUserCouponId(investRequestDto.getUserCouponId());
+        investDto.setUserCouponIds(investRequestDto.getUserCouponIds());
         return investDto;
     }
 
