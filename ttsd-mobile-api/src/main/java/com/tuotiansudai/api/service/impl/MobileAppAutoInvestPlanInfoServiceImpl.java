dcb8c033ea6075cb0c36b0a1c1122a2f88797410
zhanglong888 <zhanglong@tuotiansudai.com>
2016-01-25 19:01:08 +0800

    update auto plan

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppAutoInvestPlanInfoServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppAutoInvestPlanInfoServiceImpl.java
@@ -1,14 +1,20 @@
 package com.tuotiansudai.api.service.impl;
 
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.tuotiansudai.api.dto.*;
 import com.tuotiansudai.api.service.MobileAppAutoInvestPlanInfoService;
 import com.tuotiansudai.repository.mapper.AccountMapper;
 import com.tuotiansudai.repository.mapper.AutoInvestPlanMapper;
 import com.tuotiansudai.repository.model.AccountModel;
 import com.tuotiansudai.repository.model.AutoInvestPlanModel;
+import com.tuotiansudai.util.AutoInvestMonthPeriod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class MobileAppAutoInvestPlanInfoServiceImpl implements MobileAppAutoInvestPlanInfoService {
@@ -32,6 +38,23 @@ public class MobileAppAutoInvestPlanInfoServiceImpl implements MobileAppAutoInve
         AutoInvestPlanModel autoInvestPlanModel = autoInvestPlanMapper.findByLoginName(loginName);
         if(autoInvestPlanModel != null){
             autoInvestPlanInfoResponseDataDto.setAutoInvestPlan(new AutoInvestPlanDataDto(autoInvestPlanModel));
+        }else{
+            AutoInvestPlanDataDto autoInvestPlanDataDto = new AutoInvestPlanDataDto();
+            autoInvestPlanDataDto.setEnabled(true);
+            List<AutoInvestMonthPeriod> allPeriods = Arrays.asList(AutoInvestMonthPeriod.AllPeriods);
+            List<AutoInvestPeriodDto> autoInvestPeriodsDtos = Lists.transform(allPeriods, new Function<AutoInvestMonthPeriod, AutoInvestPeriodDto>() {
+
+                @Override
+                public AutoInvestPeriodDto apply(AutoInvestMonthPeriod input) {
+                    AutoInvestPeriodDto autoInvestPeriodDto = new AutoInvestPeriodDto();
+                    autoInvestPeriodDto.setPid("" + input.getPeriodValue());
+                    autoInvestPeriodDto.setpName("" + input.getPeriodName());
+                    autoInvestPeriodDto.setSelected(false);
+                    return autoInvestPeriodDto;
+                }
+            });
+            autoInvestPlanDataDto.setAutoInvestPeriods(autoInvestPeriodsDtos);
+            autoInvestPlanInfoResponseDataDto.setAutoInvestPlan(autoInvestPlanDataDto);
         }
 
         baseDto.setCode(ReturnMessage.SUCCESS.getCode());
