4e12487c982085b2d9f89415ae4956a5e96faeb8
zourenzheng <zourenzheng@tuotiansudai.com>
2016-02-14 16:35:39 +0800

    format code

--- ttsd-console/src/main/java/com/tuotiansudai/task/aspect/ApplicationAspect.java
+++ ttsd-console/src/main/java/com/tuotiansudai/task/aspect/ApplicationAspect.java
@@ -1,18 +1,24 @@
 package com.tuotiansudai.task.aspect;
 
+import com.tuotiansudai.client.RedisWrapperClient;
+import com.tuotiansudai.dto.EditUserDto;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Aspect
 @Service
 public class ApplicationAspect {
 
+    @Autowired
+    private RedisWrapperClient redisWrapperClient;
+
     @Around(value = "execution(* com.tuotiansudai.service.UserService.editUser(..))")
     public Object aroundEditUser(ProceedingJoinPoint proceedingJoinPoint, JoinPoint joinPoint) throws Throwable {
-        
+        EditUserDto editUserDto = (EditUserDto)joinPoint.getArgs()[1];
         return proceedingJoinPoint.proceed();
     }
 
