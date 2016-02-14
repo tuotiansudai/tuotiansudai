edd13abe4850c5bd1148f1823b145d11b1d7e6cd
zourenzheng <zourenzheng@tuotiansudai.com>
2016-02-14 16:13:29 +0800

    aop

--- ttsd-console/src/main/java/com/tuotiansudai/task/aspect/ApplicationAspect.java
+++ ttsd-console/src/main/java/com/tuotiansudai/task/aspect/ApplicationAspect.java
@@ -1,8 +1,19 @@
 package com.tuotiansudai.task.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Service;
 
+@Aspect
 @Service
 public class ApplicationAspect {
+
+    @Around(value = "execution(* com.tuotiansudai.service.UserService.editUser(..))")
+    public Object aroundEditUser(ProceedingJoinPoint proceedingJoinPoint, JoinPoint joinPoint) throws Throwable {
+        
+        return proceedingJoinPoint.proceed();
+    }
 
 }
