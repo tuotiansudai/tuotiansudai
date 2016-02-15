8c4bb60c276e89a8eff0f908d1e5411452b38f67
xvqiang <xuqiang@tuotiansudai.com>
2016-02-15 11:32:50 +0800

    finish back index

---
+++ ttsd-console/src/main/webapp/js/index.js
@@ -0,0 +1,20 @@
+require(['jquery'], function ($) {
+    $(function() {
+    	$('.table btn-danger').on('click',function(event) {
+    		event.preventDefault();
+    		$.ajax({
+    			url: '/path/to/file',
+    			type: 'POST',
+    			dataType: 'json',
+    			data: {param1: 'value1'},
+    		})
+    		.done(function() {
+    			console.log("success");
+    		})
+    		.fail(function() {
+    			console.log("error");
+    		});
+    		
+    	});
+    });
+});
