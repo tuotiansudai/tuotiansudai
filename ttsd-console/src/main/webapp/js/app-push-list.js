7d411cb962d83e5fff1c48c21a22b84a352075e3
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-02-23 17:38:20 +0800

    remove unused js code

--- ttsd-console/src/main/webapp/js/app-push-list.js
+++ ttsd-console/src/main/webapp/js/app-push-list.js
@@ -15,28 +15,13 @@ require(['jquery', 'bootstrap', 'bootstrapDatetimepicker', 'csrf'], function ($)
                     location.href='/app-push-manage/manual-app-push-list';
                 }else{
                     var msg = res.data.message || '服务端校验失败';
-                    showErrorMessage(msg);
+                    alert(msg);
                 }
             }).fail(function () {
                 console.log("error");
             }).always(function () {
                 console.log("complete");
             });
-        });
-
-        //显示警告提示
-        var currentErrorObj = null;
-        function showErrorMessage(msg, obj){
-            currentErrorObj = obj;
-            //var htm = '<div class="alert alert-danger alert-dismissible" data-dismiss="alert" aria-label="Close" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> <span class="txt">' + msg + '</span></div>';
-            //$('.form-error').append(htm);
-            alert(msg);
-        }
-
-        //关闭警告提示
-        $('body').on('click','.form-error',function(){
-            //$('.jq-btn-form').removeAttr('disabled');
-            if(!!currentErrorObj){currentErrorObj.focus();}
         });
     });
 
