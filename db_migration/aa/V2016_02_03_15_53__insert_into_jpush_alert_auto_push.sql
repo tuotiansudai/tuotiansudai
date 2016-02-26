5fe867b16cd379c0d8cee3386a2f407256a1a89e
zhanglong888 <zhanglong@tuotiansudai.com>
2016-02-26 16:47:33 +0800

    delete sql admin

--- db_migration/aa/V2016_02_03_15_53__insert_into_jpush_alert_auto_push.sql
+++ db_migration/aa/V2016_02_03_15_53__insert_into_jpush_alert_auto_push.sql
@@ -1,3 +1,3 @@
 BEGIN;
-INSERT INTO `jpush_alert` (`name`,`push_type`,`push_objects`,`push_source`,`status`,`content`,`jump_to`,`jump_to_link`,`created_time`,`created_by`,`updated_time`,`updated_by`,`is_automatic`) VALUES ('生日月加息活动提醒','BIRTHDAY_ALERT_MONTH',null,'ALL','DISABLED','生日月快乐,本月投资收益翻倍,最高可达26%,错过还要等一年哟！',null,NULL ,now(),'admin',NULL,NULL,1);
+INSERT INTO `jpush_alert` (`name`,`push_type`,`push_objects`,`push_source`,`status`,`content`,`jump_to`,`jump_to_link`,`created_time`,`created_by`,`updated_time`,`updated_by`,`is_automatic`) VALUES ('生日月加息活动提醒','BIRTHDAY_ALERT_MONTH',null,'ALL','DISABLED','生日月快乐,本月投资收益翻倍,最高可达26%,错过还要等一年哟！',null,NULL ,now(),NULL,NULL,NULL,1);
 COMMIT ;
