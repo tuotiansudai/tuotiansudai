2d1fca53953a6d0c2c4b9b4781ff3d9e1a95ccfb
zhanglong888 <zhanglong@tuotiansudai.com>
2016-02-29 19:59:29 +0800

    features/zl/sign_in_obtain_point

---
+++ ttsd-point/src/main/java/com/tuotiansudai/point/service/impl/SignInServiceImpl.java
@@ -0,0 +1,93 @@
+package com.tuotiansudai.point.service.impl;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.tuotiansudai.client.RedisWrapperClient;
+import com.tuotiansudai.coupon.repository.mapper.CouponMapper;
+import com.tuotiansudai.point.dto.SignInPoint;
+import com.tuotiansudai.point.dto.SignInPointDto;
+import com.tuotiansudai.point.repository.model.PointBusinessType;
+import com.tuotiansudai.point.service.PointBillService;
+import com.tuotiansudai.point.service.SignInService;
+import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+@Service
+public class SignInServiceImpl implements SignInService {
+    static Logger logger = Logger.getLogger(SignInServiceImpl.class);
+
+    @Autowired
+    private RedisWrapperClient redisWrapperClient;
+
+    @Autowired
+    private PointBillService pointBillService;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String POINT_SIGN_IN_KEY = "web:point:signin";
+
+    @Override
+    @Transactional
+    public SignInPointDto signIn(String loginName) {
+        DateTime today = new DateTime().withTimeAtStartOfDay();
+
+        try {
+            SignInPointDto signInPointDto = new SignInPointDto(SignInPoint.FIRST_SIGN_IN.getTimes(), today.toDate(), SignInPoint.FIRST_SIGN_IN.getPoint());;
+            String redisValue = redisWrapperClient.hget(POINT_SIGN_IN_KEY, loginName);
+            SignInPointDto lastSignInPointDto = Strings.isNullOrEmpty(redisValue) ? null : (SignInPointDto) objectMapper.readValue(redisValue, new TypeReference<SignInPointDto>() {
+            });
+
+            if (lastSignInPointDto != null) {
+                if (Days.daysBetween(new DateTime(lastSignInPointDto.getSignInDate()), new DateTime().withTimeAtStartOfDay()) == Days.ZERO) {
+                    return lastSignInPointDto;
+                }
+                if (Days.daysBetween(new DateTime(lastSignInPointDto.getSignInDate()), today) == Days.ONE) {
+                    int signInCount = lastSignInPointDto.getSignInCount() + 1;
+                    signInPointDto = new SignInPointDto(signInCount, today.toDate(), SignInPoint.getPointByTimes(signInCount));
+                }
+            }
+            redisWrapperClient.hset(POINT_SIGN_IN_KEY, loginName, objectMapper.writeValueAsString(signInPointDto));
+            pointBillService.createPointBill(loginName, null, PointBusinessType.SIGN_IN, signInPointDto.getPoint());
+            logger.debug(MessageFormat.format("{0} sign in success {1} 次", loginName, signInPointDto.getSignInCount()));
+            return signInPointDto;
+        } catch (IOException e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+        return null;
+
+    }
+
+    @Override
+    public boolean signInIsSuccess(String loginName) {
+        try {
+            if (!redisWrapperClient.hexists(POINT_SIGN_IN_KEY, loginName)) {
+                return false;
+            } else {
+                String redisValue = redisWrapperClient.hget(POINT_SIGN_IN_KEY, loginName);
+                SignInPointDto signInPointDto = objectMapper.readValue(redisValue, new TypeReference<SignInPointDto>() {
+                });
+
+                return Days.daysBetween(new DateTime(signInPointDto.getSignInDate()), new DateTime().withTimeAtStartOfDay()) == Days.ZERO;
+            }
+        } catch (IOException e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+
+        return true;
+
+    }
+}
--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/PointController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/PointController.java
@@ -4,6 +4,7 @@ import com.tuotiansudai.dto.BaseDataDto;
 import com.tuotiansudai.dto.BaseDto;
 import com.tuotiansudai.point.dto.SignInPointDto;
 import com.tuotiansudai.point.service.PointService;
+import com.tuotiansudai.point.service.SignInService;
 import com.tuotiansudai.web.util.LoginUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,23 +16,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path = "/point")
 public class PointController {
     @Autowired
-    private PointService pointService;
+    private SignInService signInService;
 
     @RequestMapping(path = "/sign-in", method = RequestMethod.POST)
     @ResponseBody
     public BaseDto<BaseDataDto> signIn() {
-        BaseDto<BaseDataDto> baseDto = new BaseDto<>();
-        BaseDataDto baseDataDto = new SignInPointDto();
-        baseDto.setData(baseDataDto);
         String loginName = LoginUserInfo.getLoginName();
-        boolean signInIsSuccess = pointService.signInIsSuccess(loginName);
-        if (signInIsSuccess){
-            baseDataDto.setStatus(false);
-            baseDataDto.setMessage("今天已经成功签到！");
-            baseDto.setSuccess(true);
-            return baseDto;
-        }
-        baseDataDto = pointService.signIn(loginName);
+        BaseDto<BaseDataDto> baseDto = new BaseDto<>();
+        BaseDataDto baseDataDto = signInService.signIn(loginName);
         baseDataDto.setStatus(true);
         baseDto.setData(baseDataDto);
         baseDto.setSuccess(true);
