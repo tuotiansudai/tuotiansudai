90c761ea5c04567606cd4e1df629ef2178aeb0ec
Xiduan Gao <gaoxiduan@gmail.com>
2016-02-29 09:42:11 +0800

    migrate user coupon start end time

--- db_migration/aa/V2016_02_26_16_50__add_start_end_time_for_user_coupon.sql
+++ db_migration/aa/V2016_02_26_16_50__add_start_end_time_for_user_coupon.sql
@@ -3,3 +3,11 @@ AFTER `coupon_id`;
 
 ALTER TABLE `aa`.`user_coupon` ADD COLUMN `end_time` DATETIME NOT NULL
 AFTER `start_time`;
+
+UPDATE user_coupon
+SET start_time = (SELECT coupon.start_time
+                  FROM coupon
+                  WHERE user_coupon.coupon_id = coupon.id),
+  end_time     = (SELECT coupon.end_time
+                  FROM coupon
+                  WHERE user_coupon.coupon_id = coupon.id)
