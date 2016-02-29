517e517c12ebbd9a10888843590c6c194b2080ff
gengbj <gengbj@tuotiansudai.com>
2016-02-29 14:32:18 -0800

    add query point page

--- ttsd-point/src/main/java/com/tuotiansudai/point/service/PointService.java
+++ ttsd-point/src/main/java/com/tuotiansudai/point/service/PointService.java
@@ -1,8 +1,5 @@
 package com.tuotiansudai.point.service;
 
 
-import com.tuotiansudai.coupon.dto.ExchangeCouponDto;
-
 public interface PointService {
-    void createCouponAndExchange(String loginName, ExchangeCouponDto exchangeCouponDto);
 }
