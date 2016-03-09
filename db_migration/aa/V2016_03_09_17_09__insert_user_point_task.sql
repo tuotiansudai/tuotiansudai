0f25e5a54a244dc9cced3ebf571c4ec7c279c003
zourenzheng <zourenzheng@tuotiansudai.com>
2016-03-09 17:16:14 +0800

    add insert user_point_task

---
+++ db_migration/aa/V2016_03_09_17_09__insert_user_point_task.sql
@@ -0,0 +1,64 @@
+BEGIN ;
+
+INSERT INTO user_point_task
+SELECT
+  t.`login_name`,
+  1,
+  t.`register_time`
+FROM
+  account t ;
+
+INSERT INTO user_point_task
+  SELECT
+    u.`login_name`,
+    2,
+    IFNULL(
+        u.`last_modified_time`,
+        u.`register_time`
+    )
+  FROM
+    `user` u
+  WHERE u.`email` IS NOT NULL
+        AND u.`email` != '' ;
+
+INSERT INTO user_point_task
+  SELECT
+    b.`login_name`,
+    3,
+    b.`created_time`
+  FROM
+    bank_card b
+  WHERE b.`status` = 'PASSED' ;
+
+INSERT INTO user_point_task
+  SELECT
+    r.`login_name`,
+    4,
+    MIN(r.`created_time`)
+  FROM
+    recharge r
+  WHERE r.`status` = 'SUCCESS'
+  GROUP BY r.`login_name` ;
+
+INSERT INTO user_point_task
+  SELECT
+    i.`login_name`,
+    5,
+    MIN(i.`created_time`)
+  FROM
+    invest i
+  WHERE i.`status` = 'SUCCESS'
+  GROUP BY i.`login_name` ;
+
+INSERT INTO user_point_task
+  SELECT
+    it.`login_name`,
+    6,
+    MAX(it.`created_time`)
+  FROM
+    invest it
+  WHERE it.`status` = 'SUCCESS'
+  GROUP BY it.`login_name`
+  HAVING SUM(it.`amount`) >= 1000000 ;
+
+COMMIT ;
