2a6a65d65335be8747fd1e5a5be3cafca047315a
gengbj <gengbj@tuotiansudai.com>
2016-03-09 16:17:51 -0800

    add column to invest and account

---
+++ db_migration/aa/V2016_03_09_11_10_add_column_to_invest_and_account.sql
@@ -0,0 +1,7 @@
+ALTER TABLE invest ADD COLUMN is_no_password_invest BOOLEAN AFTER channel;
+
+UPDATE invest SET is_no_password_invest = false WHERE status = 'SUCCESS';
+
+ALTER TABLE account ADD COLUMN no_password_invest BOOLEAN AFTER auto_invest;
+
+UPDATE account SET no_password_invest = false WHERE auto_invest = 1;
