7bdc9df77bf6c9a47d82dedec8591188b452773d
gengbj <gengbj@tuotiansudai.com>
2016-03-17 11:33:14 -0700

    api no_password_invest turn off

---
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/MobileAppNoPasswordInvestTurnOffService.java
@@ -0,0 +1,8 @@
+package com.tuotiansudai.api.service;
+
+import com.tuotiansudai.api.dto.BaseResponseDto;
+import com.tuotiansudai.api.dto.NoPasswordInvestTurnOffRequestDto;
+
+public interface MobileAppNoPasswordInvestTurnOffService {
+    BaseResponseDto noPasswordInvestTurnOff(NoPasswordInvestTurnOffRequestDto noPasswordInvestTurnOffRequestDto);
+}
