5adf04d55bb72b0f1b336aff7cd89f6323a60518
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-03-18 14:47:53 +0800

    add migrate sql for history data in audit_log table

--- db_migration/aa/V2016_03_08_18_34__alter_audit_log_table_add_columns.sql
+++ db_migration/aa/V2016_03_08_18_34__alter_audit_log_table_add_columns.sql
@@ -10,3 +10,8 @@ ALTER TABLE audit_log ADD COLUMN operation_type VARCHAR(20) NOT NULL AFTER targe
 
 ALTER TABLE audit_log ADD CONSTRAINT FK_AUDIT_LOG_AUDITOR_LOGIN_NAME_REF_USER_LOGIN_NAME FOREIGN KEY (`auditor_login_name`) REFERENCES `aa`.`user` (`login_name`);
 ALTER TABLE audit_log ADD CONSTRAINT FK_AUDIT_LOG_OPERATOR_LOGIN_NAME_REF_USER_LOGIN_NAME FOREIGN KEY (`operator_login_name`) REFERENCES `aa`.`user` (`login_name`);
+
+BEGIN;
+UPDATE audit_log SET auditor_login_name = operator_login_name, operator_login_name = target_id, target_id = null where operation_time > '2016-03-04 11:00:00';
+UPDATE audit_log SET operation_type = 'USER';
+COMMIT;
