bdbc6e57ed714230c65ac75b3f6161ac630d7963
gengbj <gengbj@tuotiansudai.com>
2016-03-21 13:45:18 -0700

    uodate script V2016_03_11_14_56__add_column_to_invest_and_account.sql

--- db_migration/aa/V2016_03_11_14_56__add_column_to_invest_and_account.sql
+++ db_migration/aa/V2016_03_11_14_56__add_column_to_invest_and_account.sql
@@ -1,5 +1,2 @@
-ALTER TABLE invest ADD COLUMN is_no_password_invest BOOLEAN default FALSE AFTER channel;
-
-ALTER TABLE account ADD COLUMN no_password_invest BOOLEAN AFTER auto_invest;
 
 UPDATE account SET no_password_invest = false WHERE auto_invest = 1;
