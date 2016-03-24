5b019b77085329b090ecc310342e7677fcefa35f
zourenzheng <zourenzheng@tuotiansudai.com>
2016-03-24 16:52:51 +0800

    format

--- db_migration/aa/V2016_03_22_17_23__create_insert_point_prize_user_point_prize.sql
+++ db_migration/aa/V2016_03_22_17_23__create_insert_point_prize_user_point_prize.sql
@@ -28,7 +28,7 @@ BEGIN ;
 INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) VALUE ('JapanKoreaTravel','日韩游',NULL,NULL,0,TRUE);
 INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) VALUE ('HKMacaoTravel','港澳游',NULL,NULL,0,TRUE);
 INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) VALUE ('Iphone6sPlus','iphone6s plus',NULL,NULL,0,TRUE);
-INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) VALUE ('ThankYouForParticipating','谢谢参与',NULL,NULL,69,TRUE);
+INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) VALUE ('ThankYou','谢谢参与',NULL,NULL,69,TRUE);
 INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) SELECT 'InterestCoupon2','0.2%加息券' ,301,NULL,25,TRUE FROM dual where EXISTS(SELECT * FROM coupon where id = 301);
 INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) SELECT 'InvestCoupon3000','3000元体验金' ,302,NULL,1,TRUE FROM dual where EXISTS(SELECT * FROM coupon where id = 302);
 INSERT INTO `point_prize` (`name`,`description`,`coupon_id`,`cash`,`probability`,`active`) VALUE ('Cash5','现金5元',NULL,500,3,TRUE);
