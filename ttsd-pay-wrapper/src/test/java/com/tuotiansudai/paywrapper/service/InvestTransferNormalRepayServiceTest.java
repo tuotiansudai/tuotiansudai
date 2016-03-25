fd2bdb242a1fa850e3b81b83d2ab2e69bee0b911
Xiduan Gao <gaoxiduan@gmail.com>
2016-03-25 11:17:10 +0800

    finished transfer rule update

--- ttsd-pay-wrapper/src/test/java/com/tuotiansudai/paywrapper/service/InvestTransferNormalRepayServiceTest.java
+++ ttsd-pay-wrapper/src/test/java/com/tuotiansudai/paywrapper/service/InvestTransferNormalRepayServiceTest.java
@@ -12,6 +12,8 @@ import com.tuotiansudai.paywrapper.client.PayAsyncClient;
 import com.tuotiansudai.paywrapper.client.PaySyncClient;
 import com.tuotiansudai.repository.mapper.*;
 import com.tuotiansudai.repository.model.*;
+import com.tuotiansudai.transfer.repository.mapper.TransferApplicationMapper;
+import com.tuotiansudai.transfer.repository.model.TransferApplicationModel;
 import com.tuotiansudai.util.IdGenerator;
 import com.tuotiansudai.util.InterestCalculator;
 import org.joda.time.DateTime;
