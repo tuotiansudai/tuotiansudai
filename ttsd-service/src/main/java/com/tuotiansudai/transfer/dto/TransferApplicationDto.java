0ca5f941cce31bceebc2c243c4429e7c2110a115
zourenzheng <zourenzheng@tuotiansudai.com>
2016-03-28 14:56:12 +0800

    format dto

--- ttsd-service/src/main/java/com/tuotiansudai/transfer/dto/TransferApplicationDto.java
+++ ttsd-service/src/main/java/com/tuotiansudai/transfer/dto/TransferApplicationDto.java
@@ -6,61 +6,14 @@ import java.util.Date;
 
 public class TransferApplicationDto {
 
-    private Long id;
-
-    private String name;
-
-    private Long loanId;
 
     private Long transferInvestId;
-
-    private Long investId;
-
-    private Integer period;
-
-    private String loginName;
-
-    private Long investAmount;
 
     private Long transferAmount;
 
     private Boolean transferInterest;
 
-    private Long transferFee;
-
-    private TransferStatus status;
-
-    private Date deadline;
-
-    private Date transferTime;
-
-    private Date applicationTime;
-
     public TransferApplicationDto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getLoanId() {
-        return loanId;
-    }
-
-    public void setLoanId(Long loanId) {
-        this.loanId = loanId;
     }
 
     public Long getTransferInvestId() {
@@ -69,38 +22,6 @@ public class TransferApplicationDto {
 
     public void setTransferInvestId(Long transferInvestId) {
         this.transferInvestId = transferInvestId;
-    }
-
-    public Long getInvestId() {
-        return investId;
-    }
-
-    public void setInvestId(Long investId) {
-        this.investId = investId;
-    }
-
-    public Integer getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(Integer period) {
-        this.period = period;
-    }
-
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
-
-    public Long getInvestAmount() {
-        return investAmount;
-    }
-
-    public void setInvestAmount(Long investAmount) {
-        this.investAmount = investAmount;
     }
 
     public Long getTransferAmount() {
@@ -119,43 +40,4 @@ public class TransferApplicationDto {
         this.transferInterest = transferInterest;
     }
 
-    public Long getTransferFee() {
-        return transferFee;
-    }
-
-    public void setTransferFee(Long transferFee) {
-        this.transferFee = transferFee;
-    }
-
-    public TransferStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TransferStatus status) {
-        this.status = status;
-    }
-
-    public Date getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(Date deadline) {
-        this.deadline = deadline;
-    }
-
-    public Date getTransferTime() {
-        return transferTime;
-    }
-
-    public void setTransferTime(Date transferTime) {
-        this.transferTime = transferTime;
-    }
-
-    public Date getApplicationTime() {
-        return applicationTime;
-    }
-
-    public void setApplicationTime(Date applicationTime) {
-        this.applicationTime = applicationTime;
-    }
 }
