98cffca69d7ec6fff195b4905d2ed0b502e54ef7
zhanglong888 <zhanglong@tuotiansudai.com>
2016-03-29 15:18:53 +0800

    features/zl/mobile_app_transfer_apply_query

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppTransferApplyQueryController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/MobileAppTransferApplyQueryController.java
@@ -1,13 +1,17 @@
 package com.tuotiansudai.api.controller;
 
 import com.tuotiansudai.api.dto.BaseResponseDto;
+import com.tuotiansudai.api.dto.ReturnMessage;
 import com.tuotiansudai.api.dto.TransferApplyQueryRequestDto;
 import com.tuotiansudai.api.service.MobileAppTransferApplyQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class MobileAppTransferApplyQueryController extends MobileAppBaseController{
@@ -16,9 +20,15 @@ public class MobileAppTransferApplyQueryController extends MobileAppBaseControll
     private MobileAppTransferApplyQueryService mobileAppTransferApplyQueryService;
 
     @RequestMapping(value = "/get/transfer-apply", method = RequestMethod.POST)
-    public BaseResponseDto transferApplyQuery(@RequestBody TransferApplyQueryRequestDto requestDto) {
-        requestDto.getBaseParam().setUserId(getLoginName());
-        return mobileAppTransferApplyQueryService.transferApplyQuery(requestDto);
+    public BaseResponseDto transferApplyQuery(@Valid @RequestBody TransferApplyQueryRequestDto requestDto,BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorCode = bindingResult.getFieldError().getDefaultMessage();
+            String errorMessage = ReturnMessage.getErrorMsgByCode(errorCode);
+            return new BaseResponseDto(errorCode, errorMessage);
+        } else {
+            requestDto.getBaseParam().setUserId(getLoginName());
+            return mobileAppTransferApplyQueryService.transferApplyQuery(requestDto);
+        }
     }
 
 }
