df7d669e7809bf5ef5bfbe22e800d8ca5260a021
zourenzheng <zourenzheng@tuotiansudai.com>
2016-04-01 14:20:49 +0800

    modify_imitate_lottery

--- ttsd-point/src/main/java/com/tuotiansudai/point/service/impl/PointLotteryServiceImpl.java
+++ ttsd-point/src/main/java/com/tuotiansudai/point/service/impl/PointLotteryServiceImpl.java
@@ -87,15 +87,14 @@ public class PointLotteryServiceImpl implements PointLotteryService{
             loginName = this.imitateLoginName();
         } while (userMapper.findByLoginName(loginName) != null);
         long notRealNum = userPointPrizeMapper.findAllNotReal();
-        UserPointPrizeModel userPointPrizeModel;
-        if (notRealNum % 10 != 0) {
-            PointPrizeModel pointPrizeModel = this.winLottery();
-            userPointPrizeModel = new UserPointPrizeModel(pointPrizeModel.getId(), loginName, false);
+        List<PointPrizeModel> pointPrizeModels;
+        if (notRealNum % 100 != 0) {
+            pointPrizeModels = pointPrizeMapper.findAllPossibleWin();
         } else {
-            List<PointPrizeModel> pointPrizeModels = pointPrizeMapper.findAllUnPossibleWin();
-            int num = new Random().nextInt(pointPrizeModels.size());
-            userPointPrizeModel = new UserPointPrizeModel(pointPrizeModels.get(num).getId(), loginName, false);
+            pointPrizeModels = pointPrizeMapper.findAllUnPossibleWin();
         }
+        int num = new Random().nextInt(pointPrizeModels.size());
+        UserPointPrizeModel userPointPrizeModel = new UserPointPrizeModel(pointPrizeModels.get(num).getId(), loginName, false);
         userPointPrizeMapper.create(userPointPrizeModel);
     }
 
