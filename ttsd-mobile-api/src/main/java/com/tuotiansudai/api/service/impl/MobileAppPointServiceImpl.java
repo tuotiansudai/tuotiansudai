bb4c02192fdc80e774cf1497ce4426fdd1945530
zhanglong888 <zhanglong@tuotiansudai.com>
2016-04-07 17:14:52 +0800

    bugs/zl/mobile_app_not_certificated_sign_in

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppPointServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/impl/MobileAppPointServiceImpl.java
@@ -47,6 +47,10 @@ public class MobileAppPointServiceImpl implements MobileAppPointService {
     private UserPointTaskMapper userPointTaskMapper;
     public BaseResponseDto signIn(BaseParamDto baseParamDto) {
         String loginName = baseParamDto.getBaseParam().getUserId();
+        AccountModel accountModel = accountMapper.findByLoginName(loginName);
+        if(accountModel == null){
+            return new BaseResponseDto(ReturnMessage.USER_IS_NOT_CERTIFICATED.getCode(),ReturnMessage.USER_IS_NOT_CERTIFICATED.getMsg());
+        }
         SignInPointDto signInPointDto = signInService.signIn(loginName);
 
         SignInResponseDataDto dataDto = new SignInResponseDataDto();
@@ -83,8 +87,13 @@ public class MobileAppPointServiceImpl implements MobileAppPointService {
 
     @Override
     public BaseResponseDto queryPointBillList(PointBillRequestDto pointBillRequestDto) {
-        BaseResponseDto dto = new BaseResponseDto();
+
         String loginName = pointBillRequestDto.getBaseParam().getUserId();
+        AccountModel accountModel = accountMapper.findByLoginName(loginName);
+        if(accountModel == null){
+            return new BaseResponseDto(ReturnMessage.USER_IS_NOT_CERTIFICATED.getCode(),ReturnMessage.USER_IS_NOT_CERTIFICATED.getMsg());
+        }
+        BaseResponseDto dto = new BaseResponseDto();
         Integer index = pointBillRequestDto.getIndex();
         Integer pageSize = pointBillRequestDto.getPageSize();
         if (index == null || index <= 0) {
@@ -182,6 +191,9 @@ public class MobileAppPointServiceImpl implements MobileAppPointService {
     public BaseResponseDto queryPoint(BaseParamDto baseParamDto) {
         String loginName = baseParamDto.getBaseParam().getUserId();
         AccountModel accountModel = accountMapper.findByLoginName(loginName);
+        if(accountModel == null){
+            return new BaseResponseDto(ReturnMessage.USER_IS_NOT_CERTIFICATED.getCode(),ReturnMessage.USER_IS_NOT_CERTIFICATED.getMsg());
+        }
 
         PointResponseDataDto dataDto = new PointResponseDataDto();
         dataDto.setPoint(accountModel.getPoint());
