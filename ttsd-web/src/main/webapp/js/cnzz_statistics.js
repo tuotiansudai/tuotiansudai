2d427e2a6eb1bffc2ce5743236a0bc05348ff594
zhanglong888 <zhanglong@tuotiansudai.com>
2016-04-07 18:53:45 +0800

    features/zl/cnzz_head_navigation

--- ttsd-web/src/main/webapp/js/cnzz_statistics.js
+++ ttsd-web/src/main/webapp/js/cnzz_statistics.js
@@ -1,7 +1,6 @@
 cnzzPush = function () {
 }
 cnzzPush.prototype.trackClick = function (category, action, label) {
-    console.log("category==" + category);
     _czc.push(['_trackEvent', category, action, label]);
 }
 
