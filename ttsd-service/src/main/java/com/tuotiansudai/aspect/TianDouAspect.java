91ef205ada2ea543b0f99d0ae7b522e4956c52f1
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-04-11 17:55:29 +0800

    exclude loaner in raking activity

--- ttsd-service/src/main/java/com/tuotiansudai/aspect/TianDouAspect.java
+++ ttsd-service/src/main/java/com/tuotiansudai/aspect/TianDouAspect.java
@@ -4,6 +4,8 @@ import com.tuotiansudai.client.RedisWrapperClient;
 import com.tuotiansudai.repository.mapper.LoanMapper;
 import com.tuotiansudai.repository.model.InvestModel;
 import com.tuotiansudai.repository.model.LoanModel;
+import com.tuotiansudai.repository.model.Role;
+import com.tuotiansudai.service.UserRoleService;
 import com.tuotiansudai.service.impl.RankingActivityServiceImpl;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.log4j.Logger;
@@ -29,6 +31,9 @@ public class TianDouAspect {
     @Autowired
     private LoanMapper loanMapper;
 
+    @Autowired
+    private UserRoleService userRoleService;
+
     @After(value = "execution(* *..InvestService.investSuccess(..))")
     public void afterReturningInvestSuccess(JoinPoint joinPoint) {
         logger.debug("after returning invest, tianDou assign starting...");
@@ -40,8 +45,10 @@ public class TianDouAspect {
             logger.info("ranking activity not started. Start time is: 2016-04-01 00:00:00. loginName:" + investModel.getLoginName()
                     + ", loanId:" + investModel.getLoanId() + ", amount:" + investModel.getAmount());
         } else {
-            try {
-                String loginName = investModel.getLoginName();
+            String loginName = investModel.getLoginName();
+            if (userRoleService.judgeUserRoleExist(loginName, Role.LOANER)) {
+                return;
+            } else {
                 long amount = investModel.getAmount();
                 long loanId = investModel.getLoanId();
                 LoanModel loanModel = loanMapper.findById(loanId);
@@ -54,9 +61,6 @@ public class TianDouAspect {
                 String value = amount + "+" + tianDouScore + "+" + loanId + "+" + time;
                 redisWrapperClient.lpush(RankingActivityServiceImpl.TIAN_DOU_INVEST_SCORE_RECORD + loginName, value);
                 redisWrapperClient.zincrby(RankingActivityServiceImpl.TIAN_DOU_USER_SCORE_RANK, tianDouScore, loginName);
-
-            } catch (Exception e) {
-                logger.error(e.getLocalizedMessage(), e);
             }
             logger.debug("after returning invest, tianDou assign completed");
         }
