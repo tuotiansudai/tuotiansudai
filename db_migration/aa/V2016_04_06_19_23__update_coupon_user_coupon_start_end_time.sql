7de87586153d1eff038050df207cdd1ca000f379
zourenzheng <zourenzheng@tuotiansudai.com>
2016-04-12 12:10:53 +0800

    format history coupon

--- db_migration/aa/V2016_04_06_19_23__update_coupon_user_coupon_start_end_time.sql
+++ db_migration/aa/V2016_04_06_19_23__update_coupon_user_coupon_start_end_time.sql
@@ -3,21 +3,6 @@ alter table coupon drop column invest_upper_limit;
 
 BEGIN ;
 
-UPDATE
-  coupon c
-  JOIN
-    (SELECT
-      t.`id`,
-      MIN(u.`start_time`) AS start_time,
-      MAX(u.`end_time`) AS end_time
-    FROM
-      coupon t
-      JOIN user_coupon u
-        ON t.`id` = u.`coupon_id`
-    WHERE t.`start_time` IS NULL
-      AND t.`end_time` IS NULL
-    GROUP BY t.`id`) temp
-    ON c.`id` = temp.id SET c.`start_time` = temp.start_time,
-  c.`end_time` = temp.end_time ;
+delete from coupon where user_group = 'EXCHANGER';
 
 COMMIT ;
