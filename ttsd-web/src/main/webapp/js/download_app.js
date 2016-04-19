389f7c2a5a3dda37f13795547f8829f2be2e38da
zhouliang66 <zhouliang@tuotiansudai.com>
2016-04-19 17:46:13 +0800

    fix js

--- ttsd-web/src/main/webapp/js/download_app.js
+++ ttsd-web/src/main/webapp/js/download_app.js
@@ -1,6 +1,6 @@
 require(['jquery'], function($) {
 	$("#downloadBtn").on('click', function(event) {
 		event.preventDefault();
-		$('body,html').animate({scrollTop:0},1000);
+		$('body,html').animate({scrollTop:0},'fast');
 	});
 });
