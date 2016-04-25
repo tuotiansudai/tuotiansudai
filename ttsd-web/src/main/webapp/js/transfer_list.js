777c4d1438f086744b35f5fe72daf243e494d7a6
xvqiang <xuqiang@tuotiansudai.com>
2016-04-25 15:16:24 +0800

    fix transfer list js

--- ttsd-web/src/main/webapp/js/transfer_list.js
+++ ttsd-web/src/main/webapp/js/transfer_list.js
@@ -1,11 +1,41 @@
 require(['jquery', 'layerWrapper','coupon-alert','red-envelope-float'], function ($, layer) {
-
     $(function() {
         $('.transfer-list-box ul li').on('click', function(event) {
             event.preventDefault();
             var $self = $(this),
                 urldata = $self.attr('data-url');
-                location.href = urldata;
+			$.ajax({
+				url: '/transfer/purchare?id',
+				type: 'POST',
+				dataType: 'json',
+				data: {param1: 'value1'},
+			})
+			.done(function(data) {
+				if(data==1){
+					layer.open({
+					  title: '温馨提示',
+					  btn: ['确定'],
+					  content: '该项目已被承接，请选择其他项目。',
+					  btn1: function(index, layero){
+					    layer.closeAll();
+					  }
+					});
+				}else if(data==2){
+					layer.open({
+					  title: '温馨提示',
+					  btn: ['确定'],
+					  content: '该项目已被取消，请选择其他项目。',
+					  btn1: function(index, layero){
+					    layer.closeAll();
+					  }
+					});
+				}else{
+					location.href = urldata;
+				}
+			})
+			.fail(function() {
+				layer.msg('请求失败');
+			});
         });
     });
 });
