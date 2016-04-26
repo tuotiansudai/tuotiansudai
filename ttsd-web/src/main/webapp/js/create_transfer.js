3122644a5ab119d839a3f0f2acab7feed8508e6f
xvqiang <xuqiang@tuotiansudai.com>
2016-04-26 15:39:18 +0800

    fix ftl and js

--- ttsd-web/src/main/webapp/js/create_transfer.js
+++ ttsd-web/src/main/webapp/js/create_transfer.js
@@ -1,90 +1,46 @@
-require(['jquery', 'mustache', 'text!/tpl/transferrer-transfer-application-table.mustache', 'pagination', 'layerWrapper', 'jquery.ajax.extension'], function ($, Mustache, transferrerListTemplate, pagination, layer) {
-	//$(function() {
-	//	//var $filtersList=$('.filters-list li');
-	//	//$filtersList.on('click', function(event) {
-	//	//	event.preventDefault();
-	//	//	var $self=$(this),
-	//	//		$recordList=$('.list-container'),
-	//	//		index=$self.index();
-	//	//	$self.addClass('active').siblings().removeClass('active');
-	//	//	$recordList.find('.record-list:eq('+index+')').addClass('active').siblings().removeClass('active');
-	//	//});
-    //
-	//});
+require(['jquery', 'mustache', 'text!/tpl/transfer-transferable-table.mustache','text!/tpl/transferrer-transfer-application-table.mustache', 'pagination', 'layerWrapper', 'jquery.ajax.extension'], function ($, Mustache, transferableListTemplate, transferrerListTemplate,pagination, layer) {
+	$(function() {
+		var $filtersList=$('.filters-list li'),
+			activeIndex=$('.filters-list li.active').index(),
+			paginationElement = $('.pagination');
+		$filtersList.on('click', function(event) {
+			event.preventDefault();
+			var $self=$(this),
+				$recordList=$('.list-container'),
+				index=$self.index(),
+				dataStatus=$self.attr('data-status');
 
-	var paginationElement = $('.pagination');
-	var loadLoanData = function (currentPage) {
-		var status = $('.status-filter .select-item.current').data('status');
-
-		var requestData = {startTime: startTime, endTime: endTime, status: status, index: currentPage || 1};
-
-		paginationElement.loadPagination(requestData, function (data) {
-			var html = Mustache.render(investListTemplate, data);
-			$('.invest-list').html(html);
-			$('.invest-list .show-invest-repay').click(function () {
-				$.ajax({
-					url: $(this).data('url'),
-					type: 'get',
-					dataType: 'json',
-					contentType: 'application/json; charset=UTF-8'
-				}).success(function (response) {
-					var data = response.data;
-					data.isLoanCompleted = status == 'COMPLETE';
-					data.csrfToken = $("meta[name='_csrf']").attr("content");
-					if (data.status) {
-						_.each(data.records, function (item) {
-							data.loanId = item.loanId;
-							switch (item.loanRepayStatus) {
-								case 'REPAYING':
-									item.status = '待还';
-									break;
-								case 'COMPLETE':
-									item.status = '完成';
-									break;
-								case 'CANCEL':
-									item.status = '流标';
-									break;
-							}
-						});
-						var html = Mustache.render(investRepayTemplate, data);
-
-						layer.open({
-							type: 1,
-							title: false,
-							offset: '80px',
-							area: ['1000px'],
-							shadeClose: true,
-							content: html
-						});
-
-					}
-				});
+			loadLoanData();
+			$self.addClass('active').siblings().removeClass('active');
+			$recordList.find('.record-list:eq('+index+')').addClass('active').siblings().removeClass('active');
+		});
+    
+		var loadLoanData = function (currentPage) {
+			var status = $('.filters-list li.active').attr('data-status').split(',');
+			var requestData = {status: status, index: currentPage || 1};
+			paginationElement.loadPagination(requestData, function (data) {
+				if(activeIndex==0){
+					var html = Mustache.render(transferableListTemplate, data);
+				}else{
+					var html = Mustache.render(transferrerListTemplate, data);
+				}
+				$('.list-container 。record-list.active').html(html);
 			});
-		});
-		$('.invest-list').on('mouseenter','.project-name',function() {
-			layer.closeAll('tips');
-			if($.trim($(this).text()).length>15){
-				layer.tips($(this).text(), $(this), {
-					tips: [1, '#efbf5c'],
-					time: 2000,
-					tipsMore: true,
-					area: 'auto',
-					maxWidth: '500'
-				});
-			}
-		});
-		$('.invest-list').on('mouseenter','.birth-icon',function() {
-			layer.closeAll('tips');
-			var num = parseFloat($(this).attr('data-benefit'));
-			var benefit = num + 1;
-			layer.tips('您已享受生日福利，首月收益翻'+benefit+'倍', $(this), {
-				tips: [1, '#efbf5c'],
-				time: 2000,
-				tipsMore: true,
-				area: 'auto',
-				maxWidth: '500'
+			$('.list-container').on('mouseenter','.project-name',function() {
+				layer.closeAll('tips');
+				if($.trim($(this).text()).length>15){
+					layer.tips($(this).text(), $(this), {
+						tips: [1, '#efbf5c'],
+						time: 2000,
+						tipsMore: true,
+						area: 'auto',
+						maxWidth: '500'
+					});
+				}
 			});
-		});
-	};
-	loadLoanData();
+
+		};
+		loadLoanData();
+	});
+
 });
--- ttsd-web/src/main/webapp/templates/create-transfer.ftl
+++ ttsd-web/src/main/webapp/templates/create-transfer.ftl
@@ -3,14 +3,21 @@
 <div class="content-container create-transfer-content">
     <h4 class="column-title"><em class="tc">债权转让</em></h4>
     <ul class="filters-list">
-        <li class="active current" data-status="">可转让债权</li>
-        <li>转让中债权</li>
-        <li>转让记录</li>
+        <li class="active" data-status="TRANSFERABLE">可转让债权</li>
+        <li data-status="TRANSFERRING">转让中债权</li>
+        <li data-status="SUCCESS,CANCEL">转让记录</li>
     </ul>
     <div class="list-container">
         <div class="record-list active">
+            
         </div>
-        <div class="pagination" data-url="/investor/invest-list-data" data-page-size="10"></div>
+        <div class="record-list">
+
+        </div>
+        <div class="record-list">
+
+        </div>
+        <div class="pagination" data-url="/transferrer/transfer-application-list-data" data-page-size="10"></div>
     </div>
 </div>
 </@global.main>
