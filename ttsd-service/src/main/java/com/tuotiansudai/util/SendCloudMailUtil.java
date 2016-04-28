767c56e2cc24191020accc4b3960da80a918f98b
zourenzheng <zourenzheng@tuotiansudai.com>
2016-04-28 13:44:00 +0800

    lf

--- ttsd-service/src/main/java/com/tuotiansudai/util/SendCloudMailUtil.java
+++ ttsd-service/src/main/java/com/tuotiansudai/util/SendCloudMailUtil.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
