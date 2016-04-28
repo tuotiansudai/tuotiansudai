f6bde07cef2f18dec62688d6feebec34f14e83de
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-04-28 12:30:27 -0700

    console_query_today

--- ttsd-console/src/main/java/com/tuotiansudai/console/controller/LoanListController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/LoanListController.java
@@ -34,11 +34,11 @@ public class LoanListController {
                                         @RequestParam(value = "loanName", required = false) String loanName,
                                         @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize) {
         int loanListCount = loanService.findLoanListCount(status, loanId, loanName,
-                startTime == null ? new DateTime(0).toDate() : startTime,
-                endTime == null ? new DateTime(9999, 12, 31, 0, 0, 0).toDate() : endTime);
+                startTime == null ? new DateTime(0).toDate() : new DateTime(startTime).withTimeAtStartOfDay().toDate(),
+                endTime == null ? new DateTime(9999, 12, 31, 0, 0, 0).toDate() : new DateTime(endTime).withTimeAtStartOfDay().plusDays(1).minusMillis(1).toDate());
         List<LoanListDto> loanListDtos = loanService.findLoanList(status, loanId, loanName,
-                startTime == null ? new DateTime(0).toDate() : startTime,
-                endTime == null ? new DateTime(9999, 12, 31, 0, 0, 0).toDate() : endTime,
+                startTime == null ? new DateTime(0).toDate() : new DateTime(startTime).withTimeAtStartOfDay().toDate(),
+                endTime == null ? new DateTime(9999, 12, 31, 0, 0, 0).toDate() : new DateTime(endTime).withTimeAtStartOfDay().plusDays(1).minusMillis(1).toDate(),
                 index, pageSize);
         ModelAndView modelAndView = new ModelAndView("/loan-list");
         modelAndView.addObject("loanListCount", loanListCount);
