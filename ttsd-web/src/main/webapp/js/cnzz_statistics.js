b334e58d90a9a4141c107fff5470b819b2c58a2e
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-03 14:33:10 -0700

    update cnzz

--- ttsd-web/src/main/webapp/js/cnzz_statistics.js
+++ ttsd-web/src/main/webapp/js/cnzz_statistics.js
@@ -1,7 +1,6 @@
 var cnzzPushConstructor = function () {
 };
 cnzzPushConstructor.prototype.trackClick = function (category, action, label) {
-    alert(category);
     _czc.push(['_trackEvent', category, action, label]);
 };
 cnzzPush = new cnzzPushConstructor();
