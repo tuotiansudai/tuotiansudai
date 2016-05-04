deab51c7591191989de7fa7089793b1ac2084114
Xiduan Gao <gaoxiduan@gmail.com>
2016-05-04 18:27:16 +0800

    fixed sql

--- db_migration/aa/V2016_05_04_16_56__migrate_red_envelope_actual_interest.sql
+++ db_migration/aa/V2016_05_04_16_56__migrate_red_envelope_actual_interest.sql
@@ -2,6 +2,8 @@ BEGIN;
 
 UPDATE `user_coupon`
 SET `actual_interest` = `expected_interest`
-WHERE coupon_type = 'RED_ENVELOPE' and `status` = 'SUCCESS';
+WHERE EXISTS(SELECT 1
+             FROM coupon
+             WHERE coupon.id = user_coupon.coupon_id AND coupon.coupon_type = 'RED_ENVELOPE') AND `status` = 'SUCCESS';
 
 COMMIT;
