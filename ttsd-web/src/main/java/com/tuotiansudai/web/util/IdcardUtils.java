776abc3361d71b0ef4c6ce8a1ea9a44e25d907cb
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-05 19:11:08 -0700

    register_identity_server_check

--- ttsd-web/src/main/java/com/tuotiansudai/web/util/IdcardUtils.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/util/IdcardUtils.java
@@ -3,21 +3,34 @@ package com.tuotiansudai.web.util;
 
 
 import org.apache.commons.lang.StringUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IdcardUtils extends StringUtils {
 
-    /*** 中国公民身份证号码最小长度。*/
+    /***
+     * 中国公民身份证号码最小长度。
+     */
     public static final int CHINA_ID_MIN_LENGTH = 15;
 
-    /*** 中国公民身份证号码最大长度。*/
+    /***
+     * 中国公民身份证号码最大长度。
+     */
     public static final int CHINA_ID_MAX_LENGTH = 18;
 
-    /** 第18位校检码 */
+    /**
+     * 第18位校检码
+     */
     private static String[] ValCodeArr = {
             "1", "0", "x", "9", "8", "7", "6", "5", "4", "3", "2"};
-    /** 每位加权因子 */
+    /**
+     * 每位加权因子
+     */
     private static String[] Wi = {
             "7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7", "9", "10", "5", "8", "4", "2"};
 
@@ -29,25 +42,29 @@ public class IdcardUtils extends StringUtils {
         if (isNumeric(code17) == false) {
             return false;
         }
-        if(!validateTime(code17)){
+        if (!validateTime(code17)) {
             return false;
         }
-        if(!compareIdCard(idCard,countCode18(code17))){
+        if (!compareIdCard(idCard, countCode18(code17))) {
             return false;
         }
         return true;
     }
 
-    private static String getIdCode17(String idCard){
+    public static boolean isNumeric(String val) {
+        return val == null || "".equals(val) ? false : val.matches("^[0-9]*$");
+    }
+
+    private static String getIdCode17(String idCard) {
         if (idCard.length() == CHINA_ID_MAX_LENGTH) {
             return idCard.substring(0, 17);
         } else if (idCard.length() == CHINA_ID_MIN_LENGTH) {
-            return idCard.substring(0, 6) + "19" + idCard.substring(6, CHINA_ID_MIN_LENGTH);
+            return conver15CardTo18(idCard).substring(0, 17);
         }
-        return "";
+        return null;
     }
 
-    private static boolean compareIdCard(String idCatd,String countIdCard){
+    private static boolean compareIdCard(String idCatd, String countIdCard) {
         if (idCatd.length() == 18) {
             if (!countIdCard.equals(idCatd)) {
                 return false;
@@ -56,55 +73,45 @@ public class IdcardUtils extends StringUtils {
         return true;
     }
 
-    private static String countCode18(String code17){
+    private static String countCode18(String code17) {
         int iSum = 0;
         for (int i = 0; i < 17; i++) {
             iSum = iSum
                     + Integer.parseInt(String.valueOf(code17.charAt(i)))
                     * Integer.parseInt(Wi[i]);
         }
-        int modValue = iSum % 11;
-        String strVerifyCode = ValCodeArr[modValue];
-        return code17 + strVerifyCode;
+        return code17 + ValCodeArr[iSum % 11];
     }
 
-    private static boolean validateTime(String idCard){
+    private static boolean validateTime(String idCard) {
         String strYear = idCard.substring(6, 10);
-        String strMonth = idCard.substring(10, 12);
-        String strDay = idCard.substring(12, 14);
-        if (isDate(strYear + "-" + strMonth + "-" + strDay) == false) {
-            return false;
-        }
         if (Integer.parseInt(strYear) < 1900 || Integer.parseInt(strYear) == 0) {
             return false;
         }
+        String strMonth = idCard.substring(10, 12);
         if (Integer.parseInt(strMonth) > 12 || Integer.parseInt(strMonth) == 0) {
             return false;
         }
+        String strDay = idCard.substring(12, 14);
         if (Integer.parseInt(strDay) > 31 || Integer.parseInt(strDay) == 0) {
             return false;
         }
         return true;
     }
 
-    public static boolean isNumeric(String str) {
-        Pattern pattern = Pattern.compile("[0-9]*");
-        Matcher isNum = pattern.matcher(str);
-        if (isNum.matches()) {
-            return true;
-        } else {
-            return false;
+    private static String conver15CardTo18(String idCard) {
+        String birthday = idCard.substring(6, 12);
+        Date birthDate = null;
+        try {
+            birthDate = new SimpleDateFormat("yyMMdd").parse(birthday);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-    }
-
-    private static boolean isDate(String strDate) {
-        Pattern pattern = Pattern
-                .compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
-        Matcher m = pattern.matcher(strDate);
-        if (m.matches()) {
-            return true;
-        } else {
-            return false;
-        }
+        Calendar cal = Calendar.getInstance();
+        if (birthDate != null)
+            cal.setTime(birthDate);
+        String sYear = String.valueOf(cal.get(Calendar.YEAR));
+        String code18 = idCard.substring(0, 6) + sYear + idCard.substring(8);
+        return code18 + countCode18(code18);
     }
 }
