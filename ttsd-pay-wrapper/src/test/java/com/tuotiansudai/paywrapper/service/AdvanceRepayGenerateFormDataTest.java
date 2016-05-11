33b9d7b089e9306071855b17319e15252095f081
gengbj <gengbj@tuotiansudai.com>
2016-05-10 18:15:19 -0700

    no modify

---
+++ ttsd-pay-wrapper/src/test/java/com/tuotiansudai/paywrapper/service/AdvanceRepayGenerateFormDataTest.java
@@ -0,0 +1,139 @@
+package com.tuotiansudai.paywrapper.service;
+
+
+import com.google.common.collect.Lists;
+import com.tuotiansudai.dto.BaseDto;
+import com.tuotiansudai.dto.PayFormDataDto;
+import com.tuotiansudai.repository.mapper.*;
+import com.tuotiansudai.repository.model.*;
+import com.tuotiansudai.util.IdGenerator;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+@Transactional
+public class AdvanceRepayGenerateFormDataTest extends RepayBaseTest {
+
+    @Autowired
+    private IdGenerator idGenerator;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Autowired
+    private LoanMapper loanMapper;
+
+    @Autowired
+    private LoanRepayMapper loanRepayMapper;
+
+    @Autowired
+    private InvestMapper investMapper;
+
+    @Autowired
+    private InvestRepayMapper investRepayMapper;
+
+    @Autowired
+    private AdvanceRepayService advanceRepayService;
+
+    @Test
+    public void shouldGenerateFirstPeriodFormDataWhenLoanIsRepaying() throws Exception {
+        UserModel loaner = this.getFakeUser("loaner");
+        AccountModel loanerAccount = this.getFakeAccount(loaner);
+        userMapper.create(loaner);
+        accountMapper.create(loanerAccount);
+
+        UserModel investor = this.getFakeUser("investor");
+        AccountModel investorAccount = this.getFakeAccount(investor);
+        userMapper.create(investor);
+        accountMapper.create(investorAccount);
+
+        DateTime recheckTime = new DateTime().minusDays(5);
+        LoanModel loan = this.getFakeNormalLoan(idGenerator.generate(), LoanType.INVEST_INTEREST_MONTHLY_REPAY, 10000, 2, 0.12, 0, 0.1, loaner.getLoginName(), recheckTime.toDate());
+        long loanRepay1ExpectedInterest = 1000;
+        long loanRepay2ExpectedInterest = 2000;
+        LoanRepayModel loanRepay1 = this.getFakeLoanRepayModel(idGenerator.generate(), loan.getId(), 1, 0, loanRepay1ExpectedInterest, recheckTime.plusDays(30).withTime(23, 59, 59, 0).toDate(), null, RepayStatus.REPAYING);
+        LoanRepayModel loanRepay2 = this.getFakeLoanRepayModel(idGenerator.generate(), loan.getId(), 2, loan.getLoanAmount(), loanRepay2ExpectedInterest, new DateTime().plusDays(60).withTime(23, 59, 59, 0).toDate(), null, RepayStatus.REPAYING);
+        loanMapper.create(loan);
+        loanRepayMapper.create(Lists.newArrayList(loanRepay1, loanRepay2));
+
+        InvestModel invest = new InvestModel(loan.getId(), loan.getLoanAmount(), investor.getLoginName(), Source.WEB, null);
+        invest.setCreatedTime(recheckTime.minusDays(1).toDate());
+        invest.setStatus(InvestStatus.SUCCESS);
+        investMapper.create(invest);
+        InvestRepayModel investRepay1 = new InvestRepayModel(idGenerator.generate(), invest.getId(), 1, loanRepay1ExpectedInterest, 100, loanRepay1.getRepayDate(), RepayStatus.REPAYING);
+        InvestRepayModel investRepay2 = new InvestRepayModel(idGenerator.generate(), invest.getId(), 2, loanRepay2ExpectedInterest, 200, loanRepay2.getRepayDate(), RepayStatus.REPAYING);
+        investRepay2.setCorpus(invest.getAmount());
+        investRepayMapper.create(Lists.newArrayList(investRepay1, investRepay2));
+
+        BaseDto<PayFormDataDto> formData = advanceRepayService.generateRepayFormData(loan.getId());
+
+        assertThat(Long.parseLong(formData.getData().getFields().get("order_id").split("X")[0]), is(loanRepay1.getId()));
+        assertThat(Long.parseLong(formData.getData().getFields().get("amount")), is(loan.getLoanAmount() + 23));
+
+        LoanRepayModel enabledLoanRepay = loanRepayMapper.findById(loanRepay1.getId());
+        assertThat(enabledLoanRepay.getStatus(), is(RepayStatus.WAIT_PAY));
+        assertThat(enabledLoanRepay.getActualInterest(), is(23L));
+        assertThat(enabledLoanRepay.getRepayAmount(), is(10000L + 23L));
+        assertThat(new DateTime(enabledLoanRepay.getActualRepayDate()).withTimeAtStartOfDay().getMillis(), is(new DateTime().withTimeAtStartOfDay().getMillis()));
+    }
+
+    @Test
+    public void shouldGenerateFormDataWhenCurrentIsRepayDateAndIsComplete() throws Exception {
+        UserModel loaner = this.getFakeUser("loaner");
+        AccountModel loanerAccount = this.getFakeAccount(loaner);
+        userMapper.create(loaner);
+        accountMapper.create(loanerAccount);
+
+        UserModel investor = this.getFakeUser("investor");
+        AccountModel investorAccount = this.getFakeAccount(investor);
+        userMapper.create(investor);
+        accountMapper.create(investorAccount);
+
+        DateTime recheckTime = new DateTime().minusDays(5);
+        LoanModel loan = this.getFakeNormalLoan(idGenerator.generate(), LoanType.INVEST_INTEREST_MONTHLY_REPAY, 10000, 2, 0.12, 0, 0.1, loaner.getLoginName(), recheckTime.toDate());
+        long loanRepay1ExpectedInterest = 1000;
+        long loanRepay2ExpectedInterest = 2000;
+        LoanRepayModel loanRepay1 = this.getFakeLoanRepayModel(idGenerator.generate(), loan.getId(), 1, 0, loanRepay1ExpectedInterest, new DateTime().withTime(23, 59, 59, 0).toDate(), new Date(), RepayStatus.COMPLETE);
+        loanRepay1.setActualInterest(loanRepay1ExpectedInterest);
+        LoanRepayModel loanRepay2 = this.getFakeLoanRepayModel(idGenerator.generate(), loan.getId(), 2, loan.getLoanAmount(), loanRepay2ExpectedInterest, new DateTime().plusDays(30).withTime(23, 59, 59, 0).toDate(), null, RepayStatus.REPAYING);
+        loanMapper.create(loan);
+        loanRepayMapper.create(Lists.newArrayList(loanRepay1, loanRepay2));
+
+        InvestModel invest = new InvestModel(loan.getId(), loan.getLoanAmount(), investor.getLoginName(), Source.WEB, null);
+        invest.setCreatedTime(recheckTime.minusDays(1).toDate());
+        invest.setStatus(InvestStatus.SUCCESS);
+        investMapper.create(invest);
+        InvestRepayModel investRepay1 = new InvestRepayModel(idGenerator.generate(), invest.getId(), 1, loanRepay1ExpectedInterest, 100, loanRepay1.getRepayDate(), RepayStatus.COMPLETE);
+        investRepay1.setActualInterest(loanRepay1ExpectedInterest);
+        investRepay1.setActualFee(100);
+        investRepay1.setActualRepayDate(loanRepay1.getActualRepayDate());
+        InvestRepayModel investRepay2 = new InvestRepayModel(idGenerator.generate(), invest.getId(), 2, loanRepay2ExpectedInterest, 200, loanRepay2.getRepayDate(), RepayStatus.REPAYING);
+        investRepay2.setCorpus(invest.getAmount());
+        investRepayMapper.create(Lists.newArrayList(investRepay1, investRepay2));
+
+        BaseDto<PayFormDataDto> formData = advanceRepayService.generateRepayFormData(loan.getId());
+
+        assertThat(Long.parseLong(formData.getData().getFields().get("order_id").split("X")[0]), is(loanRepay2.getId()));
+        assertThat(Long.parseLong(formData.getData().getFields().get("amount")), is(loan.getLoanAmount()));
+
+        LoanRepayModel enabledLoanRepay = loanRepayMapper.findById(loanRepay2.getId());
+        assertThat(enabledLoanRepay.getStatus(), is(RepayStatus.WAIT_PAY));
+        assertThat(enabledLoanRepay.getActualInterest(), is(0L));
+        assertThat(enabledLoanRepay.getRepayAmount(), is(10000L));
+        assertThat(new DateTime(enabledLoanRepay.getActualRepayDate()).withTimeAtStartOfDay().getMillis(), is(new DateTime().withTimeAtStartOfDay().getMillis()));
+    }
+}
