38c94d036372b2c900198660d0ec5c8dcfdf8de2
gengbj <gengbj@tuotiansudai.com>
2016-05-12 16:31:53 -0700

    console modify create loan

--- ttsd-console/src/main/webapp/js/createLoan.js
+++ ttsd-console/src/main/webapp/js/createLoan.js
@@ -139,23 +139,15 @@ require(['jquery', 'template', 'jquery-ui', 'bootstrap', 'bootstrapDatetimepicke
                         $('.jq-piex').text(_pix);
                     }
                     _hidden.val(_options.eq(i).attr('value'));
-                    if (_hidden.hasClass('jq-product-type')) {
-                        $('.product-line-period').html('');
-                        if (_options.eq(i).attr('value') && _options.eq(i).attr('value') != 'JYF') {
-                            $('.product-line-period').append('<input type="text" class="form-control jq-timer" placeholder="" datatype="num" errormsg="请选择产品线类型" id="loanPeriod" disabled="disabled">');
-                            $('.jq-timer').val(_options.eq(i).data('period'));
-                        } else if (_options.eq(i).attr('value') && _options.eq(i).attr('value') == 'JYF') {
-                            $('.product-line-period').append('<select class="selectpicker b-width jq-timer"><option value="6">6</option><option value="12">12</option></select>');
-                            $('.selectpicker').selectpicker({
-                                style: 'btn-default',
-                                size: 8
-                            });
-                        } else {
-                            $('.product-line-period').append('<input type="text" class="form-control jq-timer" placeholder="" datatype="num" errormsg="请选择产品线类型" id="loanPeriod" disabled="disabled">');
-                            $('.jq-timer').val('');
-                        }
+                    if (_options.eq(i).attr('value') && _options.eq(i).attr('value') != '') {
+                        $('.jq-timer').val(_options.eq(i).data('period'));
+                        $('.jq-duration').val(_options.eq(i).data('duration'));
+                        $('.jq-product-line').val(_options.eq(i).data('product-line'));
+                    }  else {
+                        $('.jq-timer').val('');
+                        $('.jq-duration').val('');
+                        $('.jq-product-line').val('');
                     }
-
                 }
             })
         });
@@ -247,11 +239,7 @@ require(['jquery', 'template', 'jquery-ui', 'bootstrap', 'bootstrapDatetimepicke
             //beforeSubmit
             beforeCheck: function (curform) {
                 $('.form-error').html('');
-                var periods = parseInt($('.jq-timer', curform).val());
-                if (periods <= 0) {
-                    showErrorMessage('借款期限最小为1', $('.jq-timer', curform));
-                    return false;
-                }
+
                 var loanAmount = parseFloat($('.jq-pay', curform).val());
                 if (loanAmount <= 0) {
                     showErrorMessage('预计出借金额应大于0', $('.jq-pay', curform));
@@ -325,6 +313,7 @@ require(['jquery', 'template', 'jquery-ui', 'bootstrap', 'bootstrapDatetimepicke
                     "loanerUserName": $('.jq-loaner-user-name').val(),
                     "type": $('.jq-mark-type').val(),
                     "periods": $('.jq-timer').val(),
+                    "duration": $('.jq-duration').val(),
                     "descriptionText": getContentTxt(),
                     "descriptionHtml": getContent(),
                     "investFeeRate": $('.jq-fee').val(),
@@ -332,7 +321,7 @@ require(['jquery', 'template', 'jquery-ui', 'bootstrap', 'bootstrapDatetimepicke
                     "maxInvestAmount": $('.jq-max-pay').val(),
                     "investIncreasingAmount": $('.jq-add-pay').val(),
                     "activityType": $('.jq-impact-type').val(),
-                    "productType": $('.jq-product-type').val(),
+                    "productType": $('.jq-product-line').val(),
                     "activityRate": $('.jq-percent').val(),
                     "contractId": $('.jq-pact').val(),
                     "basicRate": $('.jq-base-percent').val(),
