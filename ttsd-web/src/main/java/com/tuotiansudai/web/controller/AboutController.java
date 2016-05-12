10dafa808db199536f3d622af51988fed1ebbe35
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-05-12 13:48:21 +0800

    代码重构

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/AboutController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/AboutController.java
@@ -2,7 +2,6 @@ package com.tuotiansudai.web.controller;
 
 import com.tuotiansudai.dto.OperationDataDto;
 import com.tuotiansudai.service.OperationDataService;
-import com.tuotiansudai.service.InfoPublishService;
 import com.tuotiansudai.util.AmountConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping(path = "/about")
@@ -24,19 +25,15 @@ public class AboutController {
     @Autowired
     private OperationDataService operationDataService;
 
-    @Autowired
-    private InfoPublishService infoPublishService;
-
     @RequestMapping(path = "/{item:^assurance|company|contact|guide|notice|media|notice-detail|service-fee|qa|refer-reward|team|operational$}", method = RequestMethod.GET)
-    public ModelAndView about(@PathVariable String item){
-        ModelAndView modelAndView =  new ModelAndView("/about/" + item);
-        modelAndView.addObject("responsive",true);
-        if("service-fee".equals(item)){
+    public ModelAndView about(@PathVariable String item) {
+        ModelAndView modelAndView = new ModelAndView("/about/" + item);
+        modelAndView.addObject("responsive", true);
+        if ("service-fee".equals(item)) {
             modelAndView.addObject("withdrawFee", AmountConverter.convertCentToString(withdrawFee));
         }
-        if("operational".equals(item))
-        {
-            modelAndView.addObject("investDetailList", infoPublishService.getInvestDetail());
+        if ("operational".equals(item)) {
+            modelAndView.addObject("investDetailList", operationDataService.getInvestDetail(new Date()));
         }
         return modelAndView;
     }
@@ -44,9 +41,7 @@ public class AboutController {
     @RequestMapping(path = "/operation-data/chart", method = RequestMethod.GET)
     @ResponseBody
     public OperationDataDto infoPublishChart() {
-        OperationDataDto operationDataDto = operationDataService.getOperationDataFromRedis();
-
-        return operationDataDto;
+        return operationDataService.getOperationDataFromRedis(new Date());
     }
 
 }
