b55b1c6a79cb0220451f4b29cd37bed4da5e20c5
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-05-13 14:50:07 +0800

    merge

--- ttsd-service/src/main/java/com/tuotiansudai/repository/model/InvestDataView.java
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/model/InvestDataView.java
@@ -1,6 +1,7 @@
 package com.tuotiansudai.repository.model;
 
 import com.tuotiansudai.util.AmountConverter;
+import com.tuotiansudai.util.AmountTransfer;
 
 import java.io.Serializable;
 
--- ttsd-service/src/main/java/com/tuotiansudai/service/OperationDataService.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/OperationDataService.java
@@ -1,16 +1,14 @@
 package com.tuotiansudai.service;
 
 import com.tuotiansudai.dto.OperationDataDto;
-import com.tuotiansudai.repository.model.InvestDataView;
-
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by huoxuanbo on 16/5/9.
  */
 public interface OperationDataService {
-    OperationDataDto getOperationDataFromRedis(Date endDate);
+    OperationDataDto getOperationDataFromDatabase();
 
-    List<InvestDataView> getInvestDetail(Date endDate);
+    OperationDataDto getOperationDataFromRedis();
+
+    void updateRedis(OperationDataDto operationDataDto);
 }
