6de6525d7d6bb9fb1f85476c91e856d6baaa19a9
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-12 17:26:55 -0700

    features/bs/sakura_mster

---
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v2_0/MobileAppLoanListService.java
@@ -0,0 +1,9 @@
+package com.tuotiansudai.api.service.v2_0;
+
+import com.tuotiansudai.api.dto.v1_0.BaseParamDto;
+import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
+
+
+public interface MobileAppLoanListService {
+    BaseResponseDto generateIndexLoan(BaseParamDto baseParamDto);
+}
---
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v2_0/impl/MobileAppLoanListServiceImpl.java
@@ -0,0 +1,76 @@
+package com.tuotiansudai.api.service.v2_0.impl;
+
+import com.google.common.collect.Lists;
+import com.tuotiansudai.api.dto.v1_0.BaseParamDto;
+import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
+import com.tuotiansudai.api.dto.v1_0.ReturnMessage;
+import com.tuotiansudai.api.dto.v2_0.LoanListResponseDataDto;
+import com.tuotiansudai.api.dto.v2_0.LoanResponseDataDto;
+import com.tuotiansudai.api.service.v2_0.MobileAppLoanListService;
+import com.tuotiansudai.api.util.CommonUtils;
+import com.tuotiansudai.repository.mapper.InvestMapper;
+import com.tuotiansudai.repository.mapper.LoanMapper;
+import com.tuotiansudai.repository.model.LoanModel;
+import com.tuotiansudai.util.AmountConverter;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MobileAppLoanListServiceImpl implements MobileAppLoanListService{
+
+    @Autowired
+    private LoanMapper loanMapper;
+    @Autowired
+    private InvestMapper investMapper;
+
+    @Override
+    public BaseResponseDto generateIndexLoan(BaseParamDto baseParamDto) {
+        BaseResponseDto dto = new BaseResponseDto();
+        LoanListResponseDataDto loanListResponseDataDto = new LoanListResponseDataDto();
+        List<LoanModel> loanModels = loanMapper.findHomeLoan();
+        List<LoanResponseDataDto> loanDtoList = Lists.newArrayList();
+        if (CollectionUtils.isNotEmpty(loanModels)) {
+            loanDtoList = convertLoanDto(loanModels);
+            loanListResponseDataDto.setLoanList(loanDtoList);
+            dto.setData(loanListResponseDataDto);
+        }else{
+            loanListResponseDataDto.setLoanList(new ArrayList<LoanResponseDataDto>());
+            dto.setData(loanListResponseDataDto);
+        }
+        dto.setCode(ReturnMessage.SUCCESS.getCode());
+        dto.setMessage(ReturnMessage.SUCCESS.getMsg());
+        return dto;
+    }
+
+    private List<LoanResponseDataDto> convertLoanDto(List<LoanModel> loanList) {
+        List<LoanResponseDataDto> loanDtoList = new ArrayList<>();
+        DecimalFormat decimalFormat = new DecimalFormat("######0.##");
+        for (LoanModel loan : loanList) {
+            LoanResponseDataDto loanResponseDataDto = new LoanResponseDataDto();
+            loanResponseDataDto.setLoanId("" + loan.getId());
+            loanResponseDataDto.setLoanName(loan.getName());
+            loanResponseDataDto.setActivityType(loan.getActivityType().name());
+            loanResponseDataDto.setDuration(String.valueOf(loan.getDuration()));
+            loanResponseDataDto.setBaseRatePercent(decimalFormat.format(loan.getBaseRate() * 100));
+            loanResponseDataDto.setActivityRatePercent(decimalFormat.format(loan.getActivityRate() * 100));
+            loanResponseDataDto.setLoanAmount(AmountConverter.convertCentToString(loan.getLoanAmount()));
+            loanResponseDataDto.setInvestAmount("");
+            if(com.tuotiansudai.repository.model.LoanStatus.PREHEAT.equals(loan.getStatus())){
+                loanResponseDataDto.setLoanStatus(com.tuotiansudai.repository.model.LoanStatus.RAISING.name().toLowerCase());
+                loanResponseDataDto.setLoanStatusDesc(com.tuotiansudai.repository.model.LoanStatus.RAISING.getDescription());
+            }else{
+                loanResponseDataDto.setLoanStatus(loan.getStatus().name().toLowerCase());
+                loanResponseDataDto.setLoanStatusDesc(loan.getStatus().getDescription());
+            }
+            loanResponseDataDto.setFundraisingStartTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(loan.getFundraisingStartTime()));
+            loanResponseDataDto.setFundraisingCountDown(CommonUtils.calculatorInvestBeginSeconds(loan.getFundraisingStartTime()));
+            loanDtoList.add(loanResponseDataDto);
+
+            investMapper.fin
+        }
+        return loanDtoList;
+    }
+}
