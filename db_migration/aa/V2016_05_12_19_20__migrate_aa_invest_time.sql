97d3af1a3b41e44024e10da3ac2626110254ff61
Xiduan Gao <gaoxiduan@gmail.com>
2016-05-13 10:59:57 +0800

    fixed sql

--- db_migration/aa/V2016_05_12_19_20__migrate_aa_invest_time.sql
+++ db_migration/aa/V2016_05_12_19_20__migrate_aa_invest_time.sql
@@ -5,7 +5,8 @@ SET invest_time = created_time
 WHERE transfer_invest_id IS NULL;
 
 UPDATE invest
-SET invest_time = (select origin_invest.created_time from invest origin_invest where origin_invest.id = invest.transfer_invest_id)
-WHERE transfer_invest_id IS NOT NULL;
+  JOIN invest origin_invest ON origin_invest.id = invest.transfer_invest_id
+SET invest.invest_time = origin_invest.created_time
+WHERE invest.transfer_invest_id IS NOT NULL;
 
 COMMIT;
