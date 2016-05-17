f78d584a69d556253eec6683d2ba2a704feec3a3
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-05-17 19:58:49 +0800

    delete file userOpLogService.java

--- ttsd-service/src/main/java/com/tuotiansudai/service/userOpLogService.java
+++
@@ -1,7 +0,0 @@
-package com.tuotiansudai.service;
-
-import com.tuotiansudai.repository.model.UserOpLogModel;
-
-public interface UserOpLogService {
-    UserOpLogModel create(UserOpLogModel model);
-}
