d103978a57d2b510d6afc63902e183e824f9136f
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-17 16:12:27 -0700

    invest_circle_list

--- ttsd-service/src/main/java/com/tuotiansudai/service/LicaiquanArticleService.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/LicaiquanArticleService.java
@@ -1,10 +1,16 @@
 package com.tuotiansudai.service;
 
-
+import com.tuotiansudai.dto.LiCaiQuanArticleDto;
+import com.tuotiansudai.dto.BaseDto;
+import com.tuotiansudai.dto.PayDataDto;
 import com.tuotiansudai.repository.model.LicaiquanArticleModel;
 
 import java.util.List;
 
-public interface LicaiquanArticleService {
-    List<LicaiquanArticleModel> findLicaiquanArticleModel(String title,String section,Integer index);
+public interface LiCaiQuanArticleService {
+    BaseDto<PayDataDto> retrace(long articleId);
+
+    void createArticle(LiCaiQuanArticleDto liCaiQuanArticleDto);
+
+    List<LicaiquanArticleModel> findLicaiquanArticleModel(String title,String section);
 }
