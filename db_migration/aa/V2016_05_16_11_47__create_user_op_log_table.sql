3ca02084e3788603cfaf5141c5358cb366b7c370
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-05-18 17:19:12 +0800

    migrate history data for bind/replace card, open past_pay/no_password_invest agreement

--- db_migration/aa/V2016_05_16_11_47__create_user_op_log_table.sql
+++ db_migration/aa/V2016_05_16_11_47__create_user_op_log_table.sql
@@ -12,3 +12,31 @@ CREATE TABLE `aa`.`user_op_log` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
+BEGIN;
+
+insert into `aa`.`user_op_log` (login_name, op_type, ip, device_id, source, description, created_time)
+  SELECT a.login_name, 'BIND_CARD', '', '', '',
+    case when LOCATE('ret_code=0000', m.response_data) > 0 then 'Success' else 'Fail' end,
+    m.request_time
+  from `ump_operations`.`mer_bind_card_notify_request` m
+    join `aa`.`account` a on m.user_id = a.pay_user_id;
+
+insert into `aa`.`user_op_log` (login_name, op_type, ip, device_id, source, description, created_time)
+  SELECT a.login_name, 'NO_PASSWORD_AGREEMENT', '', '', '',
+    case when LOCATE('ret_code=0000', m.response_data) > 0 then 'Success' else 'Fail' end,
+    m.request_time
+  from `ump_operations`.`mer_bind_agreement_notify_request` m
+    join `aa`.`account` a on m.user_id = a.pay_user_id
+  where m.user_bind_agreement_list = 'ZTBB0G00,0000,签约成功|';
+
+insert into `aa`.`user_op_log` (login_name, op_type, ip, device_id, source, description, created_time)
+  SELECT a.login_name, 'FAST_PAY_AGREEMENT', '', '', '',
+    case when LOCATE('ret_code=0000', m.response_data) > 0 then 'Success' else 'Fail' end,
+    m.request_time
+  from `ump_operations`.`mer_bind_agreement_notify_request` m
+    join `aa`.`account` a on m.user_id = a.pay_user_id
+  where m.user_bind_agreement_list = 'ZKJP0700,0000,签约成功|';
+
+COMMIT;
+
