fca8a7f9a29e078a43899eb374aa29e8cb3848ca
zourenzheng <zourenzheng@tuotiansudai.com>
2016-05-23 16:54:11 +0800

    temp

--- ttsd-sign-in/src/main/java/com/tuotiansudai/signin/MyDaoAuthenticationProvider.java
+++ ttsd-sign-in/src/main/java/com/tuotiansudai/signin/MyDaoAuthenticationProvider.java
@@ -4,10 +4,12 @@ import com.tuotiansudai.exception.CaptchaNotMatchException;
 import com.tuotiansudai.repository.mapper.UserMapper;
 import com.tuotiansudai.repository.model.UserModel;
 import com.tuotiansudai.util.CaptchaHelper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -30,7 +32,6 @@ public class MyDaoAuthenticationProvider extends DaoAuthenticationProvider {
     @Override
     protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
         super.additionalAuthenticationChecks(userDetails, authentication);
-
         String loginName = userDetails.getUsername();
         UserModel userModel = userMapper.findByLoginName(loginName);
         boolean enabled = userModel.isActive();
@@ -38,16 +39,28 @@ public class MyDaoAuthenticationProvider extends DaoAuthenticationProvider {
             String errorMessage = MessageFormat.format("Login Error: {0} is locked!", loginName);
             throw new DisabledException(errorMessage);
         }
+    }
 
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
         if (enableCaptchaVerify) {
             String captcha = httpServletRequest.getParameter("captcha");
-            boolean result = this.captchaHelper.captchaVerify(CaptchaHelper.LOGIN_CAPTCHA, captcha);
-
-            if (!result) {
-                logger.debug("Authentication failed: captcha does not match actual value");
-                throw new CaptchaNotMatchException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.captchaNotMatch", "Captcha Not Match"));
+            if(StringUtils.isNotEmpty(captcha)) {
+                String deviceId = httpServletRequest.getParameter("j_deviceId");
+                boolean result;
+                if (StringUtils.isNotEmpty(deviceId)) {
+                    result = this.captchaHelper.captchaVerify(CaptchaHelper.LOGIN_CAPTCHA, captcha, deviceId);
+                } else {
+                    result = this.captchaHelper.captchaVerify(CaptchaHelper.LOGIN_CAPTCHA, captcha);
+                }
+                if (!result) {
+                    logger.debug("Authentication failed: captcha does not match actual value");
+                    throw new CaptchaNotMatchException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.captchaNotMatch", "Captcha Not Match"));
+                }
             }
         }
+        return super.authenticate(authentication);
     }
 
     public void setCaptchaHelper(CaptchaHelper captchaHelper) {
