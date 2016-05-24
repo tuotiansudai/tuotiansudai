37c00d9e0cf7fea528070e8f97ba32959fd8964b
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-24 17:43:16 +0800

    add task

--- ttsd-point/src/main/java/com/tuotiansudai/point/repository/model/PointTask.java
+++ ttsd-point/src/main/java/com/tuotiansudai/point/repository/model/PointTask.java
@@ -6,7 +6,18 @@ public enum PointTask {
     BIND_BANK_CARD("绑定银行卡","绑定您的常用银行卡"),
     FIRST_RECHARGE("首次充值","完成在平台的首次充值"),
     FIRST_INVEST("首次投资","完成在平台的首次投资"),
-    SUM_INVEST_10000("累计投资10000元","累计投资满10000元");
+    SUM_INVEST_10000("累计投资10000元","累计投资满10000元"),
+
+    EVERT_SUM_INVEST("累计投资满5000×n（n+1）÷2元奖励1000×n财豆","累计投资满5000×n（n+1）÷2元奖励1000×n财豆"),
+    EVERT_SINGLE_INVEST_10000("单笔投资满10000×n（n-1）元奖励1000×n财豆","单笔投资满10000×n（n-1）元奖励1000×n财豆"),
+    EVERY_INVITE_FRIEND("每邀请1名好友注册奖励200财豆","每邀请1名好友注册奖励200财豆"),
+    EVERY_INVITE_INVEST("每邀请好友投资满1000元奖励1000财豆","每邀请好友投资满1000元奖励1000财豆"),
+    FIRST_INVITE_FRIEND("首次邀请好友投资奖励5000财豆","首次邀请好友投资奖励5000财豆"),
+    FIRST_INVEST_180("首次投资180天标的奖励1000财豆","首次投资180天标的奖励1000财豆"),
+    FIRST_NO_PASSWORD_INVEST("首次开通免密支付并成功投资奖励1000财豆","首次开通免密支付并成功投资奖励1000财豆"),
+    FIRST_AUTO_INVEST("首次开通并成功自动投标奖励1000财豆","首次开通并成功自动投标奖励1000财豆"),
+    FIRST_INVEST_360("首次投资360天标的奖励1000财豆","首次投资360天标的奖励1000财豆");
+
 
     private final String title;
     private final String description;
