a00321540bfff8fbe0f21dcf979d3ec296f02292
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-05-25 17:14:03 +0800

    add log

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/security/MobileAppAccessDeniedHandler.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/security/MobileAppAccessDeniedHandler.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +28,16 @@ public class MobileAppAccessDeniedHandler extends AccessDeniedHandlerImpl {
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         BufferedRequestWrapper bufferedRequest = new BufferedRequestWrapper(request);
 
-        log.debug(MessageFormat.format("[Access Denied] uri: {0} body: {1}",request.getRequestURI(),bufferedRequest.getInputStreamString()));
+        String requestToken = bufferedRequest.getParameter("token");
+        if (StringUtils.isEmpty(requestToken)) {
+            log.debug(MessageFormat.format("[Access Denied] uri: {0} body: {1}", request.getRequestURI(), bufferedRequest.getInputStreamString()));
+        } else {
+            String redisLoginName = mobileAppTokenProvider.getUserNameByToken(requestToken);
+            if (null == redisLoginName) {
+                log.debug(MessageFormat.format("[Access Denied] uri: {0} body: {1} THIS TOKEN NOT IN REDIS!", request.getRequestURI(), bufferedRequest.getInputStreamString()));
+            }
+            log.debug(MessageFormat.format("[Access Denied] uri: {0} body: {1} redisTokenLoginName:{2}", request.getRequestURI(), bufferedRequest.getInputStreamString(), redisLoginName));
+        }
 
         BaseResponseDto dto = mobileAppTokenProvider.generateResponseDto(ReturnMessage.UNAUTHORIZED);
         response.setContentType("application/json; charset=UTF-8");
