cce9805d7ae57984d61774d3ac8b881ef392cbc4
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-25 14:14:29 +0800

    features/bs/task_base_date

--- db_migration/aa/V2016_05_25_12_03__add_new_point_task.sql
+++ db_migration/aa/V2016_05_25_12_03__add_new_point_task.sql
@@ -3,7 +3,7 @@ ALTER TABLE point_task ADD start_using BOOLEAN DEFAULT 0;
 ALTER TABLE point_task ADD repeat_status BOOLEAN DEFAULT 0;
 
 INSERT INTO `aa`.`point_task` VALUES (7,'EVERT_SUM_INVEST',5000,now(),1,1);
-INSERT INTO `aa`.`point_task` VALUES (8,'EVERT_SINGLE_INVEST',10000,now(),1,1);
+INSERT INTO `aa`.`point_task` VALUES (8,'FIRST_SINGLE_INVEST',10000,now(),1,0);
 INSERT INTO `aa`.`point_task` VALUES (9,'EVERY_INVITE_FRIEND',200,now(),1,1);
 INSERT INTO `aa`.`point_task` VALUES (10,'EVERY_INVITE_INVEST',1000,now(),1,1);
 INSERT INTO `aa`.`point_task` VALUES (11,'FIRST_INVITE_FRIEND',5000,now(),1,0);
