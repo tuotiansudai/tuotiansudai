3e6d2e6f872a6e41ce9d6391b94bc12b329559d4
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-05-25 20:10:46 +0800

    delete history data for feedback table

--- db_migration/aa/V2016_05_11_16_52__add_fields_in_feedback_table.sql
+++ db_migration/aa/V2016_05_11_16_52__add_fields_in_feedback_table.sql
@@ -3,3 +3,5 @@ ALTER TABLE `aa`.`feedback` ADD COLUMN `type` VARCHAR(20) NOT NULL AFTER `source
 ALTER TABLE `aa`.`feedback` ADD COLUMN `status` VARCHAR(10) NOT NULL AFTER `type`;
 ALTER TABLE `aa`.`feedback` CHANGE COLUMN `login_name` `login_name` VARCHAR(25);
 ALTER TABLE `aa`.`feedback` ADD COLUMN `contact` VARCHAR(50) AFTER `login_name`;
+
+DELETE FROM `aa`.`feedback`;
