0bd9657f1ade7afd436cdea6ee910f0dfb2accbc
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-05-26 20:16:24 +0800

    merge master

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/AboutController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/AboutController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
 @Controller
 @RequestMapping(path = "/about")
 public class AboutController {
@@ -23,19 +25,15 @@ public class AboutController {
     @Autowired
     private OperationDataService operationDataService;
 
-    @Autowired
-    private InfoPublishService infoPublishService;
-
     @RequestMapping(path = "/{item:^assurance|company|contact|guide|notice|media|notice-detail|service-fee|qa|refer-reward|team|operational$}", method = RequestMethod.GET)
-    public ModelAndView about(@PathVariable String item){
-        ModelAndView modelAndView =  new ModelAndView("/about/" + item);
-        modelAndView.addObject("responsive",true);
-        if("service-fee".equals(item)){
+    public ModelAndView about(@PathVariable String item) {
+        ModelAndView modelAndView = new ModelAndView("/about/" + item);
+        modelAndView.addObject("responsive", true);
+        if ("service-fee".equals(item)) {
             modelAndView.addObject("withdrawFee", AmountConverter.convertCentToString(withdrawFee));
         }
-        if("operational".equals(item))
-        {
-            modelAndView.addObject("investDetailList", infoPublishService.getInvestDetail());
+        if ("operational".equals(item)) {
+            modelAndView.addObject("investDetailList", operationDataService.getInvestDetail(new Date()));
         }
         return modelAndView;
     }
@@ -43,9 +41,7 @@ public class AboutController {
     @RequestMapping(path = "/operation-data/chart", method = RequestMethod.GET)
     @ResponseBody
     public OperationDataDto infoPublishChart() {
-        OperationDataDto operationDataDto = operationDataService.getOperationDataFromRedis();
-
-        return operationDataDto;
+        return operationDataService.getOperationDataFromRedis(new Date());
     }
 
 }
