aa25644c0b7962df9be45786630ad770e0190e2d
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-05-26 15:39:22 +0800

    update sql

--- db_migration/aa/V2016_05_25_12_03__add_column_point_task.sql
+++ db_migration/aa/V2016_05_25_12_03__add_column_point_task.sql
@@ -1,4 +1,7 @@
 ALTER TABLE point_task ADD active BOOLEAN DEFAULT 0;
 ALTER TABLE point_task ADD multiple BOOLEAN DEFAULT 0;
 ALTER TABLE user_point_task ADD point BIGINT UNSIGNED DEFAULT 0;
-ALTER TABLE user_point_task ADD task_level BIGINT UNSIGNED DEFAULT 0;
+ALTER TABLE user_point_task ADD COLUMN task_level BIGINT UNSIGNED NOT NULL AFTER point,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`login_name`, `point_task_id`, `task_level`);
+
