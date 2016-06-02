a9f467a670367d56505f2773618335ab48b7720c
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-06-02 11:58:48 +0800

    reformat code

--- db_migration/aa/V2016_05_31_19_13__create_blacklist_table.sql
+++ db_migration/aa/V2016_05_31_19_13__create_blacklist_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `aa`.`blacklist` (
   `id`           BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `login_name`   VARCHAR(25)      NOT NULL,
-  `created_time` DATETIME         NOT NULL,
-  `updated_time` DATETIME         NOT NULL,
-  `deleted`      TINYINT(1)       NOT NULL DEFAULT '0',
+  `login_name`   VARCHAR(25)         NOT NULL,
+  `created_time` DATETIME            NOT NULL,
+  `updated_time` DATETIME            NOT NULL,
+  `deleted`      TINYINT(1)          NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQUE_KEY_LOGIN_NAME` (`login_name`),
   CONSTRAINT `FK_BLACKLIST_LOGIN_NAME_REF_USER_LOGIN_NAME` FOREIGN KEY (`login_name`) REFERENCES `user` (`login_name`)
