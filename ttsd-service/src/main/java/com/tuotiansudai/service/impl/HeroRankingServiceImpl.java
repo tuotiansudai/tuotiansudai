c75eda9c1086d304c974c572c392d1cb29153795
zhanglong888 <zhanglong@tuotiansudai.com>
2016-06-03 19:28:51 +0800

    update ut

--- ttsd-service/src/main/java/com/tuotiansudai/service/impl/HeroRankingServiceImpl.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/impl/HeroRankingServiceImpl.java
@@ -12,9 +12,7 @@ import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -35,10 +33,6 @@ public class HeroRankingServiceImpl implements HeroRankingService {
         tradingTime = new DateTime(tradingTime).withTimeAtStartOfDay().plusDays(1).minusMillis(1).toDate();
 
         List<HeroRankingView> heroRankingViews = investMapper.findHeroRankingByTradingTime(tradingTime);
-
-        if(heroRankingViews.size() > 10){
-            return heroRankingViews.subList(0,10);
-        }
 
         return CollectionUtils.isNotEmpty(heroRankingViews) && heroRankingViews.size() > 10?heroRankingViews.subList(0,10):heroRankingViews;
     }
