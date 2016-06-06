5da73c6620b803e5781255a622ce7b39faa0b527
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-06-06 15:09:55 +0800

    bug fix

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppWithdrawController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppWithdrawController.java
@@ -1,8 +1,9 @@
 package com.tuotiansudai.api.controller.v1_0;
 
-import com.tuotiansudai.api.dto.v1_0.*;
+import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
+import com.tuotiansudai.api.dto.v1_0.WithdrawListRequestDto;
+import com.tuotiansudai.api.dto.v1_0.WithdrawOperateRequestDto;
 import com.tuotiansudai.api.service.v1_0.MobileAppWithdrawService;
-import com.tuotiansudai.service.BlacklistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,6 @@ public class MobileAppWithdrawController extends MobileAppBaseController {
     @Autowired
     private MobileAppWithdrawService mobileAppWithDrawService;
 
-    @Autowired
-    private BlacklistService blacklistService;
-
     @RequestMapping(value = "/get/userwithdrawlogs", method = RequestMethod.POST)
     public BaseResponseDto queryUserWithdrawLogs(@RequestBody WithdrawListRequestDto requestDto) {
         requestDto.getBaseParam().setUserId(getLoginName());
@@ -26,13 +24,6 @@ public class MobileAppWithdrawController extends MobileAppBaseController {
     @RequestMapping(value = "/withdraw", method = RequestMethod.POST)
     public BaseResponseDto generateWithdrawRequest(@RequestBody WithdrawOperateRequestDto requestDto) {
         requestDto.getBaseParam().setUserId(getLoginName());
-        String loginName = getLoginName();
-        if (blacklistService.userIsInBlacklist(loginName)) {
-            BaseResponseDto<BaseResponseDataDto> baseResponseDto = new BaseResponseDto<>();
-            baseResponseDto.setCode(ReturnMessage.WITHDRAW_IN_BLACKLIST.getCode());
-            baseResponseDto.setMessage(ReturnMessage.WITHDRAW_IN_BLACKLIST.getMsg());
-            return baseResponseDto;
-        }
         return mobileAppWithDrawService.generateWithdrawRequest(requestDto);
     }
 
