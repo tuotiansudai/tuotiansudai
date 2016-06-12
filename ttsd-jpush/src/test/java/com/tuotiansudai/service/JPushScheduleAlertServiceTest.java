6722a89c4000e47aa9256430317b9679bad48b34
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-06-12 11:42:18 +0800

    add test

--- ttsd-jpush/src/test/java/com/tuotiansudai/service/JPushScheduleAlertServiceTest.java
+++ ttsd-jpush/src/test/java/com/tuotiansudai/service/JPushScheduleAlertServiceTest.java
@@ -1,9 +1,11 @@
 package com.tuotiansudai.service;
 
 
+import cn.jpush.api.common.ClientConfig;
 import cn.jpush.api.common.TimeUnit;
 import cn.jpush.api.common.resp.APIConnectionException;
 import cn.jpush.api.common.resp.APIRequestException;
+import cn.jpush.api.common.resp.ResponseWrapper;
 import cn.jpush.api.push.model.Options;
 import cn.jpush.api.push.model.Platform;
 import cn.jpush.api.push.model.PushPayload;
@@ -16,25 +18,30 @@ import cn.jpush.api.schedule.ScheduleClient;
 import cn.jpush.api.schedule.ScheduleResult;
 import cn.jpush.api.schedule.model.SchedulePayload;
 import cn.jpush.api.schedule.model.TriggerPayload;
-import com.tuotiansudai.jpush.service.JPushScheduleAlertService;
+import com.tuotiansudai.client.RedisWrapperClient;
+import com.tuotiansudai.jpush.service.impl.JPushScheduleAlertServiceImpl;
 import com.tuotiansudai.repository.model.Environment;
 import com.tuotiansudai.util.UUIDGenerator;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,10 +50,11 @@ import static org.mockito.Mockito.when;
 public class JPushScheduleAlertServiceTest {
 
     @InjectMocks
-    private JPushScheduleAlertService jPushScheduleAlertService ;
-
+    private JPushScheduleAlertServiceImpl jPushScheduleAlertService ;
     @Mock
     private ScheduleClient scheduleClient = null;
+    @Mock
+    private RedisWrapperClient redisClient = null;
 
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
@@ -56,69 +64,68 @@ public class JPushScheduleAlertServiceTest {
     }
 
     @Test
-    @Ignore
     public void shouldSendJPushScheduleAlertIsOk() throws APIConnectionException, APIRequestException {
+        ReflectionTestUtils.setField(jPushScheduleAlertService, "scheduleClient" ,scheduleClient);
         String scheduleId = "testSchedule";
-        PushPayload pushPayload = PushPayload.newBuilder()
-                .setPlatform(Platform.ios())
-                .setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.tag("test")).build())
-                .setNotification(Notification.newBuilder()
-                        .addPlatformNotification(IosNotification.newBuilder().setAlert("手动推送测试").addExtra("jumpTo", 3).setBadge(0).build())
-                        .addPlatformNotification(AndroidNotification.newBuilder().setAlert("手动推送测试").addExtra("jumpTo", 3).setBuilderId(2).build())
-                        .build())
-                .setOptions(Options.newBuilder().setApnsProduction(Environment.isProduction(Environment.DEV)).build())
-                .build();
-
+        PushPayload pushPayload = createCommonPushPayLoad();
         Calendar now= Calendar.getInstance();
         now.add(Calendar.MINUTE,10);
         TriggerPayload triggerPayload = TriggerPayload.newBuilder().setSingleTime(sdf.format(now.getTimeInMillis())).buildSingle();
-
-        SchedulePayload.Builder schedulePayload = new SchedulePayload.Builder().setName("测试周期")
-                .setEnabled(true)
-                .setTrigger(triggerPayload).setPush(pushPayload);
-
-
         ScheduleResult scheduleResult = new ScheduleResult();
-
-        when(scheduleClient.createSchedule(schedulePayload.build())).thenReturn(scheduleResult);
-        when(scheduleClient.getSchedule(scheduleId)).thenReturn(scheduleResult);
-        when(scheduleClient.updateSchedule(scheduleId,schedulePayload.build())).thenReturn(scheduleResult);
-        when(scheduleClient.deleteSchedule(scheduleId))
+        ResponseWrapper responseWrapper = new ResponseWrapper();
+        responseWrapper.responseCode = 1;
+        responseWrapper.responseContent = "ok";
+        scheduleResult.setResponseWrapper(responseWrapper);
+        when(scheduleClient.createSchedule((SchedulePayload) anyObject())).thenReturn(scheduleResult);
+        when(scheduleClient.getSchedule(anyString())).thenReturn(scheduleResult);
+        when(scheduleClient.updateSchedule(anyString(), (SchedulePayload) anyObject())).thenReturn(scheduleResult);
+        when(redisClient.sadd(anyString(),anyString())).thenReturn(0l);
 
         scheduleResult = jPushScheduleAlertService.sendJPushScheduleAlert(scheduleId,pushPayload,triggerPayload);
-        assertNotNull(scheduleResult);
-
         scheduleResult = jPushScheduleAlertService.findPushScheduleAlert(scheduleResult.getSchedule_id());
-
         jPushScheduleAlertService.updatePushScheduleAlert(scheduleResult.getSchedule_id(),SchedulePayload.newBuilder().setName("修改测试类").build());
+        assertNotNull(scheduleResult);
+        ArgumentCaptor findId = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor argumentCaptorSchedulePayload = ArgumentCaptor.forClass(SchedulePayload.class);
+        verify(scheduleClient).createSchedule((SchedulePayload) argumentCaptorSchedulePayload.capture());
+        verify(scheduleClient).getSchedule((String)findId.capture());
+        verify(scheduleClient).updateSchedule((String)findId.capture(),(SchedulePayload) argumentCaptorSchedulePayload.capture());
     }
 
     @Test
-    @Ignore
-    public void shouldSendJPushPeriodicalScheduleAlertIsOk(){
-        PushPayload pushPayload = PushPayload.newBuilder()
-                .setPlatform(Platform.ios())
-                .setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.tag("test")).build())
-                .setNotification(Notification.newBuilder()
-                        .addPlatformNotification(IosNotification.newBuilder().setAlert("手动推送测试").addExtra("jumpTo", 3).setBadge(0).build())
-                        .addPlatformNotification(AndroidNotification.newBuilder().setAlert("手动推送测试").addExtra("jumpTo", 3).setBuilderId(2).build())
-                        .build())
-                .setOptions(Options.newBuilder().setApnsProduction(Environment.isProduction(Environment.DEV)).build())
-                .build();
-
+    public void shouldSendJPushPeriodicalScheduleAlertIsOk() throws APIConnectionException, APIRequestException {
+        PushPayload pushPayload = createCommonPushPayLoad();
         Calendar now= Calendar.getInstance();
         now.add(Calendar.MINUTE,10);
         String startTime = sdf.format(now.getTimeInMillis());
         now.add(Calendar.HOUR_OF_DAY,24);
         String endTime = sdf.format(now.getTimeInMillis());
+        ScheduleResult scheduleResult = new ScheduleResult();
 
         TriggerPayload triggerPayload = TriggerPayload.newBuilder().setPeriodTime(startTime,endTime,"10:11:00")
                 .setTimeFrequency(TimeUnit.DAY,1,null)
                 .buildPeriodical();
+        SchedulePayload.Builder schedulePayload = new SchedulePayload.Builder().setName("测试周期")
+                .setEnabled(true)
+                .setTrigger(triggerPayload).setPush(pushPayload);
+        when(scheduleClient.createSchedule(schedulePayload.build())).thenReturn(scheduleResult);
+        verify(scheduleClient).createSchedule(schedulePayload.build());
 
-        ScheduleResult scheduleResult = jPushScheduleAlertService.sendJPushScheduleAlert(UUIDGenerator.generate(),pushPayload,triggerPayload);
+        scheduleResult = jPushScheduleAlertService.sendJPushScheduleAlert(UUIDGenerator.generate(),pushPayload,triggerPayload);
         assertNotNull(scheduleResult);
-        jPushScheduleAlertService.deletePushScheduleAlert(scheduleResult.getSchedule_id());
+    }
+
+    private PushPayload createCommonPushPayLoad(){
+        PushPayload pushPayload = PushPayload.newBuilder()
+                .setPlatform(Platform.ios())
+                .setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.tag("test")).build())
+                .setNotification(Notification.newBuilder()
+                        .addPlatformNotification(IosNotification.newBuilder().setAlert("手动推送测试").addExtra("jumpTo", 3).setBadge(0).build())
+                        .addPlatformNotification(AndroidNotification.newBuilder().setAlert("手动推送测试").addExtra("jumpTo", 3).setBuilderId(2).build())
+                        .build())
+                .setOptions(Options.newBuilder().setApnsProduction(Environment.isProduction(Environment.DEV)).build())
+                .build();
+        return pushPayload;
     }
 
 }
