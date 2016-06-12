b018e8e278f2ad671731b5213475753cb9ce63b6
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-06-12 18:49:09 +0800

    add entry for app and web

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/HeroRankingController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/HeroRankingController.java
@@ -3,9 +3,11 @@ package com.tuotiansudai.web.controller;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.tuotiansudai.dto.BaseListDataDto;
+import com.tuotiansudai.repository.model.AccountModel;
 import com.tuotiansudai.repository.model.HeroRankingView;
 import com.tuotiansudai.service.HeroRankingService;
 import com.tuotiansudai.util.RandomUtils;
+import com.tuotiansudai.web.util.AppTokenParser;
 import com.tuotiansudai.web.util.LoginUserInfo;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +33,20 @@ public class HeroRankingController {
     @Autowired
     private RandomUtils randomUtils;
 
+    @Autowired
+    private AppTokenParser appTokenParser;
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView loadPageData(HttpServletRequest httpServletRequest) {
+        String loginName = appTokenParser.getLoginName(httpServletRequest);
+
+        ModelAndView modelAndView = new ModelAndView("/activities/hero-ranking");
+
+        // TODO: add page logic here
+
+        return modelAndView;
+    }
+
     @RequestMapping(value = "/referrer-invest/{tradingTime}", method = RequestMethod.GET)
     @ResponseBody
     public BaseListDataDto<HeroRankingView> obtainHeroRankingByReferrer(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date tradingTime) {
@@ -37,16 +54,16 @@ public class HeroRankingController {
     }
 
     @RequestMapping(value = "/invest/{tradingTime}", method = RequestMethod.GET)
-     @ResponseBody
-     public BaseListDataDto<HeroRankingView> obtainHeroRanking(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date tradingTime){
+    @ResponseBody
+    public BaseListDataDto<HeroRankingView> obtainHeroRanking(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date tradingTime) {
         final String loginName = LoginUserInfo.getLoginName();
         BaseListDataDto baseListDataDto = new BaseListDataDto();
         List<HeroRankingView> heroRankingViews = heroRankingService.obtainHeroRanking(tradingTime);
-        if(heroRankingViews != null){
-            baseListDataDto.setRecords(Lists.transform(heroRankingViews, new Function<HeroRankingView,HeroRankingView>() {
+        if (heroRankingViews != null) {
+            baseListDataDto.setRecords(Lists.transform(heroRankingViews, new Function<HeroRankingView, HeroRankingView>() {
                 @Override
                 public HeroRankingView apply(HeroRankingView heroRankingView) {
-                    heroRankingView.setLoginName(randomUtils.encryptLoginName(loginName,heroRankingView.getLoginName(),6));
+                    heroRankingView.setLoginName(randomUtils.encryptLoginName(loginName, heroRankingView.getLoginName(), 6));
                     return heroRankingView;
                 }
             }));
@@ -56,12 +73,12 @@ public class HeroRankingController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView obtainHeroRankingByLoginName(){
+    public ModelAndView obtainHeroRankingByLoginName() {
         ModelAndView mv = new ModelAndView();
         String loginName = LoginUserInfo.getLoginName();
         Date tradingTime = new DateTime().toDate();
-        Integer ranking = heroRankingService.obtainHeroRankingByLoginName(tradingTime,loginName);
-        mv.addObject("ranking",ranking);
+        Integer ranking = heroRankingService.obtainHeroRankingByLoginName(tradingTime, loginName);
+        mv.addObject("ranking", ranking);
         return mv;
     }
 
