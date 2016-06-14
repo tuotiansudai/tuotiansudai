3682cd0b9e2a682852495b87e553340cae61a211
xvqiang <xuqiang@tuotiansudai.com>
2016-06-14 19:23:50 +0800

    fix banner code

--- ttsd-web/src/main/webapp/js/index.js
+++ ttsd-web/src/main/webapp/js/index.js
@@ -1,4 +1,4 @@
-require(['jquery', 'underscore', 'jquery.ajax.extension', 'commonFun', 'coupon-alert', 'red-envelope-float', 'count_down'], function ($, _) {
+require(['jquery', 'underscore', 'superslide','touchslide','jquery.ajax.extension', 'commonFun', 'coupon-alert', 'red-envelope-float', 'count_down'], function ($, _) {
     $(function () {
         var $bannerBox = $('.banner-box'),
             $imgScroll = $('.banner-img-list', $bannerBox),
@@ -19,30 +19,7 @@ require(['jquery', 'underscore', 'jquery.ajax.extension', 'commonFun', 'coupon-a
             'font-size': '16px'
         });
 
-        screenWid = $(window).width(); //screen width
-        picWid = $bannerImg.first().find('img').width();
-
-        leftWid = (picWid - screenWid) / 2;
-
-        $scrollNum.css({'left': (screenWid - $scrollNum.find('li').length * 25) / 2, 'visibility': 'visible'});
-        $imgScroll.find('img').css({
-            'margin-left': '-' + leftWid + 'px'
-        });
-
-
-        $imgNum.click(function () {
-            var num_nav = $imgNum.index(this);
-            $(this).addClass("selected").siblings().removeClass("selected");
-            $bannerImg.eq(num_nav).fadeIn(1000).siblings().fadeOut(1000);
-        });
-        $bannerBox.hover(function () {
-            clearInterval(adTimer);
-        }, function () {
-            adTimer = setInterval(function () {
-                var index = ++n % $bannerImg.length;
-                $imgNum.eq(index).trigger('click');
-            }, 6000);
-        }).trigger('mouseleave');
+        
 
 
         $(".product-box .pad-m").click(function () {
@@ -53,37 +30,33 @@ require(['jquery', 'underscore', 'jquery.ajax.extension', 'commonFun', 'coupon-a
 
         if (viewport == 'pc') {
             $imgScroll.find('img.iphone-img').remove();
+            $("#bannerBox").slide({mainCell:".bd ul",effect:"left",autoPlay:true});
         } else if (viewport == 'mobile') {
             $imgScroll.find('img.pc-img').remove();
-            $imgScroll.find('img.iphone-img').css({'margin-left': '0px'});
+            screenWid = $(window).width(); //screen width
+            picWid = $bannerImg.first().find('img').width();
 
-            // 移动端滑动切换
-            (function() {
-                var startX, startY;
-                $bannerBox.on('touchstart', function(event) {
-                    startX = event.originalEvent.changedTouches[0].pageX;
-                    startY = event.originalEvent.changedTouches[0].pageY;
-                });
-                $bannerBox.on('touchmove', function(event) {
-                    event.preventDefault();
-                })
-                $bannerBox.on('touchend', function(event) {
-                    var endX, endY;
-                     endX = event.originalEvent.changedTouches[0].pageX;
-                     endY = event.originalEvent.changedTouches[0].pageY;
-                     var direction = GetSlideDirection(startX, startY, endX, endY);
-                     switch(direction) {
-                         case 3:
-                            var index = ++n % $bannerImg.length;
-                            $imgNum.eq(index).trigger('click');
-                             break;
-                         case 4:
-                            var index = --n % $bannerImg.length;
-                            $imgNum.eq(index).trigger('click');
-                             break;
-                     }
-                });
-            })();
+            leftWid = (picWid - screenWid) / 2;
+
+            $scrollNum.css({'left': (screenWid - $scrollNum.find('li').length * 25) / 2, 'visibility': 'visible'});
+            $imgScroll.find('img').css({
+                'margin-left': '-' + leftWid + 'px'
+            });
+
+
+            $imgNum.click(function () {
+                var num_nav = $imgNum.index(this);
+                $(this).addClass("selected").siblings().removeClass("selected");
+                $bannerImg.eq(num_nav).fadeIn(1000).siblings().fadeOut(1000);
+            });
+            $bannerBox.hover(function () {
+                clearInterval(adTimer);
+            }, function () {
+                adTimer = setInterval(function () {
+                    var index = ++n % $bannerImg.length;
+                    $imgNum.eq(index).trigger('click');
+                }, 6000);
+            }).trigger('mouseleave');
         }
 
         var scrollTimer;
