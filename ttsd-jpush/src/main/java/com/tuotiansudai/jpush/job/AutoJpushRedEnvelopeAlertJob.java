05353ae01362ec03cb081c2836c3573753050fc2
Yong Zhang <zhangyong@tuotiansudai.com>
2016-06-16 10:49:24 +0800

    update auto jpush red envelope alert job

--- ttsd-jpush/src/main/java/com/tuotiansudai/jpush/job/AutoJpushRedEnvelopeAlertJob.java
+++ ttsd-jpush/src/main/java/com/tuotiansudai/jpush/job/AutoJpushRedEnvelopeAlertJob.java
@@ -8,7 +8,9 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AutoJPushRedEnvelopeAlertJob implements Job {
 
     static Logger logger = Logger.getLogger(AutoJPushRedEnvelopeAlertJob.class);
