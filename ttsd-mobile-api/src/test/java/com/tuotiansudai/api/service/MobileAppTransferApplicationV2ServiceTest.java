a3bd0330306934d23145f9638fc862c185a2e00c
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-06-16 15:06:30 +0800

    update test

--- ttsd-mobile-api/src/test/java/com/tuotiansudai/api/service/MobileAppTransferApplicationV2ServiceTest.java
+++ ttsd-mobile-api/src/test/java/com/tuotiansudai/api/service/MobileAppTransferApplicationV2ServiceTest.java
@@ -110,7 +110,6 @@ public class MobileAppTransferApplicationV2ServiceTest extends ServiceTestBase {
         loanDto.setDescriptionText("asdfasd");
         loanDto.setFundraisingEndTime(new Date());
         loanDto.setFundraisingStartTime(new Date());
-//        loanDto.setInvestFeeRate("15");
         loanDto.setInvestIncreasingAmount("1");
         loanDto.setLoanAmount("10000");
         loanDto.setType(LoanType.LOAN_INTEREST_MONTHLY_REPAY);
--- ttsd-service/src/test/java/com/tuotiansudai/service/HeroRankingServiceTest.java
+++ ttsd-service/src/test/java/com/tuotiansudai/service/HeroRankingServiceTest.java
@@ -181,7 +181,6 @@ public class HeroRankingServiceTest {
         loanDto.setDescriptionText("asdfasd");
         loanDto.setFundraisingEndTime(new Date());
         loanDto.setFundraisingStartTime(new Date());
-//        loanDto.setInvestFeeRate("15");
         loanDto.setInvestIncreasingAmount("1");
         loanDto.setLoanAmount("10000");
         loanDto.setType(LoanType.INVEST_INTEREST_MONTHLY_REPAY);
