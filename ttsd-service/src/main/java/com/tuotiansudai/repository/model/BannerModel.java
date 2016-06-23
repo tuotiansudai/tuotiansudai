688c4f3b1a1cade2d6e5c9b3d4164d8ec98e6892
zourenzheng <zourenzheng@tuotiansudai.com>
2016-06-23 13:32:14 +0800

    banner sql

---
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/model/BannerModel.java
@@ -0,0 +1,180 @@
+package com.tuotiansudai.repository.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class BannerModel implements Serializable{
+
+    private long id;
+
+    private String name;
+
+    private String webImageUrl;
+
+    private String appImageUrl;
+
+    private String url;
+
+    private String content;
+
+    private List<Source> source;
+
+    private boolean isAuthenticated;
+
+    private int order;
+
+    private boolean active;
+
+    private String createdBy;
+
+    private Date createdTime = new Date();
+
+    private String activatedBy;
+
+    private Date activatedTime;
+
+    private String deactivatedBy;
+
+    private Date deactivatedTime;
+
+    private boolean isDeleted = false;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebImageUrl() {
+        return webImageUrl;
+    }
+
+    public void setWebImageUrl(String webImageUrl) {
+        this.webImageUrl = webImageUrl;
+    }
+
+    public String getAppImageUrl() {
+        return appImageUrl;
+    }
+
+    public void setAppImageUrl(String appImageUrl) {
+        this.appImageUrl = appImageUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Source> getSource() {
+        return source;
+    }
+
+    public void setSource(List<Source> source) {
+        this.source = source;
+    }
+
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    public void setAuthenticated(boolean isAuthenticated) {
+        this.isAuthenticated = isAuthenticated;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getActivatedBy() {
+        return activatedBy;
+    }
+
+    public void setActivatedBy(String activatedBy) {
+        this.activatedBy = activatedBy;
+    }
+
+    public Date getActivatedTime() {
+        return activatedTime;
+    }
+
+    public void setActivatedTime(Date activatedTime) {
+        this.activatedTime = activatedTime;
+    }
+
+    public String getDeactivatedBy() {
+        return deactivatedBy;
+    }
+
+    public void setDeactivatedBy(String deactivatedBy) {
+        this.deactivatedBy = deactivatedBy;
+    }
+
+    public Date getDeactivatedTime() {
+        return deactivatedTime;
+    }
+
+    public void setDeactivatedTime(Date deactivatedTime) {
+        this.deactivatedTime = deactivatedTime;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+}
