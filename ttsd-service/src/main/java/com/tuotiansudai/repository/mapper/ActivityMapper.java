454e7a63def29f695b9bb33afee80f17dbcef3e3
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-06-23 17:30:31 +0800

    add mapper interface

--- ttsd-service/src/main/java/com/tuotiansudai/repository/mapper/ActivityMapper.java
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/mapper/ActivityMapper.java
@@ -1,4 +1,18 @@
 package com.tuotiansudai.repository.mapper;
 
-public class ActivityMapper {
+import com.tuotiansudai.repository.model.ActivityModel;
+import com.tuotiansudai.repository.model.ActivityStatus;
+import com.tuotiansudai.repository.model.Source;
+
+import java.util.Date;
+import java.util.List;
+
+public interface ActivityMapper {
+    void create(ActivityModel activityModel);
+
+    void update(ActivityModel activityModel);
+
+    ActivityModel findById(long id);
+
+    List<ActivityModel> findActivities(Date ActivatedStartTime, Date ActivatedEndTime, ActivityStatus activityStatus, Source source);
 }
