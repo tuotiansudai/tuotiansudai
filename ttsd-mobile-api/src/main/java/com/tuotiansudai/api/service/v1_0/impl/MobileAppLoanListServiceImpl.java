bab5ef26b2eb0d84b964e1ea3ac598143f609d74
zourenzheng <zourenzheng@tuotiansudai.com>
2016-06-27 12:25:55 +0800

    bug

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppLoanListServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppLoanListServiceImpl.java
@@ -18,6 +18,7 @@ import com.tuotiansudai.util.AmountConverter;
 import org.apache.commons.collections4.CollectionUtils;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
