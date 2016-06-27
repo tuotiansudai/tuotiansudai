9b04ae869083a7688cdfe5a54e738c72743e442b
zhanglong888 <zhanglong@tuotiansudai.com>
2016-06-27 11:59:54 +0800

    update test

---
+++ ttsd-service/src/main/java/com/tuotiansudai/util/mybatis/SourceListTypeHandler.java
@@ -0,0 +1,60 @@
+package com.tuotiansudai.util.mybatis;
+
+import com.google.common.base.Joiner;
+import com.tuotiansudai.repository.model.Source;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SourceListTypeHandler extends BaseTypeHandler<List<Source>> {
+
+    public SourceListTypeHandler() {
+        super();
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, List<Source> parameter, JdbcType jdbcType) throws SQLException {
+        if (jdbcType == null) {
+            ps.setString(i, Joiner.on(",").join(parameter));
+        } else {
+            ps.setObject(i, Joiner.on(",").join(parameter), jdbcType.TYPE_CODE); // see r3589
+        }
+    }
+
+    @Override
+    public List<Source> getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String s = rs.getString(columnName);
+        return this.convertToSource(s);
+    }
+
+    @Override
+    public List<Source> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String s = rs.getString(columnIndex);
+        return this.convertToSource(s);
+    }
+
+    @Override
+    public List<Source> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String s = cs.getString(columnIndex);
+        return this.convertToSource(s);
+    }
+
+    private List<Source> convertToSource(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        List<Source> sourceList = new ArrayList<>();
+        String[] sourceStr = s.split(",");
+        for (String type : sourceStr) {
+            sourceList.add(Enum.valueOf(Source.class, type));
+        }
+        return sourceList;
+    }
+}
