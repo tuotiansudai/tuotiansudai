c33e0cc36b928ae6e7fdb4ea777746ece97b7652
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-06-27 20:01:02 +0800

    add audit log for activity

--- ttsd-console/src/main/java/com/tuotiansudai/console/controller/ActivityCenterController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/ActivityCenterController.java
@@ -5,6 +5,7 @@ import com.tuotiansudai.dto.ActivityDto;
 import com.tuotiansudai.repository.model.ActivityModel;
 import com.tuotiansudai.repository.model.ActivityStatus;
 import com.tuotiansudai.service.ActivityService;
+import com.tuotiansudai.util.RequestIPParser;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -38,10 +41,11 @@ public class ActivityCenterController {
     }
 
     @RequestMapping(value = "/activity-center/{activityStatus}",method = RequestMethod.POST)
-    public ModelAndView activityCenter(@ModelAttribute ActivityDto activityDto, @PathVariable ActivityStatus activityStatus){
+    public ModelAndView activityCenter(@ModelAttribute ActivityDto activityDto, @PathVariable ActivityStatus activityStatus, HttpServletRequest request){
 
+        String ip = RequestIPParser.parse(request);
         String loginName = LoginUserInfo.getLoginName();
-        activityService.createEditRecheckActivity(activityDto,activityStatus,loginName);
+        activityService.createEditRecheckActivity(activityDto,activityStatus,loginName,ip);
 
         return new ModelAndView("redirect:/activity-manage/activity-center");
     }
---
+++ ttsd-console/src/main/java/com/tuotiansudai/task/aspect/AuditTaskAspectActivity.java
@@ -0,0 +1,67 @@
+package com.tuotiansudai.task.aspect;
+
+
+import com.tuotiansudai.dto.ActivityDto;
+import com.tuotiansudai.repository.mapper.ActivityMapper;
+import com.tuotiansudai.repository.model.ActivityModel;
+import com.tuotiansudai.repository.model.ActivityStatus;
+import com.tuotiansudai.service.AccountService;
+import com.tuotiansudai.service.AuditLogService;
+import com.tuotiansudai.task.OperationType;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AuditTaskAspectActivity {
+
+
+    @Autowired
+    private AuditLogService auditLogService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    ActivityMapper activityMapper;
+
+    static Logger logger = Logger.getLogger(AuditTaskAspectLoan.class);
+
+    @AfterReturning(value = "execution(* com.tuotiansudai.service.ActivityService.createEditRecheckActivity(..))", returning = "returnValue")
+    public void afterCreateEditRecheckActivity(JoinPoint joinPoint, Object returnValue) {
+        logger.debug("after create edit recheck activity aspect.");
+        try {
+            ActivityDto activityDto = (ActivityDto) joinPoint.getArgs()[0];
+            ActivityStatus activityStatus = (ActivityStatus) joinPoint.getArgs()[1];
+            String loginName = (String) joinPoint.getArgs()[2];
+            String ip = (String) joinPoint.getArgs()[3];
+
+            String realName = accountService.getRealName(loginName);
+
+            ActivityModel activityModelExist = activityMapper.findById(activityDto.getId());
+            String description;
+
+            String creator = activityModelExist.getCreatedBy();
+            String creatorRealName = accountService.getRealName(creator);
+
+            switch (activityStatus) {
+                case TO_APPROVE:
+                    description = realName + " 创建/编辑了活动［" + activityDto.getTitle() + "］。";
+                    auditLogService.createAuditLog(null, loginName, OperationType.ACTIVITY, String.valueOf(activityDto.getId()), description, ip);
+                    break;
+                case REJECTION:
+                    description = realName + " 驳回了 " + creatorRealName + " 创建的活动［" + activityDto.getTitle() + "］。";
+                    auditLogService.createAuditLog(null, loginName, OperationType.ACTIVITY, String.valueOf(activityDto.getId()), description, ip);
+                case APPROVED:
+                    description = realName + " 审核通过了 " + creatorRealName + " 创建的活动［" + activityDto.getTitle() + "］。";
+                    auditLogService.createAuditLog(null, loginName, OperationType.ACTIVITY, String.valueOf(activityDto.getId()), description, ip);
+            }
+        } catch (Exception e) {
+            logger.error("after create edit recheck activity aspect fail ", e);
+        }
+    }
+}
