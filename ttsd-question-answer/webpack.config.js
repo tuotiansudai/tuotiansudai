4e6a90635ea591991d05b8eb2e1634302bf5d970
fulanyan <fly39244080@163.com>
2016-06-29 19:16:36 +0800

    make some config

--- ttsd-question-answer/webpack.config.js
+++ ttsd-question-answer/webpack.config.js
@@ -1,22 +1,63 @@
-module.exports = {
-    entry: {
-        app: ['webpack/hot/dev-server', './main.js']
-    },
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css']
+var path = require('path');
+var os = require('os');
+var webpack = require('webpack');
+var basePath = path.join(__dirname, 'src/main/webapp/');
+var port = 8080;
+var getIP = function() {
+    var ipList = os.networkInterfaces();
+    var result = 'localhost';
+    for (var key in ipList) {
+        if (/^e/.test(key)) {
+            for (var i = ipList[key].length - 1; i >= 0; i--) {
+                if (/192\.168/.test(ipList[key][i].address)) {
+                    result = ipList[key][i].address;
+                    break;
+                }
             }
-        ]
+        }
+    }
+    return result;
+};
+
+var IP = getIP();
+console.log('IP:', IP);
+
+module.exports = {
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://' + IP + ':' + port,
+        path.join(basePath, 'js/main.js')
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(basePath, 'dist'),
+        publicPath: '/assets/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+      ],
+    cache: true,
+    devServer: {
+        contentBase: basePath,
+        historyApiFallback: true,
+        watchOptions: {
+           aggregateTimeout: 300,
+           poll: 1000
+        },
+        hot: true,
+        port: port,
+        publicPath: '/templates/',
+        noInfo: false,
+        proxy: {
+          '/src/main*': {
+            target: 'https://other-server.example.com',
+            secure: false,
+            bypass: function(req, res, proxyOptions) {
+              if (req.headers.accept.indexOf('html') !== -1) {
+                console.log('Skipping proxy for browser request.');
+                return '/index.html';
+            }
+          }
+        }
     }
 };
