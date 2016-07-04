e09ec0f455b05c20285f3cfd626a88ec00f0b81c
fulanyan <fly39244080@163.com>
2016-07-04 11:24:13 +0800

    remove test demo

--- ttsd-question-answer/src/main/webapp/js/app.js
+++ ttsd-question-answer/src/main/webapp/js/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router';
 export default class App extends Component {
   render() {
     return (
-      <h1>I come from hubei</h1>
+      <div>
+        <header>
+          <ul>
+            <li><Link to="/app">Dashboard</Link></li>
+            <li><Link to="/inbox">Inbox</Link></li>
+            <li><Link to="/calendar">Calendar</Link>  Logged in as Jane</li>
+          </ul>
+          
+        </header>
+        {this.props.children}
+      </div>
     );
   }
 }
+
+
--- ttsd-question-answer/src/main/webapp/js/main.jsx
+++ ttsd-question-answer/src/main/webapp/js/main.jsx
@@ -1,20 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
 import App from './app';
-// const React = require('react');
-// const ReactDOM = require('react-dom');
-
 
 var style = require('../style/sass/main.scss'); 
-$('#app').text('lanyan');
-
-var img1 = document.createElement("img");
-img1.src = require("./dou.png");
-document.body.appendChild(img1);
 
 
-ReactDOM.render(
-  <App/>,
-  document.querySelector('#wrapper')
-);
+// var img1 = document.createElement("img");
+// img1.src = require("./dou.png");
+// document.body.appendChild(img1);
+
+var Dashboard = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>hello Dashboard</p>
+      </div>
+    );
+  }
+});
+
+var Inbox = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>hello Inbox</p>
+      </div>
+    );
+  }
+});
+
+var Calendar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>hello Calendar</p>
+      </div>
+    );
+  }
+});
+
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Dashboard}/>
+      <Route path="app" component={Dashboard}/>
+      <Route path="inbox" component={Inbox}/>
+      <Route path="calendar" component={Calendar}/>
+      <Route path="*" component={Dashboard}/>
+    </Route>
+  </Router>
+), document.querySelector('#wrapper'));
+
+
 
