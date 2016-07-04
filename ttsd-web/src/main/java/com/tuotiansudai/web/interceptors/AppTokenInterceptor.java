15d67e8cd527d6d91dc23174369a24b4f1c91b9c
zhanglong888 <zhanglong@tuotiansudai.com>
2016-07-04 14:19:29 +0800

    features/zl/app_token_obtain

--- ttsd-web/src/main/java/com/tuotiansudai/web/interceptors/AppTokenInterceptor.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/interceptors/AppTokenInterceptor.java
@@ -3,13 +3,16 @@ package com.tuotiansudai.web.interceptors;
 import com.google.common.base.Strings;
 import com.tuotiansudai.client.RedisWrapperClient;
 import com.tuotiansudai.security.MyAuthenticationManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.MessageFormat;
 
 public class AppTokenInterceptor extends HandlerInterceptorAdapter {
+    static Logger logger = Logger.getLogger(AppTokenInterceptor.class);
     @Autowired
     private RedisWrapperClient redisWrapperClient;
     @Autowired
@@ -18,8 +21,13 @@ public class AppTokenInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         String token = Strings.isNullOrEmpty(request.getHeader("token"))?request.getParameter("token"):request.getHeader("token");
+        if (Strings.isNullOrEmpty(token)) {
+            logger.debug(MessageFormat.format("url:{0},uri:{1} , token is null",request.getRequestURL(),request.getRequestURI()));
+            myAuthenticationManager.removeAuthentication();
+            return true;
+        }
 
-        String loginName = Strings.isNullOrEmpty(token)?"":redisWrapperClient.get(token);
+        String loginName = redisWrapperClient.get(token);
 
         if (Strings.isNullOrEmpty(loginName)) {
             myAuthenticationManager.removeAuthentication();
