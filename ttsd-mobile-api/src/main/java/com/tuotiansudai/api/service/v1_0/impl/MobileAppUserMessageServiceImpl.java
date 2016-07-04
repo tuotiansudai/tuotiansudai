f7681dd201625b4dd576e9bb478f7873e8a21d8d
Yong Zhang <zhangyong@tuotiansudai.com>
2016-07-04 11:23:25 +0800

    add unread message count

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppUserMessageServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppUserMessageServiceImpl.java
@@ -5,11 +5,13 @@ import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.tuotiansudai.api.dto.v1_0.*;
 import com.tuotiansudai.api.service.v1_0.MobileAppUserMessageService;
+import com.tuotiansudai.client.RedisWrapperClient;
 import com.tuotiansudai.message.repository.mapper.UserMessageMapper;
 import com.tuotiansudai.message.repository.model.MessageChannel;
 import com.tuotiansudai.message.repository.model.UserMessageModel;
 import com.tuotiansudai.message.service.UserMessageService;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,11 @@ public class MobileAppUserMessageServiceImpl implements MobileAppUserMessageServ
 
     @Autowired
     private UserMessageMapper userMessageMapper;
+
+    @Autowired
+    private RedisWrapperClient redisClient;
+
+    public static final String UNREAD_MESSAGE_COUNT_ID_KEY = "app:unread:message:count:ids:";
 
     @Override
     public BaseResponseDto getUserMessages(UserMessagesRequestDto requestDto) {
@@ -42,9 +49,11 @@ public class MobileAppUserMessageServiceImpl implements MobileAppUserMessageServ
     @Override
     public BaseResponseDto getUnreadMessageCount(BaseParamDto baseParamDto) {
         String loginName = baseParamDto.getBaseParam().getUserId();
-        long unreadMessageCount = userMessageMapper.countUnreadMessagesByLoginName(loginName, MessageChannel.APP);
-
+        long currentUnreadMessageCount = userMessageMapper.countUnreadMessagesByLoginName(loginName, MessageChannel.APP);
+        boolean existUnreadMessage = existUnreadMessage(loginName, currentUnreadMessageCount);
         MobileAppUnreadMessageCount messageCount = new MobileAppUnreadMessageCount();
+        messageCount.setUnreadMessageCount(currentUnreadMessageCount);
+        messageCount.setNewMessage(existUnreadMessage);
         BaseResponseDto responseDto = new BaseResponseDto();
         responseDto.setData(messageCount);
         return responseDto;
@@ -66,6 +75,17 @@ public class MobileAppUserMessageServiceImpl implements MobileAppUserMessageServ
         responseDataDto.setTotalCount(totalCount);
         responseDataDto.setMessages(userMessages);
         return responseDataDto;
+    }
+
+    private boolean existUnreadMessage(String loginName, long currentUnreadMessageCount) {
+        String unreadMessageCountValue = redisClient.get(UNREAD_MESSAGE_COUNT_ID_KEY + loginName);
+        long lastUnreadMessageCount = StringUtils.isNotEmpty(unreadMessageCountValue) ? 0 : Long.parseLong(unreadMessageCountValue);
+        if (lastUnreadMessageCount == currentUnreadMessageCount) {
+            return false;
+        } else {
+            redisClient.set(unreadMessageCountValue, String.valueOf(currentUnreadMessageCount));
+            return true;
+        }
     }
 
 }
