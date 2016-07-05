2537a80ef7c3e55e33c6b4c4792dc217da73492a
fulanyan <fly39244080@163.com>
2016-07-05 19:15:01 +0800

    fix scroll

--- ttsd-mobile-api/src/main/webapp/feSrc/js/components/taskCenter/taskCenter.jsx
+++ ttsd-mobile-api/src/main/webapp/feSrc/js/components/taskCenter/taskCenter.jsx
@@ -80,7 +80,7 @@ class NewbieTaskGroup extends React.Component {
                 <span className="HeaderTitle">新手任务</span>
                 <img src={taskLineRight}/>
             </div>
-            <div className="scroll-wrap" ref="scrollWrap">
+            <div className="scroll-wrap clearfix" ref="scrollWrap">
                 {rows}
                 </div>
             </div>
@@ -121,7 +121,7 @@ class AdvanceTaskGroup extends React.Component {
                 <img src={taskLineRight} />
             </div>
 
-            <div className="scroll-wrap" ref="scrollOngoing">
+            <div className="scroll-wrap clearfix" ref="scrollOngoing">
                {rows}
             </div>
             
@@ -242,13 +242,13 @@ class taskCenter extends React.Component {
 			    <div className="MenuBox" ref="tabHeader">
 			        <ul>
                         {MenuData.tabHeader.map((value, index) => {
-                            return <li className={classNames({ 'MenuBoxItemNormal': true, active: this.state.active === value.value })} key={index} data-value={value.value} onClick={this.tabHeaderClickHandler.bind(this)}>{value.label}</li>;
+                            return <li className={classNames({ 'MenuBoxItemNormal': true, active: this.state.active === value.value })} key={index} data-value={value.value} onTouchTap={this.tabHeaderClickHandler.bind(this)}>{value.label}</li>;
                         })}
 			        </ul>
 			    </div>
 		
 			<div className="ContentBox" ref="scrollWrap">
-			<div id="OngoingBox" className="OngoingBox" >
+			<div id="OngoingBox" className="OngoingBox clearfix" >
             {loading}
 			<NewbieTaskGroup data={this.state.listData.newbieTasks} jumpToEvent={this.jumpTo} />
 
