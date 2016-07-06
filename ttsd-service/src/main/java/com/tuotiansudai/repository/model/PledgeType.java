e018d8bbd95f96d6407939631f4f31ca8e625674
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-07-06 20:02:37 +0800

    create enum

---
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/model/PledgeType.java
@@ -0,0 +1,20 @@
+package com.tuotiansudai.repository.model;
+
+public enum PledgeType {
+    HOUSE("房产抵押物"),
+    VEHICLE("车辆抵押物");
+
+    private String description;
+
+    PledgeType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
