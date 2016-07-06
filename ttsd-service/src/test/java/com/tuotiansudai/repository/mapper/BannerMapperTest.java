60ecfc5d492c1d315ff5c4ca63af1a8b41bb9656
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-06 19:12:35 +0800

    add test

--- ttsd-service/src/test/java/com/tuotiansudai/repository/mapper/BannerMapperTest.java
+++ ttsd-service/src/test/java/com/tuotiansudai/repository/mapper/BannerMapperTest.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Date;
 import java.util.List;
-
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
