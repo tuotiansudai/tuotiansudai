aa4ef6389f6baf8bcf4a7d38e934814d2583adc7
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-07 18:10:07 +0800

    update

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBannerServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBannerServiceImpl.java
@@ -1,10 +1,9 @@
 package com.tuotiansudai.api.service.v1_0.impl;
 
-import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.tuotiansudai.api.dto.v1_0.*;
+import com.tuotiansudai.api.security.MobileAppTokenProvider;
 import com.tuotiansudai.api.service.v1_0.MobileAppBannerService;
-import com.tuotiansudai.api.util.BannerUtils;
 import com.tuotiansudai.repository.mapper.BannerMapper;
 import com.tuotiansudai.repository.model.BannerModel;
 import com.tuotiansudai.repository.model.Source;
@@ -26,9 +25,16 @@ public class MobileAppBannerServiceImpl implements MobileAppBannerService {
     @Value("${web.banner.server}")
     private String bannerName;
 
+    @Autowired
+    private MobileAppTokenProvider mobileAppTokenProvider;
+
     @Override
     public BaseResponseDto<BannerResponseDataDto> generateBannerList(BaseParam baseParam) {
-        List<BannerModel> bannerModelList = bannerMapper.findBannerIsAuthenticatedOrderByOrder(Strings.isNullOrEmpty(baseParam.getUserId()) ? false : true,baseParam.getPlatform().toUpperCase().equals(Source.ANDROID.name()) ? Source.ANDROID : Source.IOS);
+        boolean isAuthenticated = false;
+        if(baseParam.getToken() != null &&  !mobileAppTokenProvider.getUserNameByToken(baseParam.getToken()).isEmpty()){
+            isAuthenticated = true;
+        }
+        List<BannerModel> bannerModelList = bannerMapper.findBannerIsAuthenticatedOrderByOrder(isAuthenticated,baseParam.getPlatform().toUpperCase().equals(Source.ANDROID.name()) ? Source.ANDROID : Source.IOS);
         BannerResponseDataDto bannerResponseDataDto = new BannerResponseDataDto();
         List<BannerPictureResponseDataDto> pictures = Lists.newArrayList();
         bannerResponseDataDto.setPictures(pictures);
