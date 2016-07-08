791d351fa9d9ef8b42a9df455a2e7b2a987bd851
Yong Zhang <zhangyong@tuotiansudai.com>
2016-07-08 15:23:27 +0800

    add image captcha

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v2_0/MobileAppImageCaptchaV2Controller.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v2_0/MobileAppImageCaptchaV2Controller.java
@@ -1,9 +1,10 @@
-package com.tuotiansudai.api.controller.v1_0;
+package com.tuotiansudai.api.controller.v2_0;
 
-import com.tuotiansudai.api.dto.v1_0.BaseParamDto;
-import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
-import com.tuotiansudai.api.dto.v1_0.ImageCaptchaResponseDataDto;
-import com.tuotiansudai.api.dto.v1_0.ReturnMessage;
+
+import com.tuotiansudai.api.dto.v2_0.BaseParamDto;
+import com.tuotiansudai.api.dto.v2_0.BaseResponseDto;
+import com.tuotiansudai.api.dto.v2_0.ImageCaptchaResponseDataDto;
+import com.tuotiansudai.api.dto.v2_0.ReturnMessage;
 import com.tuotiansudai.util.CaptchaGenerator;
 import com.tuotiansudai.util.CaptchaHelper;
 import nl.captcha.Captcha;
@@ -14,27 +15,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MobileAppImageCaptchaController extends MobileAppBaseController {
-
+public class MobileAppImageCaptchaV2Controller extends MobileAppBaseController {
 
     @Autowired
     private CaptchaHelper captchaHelper;
 
     @RequestMapping(value = "/image-captcha", method = RequestMethod.POST)
-    public BaseResponseDto loginCaptcha(@RequestBody BaseParamDto baseParamDto) {
+    public BaseResponseDto getImageCaptcha(@RequestBody BaseParamDto baseParamDto) {
         int captchaWidth = 80;
         int captchaHeight = 30;
         Captcha captcha = CaptchaGenerator.generate(captchaWidth, captchaHeight);
 
-        captchaHelper.storeCaptcha(CaptchaHelper.LOGIN_CAPTCHA, captcha.getAnswer(), baseParamDto.getBaseParam().getDeviceId());
+        captchaHelper.storeCaptcha(CaptchaHelper.BASIC_CAPTCHA, captcha.getAnswer(), baseParamDto.getBaseParam().getDeviceId());
 
         String imageCaptcha = captchaHelper.transferImageToBase64(captcha.getImage());
-
         BaseResponseDto baseResponseDto = new BaseResponseDto();
         baseResponseDto.setCode(ReturnMessage.SUCCESS.getCode());
         baseResponseDto.setMessage(ReturnMessage.SUCCESS.getMsg());
         baseResponseDto.setData(new ImageCaptchaResponseDataDto(imageCaptcha));
         return baseResponseDto;
-    }
 
+    }
 }
