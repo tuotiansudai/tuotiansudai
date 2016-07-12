75667bd0772a62e344a82d7555320f367bf7345d
Xiduan Gao <gaoxiduan@gmail.com>
2016-07-12 17:22:17 +0800

    fixed point bill

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/PointController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/PointController.java
@@ -112,9 +112,9 @@ public class PointController {
                                                             @Min(value = 1) @RequestParam(name = "pageSize", defaultValue = "10", required = false) int pageSize,
                                                             @RequestParam(name = "startTime", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date startTime,
                                                             @RequestParam(name = "endTime", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime,
-                                                            @RequestParam(name = "businessType", required = false) PointBusinessType businessType) {
+                                                            @RequestParam(name = "businessType", required = false) List<PointBusinessType> businessType) {
         String loginName = LoginUserInfo.getLoginName();
-        BasePaginationDataDto<PointBillPaginationItemDataDto> dataDto = pointBillService.getPointBillPagination(loginName, index, pageSize, startTime, endTime, Lists.newArrayList(businessType));
+        BasePaginationDataDto<PointBillPaginationItemDataDto> dataDto = pointBillService.getPointBillPagination(loginName, index, pageSize, startTime, endTime, businessType);
         dataDto.setStatus(true);
         BaseDto<BasePaginationDataDto> dto = new BaseDto<>();
         dto.setData(dataDto);
