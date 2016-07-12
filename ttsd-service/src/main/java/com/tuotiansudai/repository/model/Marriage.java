0588cb2e7225eea6dc96aac4109e1a31c461f364
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-07-12 19:54:58 +0800

    review fix

--- ttsd-service/src/main/java/com/tuotiansudai/repository/model/Marriage.java
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/model/Marriage.java
@@ -1,5 +1,17 @@
 package com.tuotiansudai.repository.model;
 
 public enum Marriage {
-    UNMARRIED, MARRIED, DIVORCE
+    UNMARRIED("未婚"),
+    MARRIED("已婚"),
+    DIVORCE("离异");
+
+    private final String description;
+
+    Marriage(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
