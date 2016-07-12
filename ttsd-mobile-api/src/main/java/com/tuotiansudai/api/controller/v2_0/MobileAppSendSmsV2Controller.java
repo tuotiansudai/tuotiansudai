b34b8bb51c01413c841f926222a59a334929fe8f
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-12 13:59:34 +0800

    update

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v2_0/MobileAppSendSmsV2Controller.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v2_0/MobileAppSendSmsV2Controller.java
@@ -11,10 +11,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+@RestController
 public class MobileAppSendSmsV2Controller extends MobileAppBaseController {
 
     @Autowired
