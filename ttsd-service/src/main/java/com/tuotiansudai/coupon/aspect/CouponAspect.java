c6ea8f4ff1d57869466332658a0a66db35ade62b
zourenzheng <zourenzheng@tuotiansudai.com>
2016-07-12 23:08:09 +0800

    bug

--- ttsd-service/src/main/java/com/tuotiansudai/coupon/aspect/CouponAspect.java
+++ ttsd-service/src/main/java/com/tuotiansudai/coupon/aspect/CouponAspect.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.tuotiansudai.coupon.repository.model.UserGroup;
 import com.tuotiansudai.coupon.service.CouponAssignmentService;
 import com.tuotiansudai.dto.RegisterUserDto;
+import com.tuotiansudai.dto.SignInDto;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -12,7 +13,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Component
@@ -69,8 +69,8 @@ public class CouponAspect {
     public void afterReturningUserLogin(JoinPoint joinPoint) {
         logger.debug("assign coupon after user login success");
         try {
-            HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
-            String loginName = request.getParameter("username");
+            SignInDto signInDto = (SignInDto) joinPoint.getArgs()[1];
+            String loginName = signInDto.getUsername();
             couponAssignmentService.assignUserCoupon(loginName, userGroups);
         } catch (Exception e) {
             logger.error("assign coupon after user login is failed ", e);
