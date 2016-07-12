22d63ebf75ed9463c29414242c8b9544b970a64e
zhanglong888 <zhanglong@tuotiansudai.com>
2016-07-12 18:03:48 +0800

    activity_add_shared

--- db_migration/aa/V2016_07_12_16_39__add_title_content_url_for_activity.sql
+++ db_migration/aa/V2016_07_12_16_39__add_title_content_url_for_activity.sql
@@ -2,4 +2,4 @@ alter table activity add column share_title varchar(50) not null;
 
 alter table activity add column share_content varchar(50) not null;
 
-alter table activity add column share_url varchar(50) not null; 
+alter table activity add column share_url varchar(100) not null;
