043979e5808a742f86359e6b4652523f6490260e
zhanglong888 <zhanglong@tuotiansudai.com>
2016-07-12 17:22:00 +0800

    activity_add_shared

---
+++ db_migration/aa/V2016_07_12_16_39__add_title_content_url_for_activity.sql
@@ -0,0 +1,5 @@
+alter table activity add column share_title varchar(50) not null;
+
+alter table activity add column share_content varchar(50) not null;
+
+alter table activity add column share_url varchar(50) not null; 
