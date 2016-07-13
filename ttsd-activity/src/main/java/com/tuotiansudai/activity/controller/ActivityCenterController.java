9de6160450ce1250d093f48ba5039434d8a25821
zhanglong888 <zhanglong@tuotiansudai.com>
2016-07-13 14:37:55 +0800

    features/zl/activity_center_web_to_activity

---
+++ ttsd-activity/src/main/java/com/tuotiansudai/activity/controller/ActivityCenterController.java
@@ -0,0 +1,32 @@
+package com.tuotiansudai.activity.controller;
+
+import com.tuotiansudai.dto.ActivityDto;
+import com.tuotiansudai.repository.model.Source;
+import com.tuotiansudai.service.ActivityService;
+import com.tuotiansudai.activity.util.LoginUserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/activity-center")
+public class ActivityCenterController {
+
+    @Autowired
+    ActivityService activityService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getAllOperatingActivities() {
+        ModelAndView modelAndView = new ModelAndView("/activity-center");
+
+        String loginName = LoginUserInfo.getLoginName();
+        List<ActivityDto> activityDtos = activityService.getAllActiveActivities(loginName, Source.WEB);
+        modelAndView.addObject("data", activityDtos);
+        modelAndView.addObject("responsive",true);
+        return modelAndView;
+    }
+}
