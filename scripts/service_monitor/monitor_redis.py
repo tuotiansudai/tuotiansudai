520984cf2f84592195cddbacfa568a3d63b20fc3
Yong Zhang <zhangyong@tuotiansudai.com>
2016-07-13 15:37:31 +0800

    update monitor redis scripts

--- scripts/service_monitor/monitor_redis.py
+++ scripts/service_monitor/monitor_redis.py
@@ -6,7 +6,7 @@ import sys
 import time
 
 #
-# cron 添加执行任务 * 0 * * * ./monitor_redis.py -f redis-`date +%Y-%m-%d`.log
+# cron 添加执行任务 0 0 * * * ./monitor_redis.py -f redis-`date +%Y-%m-%d`.log
 #
 redis_keys = [
 #'pubsub_patterns',
