da664f7a8e34c0b7d595e779fc6c3196a6be576e
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-15 16:38:42 +0800

    update

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBookingLoanServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBookingLoanServiceImpl.java
@@ -4,14 +4,22 @@ package com.tuotiansudai.api.service.v1_0.impl;
 import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
 import com.tuotiansudai.api.dto.v1_0.BookingLoanListsDto;
 import com.tuotiansudai.api.service.v1_0.MobileAppBookingLoanService;
+import com.tuotiansudai.repository.model.ProductType;
 
 public class MobileAppBookingLoanServiceImpl implements MobileAppBookingLoanService{
 
-    private static final String 90_
+    private static String[] boolingloan = new String[]{
+            ProductType._90.toString()
+    };
 
     @Override
     public BaseResponseDto<BookingLoanListsDto> getBookingLoan() {
-        return null;
+        BaseResponseDto<BookingLoanListsDto> baseResponseDto = new BaseResponseDto();
+        BookingLoanListsDto bookingLoanListsDto = new BookingLoanListsDto();
+
+
+        baseResponseDto.setData(bookingLoanListsDto);
+        return baseResponseDto;
     }
 
 }
