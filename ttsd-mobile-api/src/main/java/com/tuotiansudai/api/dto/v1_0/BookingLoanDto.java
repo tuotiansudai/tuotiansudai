4a47c17bf15dc464a44e855afb635b28fa9d48d1
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-15 16:20:36 +0800

    features/bs/mobile_app_booking_loan

---
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/BookingLoanDto.java
@@ -0,0 +1,33 @@
+package com.tuotiansudai.api.dto.v1_0;
+
+
+public class BookingLoanDto {
+
+    private String productType;
+    private String duration;
+    private String rate;
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+}
---
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBookingLoanServiceImpl.java
@@ -0,0 +1,17 @@
+package com.tuotiansudai.api.service.v1_0.impl;
+
+
+import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
+import com.tuotiansudai.api.dto.v1_0.BookingLoanListsDto;
+import com.tuotiansudai.api.service.v1_0.MobileAppBookingLoanService;
+
+public class MobileAppBookingLoanServiceImpl implements MobileAppBookingLoanService{
+
+    private static final String 90_
+
+    @Override
+    public BaseResponseDto<BookingLoanListsDto> getBookingLoan() {
+        return null;
+    }
+
+}
