ca35a1a53021149c5a2a0eb7fa434bed5a45a7f7
zhanglong888 <zhanglong@tuotiansudai.com>
2016-07-25 18:23:39 +0800

    features/zl/landing_page_add_cnzz

---
+++ ttsd-service/src/main/java/com/tuotiansudai/repository/mapper/BookingLoanMapper.java
@@ -0,0 +1,36 @@
+package com.tuotiansudai.repository.mapper;
+
+
+import com.tuotiansudai.repository.model.BookingLoanModel;
+import com.tuotiansudai.repository.model.ProductType;
+import com.tuotiansudai.repository.model.Source;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface BookingLoanMapper {
+    void create(BookingLoanModel bookingLoanModel);
+
+    List<BookingLoanModel> findBookingLoanList(@Param(value = "productType") ProductType productType,
+                                               @Param(value = "bookingTimeStartTime") Date bookingTimeStartTime,
+                                               @Param(value = "bookingTimeEndTime") Date bookingTimeEndTime,
+                                               @Param(value = "mobile") String mobile,
+                                               @Param(value = "noticeTimeStartTime") Date noticeTimeStartTime,
+                                               @Param(value="noticeTimeEndTime") Date noticeTimeEndTime,
+                                               @Param(value = "source") Source source,
+                                               @Param(value = "status" ) Boolean status,
+                                               @Param(value = "index") Integer index,
+                                               @Param(value = "pageSize") Integer pageSize);
+
+    long findCountBookingLoanList(@Param(value = "productType") ProductType productType,
+                                               @Param(value = "bookingTimeStartTime") Date bookingTimeStartTime,
+                                               @Param(value = "bookingTimeEndTime") Date bookingTimeEndTime,
+                                               @Param(value = "mobile") String mobile,
+                                               @Param(value = "noticeTimeStartTime") Date noticeTimeStartTime,
+                                               @Param(value="noticeTimeEndTime") Date noticeTimeEndTime,
+                                               @Param(value = "source") Source source,
+                                               @Param(value = "status" ) Boolean status);
+}
