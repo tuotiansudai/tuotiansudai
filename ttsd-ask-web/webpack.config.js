67f381e5335686c72ce653639c6c252709759ea1
Xiduan Gao <gaoxiduan@gmail.com>
2016-07-25 17:42:41 +0800

    add frent end

---
+++ ttsd-ask-web/webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var os = require('os');
+var webpack = require('webpack');
+var objectAssign = require('object-assign');
+var commonOptions = require('./webpack.common');
+
+var basePath = path.join(__dirname, 'src/main/webapp/ask/');
+
+var port = 8080;
+var getIP = function() {
+    var ipList = os.networkInterfaces();
+    var result = 'localhost';
+    for (var key in ipList) {
+        if (/^e/.test(key)) {
+            for (var i = ipList[key].length - 1; i >= 0; i--) {
+                if (/192\.168/.test(ipList[key][i].address)) {
+                    result = ipList[key][i].address;
+                    break;
+                }
+            }
+        }
+    }
+    return result;
+};
+
+var IP = getIP();
+console.log('IP:', IP);
+IP='localhost';
+
+module.exports = objectAssign(commonOptions, {
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://'+IP+':'+port,
+        path.join(basePath, 'js/mainSite.js'),
+    ],
+    cache: true,
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase:basePath,
+        port:port,
+        historyApiFallback: true,
+        hot: true,
+        inline:true,
+        progress:true,
+        publicPath: '/js/'
+        //线下调试的虚拟目录，本地用
+    }
+});
---
+++ ttsd-ask-web/webpack.production.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var webpack = require('webpack');
+var objectAssign = require('object-assign');
+var commonOptions = require('./webpack.common');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var basePath = path.join(__dirname, 'src/main/webapp/ask');
+
+//var extractCSS = new ExtractTextPlugin('main.css');
+//var extractLESS = new ExtractTextPlugin('[name].css');
+module.exports = {
+	entry: {
+		app: path.resolve(basePath, 'js/mainSite.js'),
+		//添加要打包在vendors里面的库
+		vendors: ['jquery']
+	},
+	output: {
+		filename: "main.[hash:8].min.js",
+		path: path.join(basePath, 'prod'),
+		publicPath: '/prod/'
+	},
+	module:{
+		loaders:[
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+			},
+			{
+			    test: /\.scss$/,
+			    loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+			},
+			{
+				test: /\.(png|jpg|gif|woff|woff2)$/,
+				loader: 'url-loader?limit=8192'
+			}
+		]
+
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		//provide $, jQuery and window.jQuery to every script
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			"window.jQuery": "jquery"
+		}),
+		new ExtractTextPlugin('main.css'),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': '"production"'
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.AggressiveMergingPlugin(),
+		new webpack.NoErrorsPlugin(),
+		//把入口文件里面的数组打包成verdors.js
+		new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+	],
+	cache: false
+
+};
