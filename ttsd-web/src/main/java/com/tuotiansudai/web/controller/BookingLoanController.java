738fb7f38c113908211a5801cba3b73b1a0e45fd
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-25 17:57:52 +0800

    update controller

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/BookingLoanController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/BookingLoanController.java
@@ -1,11 +1,26 @@
 package com.tuotiansudai.web.controller;
 
 
+import com.tuotiansudai.repository.model.ProductType;
+import com.tuotiansudai.service.BookingLoanService;
+import com.tuotiansudai.web.util.LoginUserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/booking-loan")
 public class BookingLoanController {
 
+    @Autowired
+    private BookingLoanService bookingLoanService;
+
+    @RequestMapping(value = "/invest", method = RequestMethod.GET)
+    public void invest(@RequestParam(value = "productType", required = false) ProductType productType,
+                               @RequestParam(value = "pageSize", required = false) String bookingAmount) {
+        bookingLoanService.create(LoginUserInfo.getLoginName(),productType,bookingAmount);
+    }
 }
