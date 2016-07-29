7654ac5ad87190e49d9eb3b8e0938d48928fa1b7
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-07-29 14:37:11 +0800

    update

--- ttsd-console/src/main/java/com/tuotiansudai/console/bi/controller/BusinessIntelligenceController.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/bi/controller/BusinessIntelligenceController.java
@@ -197,7 +197,7 @@ public class BusinessIntelligenceController {
     @ResponseBody
     @RequestMapping(value = "/withdraw-user-count-trend", method = RequestMethod.GET)
     public List<KeyValueModel> queryWithdrawUserCountTrend(
-            @RequestParam(name = "granularity",defaultValue = "Monthly" ) Granularity granularity,
+            @RequestParam(name = "granularity") Granularity granularity,
             @RequestParam(name = "startTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startTime,
             @RequestParam(name = "endTime") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endTime) {
         return businessIntelligenceService.queryWithdrawUserCountTrend(startTime, endTime, granularity);
