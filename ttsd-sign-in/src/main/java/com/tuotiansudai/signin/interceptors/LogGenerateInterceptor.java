5ed62b8601479ee60cd9eeafcaf560377f6e7e68
Yong Zhang <zhangyong@tuotiansudai.com>
2016-08-01 18:28:37 +0800

    update log requestId and userId record

--- ttsd-sign-in/src/main/java/com/tuotiansudai/signin/interceptors/LogGenerateInterceptor.java
+++ ttsd-sign-in/src/main/java/com/tuotiansudai/signin/interceptors/LogGenerateInterceptor.java
@@ -1,8 +1,7 @@
-package com.tuotiansudai.web.interceptors;
+package com.tuotiansudai.signin.interceptors;
 
 
 import com.tuotiansudai.util.UUIDGenerator;
-import com.tuotiansudai.web.util.LoginUserInfo;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.MDC;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -13,17 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 public class LogGenerateInterceptor extends HandlerInterceptorAdapter {
 
 
-    private static final String REQUEST_ID = "requestId";
-
     private static final String USER_ID = "userId";
-
+    private static final String REQUEST_ID = "requestId";
     private static final String ANONYMOUS = "anonymous";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        MDC.put(REQUEST_ID, UUIDGenerator.generate());
-        String loginName = StringUtils.isNotEmpty(LoginUserInfo.getLoginName()) ? LoginUserInfo.getLoginName() : ANONYMOUS;
-        MDC.put(USER_ID, loginName);
+        String requestIdHeader = request.getHeader(REQUEST_ID);
+        String userIdHeader = request.getHeader(USER_ID);
+        if (StringUtils.isNotEmpty(requestIdHeader) && StringUtils.isNotEmpty(userIdHeader)) {
+            MDC.put(REQUEST_ID, requestIdHeader);
+            MDC.put(USER_ID, userIdHeader);
+        } else {
+            MDC.put(REQUEST_ID, UUIDGenerator.generate());
+            MDC.put(USER_ID, ANONYMOUS);
+        }
         return true;
     }
 
