8b06ffeeee602e9687b0000d12473ab07de210f9
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-08-05 15:18:29 +0800

    migrate SQL

--- db_migration/aa/V2016_08_04_16_52__change_coupon_table.sql
+++ db_migration/aa/V2016_08_04_16_52__change_coupon_table.sql
@@ -4,4 +4,7 @@ ADD COLUMN `coupon_source` VARCHAR(50) NOT NULL
 AFTER `deleted`,
 ADD COLUMN `comment` VARCHAR(100) NULL DEFAULT ''
 AFTER `coupon_source`;
+
+UPDATE `aa`.`coupon`
+SET `coupon_source` = '拓天速贷平台赠送';
 COMMIT;
