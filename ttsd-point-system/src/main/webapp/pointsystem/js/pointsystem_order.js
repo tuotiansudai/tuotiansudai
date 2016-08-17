e8b3d30137f057caf932dfa1a147b9d6fb0b3abb
xvqiang <xuqiang@tuotiansudai.com>
2016-08-17 11:15:24 +0800

    go to kill bug

--- ttsd-point-system/src/main/webapp/pointsystem/js/pointsystem_order.js
+++ ttsd-point-system/src/main/webapp/pointsystem/js/pointsystem_order.js
@@ -1,5 +1,23 @@
 require(['jquery'],function($){
 	$(function() {
-		
+		var $countList=$('.count-list'),
+			$numText=$countList.find('.num-text'),
+			$bigText = $countList.find('.total-num i');
+
+		$countList.on('click', '.low-btn', function(event) {//减号
+			event.preventDefault();
+			if ($bigText.text() > 0) {
+				$numText.val() > 1 ? $numText.val(function (index, num) {
+					return parseInt(num) - 1
+				}) : $numText.val('1');
+			}
+		}).on('click', '.add-btn', function(event) {//加号
+			event.preventDefault();
+			if ($bigText.text() > 0) {
+				$numText.val() < parseInt($bigText.text()) ? $numText.val(function (index, num) {
+					return parseInt(num) + 1
+				}) : $numText.val($bigText.text());
+			}
+		});
 	});
 })
