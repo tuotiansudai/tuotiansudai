ad2e47a63b7e8ba9e61fa34e0ba46dfcd2c59b2b
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-08-18 22:03:11 +0800

    update test

--- ttsd-mobile-api/src/test/java/com/tuotiansudai/api/service/MobileAppTransferApplicationServiceTest.java
+++ ttsd-mobile-api/src/test/java/com/tuotiansudai/api/service/MobileAppTransferApplicationServiceTest.java
@@ -207,7 +207,7 @@ public class MobileAppTransferApplicationServiceTest extends ServiceTestBase {
 
         assertEquals("1000.00", baseResponseDto.getData().getBalance());
         assertEquals("900.00", baseResponseDto.getData().getTransferAmount());
-        assertEquals("0.16", baseResponseDto.getData().getExpectedInterestAmount());
+        assertEquals("0.15", baseResponseDto.getData().getExpectedInterestAmount());
     }
 
     @Test
