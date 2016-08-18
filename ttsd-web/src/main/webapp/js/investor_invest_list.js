b1b41f2312e022dcd3fddd8d0b4576cf49b6cf72
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-08-18 17:34:35 +0800

    update view

--- ttsd-web/src/main/webapp/js/investor_invest_list.js
+++ ttsd-web/src/main/webapp/js/investor_invest_list.js
@@ -76,7 +76,8 @@ require(['jquery','template', 'mustache', 'text!/tpl/investor-invest-repay-table
 
                 layer.open({
                     type: 1,
-                    title: false,
+                    title: '回款详情',
+                    skin:'repay-layer-pop',
                     //offset: '80px',
                     area: ['1000px'],
                     shadeClose: true,
@@ -185,6 +186,16 @@ require(['jquery','template', 'mustache', 'text!/tpl/investor-invest-repay-table
             area: 'auto',
             maxWidth: '500'
         });
+        })
+        .on('mouseenter','.text-b',function() {//last icon event
+            layer.closeAll('tips');
+            layer.tips('逾期{{overdueDay}}天，已收违约金{{defaultInterest}}元', $(this), {
+                tips: [1, '#ff7200'],
+                time: 2000,
+                tipsMore: true,
+                area: 'auto',
+                maxWidth: '500'
+            });
         })
         .on('mouseenter', '.extra-rate', function () {//extra-rate icon event
             var num = $(this).data('benefit');
