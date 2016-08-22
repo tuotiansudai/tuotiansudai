073c9bf44ffa294a15ee15c805c1a26f492dc38d
Yong Zhang <zhangyong@tuotiansudai.com>
2016-08-22 13:16:40 +0800

    merge mobile app 3.0

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v2_0/LoanDetailV2ResponseDataDto.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v2_0/LoanDetailV2ResponseDataDto.java
@@ -95,8 +95,6 @@ public class LoanDetailV2ResponseDataDto extends BaseResponseDataDto {
 
     public String loanMoneyCent;
 
-    public ActivityType activityType;
-
 
     private List<ExtraLoanRateDto> extraRates;
 
