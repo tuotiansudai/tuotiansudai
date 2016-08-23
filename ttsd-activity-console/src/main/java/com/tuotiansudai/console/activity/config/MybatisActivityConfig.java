7aba8dd3f51fec542a3b91ea976e18107079dc04
Xiduan Gao <gaoxiduan@gmail.com>
2016-08-23 15:25:29 +0800

    multiple database config for spring boot

--- ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/config/MybatisActivityConfig.java
+++ ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/config/MybatisActivityConfig.java
@@ -3,7 +3,6 @@ package com.tuotiansudai.console.activity.config;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,40 +11,42 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import redis.clients.jedis.JedisPoolConfig;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-public class MybatisAAConfig {
+public class MybatisActivityConfig {
 
     @Bean
-    @ConfigurationProperties(prefix="spring.datasource.aa")
-    public DataSource hikariCPAADataSource() {
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.activity")
+    public DataSource hikariCPActivityDataSource() {
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
 
     @Bean
-    public MapperScannerConfigurer aaMapperScannerConfigurer() {
+    public MapperScannerConfigurer activityMapperScannerConfigurer() {
         MapperScannerConfigurer configurer = new MapperScannerConfigurer();
-        configurer.setBasePackage("com.tuotiansudai.repository.mapper");
-        configurer.setSqlSessionFactoryBeanName("aaSqlSessionFactory");
+        configurer.setBasePackage("com.tuotiansudai.activity.repository.mapper");
+        configurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
         return configurer;
     }
 
     @Bean
-    public DataSourceTransactionManager aaTransactionManager(@Qualifier("hikariCPAADataSource") DataSource hikariCPAADataSource) {
-        return new DataSourceTransactionManager(hikariCPAADataSource);
+    public DataSourceTransactionManager transactionManager(@Qualifier("hikariCPActivityDataSource") DataSource hikariCPActivityDataSource) {
+        return new DataSourceTransactionManager(hikariCPActivityDataSource);
     }
 
     @Bean
-    public SqlSessionFactory aaSqlSessionFactory(@Qualifier("hikariCPAADataSource") DataSource hikariCPAADataSource) throws Exception {
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("hikariCPActivityDataSource") DataSource hikariCPActivityDataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(hikariCPAADataSource);
-        sessionFactory.setTypeAliasesPackage("com.tuotiansudai.repository.model");
+        sessionFactory.setDataSource(hikariCPActivityDataSource);
+        sessionFactory.setTypeAliasesPackage("com.tuotiansudai.activity.repository.model");
         return sessionFactory.getObject();
     }
 }
