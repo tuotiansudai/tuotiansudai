7a1efa1fcb43726e01488a0fec91c3478bd7dfe5
zhanglong888 <zhanglong@tuotiansudai.com>
2016-08-23 16:16:57 +0800

    features/zl/luxury_prize_master

--- ttsd-activity-service/src/main/java/com/tuotiansudai/activity/dto/LuxuryPrizeDto.java
+++
@@ -1,7 +0,0 @@
-package com.tuotiansudai.activity.dto;
-
-import java.io.Serializable;
-
-public class LuxuryPrizeDto implements Serializable{
-    private static final long serialVersionUID = -8102560580155869158L;
-}
