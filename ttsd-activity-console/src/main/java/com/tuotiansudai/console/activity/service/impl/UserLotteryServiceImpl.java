c16c7d59d0665ca2543375f43f2b8427f9142700
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-08-24 18:07:25 +0800

    update

--- ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/service/impl/UserLotteryServiceImpl.java
+++ ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/service/impl/UserLotteryServiceImpl.java
@@ -2,6 +2,7 @@ package com.tuotiansudai.console.activity.service.impl;
 
 
 import com.google.common.base.Function;
+import com.google.common.base.Strings;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import com.tuotiansudai.activity.dto.LotteryPrize;
@@ -11,7 +12,10 @@ import com.tuotiansudai.activity.repository.model.UserLotteryPrizeModel;
 import com.tuotiansudai.activity.repository.model.UserLotteryPrizeView;
 import com.tuotiansudai.activity.repository.model.UserLotteryTimeView;
 import com.tuotiansudai.console.activity.service.UserLotteryService;
-import com.tuotiansudai.repository.mapper.UserMapper;
+import com.tuotiansudai.repository.mapper.*;
+import com.tuotiansudai.repository.model.AccountModel;
+import com.tuotiansudai.repository.model.BankCardModel;
+import com.tuotiansudai.repository.model.ReferrerRelationModel;
 import com.tuotiansudai.repository.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +35,22 @@ public class UserLotteryServiceImpl implements UserLotteryService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Autowired
+    private BankCardMapper bankCardMapper;
+
+    @Autowired
+    private InvestMapper investMapper;
+
+    @Autowired
+    private ReferrerRelationMapper referrerRelationMapper;
+
+    private Date startTime;
+
+    private Date endTime;
+
     @Override
     public List<UserLotteryTimeView> findUserLotteryTimeViews(final String mobile,Integer index,Integer pageSize) {
         List<UserModel> userModels = userMapper.findUserModelByMobile(mobile,index, pageSize);
@@ -40,7 +60,7 @@ public class UserLotteryServiceImpl implements UserLotteryService {
             public UserLotteryTimeView apply(UserModel input) {
                 UserLotteryTimeView model = new UserLotteryTimeView(input.getMobile(),input.getLoginName());
                 model.setUseCount(userLotteryPrizeMapper.findUserLotteryPrizeCountViews(mobile,null,null,null));
-                model.setUnUseCount(0);
+                model.setUnUseCount(findLotteryTime(model.getMobile()));
                 return model;
             }
         });
@@ -61,6 +81,44 @@ public class UserLotteryServiceImpl implements UserLotteryService {
     @Override
     public int findUserLotteryPrizeCountViews(String mobile,LotteryPrize selectPrize,Date startTime,Date endTime){
         return userLotteryPrizeMapper.findUserLotteryPrizeCountViews(mobile, selectPrize, startTime, endTime);
+    }
+
+    private int findLotteryTime(String mobile){
+        int lotteryTime = 0;
+        UserModel userModel = userMapper.findByMobile(mobile);
+        if(userModel == null){
+            return lotteryTime;
+        }
+        List<ReferrerRelationModel> referrerRelationModels = referrerRelationMapper.findByReferrerLoginNameAndLevel(userModel.getLoginName(), 1);
+        for(ReferrerRelationModel referrerRelationModel : referrerRelationModels){
+            UserModel referrerUserModel = userMapper.findByLoginName(referrerRelationModel.getLoginName());
+            if(referrerUserModel.getRegisterTime().before(endTime) && referrerUserModel.getRegisterTime().after(startTime)){
+                lotteryTime ++;
+                if(investMapper.countInvestorInvestPagination(referrerUserModel.getLoginName(),null,startTime,endTime) > 0){
+                    lotteryTime ++;
+                }
+            }
+        }
+
+        if(userModel.getRegisterTime().before(endTime) && userModel.getRegisterTime().after(startTime)){
+            lotteryTime ++;
+        }
+
+        AccountModel accountModel = accountMapper.findByLoginName(userModel.getLoginName());
+        if(accountModel.getRegisterTime().before(endTime) && accountModel.getRegisterTime().after(startTime)){
+            lotteryTime ++;
+        }
+
+        BankCardModel bankCardModel = bankCardMapper.findPassedBankCardByLoginName(userModel.getLoginName());
+        if(bankCardModel.getCreatedTime().before(endTime) && bankCardModel.getCreatedTime().after(startTime)){
+            lotteryTime ++;
+        }
+
+        if(investMapper.countInvestorInvestPagination(userModel.getLoginName(), null, startTime, endTime) > 0){
+            lotteryTime ++;
+        }
+
+        return lotteryTime;
     }
 
 }
--- ttsd-activity/src/main/java/com/tuotiansudai/activity/controller/LotteryActivityController.java
+++ ttsd-activity/src/main/java/com/tuotiansudai/activity/controller/LotteryActivityController.java
@@ -22,6 +22,6 @@ public class LotteryActivityController {
     @RequestMapping(value = "/draw-lottery", method = RequestMethod.POST)
     public BaseDto<DrawLotteryActivityDto> drawLotteryPrize() {
         String loginName = LoginUserInfo.getLoginName();
-        return lotteryActivityService.drawLotteryPrize(loginName);
+        return lotteryActivityService.drawLotteryPrize(loginName,"");
     }
 }
