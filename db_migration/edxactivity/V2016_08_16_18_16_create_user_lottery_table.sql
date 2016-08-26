7f583f5c4df4a61e328cbcfa86d11247e2ab8155
Xiduan Gao <gaoxiduan@gmail.com>
2016-08-26 20:28:51 +0800

    fixed sql

--- db_migration/edxactivity/V2016_08_16_18_16_create_user_lottery_table.sql
+++ db_migration/edxactivity/V2016_08_16_18_16_create_user_lottery_table.sql
@@ -1,14 +1,12 @@
-BEGIN;
-CREATE TABLE `edxactivity`.`user_lottery_prize` (
-  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `mobile` VARCHAR(18) NOT NULL,
-  `login_name` VARCHAR(25) NOT NULL,
-  `prize` VARCHAR(50) NOT NULL,
+CREATE TABLE `user_lottery_prize` (
+  `id`           BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `mobile`       VARCHAR(11)         NOT NULL,
+  `login_name`   VARCHAR(25)         NOT NULL,
+  `prize`        VARCHAR(50)         NOT NULL,
   `lottery_time` DATETIME,
   PRIMARY KEY (`id`),
-  CONSTRAINT FK_USER_LOTTERY_PRIZE_MOBILE_REF_USER_MOBILE FOREIGN KEY (`mobile`) REFERENCES `aa`.`user` (`mobile`),
-  CONSTRAINT FK_USER_LOTTERY_PRIZE_MOBILE_REF_USER_LOGIN_NAME FOREIGN KEY (`login_name`) REFERENCES `aa`.`user` (`login_name`)
+  INDEX INDEX_USER_LOTTERY_PRIZE_MOBILE (`mobile`),
+  INDEX INDEX_USER_LOTTERY_PRIZE_LOGIN_NAME (`login_name`)
 )
-	ENGINE = InnoDB
-	DEFAULT CHARSET = utf8;
-COMMIT;
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
