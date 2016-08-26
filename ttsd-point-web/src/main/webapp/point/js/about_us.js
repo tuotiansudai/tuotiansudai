43a73d96d0c9d521255ee3a42be424e96f830d56
xvqiang <xuqiang@tuotiansudai.com>
2016-08-26 18:17:21 +0800

    fix code

--- ttsd-point-web/src/main/webapp/point/js/about_us.js
+++ ttsd-point-web/src/main/webapp/point/js/about_us.js
@@ -5,5 +5,8 @@ require(['jquery'], function ($) {
                 window.location="/";
             },10000);
         }
+        setInterval(function(){
+            $('.jump-tip i').text()<1?window.location="/":$('.jump-tip i').text(function(index,num){return parseInt(num)-1});
+        },1000);
     });
 });
