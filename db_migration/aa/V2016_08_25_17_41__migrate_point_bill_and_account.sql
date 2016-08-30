17a5cda2dadb723a5850c75a36d1fe929486b21d
Xiduan Gao <gaoxiduan@gmail.com>
2016-08-30 13:15:34 +0800

    fixed sql

--- db_migration/aa/V2016_08_25_17_41__migrate_point_bill_and_account.sql
+++ db_migration/aa/V2016_08_25_17_41__migrate_point_bill_and_account.sql
@@ -1,12 +1,14 @@
-BEGIN ;
+BEGIN;
 
 UPDATE point_bill pb
-LEFT JOIN invest i ON pb.order_id = i.id
-LEFT JOIN loan l ON i.loan_id = l.id
-SET point = TRUNCATE(point*duration/365, 0)
+  JOIN invest i ON pb.order_id = i.id
+  JOIN loan l ON i.loan_id = l.id
+SET point = TRUNCATE(point * duration / 365, 0)
 WHERE pb.business_type = 'INVEST';
 
 UPDATE account
- SET point = (SELECT IF(SUM(point)<0, 0, SUM(point)) FROM point_bill WHERE point_bill.login_name = account.login_name);
+SET point = (SELECT IF(SUM(point) < 0, 0, SUM(point))
+             FROM point_bill
+             WHERE point_bill.login_name = account.login_name);
 
-COMMIT ;
+COMMIT;
