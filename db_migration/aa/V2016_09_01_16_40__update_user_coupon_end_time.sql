d3266eb48f10f4616992e3810738d19a144c4520
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-09-01 16:46:52 +0800

    bug/bs/acitivity_user_coupon_end_time

--- db_migration/aa/V2016_09_01_16_40__update_user_coupon_end_time.sql
+++ db_migration/aa/V2016_09_01_16_40__update_user_coupon_end_time.sql
@@ -1,4 +1,4 @@
 BEGIN ;
 update coupon set deadline = 30 where id in (305,306,307,308);
-update user_coupon set end_time = '2016-10-01 23:59:59' where coupon_id in (305,306,307,308);
+update user_coupon set end_time = '2016-10-01 23:59:59' where coupon_id in (305,306,307,308) and end_time = '2200-12-30 23:59:59';
 COMMIT;
