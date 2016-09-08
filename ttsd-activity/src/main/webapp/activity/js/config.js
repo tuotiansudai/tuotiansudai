2da8dfcebc847e1bff91460743d58e64a6bcffa3
fulanyan <fly39244080@163.com>
2016-09-08 16:47:17 +0800

    finish register

--- ttsd-activity/src/main/webapp/activity/js/config.js
+++ ttsd-activity/src/main/webapp/activity/js/config.js
@@ -3,6 +3,7 @@ var require = {
     'paths': {
         'text': staticServer+'/activity/js/libs/text-2.0.14',
         'jquery': staticServer+'/activity/js/libs/jquery-1.11.3.min',
+        'copyclip': staticServer+'/activity/js/libs/zeroclip/ZeroClipboard',
         'csrf': staticServer+'/activity/js/libs/csrf',
         'jqueryPage': staticServer+'/activity/js/libs/jquery.page',
         'jquery.validate': staticServer+'/activity/js/libs/jquery.validate-1.14.0.min',
