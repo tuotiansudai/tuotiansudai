a929a9a1ee74bc8f509bd3a0fa283b63819d4742
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-09-09 12:13:13 +0800

    review fix

--- db_migration/aa/V2016_09_09_11_55__refresh_point_history_data_change_table;.sql
+++ db_migration/aa/V2016_09_09_11_55__refresh_point_history_data_change_table;.sql
@@ -1,8 +1,8 @@
 BEGIN;
 USE aa;
 -- delete origin table
-DROP TABLE point_bill;
-DROP TABLE user_point_task;
+ALTER TABLE `aa`.`point_bill` RENAME TO `aa`.`point_bill_backup`;
+ALTER TABLE `aa`.`user_point_task` RENAME TO `aa`.`user_point_task_backup`;
 -- replace origin table with temp table
 ALTER TABLE `aa`.`point_bill_temp` RENAME TO `aa`.`point_bill`;
 ALTER TABLE `aa`.`user_point_task_temp` RENAME TO `aa`.`user_point_task`;
