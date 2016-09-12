779bea10c3dedba892bd87c483fed2050577d019
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-09-12 20:20:20 +0800

    bug fix

--- db_migration/aa/V2016_09_12_17_43__refresh_point_history_data_delete_bug_point.sql
+++ db_migration/aa/V2016_09_12_17_43__refresh_point_history_data_delete_bug_point.sql
@@ -1,3 +1,21 @@
+BEGIN;
+-- create tmp table
+CREATE TABLE tmp (
+  login_name VARCHAR(25) PRIMARY KEY,
+  point      INT
+);
+
+-- update
+INSERT INTO tmp SELECT
+                  account.login_name,
+                  account.point - (SELECT SUM(point)
+                                   FROM point_bill
+                                   WHERE point = 1000 AND business_type = 'TASK')
+                FROM point_bill
+                  JOIN account
+                    ON account.login_name = point_bill.login_name AND point_bill.point = 1000 AND business_type = 'TASK'
+                GROUP BY login_name;
+
 DELETE FROM point_bill
 WHERE point = 1000 AND business_type = 'TASK';
 DELETE FROM user_point_task
@@ -5,28 +23,10 @@ WHERE point = 1000 AND point_task_id = (SELECT id
                                         FROM point_task
                                         WHERE name = 'EACH_REFERRER_INVEST');
 
--- create tmp table
-CREATE TABLE tmp (
-  login_name VARCHAR(25) PRIMARY KEY,
-  point      INT
-);
--- default point = 0
-UPDATE `aa`.`account`
-SET point = 0;
--- update
-INSERT INTO tmp SELECT
-                  login_name,
-                  SUM(point)
-                FROM point_bill
-                GROUP BY login_name;
-
--- SUM result may negative
-UPDATE tmp
-SET tmp.point = 0
-WHERE tmp.point < 0;
-
 UPDATE `aa`.`account`, tmp
 SET account.point = tmp.point
 WHERE tmp.login_name = account.login_name;
 
 DROP TABLE tmp;
+
+COMMIT;
