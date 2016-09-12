1f14e41e7c71dcfb7f52a4b6518e58a0411eddcd
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-09-12 17:50:01 +0800

    bug fix

--- ttsd-point/src/main/java/com/tuotiansudai/point/service/impl/PointTaskServiceImpl.java
+++ ttsd-point/src/main/java/com/tuotiansudai/point/service/impl/PointTaskServiceImpl.java
@@ -285,8 +285,6 @@ public class PointTaskServiceImpl implements PointTaskService {
                 return accountMapper.findByLoginName(referrer) != null && CollectionUtils.isEmpty(userPointTaskMapper.findByLoginNameAndTask(referrer, pointTask));
             case FIRST_REFERRER_INVEST:
                 return accountMapper.findByLoginName(referrer) != null && userPointTaskMapper.findMaxTaskLevelByLoginName(referrer, pointTask) == 0;
-            case EACH_REFERRER_INVEST:
-                return accountMapper.findByLoginName(referrer) != null && investMapper.findLatestSuccessInvest(loginName).getAmount() >= 10L;
             case FIRST_INVEST_180:
                 return userPointTaskMapper.findMaxTaskLevelByLoginName(loginName, pointTask) == 0
                         && loanMapper.findById(investMapper.findLatestSuccessInvest(loginName).getLoanId()).getProductType() == ProductType._180;
