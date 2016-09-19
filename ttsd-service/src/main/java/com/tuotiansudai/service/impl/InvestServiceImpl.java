8ed861f212b09c9d040b5334cfae3953dd27ad60
Xiduan Gao <gaoxiduan@gmail.com>
2016-09-19 14:48:58 +0800

    newbie invest add zr0612, lm425

--- ttsd-service/src/main/java/com/tuotiansudai/service/impl/InvestServiceImpl.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/impl/InvestServiceImpl.java
@@ -219,7 +219,7 @@ public class InvestServiceImpl implements InvestService {
 
     private boolean canInvestNewbieLoan(String loginName) {
         int newbieInvestCount = investMapper.sumSuccessInvestCountByLoginName(loginName);
-        return newbieInvestLimit == 0 || newbieInvestCount < newbieInvestLimit;
+        return Lists.newArrayList("zr0612", "lm425").contains(loginName.toLowerCase()) || newbieInvestLimit == 0 || newbieInvestCount < newbieInvestLimit;
     }
 
     @Override
