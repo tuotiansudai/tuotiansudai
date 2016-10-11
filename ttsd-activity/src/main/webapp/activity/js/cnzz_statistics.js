c01df49eba6574190fc16130708ced30577eb830
fulanyan <fly39244080@163.com>
2016-10-11 18:55:25 +0800

    fix draw point

--- ttsd-activity/src/main/webapp/activity/js/cnzz_statistics.js
+++ ttsd-activity/src/main/webapp/activity/js/cnzz_statistics.js
@@ -4,3 +4,5 @@ cnzzPushConstructor.prototype.trackClick = function (category, action, label) {
     _czc.push(['_trackEvent', category, action, label]);
 };
 cnzzPush = new cnzzPushConstructor();
+
+console.log(cnzzPush);
