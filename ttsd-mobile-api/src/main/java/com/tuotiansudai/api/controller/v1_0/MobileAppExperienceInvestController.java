189bad249a806ae1e690bdc3c422223b3de39bd0
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-10-12 20:17:27 +0800

    fix/bs/experience_invest_null_point

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppExperienceInvestController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppExperienceInvestController.java
@@ -3,11 +3,16 @@ package com.tuotiansudai.api.controller.v1_0;
 import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
 import com.tuotiansudai.api.dto.v1_0.InvestRequestDto;
 import com.tuotiansudai.api.service.v1_0.MobileAppExperienceInvestService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.MessageFormat;
+
 @RestController
 public class MobileAppExperienceInvestController extends MobileAppBaseController {
+
+    static Logger logger = Logger.getLogger(MobileAppExperienceInvestController.class);
 
     @Autowired
     private MobileAppExperienceInvestService mobileAppExperienceInvestService;
@@ -15,6 +20,7 @@ public class MobileAppExperienceInvestController extends MobileAppBaseController
     @RequestMapping(value = "/experience-invest", method = RequestMethod.POST)
     public BaseResponseDto experienceInvest(@RequestBody InvestRequestDto investRequestDto) {
         investRequestDto.getBaseParam().setUserId(getLoginName());
+        logger.info(MessageFormat.format("[Experience Invest] the investRequestDto info:{0}", investRequestDto.getBaseParam().toString()));
         return mobileAppExperienceInvestService.experienceInvest(investRequestDto);
     }
 
