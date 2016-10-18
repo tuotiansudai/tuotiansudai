216a9ff214962cad3ef0e9029d011deb747fba82
zhanglong888 <zhanglong@tuotiansudai.com>
2016-10-18 17:55:53 +0800

    add log

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/service/RepayGeneratorService.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/service/RepayGeneratorService.java
@@ -1,7 +1,9 @@
 package com.tuotiansudai.paywrapper.service;
 
+import com.tuotiansudai.paywrapper.exception.PayException;
+
 public interface RepayGeneratorService {
 
-    void generateRepay(long loanId);
+    void generateRepay(long loanId) throws PayException;
 
 }
