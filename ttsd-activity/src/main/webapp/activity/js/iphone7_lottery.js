a441b75555691de45f23286d0ec42894a0762b9f
jroce <472228406@qq.com>
2016-10-18 14:57:42 +0800

    fix js

--- ttsd-activity/src/main/webapp/activity/js/iphone7_lottery.js
+++ ttsd-activity/src/main/webapp/activity/js/iphone7_lottery.js
@@ -3,11 +3,31 @@ require(['jquery', 'layerWrapper', 'template', 'logintip', 'jquery.ajax.extensio
 		$('.progressbar').each(function() {
 			var t = $(this),
 				dataperc = t.attr('data-perc'),
-				number=parseInt(Math.round(dataperc)/5000);
+				number=parseInt(100-Math.round(dataperc)/5000);
 				console.log(number);
 			t.find('.bar').animate({
 				width: number+'%'
 			}, number * 25);
 		});
+
+		function getDatelist(page){
+			$.ajax({
+				url: '/path/to/file',
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					param1: page,
+					user:loginname
+				}
+			})
+			.done(function(data) {
+				console.log(data);
+				// $('#codeList').html(tpl('codeListTpl',data));
+			})
+			.fail(function(data) {
+				layer.msg('请求失败，请重试！');
+			});
+			
+		}
 	});
 });
