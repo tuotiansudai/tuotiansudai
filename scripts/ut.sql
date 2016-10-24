c64590668e2bedd34378ff33b6ccadb806ba7b9a
zhanglong888 <zhanglong@tuotiansudai.com>
2016-10-24 16:51:41 +0800

    features/zl/modify_mysql_root

---
+++ scripts/ut.sql
@@ -0,0 +1,10 @@
+grant select,insert,update,delete,create,drop on aa.* to tuotiansd@'%' identified by '123abc';
+grant select,insert,update,delete,create,drop on job_worker.* to tuotiansd@'%' identified by '123abc';
+grant select,insert,update,delete,create,drop on sms_operations.* to tuotiansd@'%' identified by '123abc';
+grant select,insert,update,delete,create,drop on ump_operations.* to tuotiansd@'%' identified by '123abc';
+
+grant select,insert,update,delete,create,drop on edxactivity.* to sdactivity@'%' identified by '123abc';
+
+grant select,insert,update,delete,create,drop on edxpoint.* to sdpoint@'%' identified by '123abc';
+
+grant select,insert,update,delete,create,drop on edxask.* to sdask@'%' identified by '123abc';
