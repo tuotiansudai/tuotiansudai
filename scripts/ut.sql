75e995a1fe42912e2d9d4de7f44714f2652e54b9
zhanglong888 <zhanglong@tuotiansudai.com>
2016-10-24 21:48:16 +0800

    features/zl/modify_mysql_root

--- scripts/ut.sql
+++ scripts/ut.sql
@@ -3,11 +3,11 @@ grant select,insert,update,delete,create,drop on job_worker.* to tuotiansd@'%' i
 grant select,insert,update,delete,create,drop on sms_operations.* to tuotiansd@'%' identified by 'tuotiansd';
 grant select,insert,update,delete,create,drop on ump_operations.* to tuotiansd@'%' identified by 'tuotiansd';
 
-grant select,insert,update,delete,create,drop on edxactivity.* to sdactivity@'%' identified by 'edxactivity';
+grant select,insert,update,delete,create,drop on edxactivity.* to sdactivity@'%' identified by 'sdactivity';
 
-grant select,insert,update,delete,create,drop on edxpoint.* to sdpoint@'%' identified by 'edxpoint';
+grant select,insert,update,delete,create,drop on edxpoint.* to sdpoint@'%' identified by 'sdpoint';
 
-grant select,insert,update,delete,create,drop on edxask.* to sdask@'%' identified by 'edxask';
+grant select,insert,update,delete,create,drop on edxask.* to sdask@'%' identified by 'sdask';
 
 update user set password=password('root') where user='root';
 
