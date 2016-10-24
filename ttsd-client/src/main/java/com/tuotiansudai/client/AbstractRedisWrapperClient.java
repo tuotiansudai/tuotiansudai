2786d684dec7cb656f8f8837f7b5ffcd5c66ea2f
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-10-24 16:22:19 +0800

    update activity time

--- ttsd-client/src/main/java/com/tuotiansudai/client/AbstractRedisWrapperClient.java
+++ ttsd-client/src/main/java/com/tuotiansudai/client/AbstractRedisWrapperClient.java
@@ -59,7 +59,7 @@ public abstract class AbstractRedisWrapperClient {
             logger.debug("redisPort=" + redisPort);
             logger.debug("maxTotal=" + jedisPoolConfig.getMaxTotal());
             logger.debug("maxTotal=" + jedisPoolConfig.getMaxWaitMillis());
-            jedisPool = new JedisPool(jedisPoolConfig, redisHost, redisPort,100000);
+            jedisPool = new JedisPool(jedisPoolConfig, redisHost, redisPort);
         }
         return jedisPool;
     }
