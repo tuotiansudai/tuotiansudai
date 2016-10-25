20c948fe09d2edf8e32e4af48b8b8c7b80cb7281
zhanglong888 <zhanglong@tuotiansudai.com>
2016-10-25 14:11:30 +0800

    features/zl/modify_mysql_root

--- scripts/ut.sql
+++ scripts/ut.sql
@@ -1,13 +1,13 @@
-grant select,insert,update,delete,create,drop on aa.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop on job_worker.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop on sms_operations.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop on ump_operations.* to tuotiansd@'%' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter on aa.* to tuotiansd@'%' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter on job_worker.* to tuotiansd@'%' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter on sms_operations.* to tuotiansd@'%' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter on ump_operations.* to tuotiansd@'%' identified by 'tuotiansd';
 
-grant select,insert,update,delete,create,drop on edxactivity.* to sdactivity@'%' identified by 'sdactivity';
+grant select,insert,update,delete,create,drop,alter on edxactivity.* to sdactivity@'%' identified by 'sdactivity';
 
-grant select,insert,update,delete,create,drop on edxpoint.* to sdpoint@'%' identified by 'sdpoint';
+grant select,insert,update,delete,create,drop,alter on edxpoint.* to sdpoint@'%' identified by 'sdpoint';
 
-grant select,insert,update,delete,create,drop on edxask.* to sdask@'%' identified by 'sdask';
+grant select,insert,update,delete,create,drop,alter on edxask.* to sdask@'%' identified by 'sdask';
 
 update mysql.user set password=password('root') where user='root';
 
