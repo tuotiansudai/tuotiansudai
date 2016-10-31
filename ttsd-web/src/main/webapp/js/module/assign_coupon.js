bda287af35ec427524ea7cc1d42b5e8bc4910f7a
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-10-31 23:04:47 +0800

    add assgin coupon document readay

--- ttsd-web/src/main/webapp/js/module/assign_coupon.js
+++ ttsd-web/src/main/webapp/js/module/assign_coupon.js
@@ -1,17 +1,14 @@
 define(['jquery'], function ($) {
 
-    function assignUserCoupon()
-    {
+    $(function() {
         if($('#logout-link').length > 0){
             $.ajax({
                 url: '/assign-coupon',
-                type: 'Post',
+                type: 'POST',
                 dataType: 'json',
                 contentType: 'application/json; charset=UTF-8'
             })
         }
-    }
-
-    assignUserCoupon();
+    });
 });
 
