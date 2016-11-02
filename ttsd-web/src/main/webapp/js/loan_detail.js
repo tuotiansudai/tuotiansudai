d93aea29894fabdb7dd77d8a5df035e16973dd02
jroce <472228406@qq.com>
2016-11-02 21:28:27 +0800

    add box tip

--- ttsd-web/src/main/webapp/js/loan_detail.js
+++ ttsd-web/src/main/webapp/js/loan_detail.js
@@ -955,5 +955,44 @@ require(['jquery', 'pagination', 'mustache', 'text!/tpl/loan-invest-list.mustach
         $(this).val()!=''?$('#skipError').text('').hide():$('#skipError').text('验证码不能为空').show();;
     });
 
+    $('#serviceLayer').on('click', function(event) {
+        event.preventDefault();
+        layer.open({
+            type: 1,
+            title: '安心签服务协议',
+            area: ['950px', '600px'],
+            shadeClose: true,
+            move: false,
+            scrollbar: true,
+            skin:'register-skin',
+            content: $('#serviceBox')
+        });
+    });
+    $('#privacyLayer').on('click', function(event) {
+        event.preventDefault();
+        layer.open({
+            type: 1,
+            title: '隐私条款',
+            area: ['950px', '600px'],
+            shadeClose: true,
+            move: false,
+            scrollbar: true,
+            skin:'register-skin',
+            content: $('#privacyBox')
+        });
+    });
+    $('#numberLayer').on('click', function(event) {
+        event.preventDefault();
+        layer.open({
+            type: 1,
+            title: 'CFCA数字证书服务协议',
+            area: ['950px', '600px'],
+            shadeClose: true,
+            move: false,
+            scrollbar: true,
+            skin:'register-skin',
+            content: $('#numberBox')
+        });
+    });
 
 });
