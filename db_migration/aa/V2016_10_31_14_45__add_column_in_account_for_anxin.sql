e9451278d6d515aa939a044c5e8c531dad1c0829
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-11-03 22:35:59 +0800

    modify sql of anxin_sign_property

--- db_migration/aa/V2016_10_31_14_45__add_column_in_account_for_anxin.sql
+++ db_migration/aa/V2016_10_31_14_45__add_column_in_account_for_anxin.sql
@@ -4,8 +4,8 @@ CREATE TABLE `aa`.`anxin_sign_property` (
   `anxin_user_id` varchar(32) COMMENT '安心签用户ID',
   `skip_auth` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否开通免验',
   `project_code` varchar(32) COMMENT '上次授权时使用的projectCode',
-  `ip` VARCHAR (15) COMMENT '授权时用户的IP',
-  `auth_time` datetime NOT NULL,
+  `auth_ip` VARCHAR (15) COMMENT '授权时用户的IP',
+  `auth_time` datetime COMMENT '上一次授权的时间',
   `created_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQUE_CFCA_PROPERTY_LOGIN_NAME` (`login_name`),
@@ -18,3 +18,4 @@ OR:
 alter TABLE account add COLUMN anxin_user_id varchar(32) COMMENT '安心签用户ID';
 alter TABLE account add COLUMN skip_auth tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否开通免验';
 alter TABLE account add COLUMN project_code varchar(32) COMMENT '上次授权时使用的projectCode';
+
