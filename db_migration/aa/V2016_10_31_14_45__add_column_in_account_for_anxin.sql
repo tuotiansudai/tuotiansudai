e4d6fe66346a3f9ce36f99ebb3d2d2f745b5ab9e
fulanyan <fly39244080@163.com>
2016-11-03 19:59:33 +0800

    min file and design all pages

--- db_migration/aa/V2016_10_31_14_45__add_column_in_account_for_anxin.sql
+++ db_migration/aa/V2016_10_31_14_45__add_column_in_account_for_anxin.sql
@@ -11,9 +11,9 @@ CREATE TABLE `aa`.`anxin_sign_property` (
   CONSTRAINT `FK_CFCA_PROPERTY_LOGIN_NAME_REF_USER_LOGIN_NAME` FOREIGN KEY (`login_name`) REFERENCES `user` (`login_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
---
--- OR:
---
--- alter TABLE account add COLUMN anxin_user_id varchar(32) COMMENT '安心签用户ID';
--- alter TABLE account add COLUMN skip_auth tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否开通免验';
--- alter TABLE account add COLUMN project_code varchar(32) COMMENT '上次授权时使用的projectCode';
+
+OR:
+
+alter TABLE account add COLUMN anxin_user_id varchar(32) COMMENT '安心签用户ID';
+alter TABLE account add COLUMN skip_auth tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否开通免验';
+alter TABLE account add COLUMN project_code varchar(32) COMMENT '上次授权时使用的projectCode';
