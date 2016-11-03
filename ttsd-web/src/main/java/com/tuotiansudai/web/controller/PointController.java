f4d4bcfe0e3a3ebd2c8366d195c0584f82103429
gengbj <gengbj@tuotiansudai.com>
2016-11-03 16:49:27 +0800

    fix bug

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/PointController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/PointController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/point")
@@ -16,6 +17,12 @@ public class PointController {
 
     @Autowired
     private SignInService signInService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView myPoint() {
+        //重定向到新的积分商城
+        return new ModelAndView("redirect:/point-shop");
+    }
 
     @RequestMapping(path = "/sign-in", method = RequestMethod.POST)
     @ResponseBody
