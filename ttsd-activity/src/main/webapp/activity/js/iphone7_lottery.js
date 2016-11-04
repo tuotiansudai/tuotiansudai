cdcaad5025e4a109756f9779dd5971b052c03951
jroce <472228406@qq.com>
2016-11-05 00:25:51 +0800

    fix code

--- ttsd-activity/src/main/webapp/activity/js/iphone7_lottery.js
+++ ttsd-activity/src/main/webapp/activity/js/iphone7_lottery.js
@@ -19,7 +19,7 @@ require(['jquery', 'layerWrapper', 'template', 'logintip','jquery.validate', 'jq
 
 		$.ajax({
 			url: '/activity/iphone7-lottery/getDate',
-			type: 'POST',
+			type: 'GET',
 			dataType: 'json'
 		})
 		.done(function(data) {
@@ -31,6 +31,7 @@ require(['jquery', 'layerWrapper', 'template', 'logintip','jquery.validate', 'jq
 		})
 		.fail(function() {
 			layer.msg('请求失败，请刷新页面重试！');
+			return false;
 		});
 
 
