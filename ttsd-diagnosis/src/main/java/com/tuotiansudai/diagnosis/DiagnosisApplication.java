dbb8a62feb761d7346d2d7fb74a6bf716b944f45
zhangzhigang <zhangzhigang@tuotiansudai.com>
2016-11-07 02:22:22 +0800

    add schedule config, add email report

--- ttsd-diagnosis/src/main/java/com/tuotiansudai/diagnosis/DiagnosisApplication.java
+++ ttsd-diagnosis/src/main/java/com/tuotiansudai/diagnosis/DiagnosisApplication.java
@@ -1,19 +1,19 @@
 package com.tuotiansudai.diagnosis;
 
+import com.tuotiansudai.diagnosis.config.DiagnosisConfig;
 import com.tuotiansudai.diagnosis.support.Diagnosis;
 import com.tuotiansudai.diagnosis.support.DiagnosisResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -26,13 +26,20 @@ class DiagnosisApplication {
     private final List<Diagnosis> diagnoses;
 
     @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private DiagnosisConfig diagnosisConfig;
+
+    @Autowired
     public DiagnosisApplication(List<Diagnosis> diagnoses) {
         this.diagnoses = diagnoses;
     }
 
     void start(String[] args) {
         if (args.length > 0) {
-            runOnce(args, null);
+            String description = "diagnosis with args: " + String.join(" ", Arrays.asList(args));
+            runOnce(args, r -> printReport(description, r));
         } else {
             scheduleDiagnosis();
         }
@@ -48,46 +55,72 @@ class DiagnosisApplication {
                     logger.info("{}. {} end", idx, diagnosis.getClass().getName());
                     return results;
                 }).collect(Collectors.toList());
-        printReport(totalResult);
         if (consumer != null) {
             consumer.accept(totalResult);
         }
     }
 
     private void scheduleDiagnosis() {
-        int schedule_hour = 0;
-        int schedule_minutes = 35;
         LocalDateTime fireTime = LocalDate.now().atStartOfDay()
-                .withHour(schedule_hour).withMinute(schedule_minutes);
+                .withHour(diagnosisConfig.getSchedule().getHour())
+                .withMinute(diagnosisConfig.getSchedule().getMinute());
         logger.info("diagnosis scheduled at {}", fireTime);
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                runOnce(new String[]{}, r -> smsReport(r));
+                String description = "diagnosis bills at " + LocalDate.now().minusDays(1).toString();
+                logger.info(description);
+                runOnce(new String[]{}, r -> mailReport(description, r));
             }
         }, Date.from(fireTime.toInstant(ZoneOffset.ofHours(8))), 1000 * 60 * 60 * 24);
     }
 
-    private void printReport(List<List<DiagnosisResult>> totalResult) {
-        totalResult.forEach(resultList -> {
-            logger.info("");
-            logger.info("");
-            logger.info("  Diagnosis Report: ");
-            logger.info("");
-            resultList.stream()
-                    .filter(result -> result.getProblems().size() > 0)
-                    .forEach(result -> {
-                        logger.info("    User {} have {} problems", result.getLoginName(), result.getProblems().size());
-                        result.getProblems().forEach(p -> logger.info("        {}", p));
-                    });
-            logger.info("");
-            logger.info("  Diagnosis complete!");
-            logger.info("");
-            logger.info("");
-        });
+    private void printReport(String description, List<List<DiagnosisResult>> totalResult) {
+        logger.info(buildDiagnosisReport(description, totalResult));
     }
 
-    private void smsReport(List<List<DiagnosisResult>> totalResult) {
-        logger.info("send sms");
+    private void mailReport(String description, List<List<DiagnosisResult>> totalResult) {
+        String reportMessage = buildDiagnosisReport(description, totalResult);
+        logger.info(reportMessage);
+        try {
+            sendReportMail(reportMessage);
+        } catch (Exception e) {
+            logger.error("send report mail fail", e);
+        }
+    }
+
+    private String buildDiagnosisReport(String description, List<List<DiagnosisResult>> totalResult) {
+        List<String> reportLines = new LinkedList<>();
+        totalResult.forEach(resultList -> {
+            reportLines.add("");
+            reportLines.add("");
+            reportLines.add("  Diagnosis report ");
+            reportLines.add("    " + description);
+            reportLines.add("");
+            long problemCount = resultList.stream()
+                    .filter(result -> result.getProblems().size() > 0)
+                    .map(result -> {
+                        reportLines.add(String.format("    User %s have %d problems: ", result.getLoginName(), result.getProblems().size()));
+                        reportLines.addAll(result.getProblems().stream().map(p -> "        " + p).collect(Collectors.toList()));
+                        return result;
+                    })
+                    .count();
+            if (problemCount == 0) {
+                reportLines.add("    Congratulations, all bills are correct!");
+            }
+            reportLines.add("");
+            reportLines.add("  Diagnosis complete.");
+            reportLines.add("");
+        });
+        return String.join("\r\n", reportLines);
+    }
+
+    private void sendReportMail(String reportMessage) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(diagnosisConfig.getReport().getNotifyMailFrom());
+        message.setTo(diagnosisConfig.getReport().getNotifyMailAddress());
+        message.setSubject("diagnosis report");
+        message.setText(reportMessage);
+        mailSender.send(message);
     }
 }
