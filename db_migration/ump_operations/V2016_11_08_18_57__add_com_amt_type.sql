e6615d3848ed9872fb6c5b5393d1d1d9955ba9f0
Xiduan Gao <gaoxiduan@gmail.com>
2016-11-08 20:25:38 +0800

    add com_amt_type

---
+++ db_migration/ump_operations/V2016_11_08_18_57__add_com_amt_type.sql
@@ -0,0 +1,14 @@
+ALTER TABLE `ump_operations`.`mer_recharge_person_request` ADD COLUMN `com_amt_type` VARCHAR(2)
+AFTER `gate_id`;
+
+ALTER TABLE `ump_operations`.`mer_recharge_request` ADD COLUMN `com_amt_type` VARCHAR(2)
+AFTER `gate_id`;
+
+ALTER TABLE `ump_operations`.`cust_withdrawals_request` ADD COLUMN `com_amt_type` VARCHAR(2)
+AFTER `gate_id`;
+
+ALTER TABLE `ump_operations`.`withdraw_apply_notify_request` ADD COLUMN `com_amt` VARCHAR(26)
+AFTER `amount`;
+
+ALTER TABLE `ump_operations`.`withdraw_apply_notify_request` ADD COLUMN `com_amt_type` VARCHAR(6)
+AFTER `com_amt`;
