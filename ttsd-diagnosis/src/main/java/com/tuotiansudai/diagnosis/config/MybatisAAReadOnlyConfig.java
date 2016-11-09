d022468205afe93f8334fea955b82b6455549f89
zhangzhigang <zhangzhigang@tuotiansudai.com>
2016-11-09 12:01:57 +0800

    use hikariCp pool

--- ttsd-diagnosis/src/main/java/com/tuotiansudai/diagnosis/config/MybatisAAReadOnlyConfig.java
+++ ttsd-diagnosis/src/main/java/com/tuotiansudai/diagnosis/config/MybatisAAReadOnlyConfig.java
@@ -1,27 +1,38 @@
 package com.tuotiansudai.diagnosis.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class MybatisAAReadOnlyConfig {
 
+    @Bean(name = "hikariCPAAConfig")
+    @ConfigurationProperties(prefix = "spring.datasource.aaro")
+    public HikariConfig hikariCPAAConfig() {
+        return new HikariConfig();
+    }
+
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "spring.datasource.aaro")
-    public DataSource aaDataSource() {
-        return DataSourceBuilder
-                .create()
-                .build();
+    public DataSource aaDataSource(@Qualifier("hikariCPAAConfig") HikariConfig hikariCPAAConfig) {
+        return new HikariDataSource(hikariCPAAConfig);
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource aaDataSource) {
+        return new DataSourceTransactionManager(aaDataSource);
     }
 
     @Bean
