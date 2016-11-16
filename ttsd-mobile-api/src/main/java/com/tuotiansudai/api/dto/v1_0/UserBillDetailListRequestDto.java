abfdd2aebd357d6620e5296cb136c2097e52fa83
zhanglong888 <zhanglong@tuotiansudai.com>
2016-11-16 20:32:52 +0800

    features/zl/mobile_app_3.6

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/UserBillDetailListRequestDto.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/UserBillDetailListRequestDto.java
@@ -2,6 +2,7 @@ package com.tuotiansudai.api.dto.v1_0;
 
 public class UserBillDetailListRequestDto extends BaseParamDto{
     private String userId;
+    private UserBillCategory userBillCategory;
 
     public String getUserId() {
         return userId;
@@ -9,5 +10,13 @@ public class UserBillDetailListRequestDto extends BaseParamDto{
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public UserBillCategory getUserBillCategory() {
+        return userBillCategory;
+    }
+
+    public void setUserBillCategory(UserBillCategory userBillCategory) {
+        this.userBillCategory = userBillCategory;
     }
 }
