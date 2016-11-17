7add6ec3a7b6ad2a5a5c40760e69bf8b3539a011
Xiduan Gao <gaoxiduan@gmail.com>
2016-11-17 15:02:47 +0800

    spring session activity ask point finish

--- ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/Application.java
+++ ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication(exclude = {SessionAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.tuotiansudai.console.activity", "com.tuotiansudai.activity.service", "com.tuotiansudai.cache", "com.tuotiansudai.client", "com.tuotiansudai.repository", "com.tuotiansudai.spring", "com.tuotiansudai.mq"})
 @PropertySource(value = {"classpath:ttsd-env.properties", "classpath:ttsd-biz.properties"})
 public class Application extends SpringBootServletInitializer {
