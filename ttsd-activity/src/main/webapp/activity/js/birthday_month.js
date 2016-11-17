3d4f8a3e29ff7454a85f8e12d329ba6390ad26b9
fulanyan <fly39244080@163.com>
2016-11-17 19:23:48 +0800

    finish image scroll effect

--- ttsd-activity/src/main/webapp/activity/js/birthday_month.js
+++ ttsd-activity/src/main/webapp/activity/js/birthday_month.js
@@ -1,12 +1,40 @@
 require(['jquery'], function ($) {
 
         var $productFrame=$('#productListFrame'),
-            $leftArrow=$('.arrow-left',$productFrame),
-            $rightArrow=$('.arrow-right',$productFrame),
+            $leftBtn=$('.arrow-left',$productFrame),
+            $rightBtn=$('.arrow-right',$productFrame),
             $productBox=$('.product-box',$productFrame);
 
-    var productBoxW=$productBox.width();
+    var productBoxW=$productBox.width(),
+        productLen=$productBox.find('li').length;
+    var redirect=globalFun.browserRedirect();
+    if(redirect=='mobile') {
+        $productBox.find('li').css({"width":productBoxW});
+        var recordNum=0; //用来记录当前活动图片
 
-    $productBox.find('li').css({"width":productBoxW});
+        function animateImg(record) {
+            $productBox.find('ul').stop().animate({
+                marginLeft: -(productBoxW * record)
+            },500,function() {
+                if(record==(productLen-1)) {
+                    $rightBtn.hide();
+                    $leftBtn.show();
+                } else if(record==0) {
+                    $leftBtn.hide();
+                    $rightBtn.show();
+                }
+            });
+        }
+        $rightBtn.on('touchstart', function () {
+            recordNum++;
+            animateImg(recordNum);
+        });
+
+        $leftBtn.on('touchstart', function () {
+            recordNum--;
+            animateImg(recordNum);
+        });
+
+    }
 
 });
