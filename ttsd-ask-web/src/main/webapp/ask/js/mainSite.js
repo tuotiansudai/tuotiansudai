98e6dcd5e7ff2d24aadc7b68d293e06dc2375c25
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-11-22 22:25:20 +0800

    review fix

--- ttsd-ask-web/src/main/webapp/ask/js/mainSite.js
+++ ttsd-ask-web/src/main/webapp/ask/js/mainSite.js
@@ -1,6 +1,5 @@
 require('../style/sass/global.scss');
-
-var comm = require('./commonFun');
+var comm = require("./commonFun");
 require('./questions');
 require('./create-question');
 require('./global_page');
