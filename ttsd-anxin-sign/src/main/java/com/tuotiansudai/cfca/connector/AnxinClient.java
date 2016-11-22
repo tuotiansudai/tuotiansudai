272cdf8b1f9556b09f4ef6da0858fabbfdd560c5
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-11-22 21:09:00 +0800

    use okHttpClient instead of HttpConnector

---
+++ ttsd-anxin-sign/src/main/java/com/tuotiansudai/cfca/connector/AnxinClient.java
@@ -0,0 +1,145 @@
+package com.tuotiansudai.cfca.connector;
+
+import cfca.sadk.algorithm.common.PKIException;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.tuotiansudai.cfca.util.SecurityUtil;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class AnxinClient {
+
+    private static final Logger logger = Logger.getLogger(AnxinClient.class);
+
+    @Autowired
+    private OkHttpClient httpClient;
+
+    @Value("${anxin.jks.path}")
+    private String JKS_PATH;
+
+    @Value("${anxin.jks.pwd}")
+    private String JKS_PWD;
+
+    @Value("${anxin.alias}")
+    private String ALIAS;
+
+    @Value("${anxin.url}")
+    private String URL;
+
+    @Value("${anxin.plat.id}")
+    private String PLAT_ID;
+
+    private static final String DATA = "data";
+    private static final String SIGNATURE = "signature";
+
+    private static final int DEFAULT_CONNECT_TIMEOUT = 3000;
+    private static final int DEFAULT_READ_TIMEOUT = 10000;
+
+    private static final String DEFAULT_SSL_PROTOCOL = "TLSv1.1";
+    private static final String DEFAULT_KEY_ALGORITHM = KeyManagerFactory.getDefaultAlgorithm();
+    private static final String DEFAULT_KEY_STORE_TYPE = KeyStore.getDefaultType();
+    private static final String DEFAULT_TRUST_ALGORITHM = TrustManagerFactory.getDefaultAlgorithm();
+    private static final String DEFAULT_TRUST_STORE_TYPE = KeyStore.getDefaultType();
+
+    private SSLSocketFactory sslSocketFactory;
+
+    @PostConstruct
+    public void initSSL() throws GeneralSecurityException, IOException {
+        if (sslSocketFactory == null) {
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(DEFAULT_KEY_ALGORITHM);
+            KeyStore keyStore = KeyStore.getInstance(DEFAULT_KEY_STORE_TYPE);
+            FileInputStream keyStoreFis = null;
+            try {
+                keyStoreFis = new FileInputStream(JKS_PATH);
+                keyStore.load(keyStoreFis, JKS_PWD.toCharArray());
+                keyManagerFactory.init(keyStore, JKS_PWD.toCharArray());
+            } finally {
+                if (keyStoreFis != null) {
+                    keyStoreFis.close();
+                }
+            }
+
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(DEFAULT_TRUST_ALGORITHM);
+            KeyStore trustStore = KeyStore.getInstance(DEFAULT_TRUST_STORE_TYPE);
+            FileInputStream trustStoreFis = null;
+            try {
+                trustStoreFis = new FileInputStream(JKS_PATH);
+                trustStore.load(trustStoreFis, JKS_PWD.toCharArray());
+                trustManagerFactory.init(trustStore);
+            } finally {
+                if (trustStoreFis != null) {
+                    trustStoreFis.close();
+                }
+            }
+
+            SSLContext sslContext = SSLContext.getInstance(DEFAULT_SSL_PROTOCOL);
+            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), new SecureRandom());
+            sslSocketFactory = sslContext.getSocketFactory();
+
+            if (sslSocketFactory != null) {
+                httpClient.setSslSocketFactory(sslSocketFactory);
+            }
+
+            httpClient.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT, TimeUnit.MILLISECONDS);
+            httpClient.setReadTimeout(DEFAULT_READ_TIMEOUT, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    public String send(String txCode, String requestData) throws PKIException {
+
+        String url = URL + "platId/" + PLAT_ID + "/txCode/" + txCode + "/transaction";
+
+        String signature = SecurityUtil.p7SignMessageDetach(JKS_PATH, JKS_PWD, ALIAS, requestData);
+
+        FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
+        formEncodingBuilder.add(DATA, requestData);
+        formEncodingBuilder.add(SIGNATURE, signature);
+
+        Request request = new Request.Builder().url(url).post(formEncodingBuilder.build()).build();
+
+        try {
+            Response response = httpClient.newCall(request).execute();
+            String responseBodyString = response.body().string();
+            logger.info(" txCode: " + txCode + ", response body: " + responseBodyString);
+            return responseBodyString;
+        } catch (IOException e) {
+            logger.error(e.getLocalizedMessage(), e);
+            return e.getMessage();
+        }
+    }
+
+    public byte[] getContractFile(String contractNo) {
+        try {
+            String url = URL + "platId/" + PLAT_ID + "/contractNo/" + contractNo + "/downloading";
+
+            Request request = new Request.Builder().url(url).get().build();
+            Response response = httpClient.newCall(request).execute();
+
+            logger.info("get file, response code: " + response.code());
+            logger.info("get file, response message: " + response.message());
+
+            return response.body().bytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
--- ttsd-anxin-sign/src/main/java/com/tuotiansudai/cfca/connector/HttpConnector.java
+++ ttsd-anxin-sign/src/main/java/com/tuotiansudai/cfca/connector/HttpConnector.java
@@ -1,170 +1,177 @@
-package com.tuotiansudai.cfca.connector;
-
-import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URLEncoder;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import com.tuotiansudai.cfca.constant.MIMEType;
-import com.tuotiansudai.cfca.constant.Request;
-import com.tuotiansudai.cfca.constant.SystemConst;
-import com.tuotiansudai.cfca.util.CommonUtil;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
-public class HttpConnector {
-
-    private static final Logger logger = Logger.getLogger(HttpConnector.class);
-
-    public static String JKS_PATH;
-
-    public static String JKS_PWD;
-
-    public static String ALIAS;
-
-    public static String URL;
-
-    @Value("${anxin.jks.path}")
-    public void setJksPath(String jksPath) {
-        JKS_PATH = jksPath;
-    }
-
-    @Value("${anxin.jks.pwd}")
-    public void setJksPwd(String jksPwd) {
-        JKS_PWD = jksPwd;
-    }
-
-    @Value("${anxin.alias}")
-    public void setAlias(String alias) {
-        ALIAS = alias;
-    }
-
-    @Value("${anxin.url}")
-    public void setUrl(String url) {
-        URL = url;
-    }
-
-    public int connectTimeout = 3000;
-    public int readTimeout = 10000;
-    public String channel = "ttsd";
-    public boolean isSSL = true;
-    public String keyStorePath = JKS_PATH;
-    public String keyStorePassword = JKS_PWD;
-    public String trustStorePath = JKS_PATH;
-    public String trustStorePassword = JKS_PWD;
-
-    private HttpClient httpClient;
-
-    public void init() {
-        httpClient = new HttpClient();
-        httpClient.config.connectTimeout = connectTimeout;
-        httpClient.config.readTimeout = readTimeout;
-        httpClient.httpConfig.userAgent = "TrustSign FEP";
-        httpClient.httpConfig.contentType = MIMEType.FORM;
-        httpClient.httpConfig.accept = MIMEType.JSON;
-        try {
-            if (isSSL) {
-                httpClient.initSSL(keyStorePath, keyStorePassword.toCharArray(), trustStorePath, trustStorePassword.toCharArray());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (!URL.endsWith("/")) {
-            URL += "/";
-        }
-    }
-
-    public String post(String uri, String data, String signature) {
-        return deal(uri, "POST", prepare(data, signature, null));
-    }
-
-    public String post(String uri, String data, String signature, Map<String, String> map) {
-        return deal(uri, "POST", prepare(data, signature, map));
-    }
-
-    public String post(String uri, String data, String signature, File file) {
-        return deal(uri, "POST", data, file, signature);
-    }
-
-    public byte[] getFile(String uri) {
-        HttpURLConnection connection = null;
-        try {
-            connection = httpClient.connect(URL + uri, "GET");
-            int responseCode = httpClient.send(connection, null);
-            logger.info("responseCode: " + responseCode);
-            if (responseCode != 200) {
-                logger.info(CommonUtil.getString(httpClient.receive(connection)));
-            }
-
-            return httpClient.receive(connection);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            httpClient.disconnect(connection);
-        }
-    }
-
-    private String prepare(String data, String signature, Map<String, String> map) {
-        try {
-            StringBuilder request = new StringBuilder();
-            request.append(Request.CHANNEL).append("=").append(URLEncoder.encode(channel, SystemConst.DEFAULT_CHARSET));
-            if (CommonUtil.isNotEmpty(data)) {
-                request.append("&").append(Request.DATA).append("=").append(URLEncoder.encode(data, SystemConst.DEFAULT_CHARSET));
-            }
-            if (CommonUtil.isNotEmpty(signature)) {
-                request.append("&").append(Request.SIGNATURE).append("=").append(URLEncoder.encode(signature, SystemConst.DEFAULT_CHARSET));
-            }
-            if (CommonUtil.isNotEmpty(map)) {
-                for (Entry<String, String> pair : map.entrySet()) {
-                    request.append("&").append(pair.getKey()).append("=")
-                            .append(pair.getValue() == null ? "" : URLEncoder.encode(pair.getValue(), SystemConst.DEFAULT_CHARSET));
-                }
-            }
-            return request.toString();
-        } catch (UnsupportedEncodingException e) {
-            return null;
-        }
-    }
-
-    private String deal(String uri, String method, String request) {
-        HttpURLConnection connection = null;
-        try {
-            connection = httpClient.connect(URL + uri, method);
-            logger.info("request URL: " + URL + uri);
-            logger.info("request method: " + method);
-            logger.info("request content: " + request);
-            int responseCode = httpClient.send(connection, request == null ? null : CommonUtil.getBytes(request));
-            logger.info("responseCode: " + responseCode);
-            return CommonUtil.getString(httpClient.receive(connection));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        } finally {
-            httpClient.disconnect(connection);
-        }
-    }
-
-    private String deal(String uri, String method, String request, File file, String signature) {
-        HttpURLConnection connection = null;
-        try {
-            connection = httpClient.connect(URL + uri, method);
-            logger.info("request URL: " + URL + uri);
-            logger.info("request method: " + method);
-            logger.info("request content: " + request);
-            int responseCode = httpClient.send(connection, request == null ? null : CommonUtil.getBytes(request), file, signature);
-            logger.info("responseCode: " + responseCode);
-            return CommonUtil.getString(httpClient.receive(connection));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        } finally {
-            httpClient.disconnect(connection);
-        }
-    }
-
-}
+//package com.tuotiansudai.cfca.connector;
+//
+//import java.io.File;
+//import java.io.UnsupportedEncodingException;
+//import java.net.HttpURLConnection;
+//import java.net.URLEncoder;
+//import java.util.Map;
+//import java.util.Map.Entry;
+//
+//import com.tuotiansudai.cfca.constant.MIMEType;
+//import com.tuotiansudai.cfca.constant.SystemConst;
+//import com.tuotiansudai.cfca.util.CommonUtil;
+//import org.apache.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class HttpConnector {
+//
+//    private static final Logger logger = Logger.getLogger(HttpConnector.class);
+//
+//    public static String JKS_PATH;
+//
+//    public static String JKS_PWD;
+//
+//    public static String ALIAS;
+//
+//    public static String URL;
+//
+//    @Value("${anxin.jks.path}")
+//    public void setJksPath(String jksPath) {
+//        JKS_PATH = jksPath;
+//    }
+//
+//    @Value("${anxin.jks.pwd}")
+//    public void setJksPwd(String jksPwd) {
+//        JKS_PWD = jksPwd;
+//    }
+//
+//    @Value("${anxin.alias}")
+//    public void setAlias(String alias) {
+//        ALIAS = alias;
+//    }
+//
+//    @Value("${anxin.url}")
+//    public void setUrl(String url) {
+//        URL = url;
+//    }
+//
+//    @Autowired
+//    private AnxinClient anxinClient;
+//
+//    public int connectTimeout = 3000;
+//    public int readTimeout = 10000;
+//    public String channel = "ttsd";
+//
+//    private static final String DATA = "data";
+//    private static final String SIGNATURE = "signature";
+//
+//    public boolean isSSL = true;
+//    public String keyStorePath = JKS_PATH;
+//    public String keyStorePassword = JKS_PWD;
+//    public String trustStorePath = JKS_PATH;
+//    public String trustStorePassword = JKS_PWD;
+//
+//    private HttpClient httpClient;
+//
+//    public void init() {
+//        httpClient = new HttpClient();
+//        httpClient.config.connectTimeout = connectTimeout;
+//        httpClient.config.readTimeout = readTimeout;
+//        httpClient.httpConfig.userAgent = "TrustSign FEP";
+//        httpClient.httpConfig.contentType = MIMEType.FORM;
+//        httpClient.httpConfig.accept = MIMEType.JSON;
+//        try {
+//            if (isSSL) {
+//                httpClient.initSSL(keyStorePath, keyStorePassword.toCharArray(), trustStorePath, trustStorePassword.toCharArray());
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        if (!URL.endsWith("/")) {
+//            URL += "/";
+//        }
+//    }
+//
+//    public String post(String uri, String data, String signature) {
+//        return deal(uri, "POST", prepare(data, signature, null));
+//    }
+//
+//    public String post(String uri, String data, String signature, Map<String, String> map) {
+//        return deal(uri, "POST", prepare(data, signature, map));
+//    }
+//
+//    public String post(String uri, String data, String signature, File file) {
+//        return deal(uri, "POST", data, file, signature);
+//    }
+//
+//    public byte[] getFile(String uri) {
+//        HttpURLConnection connection = null;
+//        try {
+//            connection = httpClient.connect(URL + uri, "GET");
+//            int responseCode = httpClient.send(connection, null);
+//            logger.info("responseCode: " + responseCode);
+//            if (responseCode != 200) {
+//                logger.info(CommonUtil.getString(httpClient.receive(connection)));
+//            }
+//
+//            return httpClient.receive(connection);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        } finally {
+//            httpClient.disconnect(connection);
+//        }
+//    }
+//
+//    private String prepare(String data, String signature, Map<String, String> map) {
+//        try {
+//            StringBuilder request = new StringBuilder();
+//            request.append(channel).append("=").append(URLEncoder.encode(channel, SystemConst.DEFAULT_CHARSET));
+//            if (CommonUtil.isNotEmpty(data)) {
+//                request.append("&").append(DATA).append("=").append(URLEncoder.encode(data, SystemConst.DEFAULT_CHARSET));
+//            }
+//            if (CommonUtil.isNotEmpty(signature)) {
+//                request.append("&").append(SIGNATURE).append("=").append(URLEncoder.encode(signature, SystemConst.DEFAULT_CHARSET));
+//            }
+//            if (CommonUtil.isNotEmpty(map)) {
+//                for (Entry<String, String> pair : map.entrySet()) {
+//                    request.append("&").append(pair.getKey()).append("=")
+//                            .append(pair.getValue() == null ? "" : URLEncoder.encode(pair.getValue(), SystemConst.DEFAULT_CHARSET));
+//                }
+//            }
+//            return request.toString();
+//        } catch (UnsupportedEncodingException e) {
+//            return null;
+//        }
+//    }
+//
+//    private String deal(String uri, String method, String request) {
+//        HttpURLConnection connection = null;
+//        try {
+//            connection = httpClient.connect(URL + uri, method);
+//            logger.info("request URL: " + URL + uri);
+//            logger.info("request method: " + method);
+//            logger.info("request content: " + request);
+//            int responseCode = httpClient.send(connection, request == null ? null : CommonUtil.getBytes(request));
+//            logger.info("responseCode: " + responseCode);
+//            return CommonUtil.getString(httpClient.receive(connection));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return e.getMessage();
+//        } finally {
+//            httpClient.disconnect(connection);
+//        }
+//    }
+//
+//    private String deal(String uri, String method, String request, File file, String signature) {
+//        HttpURLConnection connection = null;
+//        try {
+//            connection = httpClient.connect(URL + uri, method);
+//            logger.info("request URL: " + URL + uri);
+//            logger.info("request method: " + method);
+//            logger.info("request content: " + request);
+//            int responseCode = httpClient.send(connection, request == null ? null : CommonUtil.getBytes(request), file, signature);
+//            logger.info("responseCode: " + responseCode);
+//            return CommonUtil.getString(httpClient.receive(connection));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return e.getMessage();
+//        } finally {
+//            httpClient.disconnect(connection);
+//        }
+//    }
+//
+//}
