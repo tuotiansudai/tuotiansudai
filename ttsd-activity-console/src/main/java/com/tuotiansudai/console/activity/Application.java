74c17dd709bdbc4bcb79046e40044c865cd3d17d
Xiduan Gao <gaoxiduan@gmail.com>
2016-11-22 14:00:10 +0800

    spring boot mydefaultcookieserializer

--- ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/Application.java
+++ ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/Application.java
@@ -2,7 +2,6 @@ package com.tuotiansudai.console.activity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
@@ -22,3 +21,4 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
 }
+
