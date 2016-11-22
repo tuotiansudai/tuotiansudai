6a861380a9c720494b20b3d8886a77b6c3e5c840
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-11-22 14:52:56 +0800

    create anxin optimize

--- ttsd-service/src/main/java/com/tuotiansudai/job/AnxinCreateContractJob.java
+++ ttsd-service/src/main/java/com/tuotiansudai/job/AnxinCreateContractJob.java
@@ -1,31 +1,14 @@
 package com.tuotiansudai.job;
 
 import com.tuotiansudai.anxin.service.AnxinSignService;
-import com.tuotiansudai.anxin.service.impl.AnxinSignServiceImpl;
-import com.tuotiansudai.cfca.dto.AnxinContractType;
-import com.tuotiansudai.client.RedisWrapperClient;
-import com.tuotiansudai.client.SmsWrapperClient;
-import com.tuotiansudai.dto.sms.GenerateContractErrorNotifyDto;
-import com.tuotiansudai.repository.model.InvestModel;
-import com.tuotiansudai.service.InvestService;
-import com.tuotiansudai.transfer.repository.mapper.TransferApplicationMapper;
-import com.tuotiansudai.transfer.repository.model.TransferApplicationModel;
-import com.tuotiansudai.util.JobManager;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.joda.time.DateTime;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
-import java.util.Date;
-import java.util.List;
 
 @Component
 public class AnxinCreateContractJob implements Job {
@@ -48,7 +31,6 @@ public class AnxinCreateContractJob implements Job {
             logger.info(MessageFormat.format("trigger anxin create contract handle job, execute job end. businessId:{0}", String.valueOf(businessId)));
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage(), e);
-            return;
         }
     }
 }
