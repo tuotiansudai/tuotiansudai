4e9183da3aed1db3804f2e9d6f4f083e2f6b9205
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-11-23 14:51:09 +0800

    mobile app annotation

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppAdvertisementController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppAdvertisementController.java
@@ -4,6 +4,8 @@ import com.tuotiansudai.api.dto.v1_0.AdvertisementResponseDataDto;
 import com.tuotiansudai.api.dto.v1_0.BaseParamDto;
 import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
 import com.tuotiansudai.api.service.v1_0.MobileAppAdvertisementService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(description = "广告页")
 public class MobileAppAdvertisementController extends MobileAppBaseController {
 
     @Autowired
     private MobileAppAdvertisementService mobileAppAdvertisementService;
 
     @RequestMapping(value = "/get/advertisement", method = RequestMethod.POST)
+    @ApiOperation("获取广告页")
     public BaseResponseDto<AdvertisementResponseDataDto> getAppAdvertisement(@RequestBody BaseParamDto requestDto) {
         return mobileAppAdvertisementService.generateAdvertisement(requestDto);
     }
--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/ActivityCenterDataDto.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/ActivityCenterDataDto.java
@@ -2,17 +2,27 @@ package com.tuotiansudai.api.dto.v1_0;
 
 
 import com.tuotiansudai.activity.repository.model.ActivityModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.joda.time.DateTime;
 
 import java.util.Date;
 
 public class ActivityCenterDataDto {
+    @ApiModelProperty(value = "描述", example = "红包、会员、神秘大奖应有尽有！")
     private String descTitle;
+    @ApiModelProperty(value = "图片地址", example = "13800138000")
     private String imageUrl;
+    @ApiModelProperty(value = "活动链接", example = "https://tuotiansudai.com/activity/hero-ranking")
     private String activityUrl;
+    @ApiModelProperty(value = "标题", example = "周年庆")
     private String title;
+    @ApiModelProperty(value = "分享链接", example = "https://tuotiansudai.com/activity/hero-ranking")
     private String sharedUrl;
+    @ApiModelProperty(value = "活动内容", example = "不知道为什么，就想送你超级会员，红包，还有神秘大奖")
     private String content;
+    @ApiModelProperty(value = "活动开始时间", example = "Fri Jul 22 17:59:32 CST 2016")
     private Date activatedTime;
+    @ApiModelProperty(value = "活动结束时间", example = "Fri Jul 22 17:59:33 CST 2016")
     private Date expiredTime;
 
 
