4eaf4b95ad0498ed564fe644ceccb2a7ccaed793
XuanboHuo <huoxuanbo@tuotiansudai.com>
2016-11-23 21:46:08 +0800

    coupon message

--- ttsd-message/src/main/java/com/tuotiansudai/message/util/UserMessageEventGenerator.java
+++ ttsd-message/src/main/java/com/tuotiansudai/message/util/UserMessageEventGenerator.java
@@ -309,7 +309,7 @@ public class UserMessageEventGenerator {
     @Transactional
     public void generateCouponExpiredAlertEvent(String loginName) {
         long times = loginLogMapper.countSuccessTimesOnDate(loginName, new Date(), MessageFormat.format("login_log_{0}", new DateTime().toString("yyyyMM")));
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy{年}MM{月}dd{日}");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");
         String endTime = simpleDateFormat.format(DateTime.now().plusDays(5).withTimeAtStartOfDay().toDate());
         if (times == 1) {
             MessageModel messageModel = messageMapper.findActiveByEventType(MessageEventType.COUPON_5DAYS_EXPIRED_ALERT);
