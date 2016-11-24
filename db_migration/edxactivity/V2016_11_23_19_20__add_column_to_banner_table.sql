f081b1d86e621be46daae4197a61a2bcc5612e36
gengbj <gengbj@tuotiansudai.com>
2016-11-24 14:14:35 +0800

    fix add column is null

--- db_migration/edxactivity/V2016_11_23_19_20__add_column_to_banner_table.sql
+++ db_migration/edxactivity/V2016_11_23_19_20__add_column_to_banner_table.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `edxactivity`.`banner`
-ADD COLUMN `jump_to_link` VARCHAR(50) NOT NULL AFTER `app_url`;
+ADD COLUMN `jump_to_link` VARCHAR(50) AFTER `app_url`;
--- db_migration/edxactivity/V2016_11_23_20_23__add_column_to_promotion_table.sql
+++ db_migration/edxactivity/V2016_11_23_20_23__add_column_to_promotion_table.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `edxactivity`.`activity`
-ADD COLUMN `jump_to_link` VARCHAR(50) NOT NULL AFTER `app_activity_url`;
+ADD COLUMN `jump_to_link` VARCHAR(50) AFTER `app_activity_url`;
