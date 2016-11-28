f66ffc4166830c256edd98a247066e21da685c81
fulanyan <fly39244080@163.com>
2016-11-28 11:10:36 +0800

    change some style

--- ttsd-activity-web/src/main/webapp/activity/js/christmas_day.js
+++ ttsd-activity-web/src/main/webapp/activity/js/christmas_day.js
@@ -1,4 +1,31 @@
 require(['jquery'], function ($) {
 
+    var redirect = globalFun.browserRedirect();
+    var $christmasDayFrame=$('#christmasDayFrame');
+    var $activitySlide = $christmasDayFrame.prev();
+
+    //为节约手机流量，把pc页面的图片在pc页上显示才增加
+    (function() {
+        if(redirect=='pc') {
+            var HTML='<div class="body-decorate" id="bodyDecorate"> ' +
+                '<div class="bg-left"></div> ' +
+                '<div class="bg-right"></div> ' +
+                '<div class="bg-bottom"></div> ' +
+                '</div>';
+            $christmasDayFrame.prepend(HTML);
+            $activitySlide.addClass('pc-img');
+        }
+        else if(redirect=='mobile'){
+            $activitySlide.html('<img src='+staticServer+'/activity/images/christmas-day/bg-title.png>');
+        }
+    })();
+
+    (function() {
+
+       
+
+
+    })();
+
 
 });
--- ttsd-activity-web/src/main/webapp/templates/activities/christmas-day.ftl
+++ ttsd-activity-web/src/main/webapp/templates/activities/christmas-day.ftl
@@ -1,14 +1,8 @@
 <#import "../macro/global.ftl" as global>
 <@global.main pageCss="${css.christmas_day}" pageJavascript="${js.christmas_day}" activeNav="" activeLeftNav="" title="圣诞节活动_活动中心_拓天速贷" keywords="圣诞礼物,圣诞节投资,拓天活动,拓天速贷" description="拓天速贷圣诞节活动帮圣诞老人投资铺路,圣诞节投资可享1%加息券,并按投资比例瓜分20000元红包大奖,还可领取圣诞礼物.">
 
-<div class="activity-slide">
-</div>
+<div class="activity-slide"></div>
 <div class="christmas-day-frame" id="christmasDayFrame">
-    <div class="body-decorate" id="bodyDecorate">
-        <div class="bg-left"></div>
-        <div class="bg-right"></div>
-        <div class="bg-bottom"></div>
-    </div>
     <div class="christmas-day-inner">
         <div class="christmas-border box-notice">
             <div class="man-decorate"></div>
