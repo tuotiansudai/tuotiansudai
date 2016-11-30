a2f850180a5d03f2fe0098956a2295a9f3b62b3d
zhangzhigang <zhangzhigang@tuotiansudai.com>
2016-11-30 12:07:19 +0800

    fix unittest bug on mq consumer

--- ttsd-message-mq-consumer/src/test/java/com/tuotiansudai/mq/consumer/message/CouponAssignedUserMessageSendingMessageConsumerTest.java
+++ ttsd-message-mq-consumer/src/test/java/com/tuotiansudai/mq/consumer/message/CouponAssignedUserMessageSendingMessageConsumerTest.java
@@ -1,17 +1,25 @@
 package com.tuotiansudai.mq.consumer.message;
 
+import com.tuotiansudai.mq.consumer.MessageConsumer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class CouponAssignedUserMessageSendingMessageConsumerTest {
+
+    @Autowired
+    @Qualifier("couponAssignedUserMessageSendingMessageConsumer")
+    private MessageConsumer consumer;
 
     @Test
     @Transactional
     public void shouldConsume() {
+        System.out.printf(consumer.toString());
     }
 }
