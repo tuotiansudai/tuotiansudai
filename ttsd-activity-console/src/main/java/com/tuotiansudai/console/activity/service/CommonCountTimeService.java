5b57efed7e5cd7cec509df5db69865bb1f657035
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-12-01 18:43:29 +0800

    nea years day

--- ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/service/CommonCountTimeService.java
+++ ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/service/CommonCountTimeService.java
@@ -3,6 +3,9 @@ package com.tuotiansudai.console.activity.service;
 
 import com.google.common.collect.Lists;
 import com.tuotiansudai.activity.repository.model.ActivityCategory;
+import com.tuotiansudai.activity.repository.model.ActivityDrawLotteryTask;
+import com.tuotiansudai.point.repository.mapper.PointBillMapper;
+import com.tuotiansudai.point.repository.model.PointBusinessType;
 import com.tuotiansudai.repository.mapper.*;
 import com.tuotiansudai.repository.model.AccountModel;
 import com.tuotiansudai.repository.model.BankCardModel;
@@ -36,6 +39,9 @@ public abstract class CommonCountTimeService {
     @Autowired
     private RechargeMapper rechargeMapper;
 
+    @Autowired
+    private PointBillMapper pointBillMapper;
+
     @Value(value = "#{new java.text.SimpleDateFormat(\"yyyy-MM-dd HH:mm:ss\").parse(\"${activity.autumn.startTime}\")}")
     private Date activityAutumnStartTime;
 
@@ -54,64 +60,95 @@ public abstract class CommonCountTimeService {
     @Value("#{'${activity.new.years.period}'.split('\\~')}")
     private List<String> newYearsTime = Lists.newArrayList();
 
+    List activityTasks = Lists.newArrayList(ActivityDrawLotteryTask.REGISTER, ActivityDrawLotteryTask.EACH_REFERRER, ActivityDrawLotteryTask.EACH_REFERRER_INVEST, ActivityDrawLotteryTask.CERTIFICATION, ActivityDrawLotteryTask.BANK_CARD, ActivityDrawLotteryTask.RECHARGE, ActivityDrawLotteryTask.INVEST);
+    List newYearsActivityTask = Lists.newArrayList(ActivityDrawLotteryTask.EACH_ACTIVITY_SIGN_IN, ActivityDrawLotteryTask.REFERRER_USER, ActivityDrawLotteryTask.EACH_INVEST_5000);
+
 
     public int countDrawLotteryTime(String mobile, ActivityCategory activityCategory) {
         int lotteryTime = 0;
         UserModel userModel = userMapper.findByMobile(mobile);
-
         if (userModel == null) return lotteryTime;
 
-        Date startTime = getActivityStartTime(activityCategory);
-        Date endTime = getActivityEndTime(activityCategory);
-
-
-        switch (activityCategory){
+        switch (activityCategory) {
             case AUTUMN_PRIZE:
             case NATIONAL_PRIZE:
             case CARNIVAL_ACTIVITY:
-
-                break;
+                return countDrawLotteryTime(userModel, activityCategory, activityTasks);
             case NEW_YEARS_DAY_ACTIVITY:
-                break;
+                return countDrawLotteryTime(userModel, activityCategory, newYearsActivityTask);
         }
-
-        List<UserModel> userModels = userMapper.findUsersByRegisterTimeOrReferrer(startTime, endTime, userModel.getLoginName());
-        for (UserModel referrerUserModel : userModels) {
-            if (referrerUserModel.getRegisterTime().before(endTime) && referrerUserModel.getRegisterTime().after(startTime)) {
-                lotteryTime++;
-                if (investMapper.countInvestorSuccessInvestByInvestTime(referrerUserModel.getLoginName(), startTime, endTime) > 0) {
-                    lotteryTime++;
-                }
-            }
-        }
-
-        if (userModel.getRegisterTime().before(endTime) && userModel.getRegisterTime().after(startTime)) {
-            lotteryTime++;
-        }
-
-        AccountModel accountModel = accountMapper.findByLoginName(userModel.getLoginName());
-        if (accountModel != null && accountModel.getRegisterTime().before(endTime) && accountModel.getRegisterTime().after(startTime)) {
-            lotteryTime++;
-        }
-
-        BankCardModel bankCardModel = bankCardMapper.findPassedBankCardByLoginName(userModel.getLoginName());
-        if (bankCardModel != null && bankCardModel.getCreatedTime().before(endTime) && bankCardModel.getCreatedTime().after(startTime)) {
-            lotteryTime++;
-        }
-
-        if (rechargeMapper.findRechargeCount(null, userModel.getMobile(), null, RechargeStatus.SUCCESS, null, startTime, endTime) > 0) {
-            lotteryTime++;
-        }
-
-        if (investMapper.countInvestorSuccessInvestByInvestTime(userModel.getLoginName(), startTime, endTime) > 0) {
-            lotteryTime++;
-        }
-
         return lotteryTime;
     }
 
+    private int countDrawLotteryTime(UserModel userModel, ActivityCategory activityCategory, List<ActivityDrawLotteryTask> activityDrawLotteryTasks) {
+        int time = 0;
+        Date startTime = getActivityStartTime(activityCategory);
+        Date endTime = getActivityEndTime(activityCategory);
 
-    protected Date getActivityStartTime(ActivityCategory activityCategory){
+        for (ActivityDrawLotteryTask activityDrawLotteryTask : activityDrawLotteryTasks) {
+            switch (activityDrawLotteryTask) {
+                case REGISTER:
+                    if (userModel.getRegisterTime().before(endTime) && userModel.getRegisterTime().after(startTime)) {
+                        time++;
+                    }
+                    break;
+                case EACH_REFERRER:
+                    List<UserModel> userModels = userMapper.findUsersByRegisterTimeOrReferrer(startTime, endTime, userModel.getLoginName());
+                    for (UserModel referrerUserModel : userModels) {
+                        if (referrerUserModel.getRegisterTime().before(endTime) && referrerUserModel.getRegisterTime().after(startTime)) {
+                            time++;
+                        }
+                    }
+                    break;
+                case EACH_REFERRER_INVEST:
+                    List<UserModel> referrerUserModels = userMapper.findUsersByRegisterTimeOrReferrer(startTime, endTime, userModel.getLoginName());
+                    for (UserModel referrerUserModel : referrerUserModels) {
+                        if (investMapper.countInvestorSuccessInvestByInvestTime(referrerUserModel.getLoginName(), startTime, endTime) > 0) {
+                            time++;
+                        }
+                    }
+                    break;
+                case CERTIFICATION:
+                    AccountModel accountModel = accountMapper.findByLoginName(userModel.getLoginName());
+                    if (accountModel != null && accountModel.getRegisterTime().before(endTime) && accountModel.getRegisterTime().after(startTime)) {
+                        time++;
+                    }
+                    break;
+                case BANK_CARD:
+                    BankCardModel bankCardModel = bankCardMapper.findPassedBankCardByLoginName(userModel.getLoginName());
+                    if (bankCardModel != null && bankCardModel.getCreatedTime().before(endTime) && bankCardModel.getCreatedTime().after(startTime)) {
+                        time++;
+                    }
+                    break;
+                case RECHARGE:
+                    if (rechargeMapper.findRechargeCount(null, userModel.getMobile(), null, RechargeStatus.SUCCESS, null, startTime, endTime) > 0) {
+                        time++;
+                    }
+                    break;
+                case INVEST:
+                    if (investMapper.countInvestorSuccessInvestByInvestTime(userModel.getLoginName(), startTime, endTime) > 0) {
+                        time++;
+                    }
+                    break;
+                case EACH_ACTIVITY_SIGN_IN:
+                    time += pointBillMapper.findCountPointBillPagination(userModel.getLoginName(), startTime, endTime, Lists.newArrayList(PointBusinessType.SIGN_IN));
+                    break;
+                case REFERRER_USER:
+                    List<UserModel> referrerUsers = userMapper.findUsersByRegisterTimeOrReferrer(startTime, endTime, userModel.getLoginName());
+                    time += referrerUsers.size() * 5;
+                    break;
+                case EACH_INVEST_5000:
+
+                    break;
+
+            }
+        }
+
+        return time;
+    }
+
+
+    private Date getActivityStartTime(ActivityCategory activityCategory) {
         switch (activityCategory) {
             case AUTUMN_PRIZE:
                 return activityAutumnStartTime;
@@ -125,7 +162,7 @@ public abstract class CommonCountTimeService {
         return null;
     }
 
-    protected Date getActivityEndTime(ActivityCategory activityCategory){
+    private Date getActivityEndTime(ActivityCategory activityCategory) {
         switch (activityCategory) {
             case AUTUMN_PRIZE:
                 return activityAutumnEndTime;
--- ttsd-edxactivity-model/src/main/java/com/tuotiansudai/activity/repository/model/ActivityDrawLotteryTask.java
+++ ttsd-edxactivity-model/src/main/java/com/tuotiansudai/activity/repository/model/ActivityDrawLotteryTask.java
@@ -1,18 +1,26 @@
 package com.tuotiansudai.activity.repository.model;
 
 
-public enum  ActivityDrawLotteryTask {
-    REGISTER_TASK("活动期间内注册"),
-    REFERRER_USER("活动期间内邀请好友"),
-    ACCOUNT_USER("活动期间内邀请好友"),
-    ;
+public enum ActivityDrawLotteryTask {
+    REGISTER("注册", 1),
+    EACH_REFERRER("邀请好友送一次", 1),
+    EACH_REFERRER_INVEST("邀请好友后投资", 1),
+    CERTIFICATION("实名认证", 1),
+    BANK_CARD("绑卡", 1),
+    RECHARGE("充值", 1),
+    INVEST("投资", 1),
+    EACH_ACTIVITY_SIGN_IN("活动签到", 1),
+    REFERRER_USER("邀请好友送5次机会", 5),
+    EACH_INVEST_5000("活动签到", 1);
 
-
-    ActivityDrawLotteryTask(String description){
+    ActivityDrawLotteryTask(String description, int time) {
         this.description = description;
+        this.time = time;
     }
 
     private String description;
+
+    private int time;
 
     public String getDescription() {
         return description;
