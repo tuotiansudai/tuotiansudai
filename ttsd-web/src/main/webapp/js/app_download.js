23040b1be86d5412b5b5e09d33957b4a4a5c2d78
fulanyan <fly39244080@163.com>
2016-12-06 18:31:28 +0800

    app download

--- ttsd-web/src/main/webapp/js/app_download.js
+++ ttsd-web/src/main/webapp/js/app_download.js
@@ -1,30 +1,45 @@
-
 window.onload=function() {
+
     function equipment() {
         var ua = navigator.userAgent.toLowerCase(),
             which={};
-        which.wechat=false;
         var is_weixin=ua.match(/MicroMessenger/i) == "micromessenger";
         if(is_weixin) {
             which.wechat=true;
+            if(ua.match('android')) {
+                which.kind='android';
+            }
+            else if(ua.match('iphone') || ua.match('ipad')) {
+                which.kind='weIos';
+            }
         }
-        if(ua.match('android')) {
-            which.kind='android';
-        }
-        else if(ua.match('iphone') || ua.match('ipad')) {
-            which.kind='ios';
+        else {
+            which.wechat=false;
+
+            if(ua.match('android')) {
+                which.kind='android';
+            }
+            else if(ua.match('iphone') || ua.match('ipad')) {
+                which.kind='ios';
+            }
         }
         return which;
     }
 
     var equipment=equipment();
-    globalFun.$('#btnDownload').onclick=function() {
-        if(equipment.kind=='android') {
-            //安卓
-            window.location.href = "https://tuotiansudai.com/app/tuotiansudai.apk";
-        }
-        else if(equipment.kind=='ios') {
-            window.location.href ="https://itunes.apple.com/cn/app/ta-tian-su-dai/id1039233966?mt=8";
-        }
+    if(equipment.wechat && equipment.kind=='android') {
+        document.getElementById('wechatAndroid').style.display='block';
+    }
+    else {
+        document.getElementById('normalFrame').style.display='block';
+    }
+
+    document.getElementById('btnDownload').onclick=function() {
+        var categoryUrl={
+            'android':'https://tuotiansudai.com/app/tuotiansudai.apk',
+            'ios':'http://itunes.apple.com/us/app/id1039233966',
+            'weIos':'http://a.app.qq.com/o/simple.jsp?pkgname=com.tuotiansudai.tuotianclient'
+        };
+        location.href = categoryUrl[equipment.kind];
     }
 }
