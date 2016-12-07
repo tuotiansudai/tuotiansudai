f600fce1b65d49ba84c47e5ff88e57ecfdd25051
fulanyan <fly39244080@163.com>
2016-12-07 19:27:34 +0800

    alert

--- ttsd-web/src/main/webapp/js/app_download.js
+++ ttsd-web/src/main/webapp/js/app_download.js
@@ -7,15 +7,15 @@ window.onload=function() {
             globalFun.removeClass(globalFun.$('#wechatAndroid'),'hide');
         }
         else {
-            console.log('ok'+globalFun.categoryCodeUrl[equipment.kind]);
+            alert('ok'+globalFun.categoryCodeUrl[equipment.kind]);
             //如果是安卓手机打开页面，不管是浏览器还是支付宝，都直接下载
-            location.href = globalFun.categoryCodeUrl[equipment.kind];
+            window.location.href = globalFun.categoryCodeUrl[equipment.kind];
         }
     }
     else {
         globalFun.removeClass(globalFun.$('#normalFrame'),'hide');
         globalFun.$('#btnDownload').onclick=function() {
-            location.href = globalFun.categoryCodeUrl[equipment.kind];
+            window.location.href = globalFun.categoryCodeUrl[equipment.kind];
         }
     }
 }
