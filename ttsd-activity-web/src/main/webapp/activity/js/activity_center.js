7ccdb3a9f426227c2238f53ddac52180656f4389
jroce <472228406@qq.com>
2016-12-08 16:30:56 +0800

    add click by activity list

--- ttsd-activity-web/src/main/webapp/activity/js/activity_center.js
+++ ttsd-activity-web/src/main/webapp/activity/js/activity_center.js
@@ -1,11 +1,12 @@
 /**
  * name: [活动中心]
- * time: 2016-09-01
+ * time: 2016-12-08
  * Author: xuqiang
  */
 require(['jquery'], function($) {
 	$(function() {
-		var $filterBtn=$('.ac-item span');
+		var $filterBtn=$('.ac-item span'),
+			$listBtn=$('.actor-list .activity-box');
 		$filterBtn.on('click', function(event) {
 			event.preventDefault();
 			var $self=$(this),
@@ -13,6 +14,13 @@ require(['jquery'], function($) {
 				$actorList=$('.activity-container');
 			$self.addClass('active').siblings().removeClass('active');
 			$actorList.find('.actor-list:eq('+index+')').addClass('active').siblings().removeClass('active');
+		});
+
+		$listBtn.on('click',  function(event) {
+			event.preventDefault();
+			var $self=$(this),
+				hrefUrl=$self.attr('data-href');
+			location.href=hrefUrl;
 		});
 	});
 });
