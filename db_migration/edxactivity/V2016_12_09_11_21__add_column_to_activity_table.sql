a52c0d816ebb70bd560dc6f628f519e1ea6e277b
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-12-13 12:14:43 +0800

    fix test

--- db_migration/edxactivity/V2016_12_09_11_21__add_column_to_activity_table.sql
+++ db_migration/edxactivity/V2016_12_09_11_21__add_column_to_activity_table.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `edxactivity`.`activity`
-ADD COLUMN `app_vertical_picture_url` VARCHAR(150) NOT NULL AFTER `app_picture_url`;
+ADD COLUMN `app_vertical_picture_url` VARCHAR(150) NULL AFTER `app_picture_url`;
