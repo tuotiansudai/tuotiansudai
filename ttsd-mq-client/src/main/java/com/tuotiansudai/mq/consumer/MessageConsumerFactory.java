7451a651be0b519983e228f565c724f3b3e294e9
zhangzhigang <zhangzhigang@tuotiansudai.com>
2016-12-14 12:05:14 +0800

    update implement for shutdown consumer thread

--- ttsd-mq-client/src/main/java/com/tuotiansudai/mq/consumer/MessageConsumerFactory.java
+++ ttsd-mq-client/src/main/java/com/tuotiansudai/mq/consumer/MessageConsumerFactory.java
@@ -2,15 +2,16 @@ package com.tuotiansudai.mq.consumer;
 
 import com.tuotiansudai.mq.client.MQConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
-import sun.misc.Signal;
-import sun.misc.SignalHandler;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class MessageConsumerFactory implements SignalHandler {
-    private final String STOP_SIGNAL = "TERM";
+public class MessageConsumerFactory implements ApplicationListener<ContextClosedEvent> {
     private final MQConsumer mqConsumer;
     private Set<MessageConsumer> messageConsumers;
+    private Set<Thread> consumerThreads;
 
     @Autowired
     public void setMessageConsumers(Set<MessageConsumer> messageConsumers) {
@@ -23,26 +24,28 @@ public class MessageConsumerFactory implements SignalHandler {
     }
 
     public void start() {
-        registerSignalHandler();
-        if (messageConsumers != null) {
-            messageConsumers.forEach(consumer ->
-                    new Thread(() ->
-                            mqConsumer.subscribe(consumer.queue(), consumer::consume),
-                            "Consumer-" + consumer.queue().getQueueName()
-                    ).start()
-            );
+        if (messageConsumers == null) {
+            return;
         }
+        consumerThreads = messageConsumers.stream()
+                .map(this::startSubscribe)
+                .collect(Collectors.toSet());
+    }
+
+    private Thread startSubscribe(MessageConsumer consumer) {
+        Thread thread = new Thread(() -> mqConsumer.subscribe(consumer.queue(), consumer::consume), "Consumer-" + consumer.queue().getQueueName());
+        thread.start();
+        return thread;
     }
 
     @Override
-    public void handle(Signal signal) {
-        if (STOP_SIGNAL.equals(signal.getName())) {
-            mqConsumer.stopSubscribe();
+    public void onApplicationEvent(ContextClosedEvent event) {
+        mqConsumer.stopSubscribe();
+        for (Thread consumerThread : consumerThreads) {
+            try {
+                consumerThread.join();
+            } catch (InterruptedException ignored) {
+            }
         }
     }
-
-    private void registerSignalHandler() {
-        Signal.handle(new Signal(STOP_SIGNAL), this);
-    }
-
 }
