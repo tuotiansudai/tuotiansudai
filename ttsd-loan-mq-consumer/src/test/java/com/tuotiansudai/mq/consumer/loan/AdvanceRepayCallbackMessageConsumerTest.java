f244a08905a4814fa49560bf04904cfcc0e7cbf1
gengbj <gengbj@tuotiansudai.com>
2016-12-20 16:44:13 +0800

    fix unit test

--- ttsd-loan-mq-consumer/src/test/java/com/tuotiansudai/mq/consumer/loan/AdvanceRepayCallbackMessageConsumerTest.java
+++ ttsd-loan-mq-consumer/src/test/java/com/tuotiansudai/mq/consumer/loan/AdvanceRepayCallbackMessageConsumerTest.java
@@ -34,7 +34,7 @@ public class AdvanceRepayCallbackMessageConsumerTest {
         when(payWrapperClient.advanceRepayInvestPayback(captor.capture())).thenReturn(new BaseDto<PayDataDto>());
         String message = "1000005";
         consumer.consume(message);
-        assertEquals("10005", captor.getValue());
+        assertEquals("1000005", captor.getValue());
     }
 
     @Test(expected = RuntimeException.class)
