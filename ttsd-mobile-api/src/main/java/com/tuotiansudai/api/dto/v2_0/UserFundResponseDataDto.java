d22515acc2701aafee09ad8d82ae09b2e28d9808
gengbj <gengbj@tuotiansudai.com>
2016-12-22 14:35:34 +0800

    fix membership expiretime format

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v2_0/UserFundResponseDataDto.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v2_0/UserFundResponseDataDto.java
@@ -4,6 +4,7 @@ import com.tuotiansudai.api.dto.v1_0.BaseResponseDataDto;
 import com.tuotiansudai.repository.model.UserFundView;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UserFundResponseDataDto extends BaseResponseDataDto {
@@ -59,8 +60,9 @@ public class UserFundResponseDataDto extends BaseResponseDataDto {
     @ApiModelProperty(value = "会员成长值", example = "10")
     private long membershipPoint; //会员成长值
 
-    @ApiModelProperty(value = "用户会员过期日yyyy-MM-dd(空表示不过期)", example = "2016-11-25 19:09:01")
-    private Date membershipExpiredDate; //用户会员过期日yyyy-MM-dd(空表示不过期)
+    @ApiModelProperty(value = "用户会员过期日yyyy-MM-dd(空表示不过期)", example = "有效期至:2016-11-25")
+    private String membershipExpiredDate; //用户会员过期日yyyy-MM-dd(空表示不过期)
+
 
     public UserFundResponseDataDto(UserFundView userFundView, long balance, long point, int membershipLevel, long membershipPoint, int usableUserCouponCount, Date membershipExpiredDate) {
         this.balance = balance;
@@ -84,7 +86,7 @@ public class UserFundResponseDataDto extends BaseResponseDataDto {
         this.membershipLevel = membershipLevel;
         this.membershipPoint = membershipPoint;
         this.usableUserCouponCount = usableUserCouponCount;
-        this.membershipExpiredDate = membershipExpiredDate;
+        this.membershipExpiredDate = membershipExpiredDate != null ? "有效期至:" + new SimpleDateFormat("yyyy-mm-dd").format(membershipExpiredDate) : null;
     }
 
     public long getBalance() {
