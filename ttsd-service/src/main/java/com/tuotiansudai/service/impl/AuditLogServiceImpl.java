b1c3a86aa5973abfed0959694cd29d739ea562ab
zhoubx <zhoubaoxin@tuotiansudai.com>
2016-12-27 20:25:44 +0800

    split auditLog query and send into two service located in console and service

--- ttsd-service/src/main/java/com/tuotiansudai/service/impl/AuditLogServiceImpl.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/impl/AuditLogServiceImpl.java
@@ -1,11 +1,7 @@
 package com.tuotiansudai.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.common.collect.Lists;
 import com.tuotiansudai.client.MQWrapperClient;
-import com.tuotiansudai.client.RedisWrapperClient;
-import com.tuotiansudai.dto.BasePaginationDataDto;
-import com.tuotiansudai.log.repository.mapper.AuditLogMapper;
 import com.tuotiansudai.log.repository.model.AuditLogModel;
 import com.tuotiansudai.log.repository.model.OperationType;
 import com.tuotiansudai.mq.client.model.MessageQueue;
@@ -14,15 +10,10 @@ import com.tuotiansudai.service.AuditLogService;
 import com.tuotiansudai.service.UserService;
 import com.tuotiansudai.util.IdGenerator;
 import com.tuotiansudai.util.JsonConverter;
-import com.tuotiansudai.util.PaginationUtil;
 import org.apache.log4j.Logger;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Date;
-import java.util.List;
 
 @Service
 public class AuditLogServiceImpl implements AuditLogService {
@@ -30,13 +21,7 @@ public class AuditLogServiceImpl implements AuditLogService {
     private static Logger logger = Logger.getLogger(AuditLogServiceImpl.class);
 
     @Autowired
-    private AuditLogMapper auditLogMapper;
-
-    @Autowired
     private IdGenerator idGenerator;
-
-    @Autowired
-    private RedisWrapperClient redisWrapperClient;
 
     @Autowired
     private MQWrapperClient mqWrapperClient;
@@ -82,35 +67,6 @@ public class AuditLogServiceImpl implements AuditLogService {
         } catch (JsonProcessingException e) {
             logger.error("[MQ] send audit log message fail.", e);
         }
-    }
-
-    public BasePaginationDataDto<AuditLogModel> getAuditLogPaginationData(OperationType operationType, String targetId, String operatorMobile, String auditorMobile, Date startTime, Date endTime, int index, int pageSize) {
-        if (startTime == null) {
-            startTime = new DateTime(0).withTimeAtStartOfDay().toDate();
-        } else {
-            startTime = new DateTime(startTime).withTimeAtStartOfDay().toDate();
-        }
-
-        if (endTime == null) {
-            endTime = new DateTime().withDate(9999, 12, 31).withTimeAtStartOfDay().toDate();
-        } else {
-            endTime = new DateTime(endTime).withTimeAtStartOfDay().plusDays(1).minusMillis(1).toDate();
-        }
-
-        long count = auditLogMapper.count(operationType, targetId, operatorMobile, auditorMobile, startTime, endTime);
-
-        List<AuditLogModel> data = Lists.newArrayList();
-        if (count > 0) {
-            int totalPages = PaginationUtil.calculateMaxPage(count, pageSize);
-            index = index > totalPages ? totalPages : index;
-            data = auditLogMapper.getPaginationData(operationType, targetId, operatorMobile, auditorMobile, startTime, endTime, (index - 1) * pageSize, pageSize);
-        }
-
-        return new BasePaginationDataDto<>(index, pageSize, count, data);
-    }
-
-    public String clearMybatisCache() {
-        return redisWrapperClient.clearMybatisCache();
     }
 
 }
