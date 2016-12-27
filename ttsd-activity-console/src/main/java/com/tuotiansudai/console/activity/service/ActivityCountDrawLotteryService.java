abdc2966998e50d6e96719baa64defbefb7c80a6
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-12-27 18:13:42 +0800

    update money

--- ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/service/ActivityCountDrawLotteryService.java
+++ ttsd-activity-console/src/main/java/com/tuotiansudai/console/activity/service/ActivityCountDrawLotteryService.java
@@ -85,9 +85,9 @@ public class ActivityCountDrawLotteryService {
     public static final String ACTIVITY_DESCRIPTION = "新年专享";
 
     //每投资5000奖励抽奖次数
-    private final long EACH_INVEST_AMOUNT_50000 = 50L;
+    private final long EACH_INVEST_AMOUNT_50000 = 500000L;
 
-    private final long EACH_INVEST_AMOUNT_20000 = 200000;
+    private final long EACH_INVEST_AMOUNT_20000 = 200000L;
 
 
     public int countDrawLotteryTime(String mobile, ActivityCategory activityCategory) {
