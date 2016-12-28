02325c22a68f26f439561656ac041eb9616f0798
fulanyan <fly39244080@163.com>
2016-12-28 19:34:12 +0800

    fix bugs

--- ttsd-activity-web/src/main/webapp/activity/js/integral_draw.js
+++ ttsd-activity-web/src/main/webapp/activity/js/integral_draw.js
@@ -6,6 +6,8 @@ require(['jquery', 'underscore','drawCircle','register_common','logintip'], func
         $myPropertyPoint=$('.gift-circle-detail .my-property',$integralDrawPage),
         tipGroupObj={};
     //以下为抽奖转盘
+    var $pointerOne = $('.pointer-img',$oneThousandPoints);
+    var $pointerTen = $('.pointer-img',$tenThousandPoints);
     var $MobileNumber=$('#MobileNumber'),
         pointAllList='/activity/point-draw/all-list',  //中奖记录接口地址
         pointUserList='/activity/point-draw/user-list',   //我的奖品接口地址
@@ -51,60 +53,64 @@ require(['jquery', 'underscore','drawCircle','register_common','logintip'], func
     drawCircleOne.MyGift();
 
     //开始抽奖
-    drawCircleOne.beginLuckDraw(function(data) {
-        //抽奖接口成功后奖品指向位置
-        if (data.returnCode == 0) {
-            var angleNum=0;
-            $myPropertyPoint.text(data.myPoint);
-            switch (data.prize) {
-                case 'BICYCLE_XM':  //平衡车
-                    angleNum=45*1-20;
-                    break;
-                case 'MASK': //口罩
-                    angleNum=45*7-20;
-                    break;
-                case 'LIPSTICK':  //唇膏
-                    angleNum=45*5-20;
-                    break;
-                case 'PORCELAIN_CUP_BY_1000':  //杯子
-                    angleNum=45*2-20;
-                    break;
-                case 'PHONE_BRACKET':  //手机支架
-                    angleNum=45*8-20;
-                    break;
-                case 'PHONE_CHARGE_10':  //10元话费
-                    angleNum=45*6-20;
-                    break;
-                case 'RED_ENVELOPE_10': //10元红包
-                    angleNum=45*4-20;
-                    break;
-                case 'INTEREST_COUPON_2_POINT_DRAW':  //加息卷
-                    angleNum=45*3-20;
-                    break;
+
+    $pointerOne.on('click', function(event) {
+        drawCircleOne.beginLuckDraw(function(data) {
+            //抽奖接口成功后奖品指向位置
+            if (data.returnCode == 0) {
+                var angleNum=0;
+                $myPropertyPoint.text(data.myPoint);
+                switch (data.prize) {
+                    case 'BICYCLE_XM':  //平衡车
+                        angleNum=45*1-20;
+                        break;
+                    case 'MASK': //口罩
+                        angleNum=45*7-20;
+                        break;
+                    case 'LIPSTICK':  //唇膏
+                        angleNum=45*5-20;
+                        break;
+                    case 'PORCELAIN_CUP_BY_1000':  //杯子
+                        angleNum=45*2-20;
+                        break;
+                    case 'PHONE_BRACKET':  //手机支架
+                        angleNum=45*8-20;
+                        break;
+                    case 'PHONE_CHARGE_10':  //10元话费
+                        angleNum=45*6-20;
+                        break;
+                    case 'RED_ENVELOPE_10': //10元红包
+                        angleNum=45*4-20;
+                        break;
+                    case 'INTEREST_COUPON_2_POINT_DRAW':  //加息卷
+                        angleNum=45*3-20;
+                        break;
+                }
+                var prizeType=data.prizeType.toLowerCase();
+                $(tipGroupObj[prizeType]).find('.prizeValue').text(data.prizeValue);
+
+                drawCircle.rotateFn(angleNum,tipGroupObj[prizeType]);
+
+            } else if(data.returnCode == 1) {
+                //积分不足
+                drawCircle.tipWindowPop(tipGroupObj['nopoint']);
             }
-            var prizeType=data.prizeType.toLowerCase();
-            $(tipGroupObj[prizeType]).find('.prizeValue').text(data.prizeValue);
+            else if (data.returnCode == 2) {
+                //未登录
 
-            drawCircle.rotateFn(angleNum,tipGroupObj[prizeType]);
+                $('.no-login-text',$integralDrawPage).trigger('click');  //弹框登录
 
-        } else if(data.returnCode == 1) {
-            //积分不足
-            drawCircle.tipWindowPop(tipGroupObj['nopoint']);
-        }
-        else if (data.returnCode == 2) {
-            //未登录
+            } else if(data.returnCode == 3){
+                //不在活动时间范围内！
+                drawCircle.tipWindowPop(tipGroupObj['expired']);
 
-            $('.no-login-text',$integralDrawPage).trigger('click');  //弹框登录
-
-        } else if(data.returnCode == 3){
-            //不在活动时间范围内！
-            drawCircle.tipWindowPop(tipGroupObj['expired']);
-
-        } else if(data.returnCode == 4){
-            //实名认证
-            drawCircle.tipWindowPop(tipGroupObj['authentication']);
-        }
+            } else if(data.returnCode == 4){
+                //实名认证
+                drawCircle.tipWindowPop(tipGroupObj['authentication']);
+            }
+        });
     });
+
 
     //点击切换按钮
     drawCircleOne.PrizeSwitch();
@@ -129,59 +135,62 @@ require(['jquery', 'underscore','drawCircle','register_common','logintip'], func
     drawCircleTen.MyGift();
 
     //开始抽奖
-    drawCircleTen.beginLuckDraw(function(data) {
-        //抽奖接口成功后奖品指向位置
-        if (data.returnCode == 0) {
-            var angleNum=0;
-            $myPropertyPoint.text(data.myPoint);
-            switch (data.prize) {
-                case 'IPHONE7_128G':  //iPhone 7手机128G
-                    angleNum=45*1-20;
-                    break;
-                case 'DELAYED_ACTION':    //通用自拍杆
-                    angleNum=45*7-20;
-                    break;
-                case 'U_DISH':  //拓天速贷U盘
-                    angleNum=45*5-20;
-                    break;
-                case 'PHONE_CHARGE_20':  //20元话费
-                    angleNum=45*2-20;
-                    break;
-                case 'HEADREST':  //车家两用U型头枕
-                    angleNum=45*8-20;
-                    break;
-                case 'IQIYI_MEMBERSHIP_30':  //爱奇艺会员月卡
-                    angleNum=45*6-20;
-                    break;
-                case 'RED_ENVELOPE_50_POINT_DRAW':  //50元投资红包
-                    angleNum=45*3-20;
-                    break;
-                case 'INTEREST_COUPON_5_POINT_DRAW':  //0.5%加息券
-                    angleNum=45*4-20;
-                    break;
+    $pointerTen.on('click', function(event) {
+        drawCircleTen.beginLuckDraw(function(data) {
+            //抽奖接口成功后奖品指向位置
+            if (data.returnCode == 0) {
+                var angleNum=0;
+                $myPropertyPoint.text(data.myPoint);
+                switch (data.prize) {
+                    case 'IPHONE7_128G':  //iPhone 7手机128G
+                        angleNum=45*1-20;
+                        break;
+                    case 'DELAYED_ACTION':    //通用自拍杆
+                        angleNum=45*7-20;
+                        break;
+                    case 'U_DISH':  //拓天速贷U盘
+                        angleNum=45*5-20;
+                        break;
+                    case 'PHONE_CHARGE_20':  //20元话费
+                        angleNum=45*2-20;
+                        break;
+                    case 'HEADREST':  //车家两用U型头枕
+                        angleNum=45*8-20;
+                        break;
+                    case 'IQIYI_MEMBERSHIP_30':  //爱奇艺会员月卡
+                        angleNum=45*6-20;
+                        break;
+                    case 'RED_ENVELOPE_50_POINT_DRAW':  //50元投资红包
+                        angleNum=45*3-20;
+                        break;
+                    case 'INTEREST_COUPON_5_POINT_DRAW':  //0.5%加息券
+                        angleNum=45*4-20;
+                        break;
+                }
+
+                var prizeType=data.prizeType.toLowerCase();
+                $(tipGroupObj[prizeType]).find('.prizeValue').text(data.prizeValue);
+
+                drawCircle.rotateFn(angleNum,tipGroupObj[prizeType]);
+            } else if(data.returnCode == 1) {
+                //积分不足
+                drawCircle.tipWindowPop(tipGroupObj['nopoint']);
             }
+            else if (data.returnCode == 2) {
+                //未登录
+                $('.no-login-text',$integralDrawPage).trigger('click');  //弹框登录
 
-            var prizeType=data.prizeType.toLowerCase();
-            $(tipGroupObj[prizeType]).find('.prizeValue').text(data.prizeValue);
+            } else if(data.returnCode == 3){
+                //不在活动时间范围内！
+                tdrawCircle.tipWindowPop(tipGroupObj['expired']);
 
-            drawCircle.rotateFn(angleNum,tipGroupObj[prizeType]);
-        } else if(data.returnCode == 1) {
-            //积分不足
-            drawCircle.tipWindowPop(tipGroupObj['nopoint']);
-        }
-        else if (data.returnCode == 2) {
-            //未登录
-            $('.no-login-text',$integralDrawPage).trigger('click');  //弹框登录
-
-        } else if(data.returnCode == 3){
-            //不在活动时间范围内！
-            tdrawCircle.tipWindowPop(tipGroupObj['expired']);
-
-        } else if(data.returnCode == 4){
-            //实名认证
-            drawCircle.tipWindowPop(tipGroupObj['authentication']);
-        }
+            } else if(data.returnCode == 4){
+                //实名认证
+                drawCircle.tipWindowPop(tipGroupObj['authentication']);
+            }
+        });
     });
+
     //点击切换按钮
     drawCircleTen.PrizeSwitch();
 
