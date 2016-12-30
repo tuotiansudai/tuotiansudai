057e8f6bfbc3e61381dd56b6999a7f4c79205c78
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2016-12-30 19:00:44 +0800

    festival activity

--- ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/LotteryDrawActivityService.java
+++ ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/LotteryDrawActivityService.java
@@ -243,6 +243,14 @@ public class LotteryDrawActivityService {
                 .put(LotteryPrize.RED_ENVELOPE_18, 327L)
                 .put(LotteryPrize.RED_ENVELOPE_8, 328L)
                 .put(LotteryPrize.INTEREST_COUPON_2_NEW_YEARS, 329L)
+                .put(LotteryPrize.SPRING_FESTIVAL_RED_ENVELOP_68, 333L)
+                .put(LotteryPrize.SPRING_FESTIVAL_RED_ENVELOP_58, 334L)
+                .put(LotteryPrize.SPRING_FESTIVAL_RED_ENVELOP_38, 335L)
+                .put(LotteryPrize.SPRING_FESTIVAL_RED_ENVELOP_18, 336L)
+                .put(LotteryPrize.SPRING_FESTIVAL_RED_ENVELOP_88, 337L)
+                .put(LotteryPrize.SPRING_FESTIVAL_RED_ENVELOP_188, 338L)
+                .put(LotteryPrize.SPRING_FESTIVAL_INTEREST_COUPON_5, 339L)
+                .put(LotteryPrize.SPRING_FESTIVAL_INTEREST_COUPON_2, 3340L)
                 .build()).get(lotteryPrize);
     }
 
