2668d574042617f4cce22fca8f6aa4c7da51c161
fulanyan <fly39244080@163.com>
2017-01-06 10:55:00 +0800

    main site

--- ttsd-frontend-manage/resources/static/ask/js/mainSite.jsx
+++ ttsd-frontend-manage/resources/static/ask/js/mainSite.jsx
@@ -1,9 +1,9 @@
 
 import {popWindow,useAjax,refreshCaptcha,countDownLoan} from "publicJs/common";
 import "publicJStyle/global.scss";
-
+import "./custom_plugins";
 import "./questions";
-import "./create-question";
+import "./create_question";
 
 //刷新验证码
 refreshCaptcha('/captcha');
