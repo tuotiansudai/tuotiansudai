25537a83201dba7aa673ae013877ffc7d01666fd
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2017-01-07 00:42:06 +0800

    bug/bs/update_message_sql

--- db_migration/edxmessage/V2017_01_04_15_25__update_message_app_url.sql
+++ db_migration/edxmessage/V2017_01_04_15_25__update_message_app_url.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
- update message set app_url = 'FUND_DETAIL_LIST' where app_url = 'ASSESS_ADMINISTER';
- update message set app_url = 'MESSAGE_CENTER_LIST' where app_url = 'NOTIFY';
- update message set app_url = 'MY_ASSESS' where app_url = 'PERSON_CENTER_HOME';
- update message set app_url = 'INVITE_FRIEND' where app_url = 'RECOMMEND_DETAIL';
- update message set app_url = 'MY_INVEST_FINISH' where app_url = 'TRANSFER_HISTORY';
+ update message set app_url = 'ASSESS_ADMINISTER' where app_url = 'FUND_DETAIL_LIST';
+ update message set app_url = 'NOTIFY' where app_url = 'MESSAGE_CENTER_LIST';
+ update message set app_url = 'PERSON_CENTER_HOME' where app_url = 'MY_ASSESS';
+ update message set app_url = 'RECOMMEND_DETAIL' where app_url = 'INVITE_FRIEND';
+ update message set app_url = 'TRANSFER_HISTORY' where app_url = 'MY_INVEST_FINISH';
 
 COMMIT;
 
