ca2cf7b62efd8309d3a386eda7f9fc804000cf28
xvqiang <xuqiang@tuotiansudai.com>
2017-01-09 11:30:17 +0800

    finish page

--- ttsd-activity-web/src/main/webapp/activity/js/spring_festival.js
+++ ttsd-activity-web/src/main/webapp/activity/js/spring_festival.js
@@ -1,46 +1,16 @@
-require(['jquery'], function ($) {
-
-        var $productFrame=$('#productListFrame'),
-            $leftBtn=$('.arrow-left',$productFrame),
-            $rightBtn=$('.arrow-right',$productFrame),
-            $productBox=$('.product-box',$productFrame);
-
-    var productBoxW=$productBox.width(),
-        productLen=$productBox.find('li').length;
-    var redirect=globalFun.browserRedirect();
-    if(redirect=='mobile') {
-        $productBox.find('li').css({"width":productBoxW});
-        var recordNum=0; //用来记录当前活动图片
-
-        function animateImg(record) {
-            $productBox.find('ul').stop().animate({
-                marginLeft: -(productBoxW * record)
-            },500,function() {
-                var length=productLen-1;
-                if(record==0) {
-                    $leftBtn.hide();
-                    $rightBtn.show();
-                }
-                else if(record>0 && record<length) {
-                    $leftBtn.show();
-                    $rightBtn.show();
-                }
-                else if(record==length) {
-                    $rightBtn.hide();
-                    $leftBtn.show();
-                }
-            });
-        }
-        $rightBtn.on('touchstart', function () {
-            recordNum++;
-            animateImg(recordNum);
-        });
-
-        $leftBtn.on('touchstart', function () {
-            recordNum--;
-            animateImg(recordNum);
-        });
-
-    }
-
+/**
+ * [name]:spring festival activity
+ * [author]:xuqiang
+ * [date]:2017-01-09
+ */
+require(['jquery', 'layerWrapper', 'template','jquery.ajax.extension'], function ($,layer,tpl) {
+    $(function() {
+         layer.open({
+          type: 1,
+          move:false,
+          area:['400px','300px'],
+          title:false,
+          content: $('#moneyTip')
+        });       
+    });        
 });
