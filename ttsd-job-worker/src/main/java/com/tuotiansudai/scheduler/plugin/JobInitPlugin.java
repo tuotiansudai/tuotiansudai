f9bc210710cf96fbf20a7bacf05522f81be270b5
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-01-11 12:07:38 +0800

    remove useless job

--- ttsd-job-worker/src/main/java/com/tuotiansudai/scheduler/plugin/JobInitPlugin.java
+++ ttsd-job-worker/src/main/java/com/tuotiansudai/scheduler/plugin/JobInitPlugin.java
@@ -40,12 +40,6 @@ public class JobInitPlugin implements SchedulerPlugin {
 
     @Override
     public void start() {
-        if (JobType.NormalRepayCallBack.name().equalsIgnoreCase(schedulerName)) {
-            createNormalRepayCallBackJobIfNotExist();
-        }
-        if (JobType.AdvanceRepayCallBack.name().equalsIgnoreCase(schedulerName)) {
-            createAdvanceRepayCallBackJobIfNotExist();
-        }
         if (JobType.CalculateDefaultInterest.name().equalsIgnoreCase(schedulerName)) {
             createCalculateDefaultInterest();
         }
@@ -57,15 +51,6 @@ public class JobInitPlugin implements SchedulerPlugin {
         }
         if (JobType.BirthdayNotify.name().equalsIgnoreCase(schedulerName)) {
             createBirthdayNotifyJob();
-        }
-        if (JobType.AutoJPushAlertBirthMonth.name().equalsIgnoreCase(schedulerName)) {
-            deleteAutoJPushAlertBirthMonth();
-        }
-        if (JobType.AutoJPushAlertBirthDay.name().equalsIgnoreCase(schedulerName)) {
-            deleteAutoJPushAlertBirthDay();
-        }
-        if (JobType.AutoJPushNoInvestAlert.name().equalsIgnoreCase(schedulerName)) {
-            deleteAutoJPushNoInvestAlert();
         }
         if (JobType.ExperienceRepay.name().equals(schedulerName)) {
             createNewbieExperienceRepayJob();
@@ -82,20 +67,8 @@ public class JobInitPlugin implements SchedulerPlugin {
         if (JobType.PlatformBalanceLowNotify.name().equals(schedulerName)) {
             platformBalanceLowNotifyJob();
         }
-        if (JobType.BirthdayMessage.name().equals(schedulerName)) {
-            deleteBirthdayMessageSendJob();
-        }
-        if (JobType.CalculateTravelLuxuryPrize.name().equalsIgnoreCase(schedulerName)) {
-            deleteCalculateTravelLuxuryPrizeJob();
-        }
         if (JobType.EventMessage.name().equals(schedulerName)) {
             eventMessageJob();
-        }
-        if (JobType.NormalRepayCallBack.name().equalsIgnoreCase(schedulerName)) {
-            deleteNormalRepayCallBackJob();
-        }
-        if (JobType.AdvanceRepayCallBack.name().equalsIgnoreCase(schedulerName)) {
-            deleteAdvanceRepayCallBackJob();
         }
         if (JobType.SendRedEnvelopSplit.name().equalsIgnoreCase(schedulerName)) {
             createRedEnvelopSplitJob();
@@ -105,40 +78,6 @@ public class JobInitPlugin implements SchedulerPlugin {
     @Override
     public void shutdown() {
 
-    }
-
-    private void createNormalRepayCallBackJobIfNotExist() {
-        final JobType jobType = JobType.NormalRepayCallBack;
-        final String jobGroup = NormalRepayCallbackJob.JOB_GROUP;
-        final String jobName = NormalRepayCallbackJob.JOB_NAME;
-        try {
-            jobManager.newJob(jobType, NormalRepayCallbackJob.class)
-                    .replaceExistingJob(true)
-                    .runWithSchedule(SimpleScheduleBuilder
-                            .repeatSecondlyForever(NormalRepayCallbackJob.RUN_INTERVAL_SECONDS)
-                            .withMisfireHandlingInstructionIgnoreMisfires())
-                    .withIdentity(jobGroup, jobName)
-                    .submit();
-        } catch (SchedulerException e) {
-            logger.info(e.getLocalizedMessage(), e);
-        }
-    }
-
-    private void createAdvanceRepayCallBackJobIfNotExist() {
-        final JobType jobType = JobType.AdvanceRepayCallBack;
-        final String jobGroup = AdvanceRepayCallbackJob.JOB_GROUP;
-        final String jobName = AdvanceRepayCallbackJob.JOB_NAME;
-        try {
-            jobManager.newJob(jobType, AdvanceRepayCallbackJob.class)
-                    .replaceExistingJob(true)
-                    .runWithSchedule(SimpleScheduleBuilder
-                            .repeatSecondlyForever(AdvanceRepayCallbackJob.RUN_INTERVAL_SECONDS)
-                            .withMisfireHandlingInstructionIgnoreMisfires())
-                    .withIdentity(jobGroup, jobName)
-                    .submit();
-        } catch (SchedulerException e) {
-            logger.info(e.getLocalizedMessage(), e);
-        }
     }
 
     private void createCalculateDefaultInterest() {
@@ -159,18 +98,6 @@ public class JobInitPlugin implements SchedulerPlugin {
         } catch (SchedulerException e) {
             logger.info(e.getLocalizedMessage(), e);
         }
-    }
-
-    private void deleteAutoJPushAlertBirthMonth() {
-        jobManager.deleteJob(JobType.AutoJPushAlertBirthMonth, JobType.AutoJPushAlertBirthMonth.name(), JobType.AutoJPushAlertBirthMonth.name());
-    }
-
-    private void deleteAutoJPushAlertBirthDay() {
-        jobManager.deleteJob(JobType.AutoJPushAlertBirthDay, JobType.AutoJPushAlertBirthDay.name(), JobType.AutoJPushAlertBirthDay.name());
-    }
-
-    private void deleteAutoJPushNoInvestAlert() {
-        jobManager.deleteJob(JobType.AutoJPushNoInvestAlert, JobType.AutoJPushNoInvestAlert.name(), JobType.AutoJPushNoInvestAlert.name());
     }
 
     private void createLoanRepayNotifyJob() {
@@ -255,22 +182,6 @@ public class JobInitPlugin implements SchedulerPlugin {
         } catch (SchedulerException e) {
             logger.info(e.getLocalizedMessage(), e);
         }
-    }
-
-    private void deleteCalculateTravelLuxuryPrizeJob() {
-        jobManager.deleteJob(JobType.CalculateTravelLuxuryPrize, JobType.CalculateTravelLuxuryPrize.name(), JobType.CalculateTravelLuxuryPrize.name());
-    }
-
-    private void deleteBirthdayMessageSendJob() {
-        jobManager.deleteJob(JobType.BirthdayMessage, JobType.BirthdayMessage.name(), JobType.BirthdayMessage.name());
-    }
-
-    private void deleteNormalRepayCallBackJob() {
-        jobManager.deleteJob(JobType.NormalRepayCallBack, JobType.NormalRepayCallBack.name(), JobType.NormalRepayCallBack.name());
-    }
-
-    private void deleteAdvanceRepayCallBackJob() {
-        jobManager.deleteJob(JobType.AdvanceRepayCallBack, JobType.AdvanceRepayCallBack.name(), JobType.AdvanceRepayCallBack.name());
     }
 
     private void eventMessageJob() {
