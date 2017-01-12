a0c69451d6d9e3aebefbf06379e1068a78f721f1
zhoubx <zhoubaoxin@tuotiansudai.com>
2017-01-12 11:36:34 +0800

    auto deploy ttsd env properties

---
+++ scripts/config_deploy.py
@@ -0,0 +1,76 @@
+# encoding=utf-8
+import redis
+
+
+def deploy(env, source_folder, dist_file):
+    if not env:
+        print "Fail: env was not set!"
+        return
+
+    # 读取QA环境公用配置 ttsd-env-QA-common.properties 生成一个 dict 对象 qa_common_prop
+    qa_common_prop = load_properties("{0}envs/QA-common.properties".format(source_folder))
+
+    # 读取每个环境特有的配置 envs/${env}.properties, 生成一个 dict 对象 env_prop
+    env_prop = load_properties("{0}envs/{1}.properties".format(source_folder, env))
+
+    # 从redis里读取 安全性配置（三方系统账号密码）
+    _r = redis.StrictRedis(host='192.168.1.30', port=6379, db=2)
+    sec_prop = _r.hgetall('qa_common_account')
+
+    # 逐行读取 ttsd-env.properties, 如果不是注释也不是空行，则split by "=",
+    # 依次从 sec_prop > env_prop > qa_common_prop 中读取key，
+    # 如果存在就替换原值，并将结果写入到结果文件 dist_file 中。
+    dist = open(dist_file, "w")
+
+    with open("{0}/ttsd-env.properties".format(source_folder), "r") as f:
+        for line in f:
+            l = line.strip()
+            if l and not l.startswith("#"):
+                key_value = l.split("=")
+                key = key_value[0].strip()
+                value = "=".join(key_value[1:]).strip()
+                if sec_prop.has_key(key):
+                    value = sec_prop[key]
+                elif env_prop.has_key(key):
+                    value = env_prop[key]
+                elif qa_common_prop.has_key(key):
+                    value = qa_common_prop[key]
+                dist.write(key + "=" + value + "\n")
+            else:
+                dist.write(l + "\n")
+    f.close()
+    dist.close()
+    verify()
+
+
+# 读取properties 文件, 生成 dict 对象
+def load_properties(file_path):
+    props = {}
+    with open(file_path, "r") as f:
+        for line in f:
+            l = line.strip()
+            if l and not l.startswith('#'):
+                key_value = l.split('=')
+                key = key_value[0].strip()
+                value = '='.join(key_value[1:]).strip()
+                props[key] = value
+    f.close()
+    return props
+
+
+def compare_a_b(a, b):
+    a_prop = load_properties(a)
+    b_prop = load_properties(b)
+    print "a len: %s" % len(a_prop)
+    print "b len: %s" % len(b_prop)
+    for key in a_prop:
+        if not b_prop.has_key(key):
+            print "b don't has key " + key
+        elif b_prop[key] != a_prop[key]:
+            print "key: %s, a_val: %s, b_val: %s" % (key, a_prop[key],  b_prop[key])
+
+
+def verify():
+    print 'verify config file ...'
+    compare_a_b('ttsd-env.properties','ttsd-env.properties.20170112')
+    compare_a_b('ttsd-env.properties.20170112','ttsd-env.properties')
