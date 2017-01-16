44bb3ecda122a05c229e44ab1477ea8f9f0725f6
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2017-01-16 18:42:40 +0800

    fix test

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/service/impl/AdvanceTransferServiceImpl.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/service/impl/AdvanceTransferServiceImpl.java
@@ -12,10 +12,12 @@ import com.tuotiansudai.transfer.repository.mapper.TransferApplicationMapper;
 import com.tuotiansudai.transfer.repository.model.TransferApplicationModel;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 import java.util.List;
 
+@Service
 public class AdvanceTransferServiceImpl implements AdvanceTransferService {
 
     private static Logger logger = Logger.getLogger(AdvanceTransferServiceImpl.class);
