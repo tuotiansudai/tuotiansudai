e0fb333bd883d5f074e32901dc3925ff11f0fc8e
fulanyan <fly39244080@163.com>
2017-01-18 15:43:55 +0800

    config

--- ttsd-frontend-manage/resources/static/public/js/global_page.jsx
+++ ttsd-frontend-manage/resources/static/public/js/global_page.jsx
@@ -293,7 +293,10 @@ window.globalFun =new Proxy_GlobalFun();
 window.globalFun.init();
 
 window.jQuery = window.$ = window.jquery_library(1);
-
+window.layer = window.jquery_library(2);
+window.layer.config({
+    path: staticServer+'/public/'
+});
 // ********* cnzz统计 BEGIN ***********
 function cnzzPushConstructor() {
     this.trackClick=function(category, action, label) {
