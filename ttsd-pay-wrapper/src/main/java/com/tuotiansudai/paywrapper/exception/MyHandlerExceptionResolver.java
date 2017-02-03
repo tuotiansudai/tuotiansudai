b87484ac2feb20a66c286cb180aaa3102e403967
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-02-03 18:50:42 +0800

    add default exception resolver to log exception

---
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/exception/MyHandlerExceptionResolver.java
@@ -0,0 +1,20 @@
+package com.tuotiansudai.paywrapper.exception;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    static Logger log = Logger.getLogger(MyHandlerExceptionResolver.class);
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        log.error(ex.getLocalizedMessage(), ex);
+        return null;
+    }
+}
