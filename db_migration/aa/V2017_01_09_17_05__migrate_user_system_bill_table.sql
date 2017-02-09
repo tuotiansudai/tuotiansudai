5a29e7f75021dcbac69d8567704d11f5ebcceb64
Xiduan Gao <gaoxiduan@gmail.com>
2017-02-09 13:35:53 +0800

    fixed same sql version

--- db_migration/aa/V2017_01_09_17_05__migrate_user_system_bill_table.sql
+++ db_migration/aa/V2017_01_09_17_05__migrate_user_system_bill_table.sql
@@ -1,4 +1,4 @@
-begin;
+BEGIN;
 UPDATE user_bill set business_type = 'MEMBERSHIP_PRIVILEGE_PURCHASE' where `business_type` ='MEMBERSHIP_PURCHASE';
 UPDATE system_bill set business_type = 'MEMBERSHIP_PRIVILEGE_PURCHASE' where `business_type` ='MEMBERSHIP_PURCHASE';
-COMMIT ;
+COMMIT;
