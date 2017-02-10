6276d1f3ec370f10de26407c1207df5b97f8bd4c
xvqiang <xuqiang@tuotiansudai.com>
2017-02-10 15:07:44 +0800

    fix code

--- ttsd-point-web/src/main/webapp/point/js/config.js
+++ ttsd-point-web/src/main/webapp/point/js/config.js
@@ -33,7 +33,9 @@ var require = {
         'fancybox':staticServer+'/point/js/libs/jquery.fancybox.min',
         'count_down': staticServer+'/point/js/dest/count_down.min',
         'placeholder': staticServer + '/point/js/libs/jquery.enplaceholder',
-        'superslide': staticServer + '/point/js/libs/jquery.SuperSlide.2.1.1'
+        'superslide': staticServer + '/point/js/libs/jquery.SuperSlide.2.1.1',
+        'drawCircle': staticServer+'/point/js/dest/gift_circle_draw.min',
+        'lottery_unit':staticServer+'/point/js/dest/lottery_unit.min'
     },
     'waitSeconds':0,
     'shim': {
--- ttsd-point-web/src/main/webapp/point/js/point_index.js
+++ ttsd-point-web/src/main/webapp/point/js/point_index.js
@@ -3,13 +3,13 @@
  * @xuqiang  
  * @2016-07-11
  */
-require(['jquery','layerWrapper','template', 'jquery.ajax.extension'], function ($,layer,tpl) {
+require(['jquery', 'layerWrapper', 'template', 'drawCircle', 'jquery.ajax.extension'], function($, layer, tpl, drawCircle) {
 	$(function() {
 		var $signBtn = $('#signBtn'),
 			$signTip = $('#signLayer'),
 			$closeSign = $('#closeSign'),
-			$materialList=$('.material-list li'),
-			$pointRuleTip=$('.point-rule-tip');
+			$materialList = $('.material-list li'),
+			$pointRuleTip = $('.point-rule-tip');
 		//show sign tip
 		$signBtn.on('click', function(event) {
 			event.preventDefault();
@@ -17,8 +17,8 @@ require(['jquery','layerWrapper','template', 'jquery.ajax.extension'], function 
 				$signText = $(".sign-text"),
 				$tomorrowText = $(".tomorrow-text"),
 				$signPoint = $(".sign-point"),
-				$introText=$('.intro-text'),
-                $nextText=$('.next-text'),
+				$introText = $('.intro-text'),
+				$nextText = $('.next-text'),
 				$signBtn = $("#signBtn");
 
 			$.ajax({
@@ -28,14 +28,14 @@ require(['jquery','layerWrapper','template', 'jquery.ajax.extension'], function 
 				contentType: 'application/json; charset=UTF-8'
 			}).done(function(response) {
 				if (response.data.status) {
-					response.data.signIn==true?$signText.html("您今天已签到"):$signText.html("签到成功");
-					$tomorrowText.html("明日签到可获得"+ response.data.nextSignInPoint + "积分");
-                    $introText.html(response.data.currentRewardDesc);
-                    $nextText.html(response.data.nextRewardDesc);
+					response.data.signIn == true ? $signText.html("您今天已签到") : $signText.html("签到成功");
+					$tomorrowText.html("明日签到可获得" + response.data.nextSignInPoint + "积分");
+					$introText.html(response.data.currentRewardDesc);
+					$nextText.html(response.data.nextRewardDesc);
 					$signBtn.addClass("no-click").html("已签到");
 					$signPoint.find('span').html(response.data.signInPoint);
 					$signTip.fadeIn('fast');
-				}else{
+				} else {
 					$('#errorTip').html(tpl('errorTipTpl', response.data));
 					layer.open({
 						type: 1,
@@ -52,12 +52,12 @@ require(['jquery','layerWrapper','template', 'jquery.ajax.extension'], function 
 			location.href = "/point-shop";
 		});
 		//go to detail
-		$materialList.on('click', function(event) {//click all model
+		$materialList.on('click', function(event) { //click all model
 			event.preventDefault();
 			location.href = $(this).attr('data-href');
-		}).on('click','.get-btn', function(event) {//click btn
+		}).on('click', '.get-btn', function(event) { //click btn
 			event.preventDefault();
-			$(this).hasClass('active')?location.href = $(this).parent('a').attr('href'):false;
+			$(this).hasClass('active') ? location.href = $(this).parent('a').attr('href') : false;
 			event.stopPropagation();
 		});
 		//show rule tip
@@ -66,9 +66,9 @@ require(['jquery','layerWrapper','template', 'jquery.ajax.extension'], function 
 			layer.open({
 				type: 1,
 				title: false,
-				closeBtn:0,
+				closeBtn: 0,
 				area: ['auto', '520px'],
-                scrollbar: true,
+				scrollbar: true,
 				content: $('#ruleInfoTip')
 			});
 		});
@@ -77,5 +77,109 @@ require(['jquery','layerWrapper','template', 'jquery.ajax.extension'], function 
 			event.preventDefault();
 			layer.closeAll();
 		});
+
+		(function(drawCircle) {
+			var $christmasDayFrame = $('#pointContainer');
+			var tipGroupObj = {};
+			//抽奖模块
+			var $rewardGiftBox = $('.nine-lottery-group', $christmasDayFrame);
+
+			var $MobileNumber = $('#MobileNumber'),
+				pointAllList = '/point/point-draw/all-list', //中奖记录接口地址
+				pointUserList = '/point/point-draw/user-list', //我的奖品接口地址
+				drawURL = '/point/point-draw/draw', //抽奖的接口链接
+				$pointerImg = $('.lottery-btn', $rewardGiftBox),
+				myMobileNumber = $MobileNumber.length ? $MobileNumber.data('mobile') : ''; //当前登录用户的手机号
+
+			$christmasDayFrame.find('.tip-list-frame .tip-list').each(function(key, option) {
+				var kind = $(option).data('return');
+				tipGroupObj[kind] = option;
+			});
+
+			var paramData = {
+				"mobile": myMobileNumber,
+				"activityCategory": "CARNIVAL_ACTIVITY"
+			};
+			var drawCircle = new drawCircle(pointAllList, pointUserList, drawURL, paramData, $rewardGiftBox);
+
+			//渲染中奖记录
+			drawCircle.GiftRecord();
+
+			//渲染我的奖品
+			drawCircle.MyGift();
+
+			//**********************开始抽奖**********************//
+			$pointerImg.on('click', function() {
+				drawCircle.beginLuckDraw(function(data) {
+					var prizeKind;
+
+					if (data.returnCode == 0) {
+
+						switch (data.prize) {
+							case 'M1_PHONE': //锤子M1手机
+								prizeKind = 7;
+								break;
+							case 'HUMIDIFIER': //小熊加湿器
+								prizeKind = 0;
+								break;
+							case 'HAIR_DRIER': //飞科电吹风机
+								prizeKind = 1;
+								break;
+							case 'IQIYI_MEMBERSHIP_REF_CARNIVAL': //爱奇艺会员
+								prizeKind = 5;
+								break;
+							case 'TELEPHONE_FARE_10_REF_CARNIVAL': //10元话费
+								prizeKind = 2;
+								break;
+							case 'BAMBOO_CHARCOAL_PACKAGE': //卡通汽车竹炭包
+								prizeKind = 6;
+								break;
+							case 'INTEREST_COUPON_5_POINT_DRAW_REF_CARNIVAL': //0.5加息券
+								prizeKind = 3;
+								break;
+							case 'RED_ENVELOPE_50_POINT_DRAW_REF_CARNIVAL': //50元红包
+								prizeKind = 4;
+								break;
+						}
+
+						var prizeType = data.prizeType.toLowerCase();
+						$(tipGroupObj[prizeType]).find('.prizeValue').text(data.prizeValue);
+
+						drawCircle.lotteryRoll({
+							elementId: 'lotteryBox',
+							speed: 100,
+							prize: prizeKind
+						}, tipGroupObj[prizeType]);
+
+					} else if (data.returnCode == 1) {
+						//没有抽奖机会
+						drawCircle.tipWindowPop(tipGroupObj['nochance']);
+					} else if (data.returnCode == 2) {
+						//未登录
+						$('.no-login-text', $christmasDayFrame).trigger('click'); //弹框登录
+
+					} else if (data.returnCode == 3) {
+						//不在活动时间范围内！
+						drawCircle.tipWindowPop(tipGroupObj['expired']);
+
+					} else if (data.returnCode == 4) {
+						//实名认证
+						drawCircle.tipWindowPop(tipGroupObj['authentication']);
+					}
+				});
+			});
+
+			//点击切换按钮
+			var menuCls = $rewardGiftBox.find('.lottery-right-group').find('h3 span');
+			menuCls.on('click', function() {
+				var $this = $(this),
+					index = $this.index(),
+					contentCls = $rewardGiftBox.find('.record-item');
+				$this.addClass('active').siblings().removeClass('active');
+				contentCls.eq(index).show().siblings().hide();
+
+			});
+
+		})(drawCircle);
 	});
 })
