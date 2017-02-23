02183f792e46b7dea4014ed96370dd92369b51a7
zhanglong888 <zhanglong@tuotiansudai.com>
2017-02-23 14:38:17 +0800

    update bug

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/loanout/impl/CouponRepayServiceImpl.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/loanout/impl/CouponRepayServiceImpl.java
@@ -131,7 +131,7 @@ public class CouponRepayServiceImpl implements CouponRepayService {
             logger.info(MessageFormat.format("[Coupon Repay {0}] user coupon({1}) repay is starting...", String.valueOf(loanRepayId), String.valueOf(userCouponModel.getId())));
 
             CouponModel couponModel = this.couponMapper.findById(userCouponModel.getCouponId());
-            if (couponModel.getPeriod() != null && currentLoanRepayModel.getPeriod() > couponModel.getPeriod()) {
+            if (couponModel.getCouponType() == CouponType.BIRTHDAY_COUPON && currentLoanRepayModel.getPeriod() > 1) {
                 continue;
             }
 
@@ -257,7 +257,7 @@ public class CouponRepayServiceImpl implements CouponRepayService {
                         logger.error(MessageFormat.format("(coupon is not exist (couponId = {0}))", userCouponModel.getCouponId()));
                         continue;
                     }
-                    if (couponModel.getPeriod() != null && couponModel.getPeriod() < period) {
+                    if (couponModel.getCouponType() == CouponType.BIRTHDAY_COUPON && period > 1) {
                         continue;
                     }
                     long expectedCouponInterest = InterestCalculator.estimateCouponRepayExpectedInterest(successInvestModel,
