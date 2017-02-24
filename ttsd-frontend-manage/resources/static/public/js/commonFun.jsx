c2719214fd70eec746366d04b8a442d3e674e125
fulanyan <fly39244080@163.com>
2017-02-24 14:16:11 +0800

    remove load script function

--- ttsd-frontend-manage/resources/static/public/js/commonFun.jsx
+++ ttsd-frontend-manage/resources/static/public/js/commonFun.jsx
@@ -185,15 +185,7 @@ function useAjax(opt,callbackDone,callbackAlways) {
             callbackAlways && callbackAlways();
         });
 }
-function loadJsFile(url,callback) {
-    debugger
-    $.ajax({
-        url:staticServer+url,
-        type:'GET',
-        async: false
-    });
-    callback && callback();
-}
+
 //倒计时
 function countDownLoan(option,callback) {
     let defaultOpt={
@@ -273,7 +265,6 @@ exports.checkedAge = checkedAge;
 exports.popWindow = popWindow;
 exports.isUserLogin = isUserLogin;
 exports.useAjax = useAjax;
-exports.loadJsFile=loadJsFile;
 exports.countDownLoan = countDownLoan;
 exports.MathDecimal = MathDecimal;
 exports.decrypt = decrypt;
--- ttsd-frontend-manage/resources/static/web/js/account/refer_list.jsx
+++ ttsd-frontend-manage/resources/static/web/js/account/refer_list.jsx
@@ -1,44 +1,47 @@
 require('webStyle/account/refer_list.scss');
 require('publicJs/plugins/daterangepicker.scss');
-let commonFun= require('publicJs/commonFun');
-let moment = require('moment');
-require('webJsModule/pagination');
-require('publicJs/plugins/jquery.daterangepicker-0.0.7.js');
-commonFun.loadJsFile('/public/plugins/jQuery.md5.js');
-commonFun.loadJsFile('/public/plugins/clipboard.js');
-commonFun.loadJsFile('/public/plugins/jquery.qrcode.min.js');
 
-var $investListContent=$('#investListContent'),
-    $clipboardText=$('#clipboard_text');
-var $searchBox=$('#search-box'),
-    dataPickerElement = $('#date-picker'),
-    loginName = $("#loginName"),
-    paginationElementRelation = $('#referRelationPagination'),
-    paginationElementInvest = $('#referInvestPagination'),
-    $btnSearch=$('.btn-search',$searchBox),
-    $btnReset=$('.btn-reset',$searchBox),
-    $searchContent=$('.search-content-tab');
 
-var mobile=$clipboardText.data('mobile')+'',
-    md5Mobile=$.md5(mobile);
+require.ensure([],function() {
+    require('publicJs/plugins/jquery.daterangepicker-0.0.7');
+    require('publicJs/plugins/jQuery.md5');
+    let Clipboard= require('publicJs/plugins/clipboard');
+    require('publicJs/plugins/jquery.qrcode.min');
+    let commonFun= require('publicJs/commonFun');
+    let moment = require('moment');
+    require('webJsModule/pagination');
 
-if (window["context"] == undefined) {
-    if (!window.location.origin) {
-        window.location.origin = window.location.protocol + "//" + window.location.hostname+ (window.location.port ? ':' + window.location.port: '');
+    var $investListContent=$('#investListContent'),
+        $clipboardText=$('#clipboard_text');
+    var $searchBox=$('#search-box'),
+        dataPickerElement = $('#date-picker'),
+        loginName = $("#loginName"),
+        paginationElementRelation = $('#referRelationPagination'),
+        paginationElementInvest = $('#referInvestPagination'),
+        $btnSearch=$('.btn-search',$searchBox),
+        $btnReset=$('.btn-reset',$searchBox),
+        $searchContent=$('.search-content-tab');
+
+    var mobile=$clipboardText.data('mobile')+'',
+        md5Mobile=$.md5(mobile);
+
+    if (window["context"] == undefined) {
+        if (!window.location.origin) {
+            window.location.origin = window.location.protocol + "//" + window.location.hostname+ (window.location.port ? ':' + window.location.port: '');
+        }
     }
-}
 
-var md5String=commonFun.decrypt.compile(md5Mobile,mobile),
-    origin=location.origin;
+    var md5String=commonFun.decrypt.compile(md5Mobile,mobile),
+        origin=location.origin;
 
-$clipboardText.val(origin+'/activity/landing-page?referrer='+md5String);
+    $clipboardText.val(origin+'/activity/landing-page?referrer='+md5String);
 
 //动态生成二维码
 
-$('.img-code',$investListContent).qrcode(origin+'/activity/app-share?referrerMobile='+mobile);
+    $('.img-code',$investListContent).qrcode(origin+'/activity/app-share?referrerMobile='+mobile);
 
-/*复制链接*/
-(function($) {
+    /*复制链接*/
+
     var clipboard = new Clipboard('#copyButtonBtn');
     clipboard.on('success', function(e) {
         layer.msg("复制成功");
@@ -47,49 +50,48 @@ $('.img-code',$investListContent).qrcode(origin+'/activity/app-share?referrerMob
     clipboard.on('error', function(e) {
         layer.msg("复制失败");
     });
-})(jQuery);
 
 
-var paginationElement = paginationElementRelation;
-var template = $('#referRelationTemplate'),
-    tableTpl=template.html();
-var investTemplate=$('#referInvestTemplate'),
-    investTpl=investTemplate.html();
-var render = _.template(tableTpl);
+    var paginationElement = paginationElementRelation;
+    var template = $('#referRelationTemplate'),
+        tableTpl=template.html();
+    var investTemplate=$('#referInvestTemplate'),
+        investTpl=investTemplate.html();
+    var render = _.template(tableTpl);
 
-var today = moment().format('YYYY-MM-DD');
+    var today = moment().format('YYYY-MM-DD');
 
-dataPickerElement.dateRangePicker({separator: ' ~ '});
-$(".search-type .select-item").click(function () {
-    $(this).addClass("current").siblings(".select-item").removeClass("current");
+    dataPickerElement.dateRangePicker({separator: ' ~ '});
+    $(".search-type .select-item").click(function () {
+        $(this).addClass("current").siblings(".select-item").removeClass("current");
 
-    if ($(this).data('type') == 'referRelation') {
-        paginationElementRelation.show();
-        paginationElementInvest.hide();
-        paginationElement = paginationElementRelation;
-        // template = referRelationTemplate;
-        render = _.template(tableTpl);
-    } else if ($(this).data('type') == 'referInvest') {
-        paginationElementRelation.hide();
-        paginationElementInvest.show();
-        paginationElement = paginationElementInvest;
-        // template = referInvestTemplate;
-        render = _.template(investTpl);
-    }
-    loadReferData();
-});
-
-var loadReferData = function (currentPage) {
-    var dates = dataPickerElement.val().split('~');
-    var startTime = $.trim(dates[0]) || '';
-    var endTime = $.trim(dates[1]) || '';
-    var loginName = $('#loginName').val();
-
-    var requestData = {startTime: startTime, endTime: endTime, loginName: loginName, index: currentPage || 1};
-    var queryParams = '';
-    _.each(requestData, function (value, key) {
-        queryParams += key + "=" + value + '&';
+        if ($(this).data('type') == 'referRelation') {
+            paginationElementRelation.show();
+            paginationElementInvest.hide();
+            paginationElement = paginationElementRelation;
+            // template = referRelationTemplate;
+            render = _.template(tableTpl);
+        } else if ($(this).data('type') == 'referInvest') {
+            paginationElementRelation.hide();
+            paginationElementInvest.show();
+            paginationElement = paginationElementInvest;
+            // template = referInvestTemplate;
+            render = _.template(investTpl);
+        }
+        loadReferData();
     });
+
+    var loadReferData = function (currentPage) {
+        var dates = dataPickerElement.val().split('~');
+        var startTime = $.trim(dates[0]) || '';
+        var endTime = $.trim(dates[1]) || '';
+        var loginName = $('#loginName').val();
+
+        var requestData = {startTime: startTime, endTime: endTime, loginName: loginName, index: currentPage || 1};
+        var queryParams = '';
+        _.each(requestData, function (value, key) {
+            queryParams += key + "=" + value + '&';
+        });
         paginationElement.loadPagination(requestData, function (data) {
             $.ajax({
                 url: 'total-reward?' + queryParams,
@@ -105,27 +107,30 @@ var loadReferData = function (currentPage) {
             });
         });
 
-    $('.search-content-tab').on('mouseenter','span.loan-name-col',function() {
-        layer.closeAll('tips');
-        if($(this).text().length>13){
-            layer.tips($(this).text(), $(this), {
-                tips: [1, '#efbf5c'],
-                time: 2000,
-                tipsMore: true,
-                area: 'auto',
-                maxWidth: '500'
-            });
-        }
-    });
-};
+        $('.search-content-tab').on('mouseenter','span.loan-name-col',function() {
+            layer.closeAll('tips');
+            if($(this).text().length>13){
+                layer.tips($(this).text(), $(this), {
+                    tips: [1, '#efbf5c'],
+                    time: 2000,
+                    tipsMore: true,
+                    area: 'auto',
+                    maxWidth: '500'
+                });
+            }
+        });
+    };
 
-loadReferData();
-
-$btnSearch.click(function () {
     loadReferData();
-});
 
-$btnReset.click(function () {
-    dataPickerElement.val('');
-    loginName.val('');
-});
+    $btnSearch.click(function () {
+        loadReferData();
+    });
+
+    $btnReset.click(function () {
+        dataPickerElement.val('');
+        loginName.val('');
+    });
+},'refer_list_chuck');
+
+
