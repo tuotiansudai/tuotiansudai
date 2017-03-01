e0d4ccbb7d40c0d0b6d9644895bc530c0142bff3
fulanyan <fly39244080@163.com>
2017-03-01 17:23:06 +0800

    design loan experience

--- ttsd-frontend-manage/resources/static/web/js/loan_detail_experience.jsx
+++ ttsd-frontend-manage/resources/static/web/js/loan_detail_experience.jsx
@@ -1,19 +1,42 @@
 require('webStyle/investment/loan_detail.scss');
 let commonFun= require('publicJs/commonFun');
+require('webJs/plugins/autoNumeric');
 //新手体验项目
 let $experienceLoan=$('#experienceLoanDetailContent');
-
-commonFun.useAjax({
-    url: '/calculate-expected-coupon-interest/loan/1/amount/0',
-    data: {
-        "couponIds":$("input[name='userCouponIds']",$experienceLoan).data("coupon-id")
-    },
-    type: 'GET'
-},function(amount) {
-    $(".principal-income",$experienceLoan).text(amount);
-});
-
+let amountInputElement = $(".text-input-amount", $experienceLoan);
 let investForm=globalFun.$('#investForm');
+
+amountInputElement.autoNumeric("init");
+
+//获取输入的金额
+function getAmount() {
+    var amount = 0;
+    if (!isNaN(amountInputElement.autoNumeric("get"))) {
+        amount = parseInt((amountInputElement.autoNumeric("get") * 100).toFixed(0));
+    }
+    return amount;
+}
+
+amountInputElement.on('blur',function() {
+    //计算体验金余额是否不足
+    let experienceAmount = $(investForm).find('.account-amount').data("user-balance") || 0,   //体验金余额
+        getInvestAmount = getAmount();  //输入的投资金额
+    if(parseFloat(getInvestAmount) > experienceAmount) {
+        $('.error-box',$experienceLoan).show();
+    }
+    else {
+        $('.error-box',$experienceLoan).hide();
+    }
+
+    commonFun.useAjax({
+        url: '/calculate-expected-coupon-interest/loan/1/amount/' + getInvestAmount,
+        type: 'GET',
+        data: {"loanId":'1',"amount":getInvestAmount,"couponIds":''}
+    },function(amount) {
+        $(".principal-income",$experienceLoan).text(amount);
+
+    });
+}).trigger('blur');
 
 investForm.onsubmit=function(event) {
     event.preventDefault();
@@ -22,13 +45,20 @@ investForm.onsubmit=function(event) {
         url: '/experience-invest',
         data:$(investForm).serialize()
     },function(response) {
-        var data = response.data;
+        let $freeSuccess=$('#freeSuccess');
+        let data = response.data,
+            htmlCon = $freeSuccess.find('.detail-word').eq(0); //默认获取没有实名认证的内容
+        //是否实名认证
+        let account=$freeSuccess.data('account');
+        if(account) {
+            htmlCon = $freeSuccess.find('.detail-word').eq(1);
+        }
         if (data.status) {
             layer.open({
                 type: 1,
                 title: '&nbsp',
                 area: ['400px'],
-                content: $('#freeSuccess')
+                content: htmlCon
             });
         }
     });
