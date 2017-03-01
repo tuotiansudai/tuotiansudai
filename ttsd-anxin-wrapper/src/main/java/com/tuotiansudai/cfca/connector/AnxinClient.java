51624c21f6c7f50aa6971dd0a8aa65506831b89f
baisong@tuotiansudai.com <baisong@tuotiansudai.com>
2017-03-01 10:56:35 +0800

    update

--- ttsd-anxin-wrapper/src/main/java/com/tuotiansudai/cfca/connector/AnxinClient.java
+++ ttsd-anxin-wrapper/src/main/java/com/tuotiansudai/cfca/connector/AnxinClient.java
@@ -55,7 +55,6 @@ public class AnxinClient {
     @PostConstruct
     public void initSSL() throws GeneralSecurityException, IOException {
         anxinSignKey = SecurityUtil.loadAnxinSignKey(JKS_CLASS_PATH, JKS_PWD);
-//        anxinSignKey=null;
         logger.info("into AnxinClient initSSl method. Thread id: " + Thread.currentThread().getId());
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(DEFAULT_KEY_ALGORITHM);
         keyManagerFactory.init(anxinSignKey, JKS_PWD.toCharArray());
