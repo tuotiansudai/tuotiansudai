39b7487ac10d16c04c664f1b528bf9f57b62a8b9
fulanyan <fly39244080@163.com>
2017-03-02 11:25:10 +0800

    fix all layer tip style

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/ExperienceInvestController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/ExperienceInvestController.java
@@ -27,7 +27,6 @@ public class ExperienceInvestController {
     public BaseDto<BaseDataDto> invest(@Valid @ModelAttribute InvestDto investDto) {
         investDto.setSource(Source.WEB);
         investDto.setLoginName(LoginUserInfo.getLoginName());
-        investDto.setAmount(String.valueOf(AmountConverter.convertStringToCent(investDto.getAmount())));
         return experienceInvestService.invest(investDto);
     }
 }
