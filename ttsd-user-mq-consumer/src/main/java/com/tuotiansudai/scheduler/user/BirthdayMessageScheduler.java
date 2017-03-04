a31fd6c078c449eeb327b10e566055c225b4f084
Xiduan Gao <gaoxiduan@gmail.com>
2017-03-04 22:19:44 +0800

    stop birthday message

--- ttsd-user-mq-consumer/src/main/java/com/tuotiansudai/scheduler/user/BirthdayMessageScheduler.java
+++ ttsd-user-mq-consumer/src/main/java/com/tuotiansudai/scheduler/user/BirthdayMessageScheduler.java
@@ -17,27 +17,27 @@ import org.springframework.stereotype.Component;
 import java.text.MessageFormat;
 import java.util.List;
 
-@Component
 public class BirthdayMessageScheduler {
 
-    @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
-    private MQWrapperClient mqWrapperClient;
-
-    @Scheduled(cron = "0 0 10 * * ?", zone = "Asia/Shanghai")
-    public void membershipExpiredMessage() {
-        List<String> birthDayUsers = userMapper.findBirthDayUsers();
-        //Title:拓天速贷为您送上生日祝福，请查收！
-        //AppTitle:拓天速贷为您送上生日祝福，请查收！
-        //Content:尊敬的{0}先生/女士，我猜今天是您的生日，拓天速贷在此送上真诚的祝福，生日当月投资即可享受收益翻倍哦！
-        String title = MessageEventType.BIRTHDAY.getTitleTemplate();
-        birthDayUsers.forEach(loginName -> {
-            String userName = userMapper.findByLoginName(loginName).getUserName();
-            String content = MessageFormat.format(MessageEventType.BIRTHDAY.getContentTemplate(), userName);
-            mqWrapperClient.sendMessage(MessageQueue.EventMessage, new EventMessage(MessageEventType.BIRTHDAY, Lists.newArrayList(loginName), title, content, null));
-            mqWrapperClient.sendMessage(MessageQueue.PushMessage, new PushMessage(Lists.newArrayList(loginName), PushSource.ALL, PushType.BIRTHDAY, title, AppUrl.MESSAGE_CENTER_LIST));
-        });
-    }
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//    @Autowired
+//    private MQWrapperClient mqWrapperClient;
+//
+//    @Scheduled(cron = "0 0 10 * * ?", zone = "Asia/Shanghai")
+//    public void membershipExpiredMessage() {
+//
+//        List<String> birthDayUsers = userMapper.findBirthDayUsers();
+//        //Title:拓天速贷为您送上生日祝福，请查收！
+//        //AppTitle:拓天速贷为您送上生日祝福，请查收！
+//        //Content:尊敬的{0}先生/女士，我猜今天是您的生日，拓天速贷在此送上真诚的祝福，生日当月投资即可享受收益翻倍哦！
+//        String title = MessageEventType.BIRTHDAY.getTitleTemplate();
+//        birthDayUsers.forEach(loginName -> {
+//            String userName = userMapper.findByLoginName(loginName).getUserName();
+//            String content = MessageFormat.format(MessageEventType.BIRTHDAY.getContentTemplate(), userName);
+//            mqWrapperClient.sendMessage(MessageQueue.EventMessage, new EventMessage(MessageEventType.BIRTHDAY, Lists.newArrayList(loginName), title, content, null));
+//            mqWrapperClient.sendMessage(MessageQueue.PushMessage, new PushMessage(Lists.newArrayList(loginName), PushSource.ALL, PushType.BIRTHDAY, title, AppUrl.MESSAGE_CENTER_LIST));
+//        });
+//    }
 }
