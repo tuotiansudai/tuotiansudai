0a7092c367149f6b1926dfb7d539ba892593cdee
qulonglong <825849558@qq.com>
2017-03-06 16:30:06 +0800

    app4.2

--- ttsd-activity-web/src/main/webapp/activity/js/money_tree.js
+++ ttsd-activity-web/src/main/webapp/activity/js/money_tree.js
@@ -4,18 +4,20 @@ require(['jquery', 'commonFun', 'layerWrapper'], function ($, commonFun, layer) 
     var sourceKind = globalFun.parseURL(location.href);
     $('#btn-shake').on('click', function () {
 
-        if ($appVersion.val() == null || $appVersion.val() < '4.2') {
-            //老版本  提示升级
-            layer.msg("你的APP版本太低请升级")
-        } else if (sourceKind.params.source == 'app') {
-            $.when(commonFun.isUserLogin())
-                .done(function () {
-                    location.href = "app/tuotian/shake";
-                })
-                .fail(function () {
-                    //未登录
-                    location.href = "app/tuotian/login";
-                });
+        if (sourceKind.params.source == 'app') {
+            if ($appVersion.val() != null && $appVersion.val() < '4.2') {
+                //老版本  提示升级
+                layer.msg("你的APP版本太低请升级")
+            } else {
+                $.when(commonFun.isUserLogin())
+                    .done(function () {
+                        location.href = "app/tuotian/shake";
+                    })
+                    .fail(function () {
+                        //未登录
+                        location.href = "app/tuotian/login";
+                    });
+            }
         } else {
             location.href = "/app/download";
         }
