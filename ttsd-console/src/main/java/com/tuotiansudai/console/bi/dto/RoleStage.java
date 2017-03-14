a13c8b35e06df815b4898df855902b046e65f86a
Xiduan Gao <gaoxiduan@gmail.com>
2017-03-14 11:54:36 +0800

    fixed role filter

--- ttsd-console/src/main/java/com/tuotiansudai/console/bi/dto/RoleStage.java
+++ ttsd-console/src/main/java/com/tuotiansudai/console/bi/dto/RoleStage.java
@@ -6,6 +6,8 @@ public enum RoleStage {
     USER("注册用户"),
     INVESTOR("出借人"),
     LOANER("借款人"),
+    SD_STAFF("速贷业务员"),
+    ZC_STAFF("资产业务员"),
     NOT_STAFF_RECOMMEND("自然用户"), //用户上溯邀请人为非业务员角色，或该用户没有邀请人，则该用户为自然用户
     SD_STAFF_RECOMMEND("速贷系用户"), //用户上溯邀请人的终点为速贷业务员时，该用户为速贷系
     ZC_STAFF_RECOMMEND("资产系用户"), //用户上溯邀请人的终点为资产业务员时，该用户为资产系
