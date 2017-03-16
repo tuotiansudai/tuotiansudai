b23b49b99ce7bcd6a7f1fa84c86a103fe89e263c
Xiduan Gao <gaoxiduan@gmail.com>
2017-03-16 15:28:41 +0800

    fixed invest referrer reward

--- db_migration/aa/V2017_03_14_11_10__migrate_invest_referrer_reward.sql
+++ db_migration/aa/V2017_03_14_11_10__migrate_invest_referrer_reward.sql
@@ -6,4 +6,10 @@ WHERE referrer_role = 'STAFF' AND EXISTS(SELECT 1 FROM user_role WHERE user_role
 UPDATE invest_referrer_reward SET referrer_role = 'ZC_STAFF'
 WHERE referrer_role = 'STAFF' AND EXISTS(SELECT 1 FROM user_role WHERE user_role.login_name = invest_referrer_reward.referrer_login_name AND user_role.role = 'ZC_STAFF');
 
+UPDATE invest_referrer_reward SET referrer_role = 'INVESTOR'
+WHERE referrer_role = 'STAFF' AND EXISTS(SELECT 1 FROM user_role WHERE user_role.login_name = invest_referrer_reward.referrer_login_name AND user_role.role = 'INVESTOR');
+
+UPDATE invest_referrer_reward SET referrer_role = 'USER'
+WHERE referrer_role = 'STAFF' AND NOT EXISTS(SELECT 1 FROM user_role WHERE user_role.login_name = invest_referrer_reward.referrer_login_name AND user_role.role = 'INVESTOR');
+
 COMMIT;
