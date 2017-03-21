63c06fa602e0bca59c55e16fcfbfcafcd16d07fa
Xiduan Gao <gaoxiduan@gmail.com>
2017-03-21 15:27:38 +0800

    support fake ump when deploy qa

--- scripts/config_deploy.py
+++ scripts/config_deploy.py
@@ -2,10 +2,9 @@
 import redis
 
 
-def deploy(env, source_folder, dist_file):
-    if not env:
-        print "Fail: env was not set!"
-        return
+def deploy(build_params, source_folder, dist_file):
+
+    env = build_params['env']
 
     # 读取QA环境公用配置 ttsd-env-QA-common.properties 生成一个 dict 对象 qa_common_prop
     qa_common_prop = load_properties("{0}envs/QA-common.properties".format(source_folder))
@@ -18,7 +17,7 @@ def deploy(env, source_folder, dist_file):
     sec_prop = _r.hgetall('qa_common_account')
 
     # 逐行读取 ttsd-env.properties, 如果不是注释也不是空行，则split by "=",
-    # 依次从 sec_prop > env_prop > qa_common_prop 中读取key，
+    # 依次从 build_params > sec_prop > env_prop > qa_common_prop 中读取key，
     # 如果存在就替换原值，并将结果写入到结果文件 dist_file 中。
 
     with open("{0}/ttsd-env.properties".format(source_folder), "r") as f, open(dist_file, "w") as dist:
@@ -27,13 +26,11 @@ def deploy(env, source_folder, dist_file):
             if l and not l.startswith("#"):
                 key_value = l.split("=")
                 key = key_value[0].strip()
-                value = "=".join(key_value[1:]).strip()
-                if sec_prop.has_key(key):
-                    value = sec_prop[key]
-                elif env_prop.has_key(key):
-                    value = env_prop[key]
-                elif qa_common_prop.has_key(key):
-                    value = qa_common_prop[key]
+                value =  get_build_params_value_by_key(key) \
+                         or (sec_prop[key] if sec_prop.has_key(key) else False) \
+                         or (env_prop[key] if env_prop.has_key(key) else False) \
+                         or (qa_common_prop[key] if qa_common_prop.has_key(key) else False) \
+                         or key_value[-1].strip()
                 dist.write(key + "=" + value + "\n")
             else:
                 dist.write(l + "\n")
@@ -51,3 +48,10 @@ def load_properties(file_path):
                 value = '='.join(key_value[1:]).strip()
                 props[key] = value
     return props
+
+def get_build_params_value_by_key(build_params, key):
+    split = key.split('.')
+    value = build_params
+    for item in split:
+        value = value[item] if value and value.has_key(item) else None
+    return value
