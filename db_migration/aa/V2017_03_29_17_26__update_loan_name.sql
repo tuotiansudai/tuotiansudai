7e9545fabd28045dd9e4a57df15ab77bed0c21ef
gengbj <gengbj@tuotiansudai.com>
2017-03-30 17:03:01 +0800

    fix test

--- db_migration/aa/V2017_03_29_17_26__update_loan_name.sql
+++ db_migration/aa/V2017_03_29_17_26__update_loan_name.sql
@@ -8,13 +8,13 @@ AND DATE_FORMAT(la.created_time,'%Y') = '2015';
 
 UPDATE loan la,
 (SELECT concat(16,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2016' ORDER BY created_time ASC) lc
-SET la.name = concat(la.name, lb.lastYearTwo, lc.loanNameSeq)
+SET la.name = concat(la.name, lc.loanNameSeq)
 WHERE la.id = lc.id
 AND DATE_FORMAT(la.created_time,'%Y') = '2016';
 
 UPDATE loan la,
-(SELECT concat(16,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2017' ORDER BY created_time ASC) lc
-SET la.name = concat(la.name, lb.lastYearTwo, lc.loanNameSeq)
+(SELECT concat(17,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2017' ORDER BY created_time ASC) lc
+SET la.name = concat(la.name, lc.loanNameSeq)
 WHERE la.id = lc.id
 AND DATE_FORMAT(la.created_time,'%Y') = '2017';
 
