18422288facc3458e8bac060b38d40e4376dd2e8
gengbj <gengbj@tuotiansudai.com>
2017-04-06 10:51:37 +0800

    remove update edxmessage.user_message

--- db_migration/aa/V2017_03_29_17_26__update_loan_name.sql
+++ db_migration/aa/V2017_03_29_17_26__update_loan_name.sql
@@ -18,23 +18,4 @@ SET la.name = concat(la.name, lc.loanNameSeq)
 WHERE la.id = lc.id
 AND DATE_FORMAT(la.created_time,'%Y') = '2017';
 
-
-UPDATE edxmessage.user_message um,
-(SELECT l.id, l.name FROM aa.loan AS l, edxmessage.user_message AS eum WHERE l.id = eum.business_id AND l.pledge_type='HOUSE') l
-SET um.content = REPLACE(um.content, '房产抵押借款', l.name),
-um.title = REPLACE(um.title, '房产抵押借款', l.name)
-WHERE um.business_id = l.id
-AND um.message_id IN (
-SELECT id FROM message WHERE event_type IN ('INVEST_SUCCESS','LOAN_OUT_SUCCESS','REPAY_SUCCESS','ADVANCED_REPAY','TRANSFER_SUCCESS')
-);
-
-UPDATE edxmessage.user_message um,
-(SELECT l.id, l.name FROM aa.loan AS l, edxmessage.user_message AS eum WHERE l.id = eum.business_id AND l.pledge_type='VEHICLE') l
-SET um.content = REPLACE(um.content, '车辆抵押借款', l.name),
-um.title = REPLACE(um.title, '车辆抵押借款', l.name)
-WHERE um.business_id = l.id
-AND um.message_id IN (
-SELECT id FROM message WHERE event_type IN ('INVEST_SUCCESS','LOAN_OUT_SUCCESS','REPAY_SUCCESS','ADVANCED_REPAY','TRANSFER_SUCCESS')
-);
-
 COMMIT;
