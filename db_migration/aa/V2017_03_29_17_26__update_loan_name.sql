e36504e359bf785d89f5747a39a7b57f85ff4af1
gengbj <gengbj@tuotiansudai.com>
2017-04-07 11:35:46 +0800

    fix migrate

--- db_migration/aa/V2017_03_29_17_26__update_loan_name.sql
+++ db_migration/aa/V2017_03_29_17_26__update_loan_name.sql
@@ -19,10 +19,16 @@ WHERE la.id = lc.id
 AND DATE_FORMAT(la.created_time,'%Y') = '2016';
 
 UPDATE loan la,
-(SELECT concat(16,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2016' AND product_type != 'EXPERIENCE' AND pledge_type= 'HOUSE' ORDER BY created_time ASC) lc
+(SELECT concat(16,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2016' AND product_type != 'EXPERIENCE' AND pledge_type= 'VEHICLE' ORDER BY created_time ASC) lc
 SET la.name = concat(la.name, lc.loanNameSeq)
 WHERE la.id = lc.id
 AND DATE_FORMAT(la.created_time,'%Y') = '2016';
+
+UPDATE loan la,
+(SELECT concat(17,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2017' AND product_type != 'EXPERIENCE' AND pledge_type= 'HOUSE' ORDER BY created_time ASC) lc
+SET la.name = concat(la.name, lc.loanNameSeq)
+WHERE la.id = lc.id
+AND DATE_FORMAT(la.created_time,'%Y') = '2017';
 
 UPDATE loan la,
 (SELECT concat(17,LPAD((@rowNO := @rowNo+1),3,'0')) AS loanNameSeq, id AS id FROM loan AS a, (select @rowNO :=0) b WHERE DATE_FORMAT(created_time,'%Y') = '2017' AND product_type != 'EXPERIENCE' AND pledge_type= 'VEHICLE' ORDER BY created_time ASC) lc
