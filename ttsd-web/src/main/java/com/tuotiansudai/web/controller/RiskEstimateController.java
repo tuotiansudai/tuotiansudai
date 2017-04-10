bc858f7f77c3f2b307b19f3869b04b793cee4345
Xiduan Gao <gaoxiduan@gmail.com>
2017-04-10 11:49:49 +0800

    add risk estimate controller

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/RiskEstimateController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/RiskEstimateController.java
@@ -9,6 +9,7 @@ import com.tuotiansudai.spring.LoginUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,6 +22,11 @@ public class RiskEstimateController {
     @Autowired
     public RiskEstimateController(RiskEstimateService riskEstimateService) {
         this.riskEstimateService = riskEstimateService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView show() {
+        return new ModelAndView("/risk-estimate");
     }
 
     @RequestMapping(method = RequestMethod.POST)
