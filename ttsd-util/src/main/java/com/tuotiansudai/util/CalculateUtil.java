fdff654789eedbe0c0da5c5f2f495a81d273263b
gengbj <gengbj@tuotiansudai.com>
2017-04-12 16:13:14 +0800

    add fundRaiseingDuration to loan detail

--- ttsd-util/src/main/java/com/tuotiansudai/util/CalculateUtil.java
+++ ttsd-util/src/main/java/com/tuotiansudai/util/CalculateUtil.java
@@ -1,6 +1,7 @@
 package com.tuotiansudai.util;
 
 import org.joda.time.DateTime;
+import org.joda.time.Days;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -13,5 +14,13 @@ public class CalculateUtil {
 
     public static Date calculateMaxDate(){
         return new DateTime(9999, 12, 31, 0, 0, 0).toDate();
+    }
+
+    //两个日期之间的天数（包括startTime和endTime）
+    public static int calculateDuration(Date startTime, Date endTime) {
+        if (startTime == null || endTime == null || startTime.after(endTime)) {
+            return 0;
+        }
+        return Days.daysBetween(new DateTime(startTime).withTimeAtStartOfDay(), new DateTime(endTime)).getDays() + 1;
     }
 }
