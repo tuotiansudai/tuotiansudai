b6d87c87acc4ee81b79e661a6a92eb7a64745eba
gengbj <gengbj@tuotiansudai.com>
2017-04-17 15:33:37 +0800

    fix bug

--- ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/GrantWelfareActivityService.java
+++ ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/GrantWelfareActivityService.java
@@ -5,7 +5,9 @@ import com.google.common.collect.Lists;
 import com.tuotiansudai.repository.mapper.InvestMapper;
 import com.tuotiansudai.repository.mapper.UserMapper;
 import com.tuotiansudai.repository.model.UserModel;
+import com.tuotiansudai.util.AmountConverter;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,8 +32,9 @@ public class GrantWelfareActivityService {
         if (Strings.isNullOrEmpty(loginName)) {
             return 0;
         }
-        Date startTime = new DateTime(grantWelfarePeriod.get(0)).toDate();
-        Date endTime = new DateTime(grantWelfarePeriod.get(1)).toDate();
+
+        Date startTime = DateTime.parse(grantWelfarePeriod.get(0), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
+        Date endTime = DateTime.parse(grantWelfarePeriod.get(1), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
         List<UserModel> userModels = userMapper.findUsersByRegisterTimeOrReferrer(startTime, endTime, loginName);
         for (UserModel referrerUserModel : userModels) {
             if (referrerUserModel.getRegisterTime().before(endTime) && referrerUserModel.getRegisterTime().after(startTime)) {
@@ -44,20 +47,21 @@ public class GrantWelfareActivityService {
         return ReferrerCount;
     }
 
-    public long findReferrerSumInvestAmountByLoginName(String loginName) {
+    public String findReferrerSumInvestAmountByLoginName(String loginName) {
         long ReferrerSumInvestAmount = 0L;
         if (Strings.isNullOrEmpty(loginName)) {
-            return 0L;
+            return "0";
         }
-        Date startTime = new DateTime(grantWelfarePeriod.get(0)).toDate();
-        Date endTime = new DateTime(grantWelfarePeriod.get(1)).toDate();
+
+        Date startTime = DateTime.parse(grantWelfarePeriod.get(0), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
+        Date endTime = DateTime.parse(grantWelfarePeriod.get(1), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
         List<UserModel> userModels = userMapper.findUsersByRegisterTimeOrReferrer(startTime, endTime, loginName);
         for (UserModel referrerUserModel : userModels) {
             if (referrerUserModel.getRegisterTime().before(endTime) && referrerUserModel.getRegisterTime().after(startTime)) {
                 ReferrerSumInvestAmount += investMapper.sumSuccessActivityInvestAmount(referrerUserModel.getLoginName(), null, startTime, endTime);
             }
         }
-        return ReferrerSumInvestAmount;
+        return AmountConverter.convertCentToString(ReferrerSumInvestAmount);
     }
 
 }
