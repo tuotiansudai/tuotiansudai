f544942a083080e938fe9f8529fc55a5d01e0f79
fulanyan <fly39244080@163.com>
2017-04-19 17:59:07 +0800

    some change

--- ttsd-frontend-manage/resources/static/web/js/wechat/we_start.jsx
+++ ttsd-frontend-manage/resources/static/web/js/wechat/we_start.jsx
@@ -1,3 +1,38 @@
 require('webStyle/wechat/wechat_login.scss');
+let ValidatorObj= require('publicJs/validator');
+let formStart=globalFun.$('#formStart');
 
-let test = '23';
+
+//用户注册表单校验
+let validator = new ValidatorObj.ValidatorForm();
+let $errorBox = $('.error-box',$(formStart));
+//验证手机号是否存在
+
+validator.add(formStart.mobile, [{
+    strategy: 'isNonEmpty',
+    errorMsg: '手机号不能为空',
+}, {
+    strategy: 'isMobile',
+    errorMsg: '手机号格式不正确'
+},{
+    strategy: 'isMobileExist',
+    errorMsg: '手机号已经存在'
+}]);
+
+
+globalFun.addEventHandler(formStart.mobile,"keyup", "blur", function() {
+    let errorMsg=validator.start(this);
+    $errorBox.text(errorMsg?errorMsg:'');
+});
+
+
+formStart.onsubmit = function(event) {
+    event.preventDefault();
+    //提交之前得先执行validateLogin验证表单是否通过验证
+    // formStartFun.before(validateLogin)();
+
+};
+
+
+
+
--- ttsd-web/src/main/webapp/templates/weChat/start.ftl
+++ ttsd-web/src/main/webapp/templates/weChat/start.ftl
@@ -13,7 +13,20 @@
 
 </head>
 <body>
+<div class="weChat-container" id="weChatStartContainer">
+    <h2 class="note">请填写手机号码，以补全您的拓天速贷账户信息。</h2>
 
+    <form id="formStart">
+        <input validate class="login-name" type="text" value="" name="username" placeholder="请输入账号/手机号" maxlength="25"/>
+        <div class="error-box">sdsd</div>
+        <button type="submit" class="btn-normal">下一步</button>
+    </form>
+
+    <div class="notice-tip">
+        <b>温馨提示:</b>
+        <p>若您输入的手机号码已被注册，需输入相应的登录密码补全账户信息；若未被注册需要设置密码补全信息。</p>
+    </div>
+</div>
 
 <script>
     window.staticServer='${commonStaticServer}';
