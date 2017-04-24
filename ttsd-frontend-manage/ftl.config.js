0578949c42a2da0a0272c09299f6f5db2f3aa952
fulanyan <fly39244080@163.com>
2017-04-24 12:06:37 +0800

    ftl config

--- ttsd-frontend-manage/ftl.config.js
+++ ttsd-frontend-manage/ftl.config.js
@@ -1,5 +1,8 @@
 var path = require('path');
-var webTemplatePath='../ttsd-web/src/main/webapp/templates/';
+var webTemplatePath='../ttsd-web/src/main/webapp/templates/'; //web站点ftl目录
+var activityTemplatePath='../ttsd-activity-web/src/main/webapp/templates/'; //activity站点ftl目录
+var askTemplatePath='../ttsd-ask-web/src/main/webapp/ask/templates/'; //ask站点ftl目录
+var pointTemplatePath='../ttsd-point-web/src/main/webapp/templates/'; //point站点ftl目录
 
 var basePath = path.join(__dirname, 'resources'),
     outputPath=path.join(basePath, 'develop'); //默认打包路径
