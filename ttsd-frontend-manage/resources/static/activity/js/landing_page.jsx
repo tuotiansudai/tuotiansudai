26ac5f7e6eced5147b535ac6396e5497999e8afa
xuqiang <xuqiang@tuotiansudai.com>
2017-04-26 15:09:57 +0800

    fix jsx

--- ttsd-frontend-manage/resources/static/activity/js/landing_page.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/landing_page.jsx
@@ -55,7 +55,7 @@ function showReferrerInfoIfNeeded() {
 
     if (referNum) {
         //有推荐人
-        var mobileNum = commonFun.uncompile(referNum);
+        var mobileNum = commonFun.decrypt.uncompile(referNum);
         $('input[name="referrer"]', $landingContainerBox).val(mobileNum);
         //通过手机号得到用户名
         commonFun.useAjax({
