733dc2cca2001198bb3b28e39568d3b9e844103b
zhoubx <zhoubaoxin@tuotiansudai.com>
2017-05-10 19:13:00 +0800

    dragon_boat_wechat_punch_card

---
+++ db_migration/aa/V2017_05_10_15_10__create_dragon_boat_exchange_coupon.sql
@@ -0,0 +1,41 @@
+INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
+`active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
+`user_group`, `deleted`, `coupon_source`, `comment`)
+VALUES (410, 500, 0, 0, NULL, 0, '2017-05-16 00:00:00', '2017-05-31 23:59:59', '15', '0', '10000', '0',
+'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '200000', '_90,_180,_360', 'RED_ENVELOPE',
+'EXCHANGER_CODE', 0, '端午节活动微信打卡奖励', '');
+
+INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
+`active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
+`user_group`, `deleted`, `coupon_source`, `comment`)
+VALUES (411, 800, 0, 0, NULL, 0, '2017-05-16 00:00:00', '2017-05-31 23:59:59', '15', '0', '10000', '0',
+'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '320000', '_90,_180,_360', 'RED_ENVELOPE',
+'EXCHANGER_CODE', 0, '端午节活动微信打卡奖励', '');
+
+INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
+`active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
+`user_group`, `deleted`, `coupon_source`, `comment`)
+VALUES (412, 1000, 0, 0, NULL, 0, '2017-05-16 00:00:00', '2017-05-31 23:59:59', '15', '0', '3000', '0',
+'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '400000', '_90,_180,_360', 'RED_ENVELOPE',
+'EXCHANGER_CODE', 0, '端午节活动微信打卡奖励', '');
+
+INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
+`active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
+`user_group`, `deleted`, `coupon_source`, `comment`)
+VALUES (413, 1800, 0, 0, NULL, 0, '2017-05-16 00:00:00', '2017-05-31 23:59:59', '15', '0', '3000', '0',
+'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '720000', '_90,_180,_360', 'RED_ENVELOPE',
+'EXCHANGER_CODE', 0, '端午节活动微信打卡奖励', '');
+
+INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
+`active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
+`user_group`, `deleted`, `coupon_source`, `comment`)
+VALUES (414, 2800, 0, 0, NULL, 0, '2017-05-16 00:00:00', '2017-05-31 23:59:59', '15', '0', '3000', '0',
+'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '1120000', '_90,_180,_360', 'RED_ENVELOPE',
+'EXCHANGER_CODE', 0, '端午节活动微信打卡奖励', '');
+
+INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
+`active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
+`user_group`, `deleted`, `coupon_source`, `comment`)
+VALUES (415, 0, 0.005, 0, NULL, 0, '2017-05-26 00:00:00', '2017-06-11 23:59:59', '15', '0', '3000', '0',
+'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '9000', '_90,_180,_360', 'INTEREST_COUPON',
+'EXCHANGER_CODE', 0, '端午节活动微信打卡奖励', '');
