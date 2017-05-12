906b9c627b00b599edf6546de1f8dc1ef5aa8e16
xuqiang <xuqiang@tuotiansudai.com>
2017-05-12 11:21:59 +0800

    wechat finished

---
+++ ttsd-frontend-manage/resources/static/activity/js/wechat/wechat_invite.jsx
@@ -0,0 +1,18 @@
+require("activityStyle/wechat/wechat_invite.scss");
+
+
+let redBag=require('../../images/dragon-boat/red-bag.png'),
+	$redBag=$('#redBag');
+
+$redBag.attr('src',redBag);
+
+
+
+
+
+
+
+
+
+
+
