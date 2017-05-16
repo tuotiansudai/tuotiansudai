d36bcdc3e6e884dac63ee3d2da2c313e1f469a3d
zhoubx <zhoubaoxin@tuotiansudai.com>
2017-05-16 20:32:35 +0800

    fix sql

--- db_migration/aa/V2017_05_11_17_19__create_coupon_dragon_boat_share_prize.sql
+++ db_migration/aa/V2017_05_11_17_19__create_coupon_dragon_boat_share_prize.sql
@@ -2,5 +2,5 @@ INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `mul
 `active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
 `user_group`, `deleted`, `coupon_source`, `comment`)
 VALUES (421, 1000, 0, 0, NULL, 0, '2017-05-16 00:00:00', '2017-05-31 23:59:59', '15', '0', '99999', '0',
-'0', '0', 'sidneygao', now(), NULL, NULL, NULL, NULL, '400000', '_90,_180,_360', 'RED_ENVELOPE',
+'1', '0', 'sidneygao', now(), 'sidneygao', now(), NULL, NULL, '400000', '_90,_180,_360', 'RED_ENVELOPE',
 'WINNER', 0, '端午节活动好友分享奖励', '');
