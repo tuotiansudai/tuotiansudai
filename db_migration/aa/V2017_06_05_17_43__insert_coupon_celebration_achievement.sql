303805a5ed0681ddbf0153c4e040aaaa528b09e9
zhanglong888 <zhanglong@tuotiansudai.com>
2017-06-05 19:37:37 +0800

    features/zl/celebration_max_invest_amount

--- db_migration/aa/V2017_06_05_17_43__insert_coupon_celebration_achievement.sql
+++ db_migration/aa/V2017_06_05_17_43__insert_coupon_celebration_achievement.sql
@@ -2,23 +2,23 @@ INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `mul
                       `active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
                       `user_group`, `deleted`, `coupon_source`, `comment`)
 VALUES (439, 5000, 0, 0, NULL, 1, '2017-07-01 00:00:00', '2017-07-16 23:59:59', '7', '0', '999999', '0',
-                                                                                                     '1', '0',
-                                                                                                     'sidneygao', now(),
-                                                                                                     'sidneygao', now(),
-                                                                                                     NULL, NULL,
-                                                                                                     '1500000',
+                                                                                                    '1', '0',
+                                                                                                    'sidneygao', now(),
+                                                                                                    'sidneygao', now(),
+                                                                                                    NULL, NULL,
+                                                                                                    '1500000',
         '_90,_180,_360', 'RED_ENVELOPE',
-        'WINNER', 0, '标王争霸场', '');
+        'WINNER', 0, '标王争霸场第二名', '');
 
 
 INSERT INTO `coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `period`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`,
                       `active`, `shared`, `created_by`, `created_time`, `activated_by`, `activated_time`, `updated_by`, `updated_time`, `invest_lower_limit`, `product_types`, `coupon_type`,
                       `user_group`, `deleted`, `coupon_source`, `comment`)
 VALUES (440, 2000, 0, 0, NULL, 1, '2017-07-01 00:00:00', '2017-07-16 23:59:59', '7', '0', '999999', '0',
-                                                                                                     '1', '0',
-                                                                                                     'sidneygao', now(),
-                                                                                                     'sidneygao', now(),
-                                                                                                     NULL, NULL,
-                                                                                                     '800000',
+                                                                                                    '1', '0',
+                                                                                                    'sidneygao', now(),
+                                                                                                    'sidneygao', now(),
+                                                                                                    NULL, NULL,
+                                                                                                    '800000',
         '_90,_180,_360', 'RED_ENVELOPE',
-        'WINNER', 0, '幸运大抽奖获得', '');
+        'WINNER', 0, '标王争霸场第三名', '');
