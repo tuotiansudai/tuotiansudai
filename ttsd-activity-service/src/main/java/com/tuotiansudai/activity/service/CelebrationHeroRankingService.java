8b0c29d5e24ce8a627acb75dcbd27a3d54d235e8
朱坤 <zhukun@zhukundeMacBook-Pro.local>
2017-06-06 17:19:50 +0800

    update

--- ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/CelebrationHeroRankingService.java
+++ ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/CelebrationHeroRankingService.java
@@ -43,7 +43,7 @@ public class CelebrationHeroRankingService {
 
         tradingTime = new DateTime(tradingTime).withTimeAtStartOfDay().plusDays(1).minusMillis(1).toDate();
         List<NewmanTyrantView> celebrationHeroRankingViews=investCelebrationHeroRankingMapper.findCelebrationHeroRankingByTradingTime(tradingTime, celebrationHeroRankingActivityPeriod.get(0), celebrationHeroRankingActivityPeriod.get(1));
-        return CollectionUtils.isNotEmpty(celebrationHeroRankingViews) && celebrationHeroRankingViews.size() > 10 ? celebrationHeroRankingViews.subList(0, 10) : celebrationHeroRankingViews;
+        return celebrationHeroRankingViews;
 
     }
 
