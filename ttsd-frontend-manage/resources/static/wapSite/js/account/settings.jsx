04d3f81d66382c9101a25e752b5fac3e0f3f6f33
fly39244080 <fly39244080@163.com>
2017-07-06 19:22:34 +0800

    some change for setting

--- ttsd-frontend-manage/resources/static/wapSite/js/account/settings.jsx
+++ ttsd-frontend-manage/resources/static/wapSite/js/account/settings.jsx
@@ -1,38 +1,208 @@
-
+let commonFun = require('publicJs/commonFun');
 require('wapSiteStyle/account/settings.scss');
 
 let $settingBox = $('#settingBox');
+
+let UrlOption = {
+    //关闭免密投资
+    disabled:'no-password-invest/disabled',
+    //打开免密投资
+    enabled:'no-password-invest/enabled',
+    //发送手机短信验证码
+    sendCaptcha:'no-password-invest/send-captcha',
+    //刷新图形验证码
+    imageCaptcha:'no-password-invest/image-captcha',
+
+    //去联动优势授权
+    agreement:'/agreement'
+};
+
 //开启免密投资
 let $btnOpenNopwd = $('.update-payment-nopwd',$settingBox);
 
 $btnOpenNopwd.on('click',function() {
-    let isOpen = $btnOpenNopwd.hasClass('opened');
-
+    let isOpen = $btnOpenNopwd.hasClass('opened'); //
+    let firstopen = $btnOpenNopwd.data('firstopen'); //用来判断是否第一次开启
     if(isOpen) {
-        //目前是开启的状态，现在做的是要去关闭
+        //之前是开启的状态，现在做的是要去关闭
+        $btnOpenNopwd.removeClass('opened');
 
-        layer.open({
-            type: 1,
-            title: false,
-            closeBtn: 0,
-            area: ['280px', '230px'],
-            shadeClose: false,
-            skin: 'tip-square-box',
-            btn: ['确定','取消'],
-            content: $('.tip-to-close'),
-            btn1:function() {
-                //确定
-
-
-            },
-            btn2:function() {
-                //取消
-            }
-        });
 
     } else {
-        //目前是开闭的状态，现在做的是要去开启
-
+        //之前是开闭的状态，现在做的是要去开启
+        $btnOpenNopwd.addClass('opened');
+        OpenNoPasswordInvest(firstopen);
 
     }
 });
+
+//去开启免密投资业务，第一次开启免密投资，需要去联动优势授权，之后不需要
+function OpenNoPasswordInvest(firstopen) {
+
+    if(firstopen) {
+        CommonLayerTip({
+            btn: ['确定','取消'],
+            content: $('#turnOnNoPassword')
+        },function() {
+            //确定打开，去联动优势授权
+            commonFun.useAjax({
+                type: 'POST',
+                url:UrlOption['agreement']
+            },function() {
+                //授权成功
+                CommonLayerTip({
+                    btn: ['我知道了'],
+                    content: $('#noPasswordInvestDOM')
+                });
+
+            });
+        });
+    } else {
+
+        //如果是第二次打开 ，无需走联动优势，直接打开
+        commonFun.useAjax({
+            type: 'POST',
+            url:UrlOption['enabled']
+        },function() {
+            CommonLayerTip({
+                btn: ['我知道了'],
+                content: '<div class="tip-result-success pad"> <em class="icon-success"></em><span>免密支付已开启</span></div>',
+            },function() {
+                location.reload();
+                layer.closeAll();
+            });
+
+        });
+    }
+}
+
+//去关闭免密投资业务
+function turnOffNoPassword() {
+    CommonLayerTip({
+        btn: ['确定', '取消'],
+        content: $('#turnOffNoPassword')
+    },function() {
+        //确定关闭免密支付，然后会弹出需要输入验证码的框
+
+        CommonLayerTip({
+            btn: ['确定', '取消'],
+            content: $('#turnOnSendCaptcha')
+        },function() {
+            //确认， 这里没做
+        });
+
+    });
+}
+
+function CommonLayerTip(option,firstCallback,secondCallback) {
+    layer.closeAll();
+    let defaultOption = {
+        btn:['确定', '取消'],
+        content:$('#turnOnSendCaptcha'),
+        area:['280px', '230px']
+    };
+
+    let optionOk = $.extend({},defaultOption,option);
+    layer.open({
+        type: 1,
+        title: false,
+        closeBtn: 0,
+        area: btn.area,
+        shadeClose: false,
+        skin: 'tip-square-box',
+        btn: optionOk.btn,
+        content: optionOk.content,
+        btn1: function () {
+            firstCallback && firstCallback();
+        },
+        btn2: function () {
+            secondCallback && secondCallback();
+        }
+    });
+}
+
+// 下面的代码是安心签的
+
+// $btnOpenNopwd.on('click',function() {
+//     let isOpen = $btnOpenNopwd.hasClass('opened');
+//
+//     if(isOpen) {
+//         //目前是开启的状态，现在做的是要去关闭
+//
+//         layer.open({
+//             type: 1,
+//             title: false,
+//             closeBtn: 0,
+//             area: ['280px', '230px'],
+//             shadeClose: false,
+//             skin: 'tip-square-box',
+//             btn: ['确定','取消'],
+//             content: $('.tip-to-close'),
+//             btn1:function() {
+//                 //确定关闭
+//                 switchSkipAuth(false);
+//
+//
+//             },
+//             btn2:function() {
+//                 //取消,无需做额外的操作
+//             }
+//         });
+//
+//     } else {
+//         //目前是开闭的状态，现在做的是要去开启
+//
+//         layer.open({
+//             type: 1,
+//             title: false,
+//             closeBtn: 0,
+//             area: ['280px', '230px'],
+//             shadeClose: false,
+//             skin: 'tip-square-box',
+//             btn: ['去授权','取消'],
+//             content: $('.tip-to-open'),
+//             btn1:function() {
+//                 //确定关闭
+//                 switchSkipAuth(false);
+//
+//             },
+//             btn2:function() {
+//                 //取消,无需做额外的操作
+//             }
+//         });
+//
+//     }
+// });
+//
+// //改变安心签的状态，开启还是关闭
+// function switchSkipAuth(openStatue=true) {
+//     commonFun.useAjax({
+//             url:'/anxinSign/switchSkipAuth',
+//             type:'POST',
+//             data:{
+//                 "open":openStatue
+//             }
+//         },function(data) {
+//         if(data.success) {
+//             layer.closeAll();
+//             let timMsg = '免密支付已关闭';
+//             if(openStatue) {
+//                 // 开启成功
+//                 timMsg = '免密支付已开启';
+//             } else {
+//                 // 关闭成功
+//             }
+//
+//             layer.open({
+//                 type: 1,
+//                 title: false,
+//                 closeBtn: 0,
+//                 area: ['280px', '180px'],
+//                 shadeClose: false,
+//                 skin: 'tip-square-box',
+//                 btn: ['我知道了'],
+//                 content: `<div class="tip-result-success pad"> <em class="icon-success"></em><span>${timMsg}</span></div>`
+//             });
+//         }
+//     })
+// }
--- ttsd-wap-site/src/main/webapp/templates/myAccount/settings.ftl
+++ ttsd-wap-site/src/main/webapp/templates/myAccount/settings.ftl
@@ -22,7 +22,7 @@
         </li>
         <li>
             <label for="perCard">免密支付</label>
-            <span class="update-payment-nopwd opened">
+            <span class="update-payment-nopwd opened" data-firstopen="true">
                 <i></i>
             </span>
         </li>
@@ -37,22 +37,45 @@
     </ul>
 
     <button type="button" class="btn-wap-normal next-step" >退出登录</button>
+</div>
 
 
-    <div class="tip-to-close pad" style="display: none">
-        <em class="icon-notice"></em>
-        <span>
+<#--开启免密投资 如果是第一次，需要有联动优势授权-->
+<div id="turnOnNoPassword" class="pad" style="display: none;">
+    <form name="turnOnNoPasswordInvestForm" >
+        <b>开通免密支付</b>
+        <span>理财快人一步，专享快速购买标的的便捷体验</span>
+
+        <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+        <input type="hidden" name="noPasswordInvest" value="true"/>
+    </form>
+</div>
+
+<div id="noPasswordInvestDOM" class="pad-m" style="display: none;">
+    <p>请在新打开的联动优势页面充值完成后选择：</p>
+    <p><a href="/personal-info" class="btn-success" data-category="确认成功" >继续</a>(授权后视情况可能会有一秒或更长的延迟)</p>
+    <span>遇到问题请拨打我们的客服热线：400-169-1188（工作日 9:00-20:00）</span>
+</div>
+
+
+<#--关闭免密投资 需要有短信验证码-->
+
+<div id="turnOffNoPassword" class="tip-to-close pad" style="display: none">
+    <em class="icon-notice"></em>
+    <span>
             免密支付可以帮助你<br/>
             在投资时快速购买标的；<br/>
             您是否确认关闭免密支付？
         </span>
 
-    </div>
-
-    <div class="tip-closed pad" style="display: none">
-        <em class="icon-success"></em>
-        免密支付已关闭
-    </div>
-
 </div>
+<div id="turnOnSendCaptcha" class="pad" style="display: none;">
+    <b>开通免密支付</b>
+    <span>向185****4386发送验证码</span>
+    <form name="">
+    <input type="hidden" value="123" name="mobile">
+    <input type="text" name="Captcha"> <button type="button">取消</button>
+    </form>
+</div>
+
 </@global.main>
