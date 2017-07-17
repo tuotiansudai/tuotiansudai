8ee4512db5d6b259f118f8c8590ef4f068e1a5d2
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-07-17 20:30:12 +0800

    fix unittest

--- ttsd-ask-service/src/test/java/com/tuotiansudai/ask/client/AskRestClientTest.java
+++ ttsd-ask-service/src/test/java/com/tuotiansudai/ask/client/AskRestClientTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,6 +24,7 @@ import java.net.URL;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@PropertySource(value = {"classpath:ttsd-env.properties", "classpath:ttsd-env-test.properties"}, ignoreResourceNotFound = true)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class AskRestClientTest {
     @Value("${ask.rest.server}")
