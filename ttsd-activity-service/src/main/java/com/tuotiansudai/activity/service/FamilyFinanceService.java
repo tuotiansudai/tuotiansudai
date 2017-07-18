46e6b0a0f7ef8004370b785e8f0b901ec9d22831
zhukun5764 <zhukun@tuotiansudai.com>
2017-07-18 16:40:43 +0800

    818 activity start

---
+++ ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/FamilyFinanceService.java
@@ -0,0 +1,31 @@
+package com.tuotiansudai.activity.service;
+
+import com.tuotiansudai.activity.repository.dto.ExchangePrizeDto;
+import com.tuotiansudai.repository.mapper.InvestMapper;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class FamilyFinanceService {
+
+    @Autowired
+    private InvestMapper investMapper;
+
+    @Value(value = "#{new java.text.SimpleDateFormat(\"yyyy-MM-dd HH:mm:ss\").parse(\"${activity.family.finance.startTime}\")}")
+    private Date startTime;
+
+    @Value(value = "#{new java.text.SimpleDateFormat(\"yyyy-MM-dd HH:mm:ss\").parse(\"${activity.family.finance.endTime}\")}")
+    private Date endTime;
+
+    public long toDayInvestAmount(String loginName){
+        Date nowDate= DateTime.now().toDate();
+        if (nowDate.before(startTime) || nowDate.after(endTime)) {
+            return 0;
+        }
+        return investMapper.findSuccessByLoginNameExceptTransferAndTime(loginName,DateTime.now().withTimeAtStartOfDay().toDate(), DateTime.now().plusDays(1).withTimeAtStartOfDay().plusMillis(-1).toDate()).stream().mapToLong(i->i.getAmount()).sum();
+    }
+}
