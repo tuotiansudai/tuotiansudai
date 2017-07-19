abd825620340e1ac3619b03fea51b24d9d5c6c1b
zhoubx <zhoubaoxin@tuotiansudai.com>
2017-07-19 14:56:47 +0800

    try

--- scripts/unit_test.py
+++ scripts/unit_test.py
@@ -17,7 +17,7 @@ class UTRunner(object):
         self.init_docker()
         self.run_test()
         self.clean_env()
-        self.current_ut()
+        self.current_test()
 
     def config_file(self):
         print "Generate config file..."
@@ -39,13 +39,14 @@ class UTRunner(object):
         print "Starting test..."
         # sh('/opt/gradle/latest/bin/gradle -Pdbhost={0} -Pdbport={1} -Predishost={2} -Predisport={3} clean compileJava ttsd-config:flywayAA ttsd-config:flywayUMP ttsd-config:flywayAnxin ttsd-config:flywaySms ttsd-config:flywayWorker ttsd-config:flywayAsk ttsd-config:flywayActivity ttsd-config:flywayPoint ttsd-config:flywayMessage ttsd-config:flywayLog test'.format(
         #         self.db_host, self.db_port, self.redis_host, self.redis_port))
-        sh('/opt/gradle/latest/bin/gradle -Pdbhost={0} -Pdbport={1} -Predishost={2} -Predisport={3} clean compileJava ttsd-config:flywayAnxin test'.format(
+        sh('/opt/gradle/latest/bin/gradle -Pdbhost={0} -Pdbport={1} -Predishost={2} -Predisport={3} clean compileJava ttsd-config:flywayAnxin ttsd-anxin-wrapper:test'.format(
             self.db_host, self.db_port, self.redis_host, self.redis_port))
 
     def clean_env(self):
         self._remove_old_container()
 
-    def current_ut(self):
+    def current_test(self):
+        print "Starting current testing ..."
         sh('cd ttsd-current && sh current_ut.sh')
 
 
