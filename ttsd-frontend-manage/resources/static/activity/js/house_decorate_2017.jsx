9b832cf48159256d0b7ba4915671f7e59b12d9af
xuqiang <xuqiang@tuotiansudai.com>
2017-07-24 15:03:15 +0800

    fix responsive

--- ttsd-frontend-manage/resources/static/activity/js/house_decorate_2017.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/house_decorate_2017.jsx
@@ -12,11 +12,9 @@ let topimg=require('../images/2017/house-decorate/top-img.png'),
 
 $houseDecorateContainer.find('.top-img .media-pc').attr('src',topimg).siblings('.media-phone').attr('src',topimgPhone);
 
-let drawBtn=require('../images/2017/sport-play/draw-btn.png');
-$houseDecorateContainer.find('.draw-model img').attr('src',drawBtn);
 
 
-let $pointerImg = $('.draw-btn', $houseDecorateContainer);
+let $pointerImg = $('.bag-item strong', $houseDecorateContainer);
 $pointerImg.on('click', function () {
     //未登录
     $.when(commonFun.isUserLogin())
@@ -65,19 +63,12 @@ function getGift() {
                     content: $('#loginTip')
                 });
             } else {
-                commonFun.useAjax({
-                    dataType: 'json',
-                    url:'/activity/exercise-work/draw-time'
-                },function(data) {
-                    $houseDecorateContainer.find('.draw-time').text(data);
-                });
-
                 $('#lotteryTip').html(tpl('lotteryTipTpl', data));
                 layer.open({
                   type: 1,
                   title: false,
                   closeBtn: 0,
-                  area: $(window).width()>700?['450px', '230px']:['300px','155px'],
+                  area: $(window).width()>700?['405px', '350px']:['300px','255px'],
                   content: $('#lotteryTip') 
                 });
             }
@@ -89,3 +80,21 @@ $('body').on('click', '.close-tip', function(event) {
     event.preventDefault();
     layer.closeAll();
 });
+
+
+// 登录弹框
+$houseDecorateContainer.find('.to-login').on('click', function(event) {
+    event.preventDefault();
+    if (sourceKind.params.source == 'app') {
+        location.href = "/login";
+    } else {
+        layer.open({
+            type: 1,
+            title: false,
+            closeBtn: 0,
+            area: ['auto', 'auto'],
+            content: $('#loginTip')
+        });
+    }
+});
+
