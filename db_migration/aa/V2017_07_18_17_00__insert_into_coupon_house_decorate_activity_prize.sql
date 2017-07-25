f550b6cc276f11ec5f0b81afb5e8e7b1cd373821
zhukun5764 <zhukun@tuotiansudai.com>
2017-07-25 16:49:35 +0800

    sql red  endtime

--- db_migration/aa/V2017_07_18_17_00__insert_into_coupon_house_decorate_activity_prize.sql
+++ db_migration/aa/V2017_07_18_17_00__insert_into_coupon_house_decorate_activity_prize.sql
@@ -1,6 +1,6 @@
 BEGIN;
 INSERT INTO `aa`.`coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`, `active`, `shared`, `created_by`, `created_time`, `invest_lower_limit`, `product_types`, `coupon_type`, `user_group`, `deleted`, `coupon_source`)
-VALUES ('451', '500', '0', '0', '1', '2017-08-18 00:00:00', '2017-08-18 23:59:59', '15', '0', '999999', '0', '1', '0',
+VALUES ('451', '500', '0', '0', '1', '2017-08-18 00:00:00', '2017-09-01 23:59:59', '15', '0', '999999', '0', '1', '0',
                                                                                                              'sidneygao',
                                                                                                              '2017-07-18 00:00:00',
                                                                                                              '200000',
