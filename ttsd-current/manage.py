b132a2386c32899c3433325c60776b2a71ea557f
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-07-27 17:10:27 +0800

    revert manage

--- ttsd-current/manage.py
+++ ttsd-current/manage.py
@@ -13,27 +13,28 @@ if __name__ == "__main__":
         exit(1)
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-try:
-    from django.core.management import execute_from_command_line
-except ImportError:
-    # The above import may fail for some other reason. Ensure that the
-    # issue is really that Django is missing to avoid masking other
-    # exceptions on Python 2.
+
     try:
-        import django
+        from django.core.management import execute_from_command_line
     except ImportError:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        )
-    raise
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
 
-try:
-    import pymysql
+    try:
+        import pymysql
 
-    pymysql.install_as_MySQLdb()
-except ImportError:
-    pass
+        pymysql.install_as_MySQLdb()
+    except ImportError:
+        pass
 
-execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
