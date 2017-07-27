eebec3944e9a3f56b712839dd2188c610fbc82cb
zhanglong888 <zhanglong@tuotiansudai.com>
2017-07-27 20:28:39 +0800

    fix ut add test charset

--- ttsd-current/current_console/settings.py
+++ ttsd-current/current_console/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'CHARSET': 'UTF8',
+        },
+
     }
 }
 
