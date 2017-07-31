369ca731d3a9fe4c7fc85042868bc240619e9ff3
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-07-31 17:28:16 +0800

    add unit test for redeem get and update

--- ttsd-current/current_rest/views/redeem.py
+++ ttsd-current/current_rest/views/redeem.py
@@ -3,12 +3,12 @@
 from rest_framework import mixins
 from rest_framework import viewsets
 
-from current_rest import serializers
+from current_rest import serializers, models
 
 
 class RedeemViewSet(mixins.CreateModelMixin,
                     mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     viewsets.GenericViewSet):
-
     serializer_class = serializers.CurrentRedeemSerializer
+    queryset = models.CurrentRedeem.objects.all()
