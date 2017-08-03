8ccec1a7d01f5da7eac571a75a2d98436d125f2d
zhukun5764 <zhukun@tuotiansudai.com>
2017-08-03 13:37:35 +0800

    fix redeem review

--- ttsd-current/current_console/views/redeem.py
+++ ttsd-current/current_console/views/redeem.py
@@ -7,30 +7,35 @@ from rest_framework import status
 
 from current_console import constants
 from current_console.decorators import user_roles_check
+from current_console.forms import RedeemForm
 from current_console.rest_client import RestClient
 
 
 @require_http_methods(["GET"])
 @user_roles_check(['ADMIN', 'OPERATOR', 'OPERATOR_ADMIN'])
 def redeem_list(request):
-    redeem_list = RestClient('redeem-list').get(params=request.GET)
-    sumAmount = 0
-    if redeem_list['results']:
-        for redeem in redeem_list['results']:
-            sumAmount += redeem['amount']
-            redeem['amount'] = '{0:.2f}'.format(redeem['amount']/100.0)
-    return render(request, 'console/redeem/list.html',
-                  {'redeem_list': redeem_list,
-                   'types': constants.REDEEM_STATUS_CHOICE,
-                   'sumAmount': '{0:.2f}'.format(sumAmount/100.0),
-                   'page': request.GET.get('page')})
+    form = RedeemForm(request.GET)
+    if form.is_valid():
+        redeem_list = RestClient('redeem-list').get(params=request.GET)
+        sumAmount = 0
+        if redeem_list['results']:
+            for redeem in redeem_list['results']:
+                sumAmount += redeem['amount']
+                redeem['amount'] = '{0:.2f}'.format(redeem['amount']/100.0)
+        return render(request, 'console/redeem/list.html',
+                      {'redeem_list': redeem_list,
+                       'types': constants.REDEEM_STATUS_CHOICE,
+                       'sumAmount': '{0:.2f}'.format(sumAmount/100.0),
+                       'page': request.GET.get('page')})
+    else:
+        return render(request, 'console/redeem/list.html')
 
 
 @require_http_methods(["PUT"])
 @user_roles_check(['ADMIN', 'OPERATOR', 'OPERATOR_ADMIN'])
 def audit_redeem(request, result, pk):
     data = {
-        'auditor': 'auditor'
+        'auditor': request.session['login_name']
     }
 
     response = RestClient('redeem-audit/{}/{}'.format(pk, result)).put(data=data)
@@ -38,4 +43,4 @@ def audit_redeem(request, result, pk):
     if response:
         return JsonResponse({'message': response[1]}, status=status.HTTP_200_OK)
 
-    return JsonResponse({'message', '內部服务器错误'}, status=status.HTTP_200_OK)
+    return JsonResponse({'message', '內部服务器错误'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
