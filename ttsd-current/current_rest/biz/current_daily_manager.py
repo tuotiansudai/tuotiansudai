f2818adac9d8ac01bcd8da04b27e09e3e1e79fff
gengbj <gengbj@tuotiansudai.com>
2017-08-07 18:42:04 +0800

    refactor after review code

--- ttsd-current/current_rest/biz/current_daily_manager.py
+++ ttsd-current/current_rest/biz/current_daily_manager.py
@@ -15,7 +15,7 @@ class CurrentDailyManager(object):
     daily_amount_limit_key = 'current:daily:amount'
 
     def get_current_daily_amount(self):
-        daily_amount = int(redis_client.get(self.daily_amount_limit_key) is not None)
+        daily_amount = int(redis_client.get(self.daily_amount_limit_key) or 0)
         return daily_amount if daily_amount else 0
 
 
@@ -25,18 +25,18 @@ def calculate_success_deposit_today():
     amount_sum = models.CurrentDeposit.objects.filter(status=constants.DEPOSIT_SUCCESS,
                                                       updated_time__range=(today, tomorrow)) \
         .all().aggregate(Sum('amount')) \
-        .get('amount__sum', 0)
-    return amount_sum if amount_sum else 0
+        .get('amount__sum') or 0
+    return amount_sum
 
 
 def calculate_loan_remain_amount_today():
     loan_amount_sum = models.Loan.objects.filter(status=constants.LOAN_STATUS_APPROVED,
-                                                 effective_date__lte=datetime.now(),
-                                                 expiration_date__gte=datetime.now()).aggregate(
-        Sum('amount')).get('amount__sum', 0)
+                                                 effective_date__lte=datetime.now().date(),
+                                                 expiration_date__gte=datetime.now().date()).aggregate(
+        Sum('amount')).get('amount__sum') or 0
 
     account_balance_sum = models.CurrentAccount.objects.all().aggregate(
-        Sum('balance')).get('balance__sum', 0)
+        Sum('balance')).get('balance__sum') or 0
 
     return account_balance_sum, max(0, loan_amount_sum - account_balance_sum)
 
