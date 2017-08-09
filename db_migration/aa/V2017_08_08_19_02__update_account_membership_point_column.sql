0ec342d3fe6c2d172673bc6b89f75608ca2ebaac
gengbj <gengbj@tuotiansudai.com>
2017-08-09 10:19:05 +0800

    fix ut

--- db_migration/aa/V2017_08_08_19_02__update_account_membership_point_column.sql
+++ db_migration/aa/V2017_08_08_19_02__update_account_membership_point_column.sql
@@ -10,6 +10,6 @@ GROUP BY a.login_name) b
 WHERE sum_eb != mp) temp
 SET account.membership_point = temp.sum_eb
 WHERE account.login_name = temp.login_name
-                                                                                                 'INTEREST_COUPON',
-                                                                                                               '家装节奖励');
+
+
 COMMIT;
