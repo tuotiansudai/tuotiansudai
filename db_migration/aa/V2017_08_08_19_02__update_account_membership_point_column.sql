4fcf57af25b64a7ce2599613b344dee8f3259911
gengbj <gengbj@tuotiansudai.com>
2017-08-09 10:30:54 +0800

    fix migration

--- db_migration/aa/V2017_08_08_19_02__update_account_membership_point_column.sql
+++ db_migration/aa/V2017_08_08_19_02__update_account_membership_point_column.sql
@@ -9,7 +9,7 @@ ON a.login_name = meb.login_name
 GROUP BY a.login_name) b
 WHERE sum_eb != mp) temp
 SET account.membership_point = temp.sum_eb
-WHERE account.login_name = temp.login_name
+WHERE account.login_name = temp.login_name;
 
 
 COMMIT;
