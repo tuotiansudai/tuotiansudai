618a73f5dd9b9dcec2bb763eb5f15928b47f03ad
zhukun5764 <zhukun@tuotiansudai.com>
2017-09-26 10:00:17 +0800

    add scheduler component

--- ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/CreditLoanTransferAgentScheduler.java
+++ ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/CreditLoanTransferAgentScheduler.java
@@ -5,7 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CreditLoanTransferAgentScheduler {
     static Logger logger = LoggerFactory.getLogger(CreditLoanTransferAgentScheduler.class);
 
