225d86d627b4d44482377786aa132acb83c6060d
gengbj <gengbj@tuotiansudai.com>
2017-09-28 20:34:35 +0800

    fix ut

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/TransferApplicationDetailRequestDto.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/dto/v1_0/TransferApplicationDetailRequestDto.java
@@ -1,11 +1,13 @@
 package com.tuotiansudai.api.dto.v1_0;
 
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Pattern;
 
 public class TransferApplicationDetailRequestDto extends BaseParamDto {
 
+    @NotEmpty
     @Pattern(regexp = "^\\d+$")
     @ApiModelProperty(value = "债权转让ID", example = "1")
     private long transferApplicationId;
