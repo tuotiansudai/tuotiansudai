68b2c5f80398507a944e37ddfaa5bed2d67dd0a2
zhanglong888 <zhanglong@tuotiansudai.com>
2017-09-28 20:17:05 +0800

    huizu rent repay

--- ttsd-util/src/main/java/com/tuotiansudai/enums/UserBillBusinessType.java
+++ ttsd-util/src/main/java/com/tuotiansudai/enums/UserBillBusinessType.java
@@ -31,7 +31,7 @@ public enum UserBillBusinessType {
     INVEST_CASH_BACK("现金补贴"),
     NATIONAL_DAY_INVEST("国庆节逢万返百奖励"),
     MEMBERSHIP_PRIVILEGE_PURCHASE("增值特权购买"),
-    HUI_ZU_REPAY_IN("慧租还款"),
+    HUI_ZU_REPAY_IN("慧租租金还款"),
     CREDIT_LOAN_ACTIVATE_ACCOUNT("惠租一分钱激活账户"),
     CREDIT_LOAN_TRANSFER_AGENT("信用贷转账给代理人"),
     CREDIT_LOAN_RECHARGE("信用贷账户充值"),
