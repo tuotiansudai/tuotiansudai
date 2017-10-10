fdca1038eb6e510225537a2c4f85cb91cb4ba68b
zhukun5764 <zhukun@tuotiansudai.com>
2017-10-10 15:24:56 +0800

    update coupon

--- db_migration/aa/V2017_09_28_17_00__insert_into_coupon_iphonex_activity_prize.sql
+++ db_migration/aa/V2017_09_28_17_00__insert_into_coupon_iphonex_activity_prize.sql
@@ -1,6 +1,6 @@
 BEGIN;
 INSERT INTO `aa`.`coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`, `active`, `shared`, `created_by`, `created_time`, `invest_lower_limit`, `product_types`, `coupon_type`, `user_group`, `deleted`, `coupon_source`)
-VALUES ('464', '18', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
+VALUES ('464', '1800', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
                                                                                                              'sidneygao',
                                                                                                              '2017-09-28 00:00:00',
                                                                                                              '1200000',
@@ -10,7 +10,7 @@ VALUES ('464', '18', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00'
                                                                                                              '0',
                                                                                                              'iphoneX活动奖励');
 INSERT INTO `aa`.`coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`, `active`, `shared`, `created_by`, `created_time`, `invest_lower_limit`, `product_types`, `coupon_type`, `user_group`, `deleted`, `coupon_source`)
-VALUES ('465', '188', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
+VALUES ('465', '18800', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
                                                                                                               'sidneygao',
                                                                                                               '2017-09-28 00:00:00',
                                                                                                               '8000000',
@@ -20,7 +20,7 @@ VALUES ('465', '188', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00
                                                                                                               '0',
                                                                                                               'iphoneX活动奖励');
 INSERT INTO `aa`.`coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`, `active`, `shared`, `created_by`, `created_time`, `invest_lower_limit`, `product_types`, `coupon_type`, `user_group`, `deleted`, `coupon_source`)
-VALUES ('466', '288', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
+VALUES ('466', '28800', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
                                                                                                               'sidneygao',
                                                                                                               '2017-09-28 00:00:00',
                                                                                                               '10000000',
@@ -30,7 +30,7 @@ VALUES ('466', '288', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00
                                                                                                               '0',
                                                                                                               'iphoneX活动奖励');
 INSERT INTO `aa`.`coupon` (`id`, `amount`, `rate`, `birthday_benefit`, `multiple`, `start_time`, `end_time`, `deadline`, `used_count`, `total_count`, `issued_count`, `active`, `shared`, `created_by`, `created_time`, `invest_lower_limit`, `product_types`, `coupon_type`, `user_group`, `deleted`, `coupon_source`)
-VALUES ('467', '588', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
+VALUES ('467', '58800', '0', '0', '1', '2017-10-16 00:00:00', '2017-11-05 00:00:00', '15', '0', '999999', '0', '1', '0',
                                                                                                               'sidneygao',
                                                                                                               '2017-09-28 00:00:00',
                                                                                                               '20000000',
