649df1f05b7113c1d1dcd3a2c9fd5ed568ab32d4
zhukun5764 <zhukun@tuotiansudai.com>
2017-10-11 16:34:08 +0800

    fix scheduler

--- ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/IphoneXActivitySendCashScheduler.java
+++ ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/IphoneXActivitySendCashScheduler.java
@@ -18,10 +18,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
 import java.util.*;
 
+@Component
 public class IphoneXActivitySendCashScheduler {
 
     private static Logger logger = LoggerFactory.getLogger(IphoneXActivitySendCashScheduler.class);
