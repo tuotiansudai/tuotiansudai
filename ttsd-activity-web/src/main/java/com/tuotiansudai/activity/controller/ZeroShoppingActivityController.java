37744f63bb1d68f980350ece5116d68af023ca0a
zhukun5764 <zhukun@tuotiansudai.com>
2017-10-16 18:51:01 +0800

    fix bug

--- ttsd-activity-web/src/main/java/com/tuotiansudai/activity/controller/ZeroShoppingActivityController.java
+++ ttsd-activity-web/src/main/java/com/tuotiansudai/activity/controller/ZeroShoppingActivityController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,8 +44,10 @@ public class ZeroShoppingActivityController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "/activity-loan-detail/{loanId:^\\d+$}", method = {RequestMethod.GET})
-    public ModelAndView redirectLoanDetail(@PathVariable long loanId, ZeroShoppingPrize zeroShoppingPrize, RedirectAttributes redirectAttributes){
+    @RequestMapping(value = "/activity-loan-detail", method = {RequestMethod.GET})
+    public ModelAndView redirectLoanDetail(@RequestParam(value = "loanId", required = false) long loanId,
+                                           @RequestParam(value = "zeroShoppingPrize", required = false) ZeroShoppingPrize zeroShoppingPrize,
+                                           RedirectAttributes redirectAttributes){
         redirectAttributes.addFlashAttribute("zeroShoppingPrize", zeroShoppingPrize);
         return new ModelAndView("redirect:/loan/" + loanId);
     }
