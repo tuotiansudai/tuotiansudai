2f44aaf26e3be3990bfb111ae182b676aeb0c462
zhanglong888 <zhanglong@tuotiansudai.com>
2017-10-16 15:01:48 +0800

    app url

--- ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/service/impl/WithdrawServiceImpl.java
+++ ttsd-pay-wrapper/src/main/java/com/tuotiansudai/paywrapper/service/impl/WithdrawServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tuotiansudai.paywrapper.service.impl;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.tuotiansudai.client.MQWrapperClient;
 import com.tuotiansudai.dto.BaseDto;
 import com.tuotiansudai.dto.PayFormDataDto;
