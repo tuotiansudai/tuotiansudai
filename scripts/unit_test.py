966620eaa2dd86e7c1c65c7331740353c8d2e228
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-10-18 19:40:40 +0800

    fix test scope class reference

--- scripts/unit_test.py
+++ scripts/unit_test.py
@@ -1,9 +1,8 @@
-from paver.shell import sh
-import config_deploy
 import os
+from paver.shell import sh
+
 
 class UTRunner(object):
-
     _config_path = os.getenv('TTSD_CONFIG_PATH', '/workspace/deploy-config')
 
     def __init__(self, db_host, db_port, redis_host, redis_port):
@@ -13,14 +12,9 @@ class UTRunner(object):
         self.redis_port = redis_port
 
     def test(self):
-        self.config_file()
         self.init_docker()
         self.run_test()
         self.clean_env()
-
-    def config_file(self):
-        print "Generate config file..."
-        config_deploy.deploy({'env':'UT'}, "./ttsd-config/src/main/resources/", "{0}/ttsd-config/ttsd-env.properties".format(self._config_path))
 
     def init_docker(self):
         print "Initialing docker..."
@@ -36,10 +30,11 @@ class UTRunner(object):
 
     def run_test(self):
         print "Starting test..."
-        sh('/opt/gradle/latest/bin/gradle -Pdbhost={0} -Pdbport={1} -Predishost={2} -Predisport={3} clean compileJava ttsd-config:flywayAA ttsd-config:flywayUMP ttsd-config:flywayAnxin ttsd-config:flywaySms ttsd-config:flywayWorker ttsd-config:flywayAsk ttsd-config:flywayActivity ttsd-config:flywayPoint ttsd-config:flywayMessage ttsd-config:flywayLog test'.format(
+        sh(
+            '/opt/gradle/latest/bin/gradle -Pdbhost={0} -Pdbport={1} -Predishost={2} -Predisport={3} clean compileJava ttsd-config:flywayAA ttsd-config:flywayUMP ttsd-config:flywayAnxin ttsd-config:flywaySms ttsd-config:flywayWorker ttsd-config:flywayAsk ttsd-config:flywayActivity ttsd-config:flywayPoint ttsd-config:flywayMessage ttsd-config:flywayLog test'.format(
                 self.db_host, self.db_port, self.redis_host, self.redis_port))
-        sh('docker run -v `pwd`/signin_service:/app --rm --net=bridge --link test-db-server --link test-redis-server leoshi/ttsd-signin-flask python test.py')
+        sh(
+            'docker run -v `pwd`/signin_service:/app --rm --net=bridge --link test-db-server --link test-redis-server leoshi/ttsd-signin-flask python test.py')
 
     def clean_env(self):
         self._remove_old_container()
-
--- ttsd-client/src/main/java/com/tuotiansudai/client/MQWrapperClientConf.java
+++ ttsd-client/src/main/java/com/tuotiansudai/client/MQWrapperClientConf.java
@@ -1,22 +1,21 @@
 package com.tuotiansudai.client;
 
-import com.tuotiansudai.dto.Environment;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@Component
+@Configuration
 public class MQWrapperClientConf {
 
-    @Value("${common.environment}")
-    private Environment env;
-
     @Bean
+    @Profile("!test")
     public MQWrapperClient mqWrapperClient() {
-        return isFormalEnv() ? new MQWrapperClientFormal() : new MQWrapperClientUT();
+        return new MQWrapperClientFormal();
     }
 
-    private boolean isFormalEnv() {
-        return env != Environment.UT && env != Environment.DEV;
+    @Bean
+    @Profile("test")
+    public MQWrapperClient mqWrapperClientTest() {
+        return new MQWrapperClientUT();
     }
 }
