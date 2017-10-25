3acc48406b9e35cdf4a146c1ffdc24e98c64f0b6
zhoubx <zhoubaoxin@tuotiansudai.com>
2017-10-25 15:03:29 +0800

    change job running time for test

--- ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/CheckUserBalanceScheduler.java
+++ ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/CheckUserBalanceScheduler.java
@@ -56,7 +56,7 @@ public class CheckUserBalanceScheduler {
     private static final int RETRY_TIMES = 6;
 
     //    @Scheduled(cron = "0 30 1 * * SUN,SAT", zone = "Asia/Shanghai")
-    @Scheduled(cron = "0 52 14 * * *", zone = "Asia/Shanghai")
+    @Scheduled(cron = "0 18 15 * * *", zone = "Asia/Shanghai")
     public void checkUserBalance() {
         logger.info("[checkUserBalance:] start .");
 //        if (Environment.PRODUCTION != environment) {
