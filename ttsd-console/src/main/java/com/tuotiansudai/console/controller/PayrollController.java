3cde8981a22b83189f27bafa314f4e279ed8f387
zhanglong888 <zhanglong@tuotiansudai.com>
2017-10-27 10:36:45 +0800

    audit process

---
+++ ttsd-console/src/main/java/com/tuotiansudai/console/controller/PayrollController.java
@@ -0,0 +1,31 @@
+package com.tuotiansudai.console.controller;
+
+import com.tuotiansudai.console.service.ConsolePayrollService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/finance-manage/payroll-manage")
+public class PayrollController {
+    private static Logger logger = Logger.getLogger(PayrollController.class);
+
+    @Autowired
+    private ConsolePayrollService consolePayrollService;
+
+    @RequestMapping(value = "/primary-audit/{payRollId:^\\d+$}", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView primaryAudit(@PathVariable long payRollId) {
+
+
+
+        return null;
+    }
+
+
+}
---
+++ ttsd-console/src/main/java/com/tuotiansudai/console/service/ConsolePayrollService.java
@@ -0,0 +1,42 @@
+package com.tuotiansudai.console.service;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.tuotiansudai.repository.mapper.PayrollMapper;
+import com.tuotiansudai.repository.model.PayrollModel;
+import com.tuotiansudai.repository.model.PayrollStatusType;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ConsolePayrollService {
+
+    private static Logger logger = Logger.getLogger(ConsolePayrollService.class);
+
+    @Autowired
+    private PayrollMapper payrollMapper;
+
+    @Transactional
+    public void primaryAudit(long payRollId) {
+        PayrollModel payrollModel = payrollMapper.findById(payRollId);
+        if (payrollModel == null
+                || Sets.newHashSet(PayrollStatusType.PENDING, PayrollStatusType.REJECTED).contains(payrollModel.getStatus())) {
+            logger.debug("payRollId not exist or status no pending rejected ");
+            return;
+        }
+
+
+    }
+
+    public void advancedAudit(long payRollId) {
+
+    }
+
+    public boolean checkSufficientBalance(long payRollId) {
+        return true;
+    }
+
+
+}
