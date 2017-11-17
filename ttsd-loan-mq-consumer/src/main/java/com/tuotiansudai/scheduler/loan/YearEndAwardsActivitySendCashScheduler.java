866aaa111aedad4659766b7c2fc5e54859310e7a
zhukun5764 <zhukun@tuotiansudai.com>
2017-11-17 16:31:53 +0800

    update job

--- ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/YearEndAwardsActivitySendCashScheduler.java
+++ ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/YearEndAwardsActivitySendCashScheduler.java
@@ -54,7 +54,7 @@ public class YearEndAwardsActivitySendCashScheduler {
             new AnnualizedAmount(3000000000l, Long.MAX_VALUE, 0.01));
 
 //    @Scheduled(cron = "0 0 10 1 1 ?", zone = "Asia/Shanghai")
-    @Scheduled(cron = "0 0 16 17 11 ?", zone = "Asia/Shanghai")
+    @Scheduled(cron = "0 0 17 17 11 ?", zone = "Asia/Shanghai")
     public void yearEndAwardsSendCash(){
         logger.info("[year end awards activity] send cash begin");
 
