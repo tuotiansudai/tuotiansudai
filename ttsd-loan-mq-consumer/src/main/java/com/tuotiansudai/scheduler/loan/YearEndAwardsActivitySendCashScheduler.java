d3adddf6af2b23e7d48f14a951493f708cdf90b1
zhukun5764 <zhukun@tuotiansudai.com>
2017-11-22 19:14:46 +0800

    text job

--- ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/YearEndAwardsActivitySendCashScheduler.java
+++ ttsd-loan-mq-consumer/src/main/java/com/tuotiansudai/scheduler/loan/YearEndAwardsActivitySendCashScheduler.java
@@ -53,14 +53,15 @@ public class YearEndAwardsActivitySendCashScheduler {
             new AnnualizedAmount(1500000000l, 2000000000l, 0.008),
             new AnnualizedAmount(2000000000l, Long.MAX_VALUE, 0.01));
 
-    @Scheduled(cron = "0 0 10 1 1 ?", zone = "Asia/Shanghai")
+//    @Scheduled(cron = "0 0 10 1 1 ?", zone = "Asia/Shanghai")
+    @Scheduled(cron = "0 25 19 22 11 ?", zone = "Asia/Shanghai")
     public void yearEndAwardsSendCash(){
         logger.info("[year end awards activity] send cash begin");
 
-        if (DateTime.now().getYear() != 2018){
-            logger.info("[year end awards activity] send cash is over");
-            return;
-        }
+//        if (DateTime.now().getYear() != 2018){
+//            logger.info("[year end awards activity] send cash is over");
+//            return;
+//        }
 
         List<InvestProductTypeView> list = investMapper.findAmountOrderByNameAndProductType(activityStartTime, activityEndTime, "岁末专享");
         Map<String, Long> amountMaps = list.stream().collect(Collectors.toMap(k -> k.getLoginName(), v -> v.getSumAmount() * v.getProductType().getDuration() / 360, (v, newV) -> v + newV));
