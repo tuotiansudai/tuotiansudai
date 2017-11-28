3ba0ad17eb404c967386c611f65d8af0663b8c8f
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-11-28 16:14:52 +0800

    remove etcd_config.get_int()

--- ttsd-user-rest-service/etcd_config.py
+++ ttsd-user-rest-service/etcd_config.py
@@ -1,33 +1,35 @@
+import os
 import random
 
 import etcd3
 import yaml
 
+ETCD_ENDPOINT_CONFIG_FILE = '/app/etcd-endpoints.yml'
+DEFAULT_ETCD_HOST = '127.0.0.1'
+DEFAULT_ETCD_PORT = 2379
+
 
 class EtcdConfig(object):
     def __init__(self, env):
-        with open('/app/etcd-endpoints.yml', 'r') as stream:
-            yaml_data = yaml.load(stream)
-
         self.env = (env or 'dev').lower()
-        endpoints = yaml_data.get(self.env)
-        hosts = endpoints.get('host')
-        ports = endpoints.get('port')
-        host = hosts[random.randint(0, len(hosts) - 1)]
-        port = ports[random.randint(0, len(ports) - 1)]
+
+        host = DEFAULT_ETCD_HOST
+        port = DEFAULT_ETCD_PORT
+        if os.path.exists(ETCD_ENDPOINT_CONFIG_FILE):
+            with open(ETCD_ENDPOINT_CONFIG_FILE, 'r') as stream:
+                yaml_data = yaml.load(stream)
+
+            endpoints = yaml_data.get(self.env)
+            hosts = endpoints.get('host')
+            ports = endpoints.get('port')
+            host = hosts[random.randint(0, len(hosts) - 1)]
+            port = ports[random.randint(0, len(ports) - 1)]
         self.client = etcd3.client(host=host, port=port)
 
     def get(self, key, default_value=None):
         value, _ = self.client.get('/{}/{}'.format(self.env, key))
-        return value if value else default_value
-
-    def get_int(self, key, default_value=None):
-        str_value = self.get(key)
-        if str_value is None:
+        if value is None:
             return default_value
-        else:
-            return int(str_value)
-
-    def get_bool(self, key):
-        str_value = self.get(key)
-        return str_value and 'true' == str_value.lower()
+        if default_value is not None and type(default_value) == int:
+            return int(value)
+        return value
--- ttsd-user-rest-service/settings.py
+++ ttsd-user-rest-service/settings.py
@@ -2,7 +2,6 @@ import os
 
 from etcd_config import EtcdConfig
 
-ETCD_ENDPOINT = os.getenv('TTSD_ETCD_ENDPOINT', 'http://127.0.0.1:2379')
 ENV = os.getenv('TTSD_ETCD_ENV', 'dev')
 
 DEBUG = True
@@ -10,24 +9,24 @@ DEBUG = True
 config = EtcdConfig(ENV)
 
 REDIS_HOST = config.get('common.redis.host', default_value='192.168.33.10')
-REDIS_PORT = config.get_int('common.redis.port', default_value=6379)
-REDIS_DB = config.get_int('common.token.db', default_value=0)
+REDIS_PORT = config.get('common.redis.port', default_value=6379)
+REDIS_DB = config.get('common.token.db', default_value=0)
 
-MOBILE_TOKEN_EXPIRED_SECONDS = config.get_int('mobile.app.token.expire.seconds', default_value=2592000)  # 30 days
-WEB_TOKEN_EXPIRED_SECONDS = config.get_int('web.login.lock.seconds', default_value=1800)  # 30 minutes
-LOGIN_FAILED_MAXIMAL_TIMES = config.get_int('mobile.login.max.failed.times', default_value=3)
-LOGIN_FAILED_EXPIRED_SECONDS = config.get_int('mobile.login.lock.seconds', default_value=1800)  # 30 minutes
+MOBILE_TOKEN_EXPIRED_SECONDS = config.get('mobile.app.token.expire.seconds', default_value=2592000)  # 30 days
+WEB_TOKEN_EXPIRED_SECONDS = config.get('web.login.lock.seconds', default_value=1800)  # 30 minutes
+LOGIN_FAILED_MAXIMAL_TIMES = config.get('mobile.login.max.failed.times', default_value=3)
+LOGIN_FAILED_EXPIRED_SECONDS = config.get('mobile.login.lock.seconds', default_value=1800)  # 30 minutes
 
-ALIYUN_MNS_ENABLED = config.get_bool('aliyun.mns.enabled')
+ALIYUN_MNS_ENABLED = config.get('aliyun.mns.enabled', 'false').lower() == 'true'
 ALIYUN_MNS_ACCESS_KEY_ID = config.get('aliyun.mns.accessKeyId', default_value='mns')
 ALIYUN_MNS_ACCESS_KEY_SECRET = config.get('aliyun.mns.accessKeySecret', default_value='mns')
 ALIYUN_MNS_END_POINT = config.get('aliyun.mns.endpoint', default_value='https://mns')
 ALIYUN_MNS_QUEUE_NAME = "LoginLog"
 
-MQ_REDIS_DB = config.get_int('common.redis.db', default_value=2)
+MQ_REDIS_DB = config.get('common.redis.db', default_value=2)
 
 MYSQL_HOST = config.get('common.jdbc.host', default_value='192.168.33.10')
-MYSQL_PORT = config.get_int('common.jdbc.port', default_value=3306)
+MYSQL_PORT = config.get('common.jdbc.port', default_value=3306)
 MYSQL_USER = config.get('common.jdbc.username', default_value='tuotiansd')
 MYSQL_PASS = config.get('common.jdbc.password', default_value='tuotiansd')
 
