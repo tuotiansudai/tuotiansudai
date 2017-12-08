200807f1c62f8ac8b9de517395db0e6ef99b9185
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2017-12-08 19:41:20 +0800

    login and the red ware light

--- ttsd-frontend-manage/resources/static/activity/js/cash_snowball_2017.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/cash_snowball_2017.jsx
@@ -1,4 +1,5 @@
 require("activityStyle/cash_snowball_2017.scss");
+require("activityStyle/media.scss");
 let commonFun= require('publicJs/commonFun');
 require('publicJs/login_tip');
 let sourceKind = globalFun.parseURL(location.href);
@@ -22,189 +23,188 @@ let wareIndex = Math.floor(myWare/100);
 let moneyTipWidth = $money_tip.innerWidth(),
     mobileMoney_tip_width = $mobileMoney_tip.innerWidth();
 
-//返现红包金额提示
-$money_tip.find('em').text(myWare);
-$mobileMoney_tip.find('em').text(myWare);
+$.when(commonFun.isUserLogin())
+    .done(function () {
+        //返现红包金额提示
+        $money_tip.find('em').text(myWare);
+        $mobileMoney_tip.find('em').text(myWare);
 
 //返现红包提示
-if(wareIndex >0 && wareIndex < 3){
-    $money_tip.css({
-        'left':-12+28*(wareIndex-1) + 174*(wareIndex-1) +28 -moneyTipWidth/2,
-    })
-    if(wareIndex%2 == 0){
-        $money_tip.css({
-            'top':-40
-        })
-    }else {
-        $money_tip.css({
-            'top':54
-        })
-    }
-}else {
-    $money_tip.css({
-        'left':370,
-        'top':54
-    })
-}
+        if(wareIndex >0 && wareIndex < 3){
+            $money_tip.css({
+                'left':-12+28*(wareIndex-1) + 174*(wareIndex-1) +28 -moneyTipWidth/2,
+            })
+            if(wareIndex%2 == 0){
+                $money_tip.css({
+                    'top':-40
+                })
+            }else {
+                $money_tip.css({
+                    'top':54
+                })
+            }
+        }else {
+            $money_tip.css({
+                'left':370,
+                'top':54
+            })
+        }
 
 
 
 //根据获取的红包大小一次点亮红包
-let wareArr = [];
-if(myWare < 400 && myWare > 0){
-    wareArr = [100,200,300,400,500]
-    $ware_DOM.find('.red-ware').each((index,item)=>{
-        if(index < wareIndex){
-            $(item).css({
-                'opacity':1
+        let wareArr = [];
+        if(myWare < 400 && myWare > 0){
+            wareArr = [100,200,300,400,500]
+            $ware_DOM.find('.red-ware').each((index,item)=>{
+                if(index < wareIndex){
+                    $(item).css({
+                        'opacity':1
+                    })
+
+                }
+
+            })
+            //圆点点亮
+            $progress.find('.circle').each((index,item)=>{
+
+                if(index < wareIndex){
+                    $(item).css({
+                        'borderColor':'#ffd41d'
+                    })
+                }
+            })
+            //线条点亮
+            $progress.find('.thread').each((index,item)=>{
+                if(index < wareIndex - 1){
+                    $(item).css({
+                        'background':'#ffd41d'
+                    })
+                }
             })
 
-        }
+        }else if(myWare >= 400) {
+            wareArr = [myWare-200,myWare-100,myWare,myWare+100,myWare+200];
+            $ware_DOM.find('.red-ware').each((index,item)=>{
+                if(index <= 2 ){
+                    $(item).css({
+                        'opacity':1
+                    })
+                }
 
-    })
-    //圆点点亮
-    $progress.find('.circle').each((index,item)=>{
+            })
+            $progress.find('.circle').each((index,item)=>{
 
-        if(index < wareIndex){
-            $(item).css({
-                'borderColor':'#ffd41d'
+                if(index < 3){
+                    $(item).css({
+                        'borderColor':'#ffd41d'
+                    })
+                }
+            })
+            $progress.find('.thread').each((index,item)=>{
+                if(index < 2){
+                    $(item).css({
+                        'background':'#ffd41d'
+                    })
+                }
             })
         }
-    })
-    //线条点亮
-    $progress.find('.thread').each((index,item)=>{
-        if(index < wareIndex - 1){
-            $(item).css({
-                'background':'#ffd41d'
-            })
-        }
-    })
-
-}else if(myWare >= 400) {
-    wareArr = [myWare-200,myWare-100,myWare,myWare+100,myWare+200];
-    $ware_DOM.find('.red-ware').each((index,item)=>{
-        if(index <= 2 ){
-            $(item).css({
-                'opacity':1
-            })
-        }
-
-    })
-    $progress.find('.circle').each((index,item)=>{
-
-        if(index < 3){
-            $(item).css({
-                'borderColor':'#ffd41d'
-            })
-        }
-    })
-    $progress.find('.thread').each((index,item)=>{
-        if(index < 2){
-            $(item).css({
-                'background':'#ffd41d'
-            })
-        }
-    })
-}
 //5个红包的值
-wareArr.forEach((item,index)=>{
-    if(item >= 1000){
-        //红包大于等于1000调整样式
-        $ware_DOM.find('.red-ware .money').eq(index).css({
-            fontSize:'20px'
+        wareArr.forEach((item,index)=>{
+            if(item >= 1000){
+                //红包大于等于1000调整样式
+                $ware_DOM.find('.red-ware .money').eq(index).css({
+                    fontSize:'20px'
+                })
+                $ware_DOM.find('.red-ware em').eq(index).css({
+                    left:'4px'
+                })
+            }
+            $ware_DOM.find('.red-ware .money').eq(index).text(item);
         })
-        $ware_DOM.find('.red-ware em').eq(index).css({
-            left:'4px'
-        })
-    }
-    $ware_DOM.find('.red-ware .money').eq(index).text(item);
-})
-//点击去投资按钮跳转到投资列表
-$('.to-invest').on('click',function () {
-    window.location.href = '/loan-list';
-})
-$('.to-login').on('click',function () {
-    toLogin();
-})
+
 //wap端点亮红包
-let mobileWareArr = [];
-if(wareIndex > 0 && wareIndex < 2) {
-    mobileWareArr = [100,200,300];
-    $mobileWareDOM.find('.red-ware').each((index,item)=>{
-        if(index < 1){
-            $(item).css({
-                'opacity':1
-            })
+        let mobileWareArr = [];
+        if(wareIndex > 0 && wareIndex < 2) {
+            mobileWareArr = [100,200,300];
+            $mobileWareDOM.find('.red-ware').each((index,item)=>{
+                if(index < 1){
+                    $(item).css({
+                        'opacity':1
+                    })
 
-        }
+                }
 
-    })
-    //wap红包提示位置
-    $mobileMoney_tip.css({
-        left:'-0.35rem'
-    })
-    //点亮圆点和轴线
-    $mobileProgress.find('.circle').each((index,item)=>{
+            })
+            //wap红包提示位置
+            $mobileMoney_tip.css({
+                left:'-0.35rem'
+            })
+            //点亮圆点和轴线
+            $mobileProgress.find('.circle').each((index,item)=>{
 
-        if(index < wareIndex){
-            $(item).css({
-                'borderColor':'#ffd41d'
+                if(index < wareIndex){
+                    $(item).css({
+                        'borderColor':'#ffd41d'
+                    })
+                }
+            })
+            $mobileProgress.find('.thread').each((index,item)=>{
+                if(index < wareIndex - 1){
+                    $(item).css({
+                        'background':'#ffd41d'
+                    })
+                }
+            })
+        }else if(wareIndex >= 2) {
+            mobileWareArr = [myWare-100,myWare,myWare+100];
+            $mobileWareDOM.find('.red-ware').each((index,item)=>{
+                if(index < 2){
+                    $(item).css({
+                        'opacity':1
+                    })
+
+                }
+
+            })
+            //wap红包提示位置
+            $mobileMoney_tip.css({
+                left:'2.5rem'
+            })
+            //点亮圆点和轴线
+            $mobileProgress.find('.circle').each((index,item)=>{
+
+                if(index < 2){
+                    $(item).css({
+                        'borderColor':'#ffd41d'
+                    })
+                }
+            })
+            $mobileProgress.find('.thread').each((index,item)=>{
+                if(index < 1){
+                    $(item).css({
+                        'background':'#ffd41d'
+                    })
+                }
             })
         }
-    })
-    $mobileProgress.find('.thread').each((index,item)=>{
-        if(index < wareIndex - 1){
-            $(item).css({
-                'background':'#ffd41d'
-            })
-        }
-    })
-}else if(wareIndex >= 2) {
-    mobileWareArr = [myWare-100,myWare,myWare+100];
-    $mobileWareDOM.find('.red-ware').each((index,item)=>{
-        if(index < 2){
-            $(item).css({
-                'opacity':1
-            })
-
-        }
-
-    })
-    //wap红包提示位置
-    $mobileMoney_tip.css({
-        left:'2.5rem'
-    })
-    //点亮圆点和轴线
-    $mobileProgress.find('.circle').each((index,item)=>{
-
-        if(index < 2){
-            $(item).css({
-                'borderColor':'#ffd41d'
-            })
-        }
-    })
-    $mobileProgress.find('.thread').each((index,item)=>{
-        if(index < 1){
-            $(item).css({
-                'background':'#ffd41d'
-            })
-        }
-    })
-}
 
 //3个红包的值
-mobileWareArr.forEach((item,index)=>{
-    if(item >= 1000){
-        //红包大于等于1000调整样式
-        $mobileWareDOM.find('.red-ware .money').eq(index).css({
-            fontSize:'20px'
+        mobileWareArr.forEach((item,index)=>{
+            if(item >= 1000){
+                //红包大于等于1000调整样式
+                $mobileWareDOM.find('.red-ware .money').eq(index).css({
+                    fontSize:'20px'
+                })
+                $mobileWareDOM.find('.red-ware em').eq(index).css({
+                    left:'0.12rem'
+                })
+            }
+            $mobileWareDOM.find('.red-ware .money').eq(index).text(item);
         })
-        $mobileWareDOM.find('.red-ware em').eq(index).css({
-            left:'0.12rem'
-        })
-    }
-    $mobileWareDOM.find('.red-ware .money').eq(index).text(item);
-})
+    })
+
+
 //未登录
 function toLogin(){
     if (sourceKind.params.source == 'app') {
@@ -222,6 +222,14 @@ function toLogin(){
 }
 //中奖记录滚动
 commonFun.scrollList($record_list,5,1500)
+
+//点击去投资按钮跳转到投资列表
+$('.to-invest').on('click',function () {
+    window.location.href = '/loan-list';
+})
+$('.to-login').on('click',function () {
+    toLogin();
+})
 
 
 
