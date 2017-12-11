91d1fbbb3fb1e8ddba80917ab6a5c7ac74eab0ec
zhukun5764 <zhukun@tuotiansudai.com>
2017-12-11 10:44:45 +0800

    user activity amount and cash

--- ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/CashSnowballService.java
+++ ttsd-activity-service/src/main/java/com/tuotiansudai/activity/service/CashSnowballService.java
@@ -1,12 +1,14 @@
 package com.tuotiansudai.activity.service;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.tuotiansudai.activity.repository.mapper.CashSnowballActivityMapper;
 import com.tuotiansudai.activity.repository.model.CashSnowballActivityModel;
+import com.tuotiansudai.util.AmountConverter;
 import com.tuotiansudai.util.MobileEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,9 +24,16 @@ public class CashSnowballService {
         return cashSnowballActivityModelList.size() > 20 ? cashSnowballActivityModelList.subList(0, 20) : cashSnowballActivityModelList;
     }
 
-//    public Map<String, String> userInvestAmount(String loginName) {
-//        Map<String, String> map = new HashMap<>();
-//        CashSnowballActivityModel cashSnowballActivityModel = loginName == null ? null : cashSnowballActivityMapper.findByLoginName(loginName);
-//        long annualizedAmount = cashSnowballActivityModel == null ? 0l : cashSnowballActivityModel.getAnnualizedAmount();
-//    }
+    public Map<String, String> userInvestAmount(String loginName) {
+        CashSnowballActivityModel cashSnowballActivityModel = loginName == null ? null : cashSnowballActivityMapper.findByLoginName(loginName);
+        long annualizedAmount = cashSnowballActivityModel == null ? 0l : cashSnowballActivityModel.getAnnualizedAmount();
+        long cashAmount = cashSnowballActivityModel == null ? 0l : cashSnowballActivityModel.getCashAmount();
+        long nextAmount = ((annualizedAmount / 1000000l + 1) * 1000000l) - annualizedAmount;
+
+        return Maps.newHashMap(ImmutableMap.<String, String>builder()
+                .put("annualizedAmount", AmountConverter.convertCentToString(annualizedAmount))
+                .put("cashAmount", AmountConverter.convertCentToString(cashAmount))
+                .put("nextAmount", AmountConverter.convertCentToString(nextAmount))
+                .build());
+    }
 }
--- ttsd-activity-web/src/main/java/com/tuotiansudai/activity/controller/CashSnowballController.java
+++ ttsd-activity-web/src/main/java/com/tuotiansudai/activity/controller/CashSnowballController.java
@@ -1,19 +1,15 @@
 package com.tuotiansudai.activity.controller;
 
 
-import com.google.common.collect.Iterators;
-import com.tuotiansudai.activity.repository.model.CashSnowballActivityModel;
 import com.tuotiansudai.activity.service.CashSnowballService;
 import com.tuotiansudai.spring.LoginUserInfo;
-import com.tuotiansudai.util.MobileEncryptor;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/activity/cash-snowball")
@@ -25,11 +21,12 @@ public class CashSnowballController {
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView cashSnowBall() {
         ModelAndView modelAndView = new ModelAndView("/activities/2017/cash-snowball", "responsive", true);
-        String loginName = LoginUserInfo.getLoginName();
         modelAndView.addObject("record", cashSnowballService.findAll());
-//        modelAndView.addObject("annualizedAmount", cashSnowballActivity == null ? 0l : cashSnowballActivity.getAnnualizedAmount());
-//        modelAndView.addObject("cashAmount", cashSnowballActivity == null ? 0l : cashSnowballActivity.getCashAmount());
-//        modelAndView.addObject("")
+
+        Map<String, String> maps = cashSnowballService.userInvestAmount(LoginUserInfo.getLoginName());
+        modelAndView.addObject("annualizedAmount", maps.get("annualizedAmount"));
+        modelAndView.addObject("cashAmount", maps.get("cashAmount"));
+        modelAndView.addObject("nextAmount", maps.get("nextAmount"));
         return modelAndView;
     }
 }
