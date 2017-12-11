ba322350e013ec20476b0d156191390e8d736d03
Xiduan Gao <gaoxiduan@gmail.com>
2017-12-11 16:02:20 +0800

    fixt ft

--- scripts/config_deploy.py
+++ scripts/config_deploy.py
@@ -7,7 +7,7 @@ def deploy(etcd, env, pay_fake):
     deploy_prop = [load_properties("./ttsd-config/src/main/resources/ttsd-env.properties"),
                    load_properties("./ttsd-config/src/main/resources/ttsd-biz.properties")]
 
-    if re.match('^qa\d$', env, re.I):
+    if re.match('^qa\d$', env, re.I) or env.lower() == 'ft':
         # 读取QA环境公用配置 ttsd-env-QA-common.properties 生成一个 dict 对象 qa_common_prop
         deploy_prop.append(load_properties("./ttsd-config/src/main/resources/envs/QA-common.properties"))
         # 读取每个环境特有的配置 envs/${env}.properties, 生成一个 dict 对象 specified_prop
@@ -19,18 +19,9 @@ def deploy(etcd, env, pay_fake):
         flush_etcd(etcd, deploy_prop, pay_fake)
         return
 
-    if env.lower() == 'ft':
-        # 读取FT环境配置 FT.properties 生成一个 dict 对象
-        deploy_prop.append(load_properties("./ttsd-config/src/main/resources/envs/FT.properties"))
-        # 从redis里读取 三方账号配置
-        _r = redis.StrictRedis(host=('192.168.1.30' if env != 'CI1' else '127.0.0.1'), port=6379, db=2)
-        deploy_prop.append(_r.hgetall('qa_common_account'))
-
-        flush_etcd(etcd, deploy_prop, pay_fake)
-        return
-
     # 读取每个环境特有的配置 envs/${env}.properties, 生成一个 dict 对象 specified_prop
     deploy_prop.append(load_properties("./ttsd-config/src/main/resources/envs/{0}.properties".format(env)))
+    flush_etcd(etcd, deploy_prop, pay_fake)
 
 
 def flush_etcd(etcd, deploy_prop, pay_fake):
