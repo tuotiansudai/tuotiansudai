144e066b39cb961e56abef52a3e4df8fa4ef17b0
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2017-12-15 18:51:12 +0800

    test

--- ttsd-frontend-manage/resources/static/m-web/js/investment/loan_list.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/investment/loan_list.jsx
@@ -3,65 +3,6 @@ let commonFun = require('publicJs/commonFun');
 let tpl = require('art-template/dist/template');
 
 let $content = $('.loan-list-content');
-let data = {
-    "count": 1,
-    "has_next": false,
-    "has_previous": false,
-    "page": 1,
-    "page_count": 1,
-    "results": [
-        {
-            "id": 1,
-            "financing_deadline": "2018-01-01",
-            "created_time": "2018-01-01T00:00:00Z",
-            "updated_time": "2018-01-01T00:00:00Z",
-            "upstream_firm_name": "sfasf",
-            "upstream_firm_legal_person": "asf",
-            "upstream_firm_mobile": "123123",
-            "credit_limit": 123123,
-            "approve_limit": 123,
-            "check_taker": "12313",
-            "guarantor": "123",
-            "ticket_number": "123",
-            "ticket_image": "123",
-            "invoice": 1
-        },
-        {
-            "id": 1,
-            "financing_deadline": "2018-01-01",
-            "created_time": "2018-01-01T00:00:00Z",
-            "updated_time": "2018-01-01T00:00:00Z",
-            "upstream_firm_name": "sfasf",
-            "upstream_firm_legal_person": "asf",
-            "upstream_firm_mobile": "123123",
-            "credit_limit": 123123,
-            "approve_limit": 123,
-            "check_taker": "12313",
-            "guarantor": "123",
-            "ticket_number": "123",
-            "ticket_image": "123",
-            "invoice": 1
-        },
-        {
-            "id": 1,
-            "financing_deadline": "2018-01-01",
-            "created_time": "2018-01-01T00:00:00Z",
-            "updated_time": "2018-01-01T00:00:00Z",
-            "upstream_firm_name": "sfasf",
-            "upstream_firm_legal_person": "asf",
-            "upstream_firm_mobile": "123123",
-            "credit_limit": 123123,
-            "approve_limit": 123,
-            "check_taker": "12313",
-            "guarantor": "123",
-            "ticket_number": "123",
-            "ticket_image": "123",
-            "invoice": 1
-        }
-    ]
-} ;
-
-let $data = JSON.parse(JSON.stringify(data));
 
 
 let $loanList = $('#loanList'),
@@ -80,7 +21,7 @@ let myScroll = new IScroll('#wrapperOut', {
     probeType: 2,
     mouseWheel: true
 });
-getMore();//获取更多数据
+
 
 myScroll.on('scrollEnd',function () {
     console.log(this.y +"|||"+this.maxScrollY);
