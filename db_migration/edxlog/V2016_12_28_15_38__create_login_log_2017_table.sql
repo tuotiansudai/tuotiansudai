c880330b74676b5de984040ccb17922c40b77d49
zhanglong888 <zhanglong@tuotiansudai.com>
2018-01-02 14:23:06 +0800

    ut add 2018 login log

--- db_migration/edxlog/V2016_12_28_15_38__create_login_log_2017_table.sql
+++ db_migration/edxlog/V2016_12_28_15_38__create_login_log_2017_table.sql
@@ -168,3 +168,178 @@ CREATE TABLE `login_log_201712` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
+
+
+
+CREATE TABLE IF NOT EXISTS `login_log_201801` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201801_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1001
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201802` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201802_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201803` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201803_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201804` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201804_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201805` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201705_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201806` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201806_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201807` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201807_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201808` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201808_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201809` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201809_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ CREATE TABLE IF NOT EXISTS `login_log_201810` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201810_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+ CREATE TABLE IF NOT EXISTS `login_log_201811` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201811_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+ CREATE TABLE IF NOT EXISTS `login_log_201812` (
+  `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `login_name` VARCHAR(25)     NOT NULL,
+  `source`     VARCHAR(10)     NOT NULL,
+  `ip`         VARCHAR(16),
+  `device`     VARCHAR(255),
+  `login_time` DATETIME        NOT NULL,
+  `success`    BOOLEAN         NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_LOGIN_LOG_201812_LOGIN_NAME` (`login_name`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
