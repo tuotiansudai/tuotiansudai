c4dede50f4b6afd1619854f452eda0dcfca90c92
Xiduan Gao <gaoxiduan@gmail.com>
2018-01-09 11:59:57 +0800

    add bind card m site

--- ttsd-frontend-manage/resources/static/m-web/js/account/bank_card_manage.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/account/bank_card_manage.jsx
@@ -3,23 +3,15 @@ require('mWebStyle/account/bank_card_manage.scss');
 let $bankCardManage = $('#bankCardManage'),
     $bankCardSelect = $('#bankCardSelect');
 
-if($bankCardManage.length) {
-    let $bankColumn = $('.bank-column',$bankCardManage),
+if ($bankCardManage.length) {
+    let $bankColumn = $('.bank-column', $bankCardManage),
         bankForm = globalFun.$('#bankForm'),
         cardNumberDom = globalFun.$('#cardNumber'),
         $submitBtn = $(bankForm).find('button');
-
-    let bankInfo = sessionStorage.getItem('bankInfo');
-    let cardNumber = sessionStorage.getItem('cardNumber');
-    if(bankInfo) {
-        $bankColumn.find('.bank-info').html(bankInfo).removeClass('key');
-        bankForm.bankName.value = bankInfo;
-    }
-    cardNumberDom.value = cardNumber || '';
     isDisabledButton();
 
     //持卡人提示
-    $bankCardManage.find('.icon-notice').on('click',function() {
+    $bankCardManage.find('.icon-notice').on('click', function () {
         layer.open({
             type: 1,
             title: false,
@@ -29,57 +21,50 @@ if($bankCardManage.length) {
             skin: 'tip-square-box',
             btn: ['我知道了'],
             content: $('.tip-user-info')
-        },function(index, layero){
-          // alert('pp');
+        }, function (index, layero) {
+            // alert('pp');
         });
     });
 
-    $bankColumn.on('click',function() {
-        let  cardNumber = cardNumberDom.value;
-        sessionStorage.setItem("cardNumber",cardNumber);
-        window.location.href='/myAccount/bank-card-select.ftl';
+    $bankColumn.on('click', function () {
+        $bankCardManage.hide();
+        $bankCardSelect.show();
     });
 
     !function () {
         cardNumberDom.onkeyup = function (event) {
             isDisabledButton();
-            if((event.which >= 48 && event.which <= 57) ||(event.which >= 96 && event.which <= 105 )){
+            if ((event.which >= 48 && event.which <= 57) || (event.which >= 96 && event.which <= 105 )) {
                 var v = this.value;
-                if(/\S{5}/.test(v)){
+                if (/\S{5}/.test(v)) {
                     this.value = v.replace(/\s/g, '').replace(/(.{4})/g, "$1 ");
                 }
-            };
+            }
         }
     }();
 
     //按钮是否点亮
     function isDisabledButton() {
-        let bankName = bankForm.bankName.value,
-            bankNumber = bankForm.cardNumber.value.replace(/ /g,'');
+        let bankName = $bankCardManage.find('.bank-show').html().trim() !== '请选择银行',
+            bankNumber = bankForm.cardNumber.value.replace(/ /g, '');
         let isDisableBtn = bankName && /^(\d{15,})$/.test(bankNumber);
 
         $submitBtn.prop('disabled', !isDisableBtn);
     }
 }
 
-if($bankCardSelect.length) {
-    let $bankList = $('.bank-list',$bankCardSelect);
-    let bankInfo = sessionStorage.getItem('bankInfo');
-    let bankIndex = sessionStorage.getItem('bankIndex');
+if ($bankCardSelect.length) {
+    let $bankList = $('.bank-list', $bankCardSelect);
 
-    if(bankInfo) {
-        $bankList.find('li').eq(bankIndex).addClass('checked').siblings('li').removeClass('checked');
-    }
-
-    $bankList.find('li').on('click',function() {
-        let  $this = $(this),
-            index = $this.index();
+    $bankList.find('li').on('click', function () {
+        let $this = $(this);
         $this.addClass('checked').siblings('li').removeClass('checked');
         let thisBank = $this.find('span').html();
+        $bankCardManage.find('.bank-show').removeClass('key');
+        $bankCardManage.find('.bank-show').html(thisBank);
 
-        sessionStorage.setItem("bankInfo",thisBank);
-        sessionStorage.setItem("bankIndex",index);
-        window.location.href='/myAccount/bank-card-manage.ftl';
+        $bankCardManage.show();
+        $bankCardSelect.hide();
     })
 }
 
---
+++ ttsd-web/src/main/webapp/templates/bind-card_m.ftl
@@ -0,0 +1,58 @@
+<#import "macro/global_m.ftl" as global>
+
+<@global.main pageCss="${m_css.bank_card_manage}" pageJavascript="${m_js.bank_card_manage}" title="绑定银行卡">
+
+<div class="my-account-content bank-card-manage" id="bankCardManage">
+    <div class="info-note">请绑定持卡人本人的银行储蓄卡</div>
+    <form method="post" id="bankForm">
+        <ul class="input-list align-flex-start">
+            <li>
+                <label for="perName">持卡人</label>
+                <input type="text" name="userName" id="perName" value="${userName}" disabled="disabled">
+                <i class="icon-notice"></i>
+            </li>
+            <li class="bank-column">
+                <label for="perNum">银行</label>
+                <span class="bank-info bank-show key">
+                    请选择银行
+                </span>
+                <i class="fa fa-angle-right"></i>
+            </li>
+            <li>
+                <label for="cardNumber">银行卡号</label>
+                <input type="text" name="cardNumber" id="cardNumber" placeholder="请输入银行卡号">
+                <i></i>
+            </li>
+        </ul>
+        <input type="hidden" name="source" value="WEB">
+        <button type="submit" class="btn-wap-normal next-step" disabled>确认提交</button>
+    </form>
+
+    <div class="tip-user-info" style="display: none">
+        <em>温馨提示</em>
+        为了您的账户资金安全
+        只能绑定持卡人的银行储蓄卡
+    </div>
+</div>
+
+
+<div class="my-account-content bank-card-manage" id="bankCardSelect" style="display: none">
+    <div class="info-note">银行卡添加完成后，充值和提现只能使用该银行卡，建议选择充值限额较大的银行</div>
+    <ul class="bank-list">
+        <#list bankList as bank>
+            <li>
+                <i class="icon-bank"></i>
+                <span class="bank-show">
+                    <em>${bank.name}</em>
+                    单笔限额${(bank.singleAmount?number)}元，单日限额${(bank.singleDayAmount?number)}元
+                </span>
+                <i class="icon-radio"></i>
+            </li>
+        </#list>
+    </ul>
+
+    <div class="info-more-bank">
+        更多银行努力接入中…
+    </div>
+</div>
+</@global.main>
