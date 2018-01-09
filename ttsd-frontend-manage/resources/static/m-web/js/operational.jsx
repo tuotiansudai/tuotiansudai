9c924841f6827b115e35312139268ebc417907ae
zhaozhen <zhaozhen@tuotiansudai.com>
2018-01-09 19:41:25 +0800

    operate data

--- ttsd-frontend-manage/resources/static/m-web/js/operational.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/operational.jsx
@@ -1,9 +1,6 @@
 require('mWebStyle/operational.scss');
-
 import Swiper from 'swiper';
-
 let echarts = require('echarts');
-
 let commonFun=require('publicJs/commonFun');
 
 (function (doc, win) {
@@ -22,124 +19,77 @@ let commonFun=require('publicJs/commonFun');
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 
-let myDate = new Date();
-
-let currentMonth = myDate.getMonth() + 1;
-let currentDay = myDate.getDate();
-
-currentMonth = currentMonth < 10 ? '0' + currentMonth : currentMonth;
-currentDay = currentDay < 10 ? '0' + currentDay : currentDay;
-
-$('.currentMonth').html(currentMonth);
-$('.currentDay').html(currentDay);
-
-console.log(currentMonth)
-console.log(currentDay)
-
 let mySwiper = new Swiper('.swiper-container', {
     direction : 'vertical',
     width: window.screen.width,
     height: document.body.clientHeight - 44,
 });
 
-
 $('#goBack_experienceAmount').on('click',() => {
     history.go(-1);
 });
 
-
 $('.side_to_page').click(function(e){
     let index = Number(e.currentTarget.dataset.index);
-    index = index == 4 ? 1 : index;
+    index = index == 5 ? index - 2 : index;
     mySwiper.slideTo(index, 500, false);//切换到第一个slide，速度为1秒
 });
 
 let myChart1 = echarts.init(document.getElementById('main_part1'));
-// 绘制图表
-myChart1.setOption({
-    title: {
-        text: '',
-    },
-    tooltip: {
-        show: false
-    },
-    xAxis: {
-        type: 'category',
-        name: '(标的)',
-        nameGap: 5,
-        nameTextStyle: {
-          color: '#333'
+let myChart2 = echarts.init(document.getElementById('main_part2'));
+let myChart3 = echarts.init(document.getElementById('main_part3'));
+let myChart4 = echarts.init(document.getElementById('main_part4'));
+let myChart5 = echarts.init(document.getElementById('main_part5'));
+let myChart6 = echarts.init(document.getElementById('main_part6'));
+
+let drawBarChart = (data) => {  // 柱状图
+    return {
+        xAxis: {
+            type: 'category',
+            name: '(标的)',
+            nameGap: 5,
+            nameTextStyle: {color: '#333'},
+            legendHoverLink: false,
+            data: ['30天', '60天', '90天', '360天'],
+            axisTick: {show: false,},
+            axisLine: {
+                lineStyle: {color: '#ccc'}
+            },
+            axisLabel: {color: '#333'}
         },
-        legendHoverLink: false,
-        data: ['30天', '60天', '90天', '360天'],
-        axisTick: {
-            show: false,
-        },
-        axisLine: {
-            lineStyle: {
-                color: '#ccc'
-            }
-        },
-        axisLabel: {
-            color: '#333'
-        }
-    },
-    yAxis: {
-        show: false
-    },
-    series: [{
-        name: '销量',
-        type: 'bar',
-        barWidth: '30%',
-        data: [3859, 13014, 4275, 6176],
-        itemStyle: {
-            normal: {
-                color: function (params){
-                    let colorList = ['#1691e8','#74bbf3','#7cd8ef','#28c37c'];
-                    return colorList[params.dataIndex];
+        yAxis: {show: false},
+        series: [{
+            type: 'bar',
+            barWidth: '30%',
+            data: data,
+            itemStyle: {
+                normal: {
+                    color: function (params){
+                        let colorList = ['#1691e8','#74bbf3','#7cd8ef','#28c37c'];
+                        return colorList[params.dataIndex];
+                    }
                 }
             }
-        }
-    }],
-    label: {
-        normal: {
-            show: true,
-            position: 'top',
-
+        }],
+        label: {
+            normal: {show: true, position: 'top'}
         }
     }
-});
+};
 
-let myChart2 = echarts.init(document.getElementById('main_part2'));
-// 绘制图表
-
-function getChartData(data) {
+let drawLineChart = (data,monthArr) => {  // 折线图
     return {
-        title: {
-            text: '',
-        },
-        tooltip: {
-            show: false
-        },
         xAxis: {
             type: 'category',
             nameGap: 5,
-            data: ['7月', '8月', '9月', '10月','11月','12月'],
-            axisTick: {
-                show: false,
-            },
+            data: monthArr,
+            axisTick: {show: false},
             axisLine: {
-                lineStyle: {
-                    color: '#ccc'
-                }
+                lineStyle: {color: '#ccc'}
             },
-            axisLabel: {
-                color: '#333'
-            }
+            axisLabel: {color: '#333'}
         },
-        yAxis: {
-            show: false
-        },
+        yAxis: {show: false},
         series: [{
             type: 'line',
             hoverAnimation: false,
@@ -151,39 +101,100 @@ function getChartData(data) {
                     color: '#019bd9',
                 }
             },
-            data: [3859, 13014, 4275, 6176, 5000, 3000],
+            data: data,
         }],
     }
-}
+};
 
+let circularChart = (data,legendData,color) => { // 环形图
+    return {
+        legend: {
+            orient: 'vertical',
+            selectedMode:false,
+            right: '10%',
+            y: 'middle',
+            data:legendData
+        },
+        series: [
+            {
+                type:'pie',
+                radius: ['40%', '70%'],
+                center: ['30%','50%'],
+                legendHoverLink: false,
+                hoverAnimation: false,
+                label: {
+                    normal: {
+                        show: false,
+                        position: 'center'
+                    }
+                },
+                color: color,
+                labelLine: {
+                    normal: {
+                        show: false
+                    }
+                },
+                data: data
+            }
+        ]
+    }
+};
 
-commonFun.useAjax({
+let drawBarTransverse = (cityName,cityData,colorArr) => { // 横向柱状图
+    return {
+        xAxis: {type: 'value', show: false},
+        yAxis: {
+            type: 'category',
+            data: cityName,
+            axisLine: {show: false},
+            axisTick: {show: false},
+            axisLabel: {show: false}
+        },
+        series: [{
+            type: 'bar',
+            barWidth: '80%',
+            data: cityData,
+            itemStyle: {
+                normal: {
+                    color: function (params){
+                        let colorList = colorArr;
+                        return colorList[params.dataIndex];
+                    }
+                }
+            }
+        }],
+        label: {
+            normal: {
+                show: true,
+                position: 'right',
+                color: '#333',
+                formatter: '{b} {c}%'
+            }
+        }
+    }
+};
+
+commonFun.useAjax({  // 拉取页面数据
     url: '/about/operation-data/chart',
     type: 'GET'
 },(data) => {
     let dataStr = data.operationDays.toString();
-    let dom = '';
-    for (let i = 0;i < dataStr.length;i++) {
-        dom += `<div class="safe_day">${dataStr.charAt(i)}</div>`
-    }
-    $('.safe_day_wrapper').prepend(dom);
-    $('#grand_total_amount').html(fmoney(data.tradeAmount,2));
-    $('#earn_total_amount').html(fmoney(data.totalInterest,2));
+    getPartOnePage(data,dataStr);
+    getPartTwoPage(data);
+    getPartThreePage(data);
+    getPartFourPage(data);
+
 }, () => {
-    // let data = {"operationDays":922,"usersCount":522,"totalInterest":"108992411","maleScale":null,"femaleScale":null,"month":["2015.7","2015.8","2015.9","2015.10","2015.11","2015.12","2016.1","2016.2","2016.3","2016.4","2016.5","2016.6","2016.7","2016.8","2016.9","2016.10","2016.11","2016.12","2017.1","2017.2","2017.3","2017.4","2017.5","2017.6","2017.7","2017.8","2017.9","2017.10","2017.11","2017.12"],"money":["0.00","0.00","0.00","0.00","246.00","828.00","718.00","354.00","139.00","156.00","398.00","513.68","56.11","1066.97","1056.37","2000012.51","48.34","133.50","620411.32","21.00","172.44","100033.00","301018.00","200.00","888017.00","9640025.00","4491098.00","20204064.00","17561894.00","0.00"],"ageDistribution":[{"name":"20岁以下","scale":"2.3"},{"name":"20~30岁","scale":"37.5"},{"name":"30~40岁","scale":"42.0"},{"name":"40~50岁","scale":"15.9"},{"name":"50岁以上","scale":"2.3"}],"investCityScaleTop3":[{"city":"北京","scale":"52.2"},{"city":"南宁","scale":"31.1"},{"city":"成都","scale":"5.6"}],"investAmountScaleTop3":[{"city":"北京","scale":"65.0"},{"city":"南宁","scale":"12.7"},{"city":"成都","scale":"8.6"}],"tradeAmount":"55812680.24"}
-    // myChart2.setOption(getChartData(data));
+    // let data = {"operationDays":922,"usersCount":522,"totalInterest":"108992411","maleScale":"0.7","femaleScale":"0.3","month":["2015.7","2015.8","2015.9","2015.10","2015.11","2015.12","2016.1","2016.2","2016.3","2016.4","2016.5","2016.6","2016.7","2016.8","2016.9","2016.10","2016.11","2016.12","2017.1","2017.2","2017.3","2017.4","2017.5","2017.6","2017.7","2017.8","2017.9","2017.10","2017.11","2017.12"],"money":["0.00","0.00","0.00","0.00","246.00","828.00","718.00","354.00","139.00","156.00","398.00","513.68","56.11","1066.97","1056.37","2000012.51","48.34","133.50","620411.32","21.00","172.44","100033.00","301018.00","200.00","888017.00","9640025.00","4491098.00","20204064.00","17561894.00","0.00"],"ageDistribution":[{"name":"20岁以下","scale":"2.3"},{"name":"20~30岁","scale":"37.5"},{"name":"30~40岁","scale":"42.0"},{"name":"40~50岁","scale":"15.9"},{"name":"50岁以上","scale":"2.3"}],"investCityScaleTop3":[{"city":"北京","scale":"52.2"},{"city":"南宁","scale":"31.1"},{"city":"成都","scale":"5.6"}],"investAmountScaleTop3":[{"city":"北京","scale":"65.0"},{"city":"南宁","scale":"12.7"},{"city":"成都","scale":"8.6"}],"tradeAmount":"55812680.24"}
     // let dataStr = data.operationDays.toString();
-    // let dom = '';
-    // for (let i = 0;i < dataStr.length;i++) {
-    //     dom += `<div class="safe_day">${dataStr.charAt(i)}</div>`
-    // }
-    // $('.safe_day_wrapper').prepend(dom);
-    // $('#grand_total_amount').html(fmoney(data.tradeAmount,2));
-    // $('#earn_total_amount').html(fmoney(data.totalInterest,2));
+    // getPartOnePage(data,dataStr);
+    // getPartTwoPage(data);
+    // getPartThreePage(data);
+    // getPartFourPage(data);
 });
 
 
-function fmoney(s, n) {
+function formatNumber(s, n) {  // 金额格式化
     n = n > 0 && n <= 20 ? n : 2;
     s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
     var l = s.split(".")[0].split("").reverse(), r = s.split(".")[1];
@@ -192,4 +203,83 @@ function fmoney(s, n) {
         t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
     }
     return t.split("").reverse().join("") + "." + r;
-}
+};
+
+let getPartOnePage = (data,dataStr) => {
+    let dom = '';
+    for (let i = 0;i < dataStr.length;i++) {
+        dom += `<div class="safe_day">${dataStr.charAt(i)}</div>`
+    }
+    $('.safe_day_wrapper').prepend(dom);
+    $('#grand_total_amount').html(formatNumber(data.tradeAmount,2));
+    $('#earn_total_amount').html(formatNumber(data.totalInterest,2));
+
+    let myDate = new Date();
+    let currentMonth = myDate.getMonth() + 1;
+    let currentDay = myDate.getDate();
+    currentMonth = currentMonth < 10 ? '0' + currentMonth : currentMonth;
+    currentDay = currentDay < 10 ? '0' + currentDay : currentDay;
+    $('.currentMonth').html(currentMonth);
+    $('.currentDay').html(currentDay);
+};
+
+let getPartTwoPage = (data) => {
+    let barChartArr = [];
+    let num = 0;
+    for (let i = 0;i < 4;i++) {
+        let $item = $('#investItem' + i);
+        let amount = parseInt($item.data('amount') / 10000);
+        barChartArr.push(amount);
+        let count = Number($item.data('count')) || 0;
+        num += count;
+    }
+    $('#total_trade_count').html(formatNumber(num));
+    myChart1.setOption(drawBarChart(barChartArr));
+    let monthArr = data.month.slice(-6).map(item => {
+        return item.split('.')[1] + '月';
+    });
+    let amountArr = data.money.slice(-6).map(item => {
+        return parseInt(item/10000);
+    });
+    myChart2.setOption(drawLineChart(amountArr,monthArr));
+};
+
+let getPartThreePage = (data) => {
+    let ageArr = [];
+    let ageLegendArr = [];
+    let maleScale = data.maleScale;
+    let femaleScale = data.femaleScale;
+    let ageDistribution = data.ageDistribution;
+    for (let i = 0;i < ageDistribution.length;i++) {
+        let item = ageDistribution[i];
+        ageArr[i] = {};
+        ageArr[i].value = item.scale;
+        ageArr[i].name = item.name + item.scale + '%';
+        ageLegendArr[i] = item.name + item.scale + '%';
+    }
+    myChart3.setOption(circularChart([
+        {value: `${femaleScale}`, name:`女性${femaleScale * 100}%`},
+        {value: `${maleScale}`, name:`男性${maleScale * 100}%`},
+    ],[`男性${maleScale * 100}%`,`女性${femaleScale * 100}%`],['#fdb560','#74bbf3']));
+
+    myChart4.setOption(circularChart(ageArr,ageLegendArr,['#a47cf3','#fdb560','#fcee74','#87e376','#69e2ab']));
+};
+
+let getPartFourPage = (data) => {
+    let investCityScaleTop3 = data.investCityScaleTop3; // 投资人数top3
+    let investAmountScaleTop3 = data.investAmountScaleTop3; // 投资金额top3
+    let cityName_count = [];
+    let cityData_count = [];
+    let cityName_amount = [];
+    let cityData_amount = [];
+    investCityScaleTop3.forEach((item,index) => {
+        cityName_count[index] = item.city;
+        cityData_count[index] = item.scale;
+    });
+    investAmountScaleTop3.forEach((item,index) => {
+        cityName_amount[index] = item.city;
+        cityData_amount[index] = item.scale;
+    });
+    myChart5.setOption(drawBarTransverse(cityName_count,cityData_count,['#c2eef2','#81e9f2','#00def2']));
+    myChart6.setOption(drawBarTransverse(cityName_amount,cityData_amount, ['#ffecac','#ffd74f','#ffc601']));
+};
--- ttsd-web/src/main/webapp/templates/about/operational_m.ftl
+++ ttsd-web/src/main/webapp/templates/about/operational_m.ftl
@@ -7,8 +7,9 @@
     </div>
     <div class="swiper-container">
         <div class="swiper-wrapper">
+
             <div class="swiper-item swiper-slide1 swiper-slide">
-                <div class="data_dealine">数据截止到<span class="currentMonth">02</span>月<span class="currentDay">10</span>日</div>
+                <div class="data_dealine">数据截止到<span class="currentMonth"></span>月<span class="currentDay"></span>日</div>
                 <div class="safe_operational_text">拓天速贷已安全运营</div>
                 <div class="safe_day_wrapper_center">
                     <div class="safe_day_wrapper">
@@ -29,11 +30,12 @@
                 </div>
                 <div class="button_under_arrow side_to_page" data-index="1"></div>
             </div>
+
             <div class="swiper-item swiper-slide2 swiper-slide">
                 <div class="part-2-topBar"></div>
                 <div class="total_trade_text">累计交易笔数</div>
                 <div class="text_underLine_blue"></div>
-                <div class="total_trade_count">11,111笔</div>
+                <div class="total_trade_count"><span id="total_trade_count"></span>笔</div>
                 <div class="unit_symbol">[单位:万元]</div>
                 <div id="main_part1"></div>
                 <div class="chart1_title">近半年<span class="chart1_title_strong">标的投资数据</span>统计</div>
@@ -42,15 +44,48 @@
                 <div class="chart1_title">近半年<span class="chart1_title_strong">交易金额数据</span>统计</div>
                 <div class="button_under_arrow side_to_page" data-index="2"></div>
             </div>
+
+            <div class="swiper-item swiper-slide3 swiper-slide">
+                <div class="part-3-topBar"><div class="user-statistics">平台用户统计</div></div>
+                <div class="total_trade_text">累计注册投资用户</div>
+                <div class="text_underLine_blue"></div>
+                <div class="total_trade_count"><span id="total_trade_count">11,111</span>人</div>
+                <div id="main_part3"></div>
+                <div id="main_part4"></div>
+                <div class="button_under_arrow side_to_page" data-index="3"></div>
+            </div>
+
+            <div class="swiper-item swiper-slide4 swiper-slide">
+                <div class="part-4-topBar"><div class="user-area">平台地区统计</div></div>
+                <div class="count_proportion">
+                    <div class="symbol_line"></div>
+                    <div class="count_title">投资人数<span class="title_strong">TOP3占比</span></div>
+                </div>
+                <div id="main_part5"></div>
+                <div class="amount_proportion">
+                    <div class="symbol_line"></div>
+                    <div class="amount_title">投资金额<span class="title_strong">TOP3占比</span></div>
+                </div>
+                <div id="main_part6"></div>
+                <div class="button_under_arrow side_to_page" data-index="4"></div>
+            </div>
+
             <div class="swiper-item swiper-slide5 swiper-slide">
                 <div class="tuotian_logo"></div>
-                <div class="button_up_arrow side_to_page" data-index="4"></div>
+                <div class="button_up_arrow side_to_page" data-index="5"></div>
                 <div class="company_desc_text">
                     <div style="margin-bottom: .25rem">拓天速贷</div>
                     <div>互联网金融信息服务平台</div>
                 </div>
             </div>
+
         </div>
     </div>
+
+    <#if investDetailList??>
+        <#list investDetailList as investDetailItem>
+            <div id="investItem${investDetailItem_index}" data-day="${investDetailItem.productName!}" data-amount="${investDetailItem.totalInvestAmount!}" data-count="${investDetailItem.countInvest!}"></div>
+        </#list>
+    </#if>
 </div>
 </@global.main>
