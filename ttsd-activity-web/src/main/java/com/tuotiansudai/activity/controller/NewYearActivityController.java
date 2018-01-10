7035f8093e223cc6015f0272d44c96b911c39944
zhukun5764 <zhukun@tuotiansudai.com>
2018-01-10 19:04:46 +0800

    add controller

---
+++ ttsd-activity-web/src/main/java/com/tuotiansudai/activity/controller/NewYearActivityController.java
@@ -0,0 +1,17 @@
+package com.tuotiansudai.activity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/activity/new-year")
+public class NewYearActivityController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("/activities/2017/new-year-increase-interest", "responsive", false);
+        return modelAndView;
+    }
+}
