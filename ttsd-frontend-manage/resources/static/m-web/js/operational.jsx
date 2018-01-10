2a0b3b9013572abe5c989caaff55e976475f4d64
zhaozhen <zhaozhen@tuotiansudai.com>
2018-01-10 12:23:03 +0800

    format amount

--- ttsd-frontend-manage/resources/static/m-web/js/operational.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/operational.jsx
@@ -69,10 +69,32 @@ let drawBarChart = (data) => {  // 柱状图
                         return colorList[params.dataIndex];
                     }
                 }
-            }
+            },
         }],
         label: {
-            normal: {show: true, position: 'top'}
+            normal: {
+                formatter:(a) => {
+                    var result = [ ], counter = 0;
+                    var num_array = a.data.toFixed(2).split('.');
+                    var num = num_array[0];
+                    var newStr = '';
+                    for (var i = num.length - 1; i >= 0; i--) {
+                        counter++;
+                        result.unshift(num[i]);
+                        if (!(counter % 3) && i != 0) { result.unshift(','); }
+                    }
+                    return result.join('');
+                    if(num_array.length>1){
+                        newStr = result.join('');
+                        newStr += '.'+num_array[1];
+                        return newStr;
+                    }else{
+                        return result.join('');
+                    }
+                },
+                show: true,
+                position: 'top'
+            }
         }
     }
 };
@@ -103,15 +125,40 @@ let drawLineChart = (data,monthArr) => {  // 折线图
             },
             data: data,
         }],
+        label: {
+            normal: {
+                formatter:(a) => {
+                    var result = [ ], counter = 0;
+                    var num_array = a.data.toFixed(2).split('.');
+                    var num = num_array[0];
+                    var newStr = '';
+                    for (var i = num.length - 1; i >= 0; i--) {
+                        counter++;
+                        result.unshift(num[i]);
+                        if (!(counter % 3) && i != 0) { result.unshift(','); }
+                    }
+                    return result.join('');
+                    if(num_array.length>1){
+                        newStr = result.join('');
+                        newStr += '.'+num_array[1];
+                        return newStr;
+                    }else{
+                        return result.join('');
+                    }
+                },
+                show: true,
+                position: 'top'
+            }
+        }
     }
 };
 
-let circularChart = (data,legendData,color) => { // 环形图
+let circularChart = (data,legendData,color,rightPosition) => { // 环形图
     return {
         legend: {
             orient: 'vertical',
             selectedMode:false,
-            right: '10%',
+            right: rightPosition,
             y: 'middle',
             data:legendData
         },
@@ -185,14 +232,13 @@ commonFun.useAjax({  // 拉取页面数据
     getPartFourPage(data);
 
 }, () => {
-    let data = {"operationDays":922,"usersCount":522,"totalInterest":"108992411","maleScale":"0.7","femaleScale":"0.3","month":["2015.7","2015.8","2015.9","2015.10","2015.11","2015.12","2016.1","2016.2","2016.3","2016.4","2016.5","2016.6","2016.7","2016.8","2016.9","2016.10","2016.11","2016.12","2017.1","2017.2","2017.3","2017.4","2017.5","2017.6","2017.7","2017.8","2017.9","2017.10","2017.11","2017.12"],"money":["0.00","0.00","0.00","0.00","246.00","828.00","718.00","354.00","139.00","156.00","398.00","513.68","56.11","1066.97","1056.37","2000012.51","48.34","133.50","620411.32","21.00","172.44","100033.00","301018.00","200.00","888017.00","9640025.00","4491098.00","20204064.00","17561894.00","0.00"],"ageDistribution":[{"name":"20岁以下","scale":"2.3"},{"name":"20~30岁","scale":"37.5"},{"name":"30~40岁","scale":"42.0"},{"name":"40~50岁","scale":"15.9"},{"name":"50岁以上","scale":"2.3"}],"investCityScaleTop3":[{"city":"北京","scale":"52.2"},{"city":"南宁","scale":"31.1"},{"city":"成都","scale":"5.6"}],"investAmountScaleTop3":[{"city":"北京","scale":"0"},{"city":"南宁","scale":"0"},{"city":"成都","scale":"100"}],"tradeAmount":"55812680.24"}
+    let data = {"operationDays":922,"usersCount":11522,"totalInterest":"108992411","maleScale":"0.7","femaleScale":"0.3","month":["2015.7","2015.8","2015.9","2015.10","2015.11","2015.12","2016.1","2016.2","2016.3","2016.4","2016.5","2016.6","2016.7","2016.8","2016.9","2016.10","2016.11","2016.12","2017.1","2017.2","2017.3","2017.4","2017.5","2017.6","2017.7","2017.8","2017.9","2017.10","2017.11","2017.12"],"money":["0.00","0.00","0.00","0.00","246.00","828.00","718.00","354.00","139.00","156.00","398.00","513.68","56.11","1066.97","1056.37","2000012.51","48.34","133.50","620411.32","21.00","172.44","100033.00","301018.00","200.00","888017.00","9640025.00","4491098.00","20204064.00","17561894.00","0.00"],"ageDistribution":[{"name":"20岁以下","scale":"2.3"},{"name":"20~30岁","scale":"37.5"},{"name":"30~40岁","scale":"42.0"},{"name":"40~50岁","scale":"15.9"},{"name":"50岁以上","scale":"2.3"}],"investCityScaleTop3":[{"city":"北京","scale":"52.2"},{"city":"南宁","scale":"31.1"},{"city":"成都","scale":"5.6"}],"investAmountScaleTop3":[{"city":"北京","scale":"0"},{"city":"南宁","scale":"0"},{"city":"成都","scale":"100"}],"tradeAmount":"55812680.24"}
     let dataStr = data.operationDays.toString();
     getPartOnePage(data,dataStr);
     getPartTwoPage(data);
     getPartThreePage(data);
     getPartFourPage(data);
 });
-
 
 function formatNumber(s, n) {  // 金额格式化
     n = n > 0 && n <= 20 ? n : 2;
@@ -204,6 +250,16 @@ function formatNumber(s, n) {  // 金额格式化
     }
     return t.split("").reverse().join("") + "." + r;
 };
+
+function toThousands(num) {
+    var num = (num || 0).toString(), result = '';
+    while (num.length > 3) {
+        result = ',' + num.slice(-3) + result;
+        num = num.slice(0, num.length - 3);
+    }
+    if (num) { result = num + result; }
+    return result;
+}
 
 let getPartOnePage = (data,dataStr) => {
     let dom = '';
@@ -233,7 +289,7 @@ let getPartTwoPage = (data) => {
         let count = Number($item.data('count')) || 0;
         num += count;
     }
-    $('#total_trade_count').html(formatNumber(num));
+    $('#total_trade_count').html(toThousands(num));
     myChart1.setOption(drawBarChart(barChartArr));
     let monthArr = data.month.slice(-6).map(item => {
         return item.split('.')[1] + '月';
@@ -250,19 +306,20 @@ let getPartThreePage = (data) => {
     let maleScale = data.maleScale;
     let femaleScale = data.femaleScale;
     let ageDistribution = data.ageDistribution;
+    $('#total_trade_person').html(toThousands(data.usersCount));
     for (let i = 0;i < ageDistribution.length;i++) {
         let item = ageDistribution[i];
         ageArr[i] = {};
         ageArr[i].value = item.scale;
-        ageArr[i].name = item.name + item.scale + '%';
-        ageLegendArr[i] = item.name + item.scale + '%';
+        ageArr[i].name = item.name + ' ' + item.scale + '%';
+        ageLegendArr[i] = item.name + ' ' + item.scale + '%';
     }
     myChart3.setOption(circularChart([
-        {value: `${femaleScale}`, name:`女性${femaleScale * 100}%`},
-        {value: `${maleScale}`, name:`男性${maleScale * 100}%`},
-    ],[`男性${maleScale * 100}%`,`女性${femaleScale * 100}%`],['#fdb560','#74bbf3']));
+        {value: `${femaleScale}`, name:`女性 ${femaleScale * 100}%`},
+        {value: `${maleScale}`, name:`男性 ${maleScale * 100}%`},
+    ],[`男性 ${maleScale * 100}%`,`女性 ${femaleScale * 100}%`],['#fdb560','#74bbf3'],'18%'));
 
-    myChart4.setOption(circularChart(ageArr,ageLegendArr,['#a47cf3','#fdb560','#fcee74','#87e376','#69e2ab']));
+    myChart4.setOption(circularChart(ageArr,ageLegendArr,['#a47cf3','#fdb560','#fcee74','#87e376','#69e2ab'],'10%'));
 };
 
 let getPartFourPage = (data) => {
--- ttsd-web/src/main/webapp/templates/about/operational_m.ftl
+++ ttsd-web/src/main/webapp/templates/about/operational_m.ftl
@@ -8,21 +8,6 @@
     <div class="swiper-container">
         <div class="swiper-wrapper">
 
-            <div class="swiper-item swiper-slide4 swiper-slide">
-                <div class="part-4-topBar"><div class="user-area">平台地区统计</div></div>
-                <div class="count_proportion">
-                    <div class="symbol_line"></div>
-                    <div class="count_title">投资人数<span class="title_strong">TOP3占比</span></div>
-                </div>
-                <div id="main_part5"></div>
-                <div class="amount_proportion">
-                    <div class="symbol_line"></div>
-                    <div class="amount_title">投资金额<span class="title_strong">TOP3占比</span></div>
-                </div>
-                <div id="main_part6"></div>
-                <div class="button_under_arrow side_to_page" data-index="4"></div>
-            </div>
-
             <div class="swiper-item swiper-slide1 swiper-slide">
                 <div class="data_dealine">数据截止到<span class="currentMonth"></span>月<span class="currentDay"></span>日</div>
                 <div class="safe_operational_text">拓天速贷已安全运营</div>
@@ -47,7 +32,7 @@
             </div>
 
             <div class="swiper-item swiper-slide2 swiper-slide">
-                <div class="part-2-topBar"></div>
+                <div class="part-2-topBar"><div class="trade-detail">平台交易明细</div></div>
                 <div class="total_trade_text">累计交易笔数</div>
                 <div class="text_underLine_blue"></div>
                 <div class="total_trade_count"><span id="total_trade_count"></span>笔</div>
@@ -64,13 +49,26 @@
                 <div class="part-3-topBar"><div class="user-statistics">平台用户统计</div></div>
                 <div class="total_trade_text">累计注册投资用户</div>
                 <div class="text_underLine_blue"></div>
-                <div class="total_trade_count"><span id="total_trade_count"></span>人</div>
+                <div class="total_trade_count"><span id="total_trade_person"></span>人</div>
                 <div id="main_part3"></div>
                 <div id="main_part4"></div>
                 <div class="button_under_arrow side_to_page" data-index="3"></div>
             </div>
 
-
+            <div class="swiper-item swiper-slide4 swiper-slide">
+                <div class="part-4-topBar"><div class="user-area">平台地区统计</div></div>
+                <div class="count_proportion">
+                    <div class="symbol_line"></div>
+                    <div class="count_title">投资人数<span class="title_strong">TOP3占比</span></div>
+                </div>
+                <div id="main_part5"></div>
+                <div class="amount_proportion">
+                    <div class="symbol_line"></div>
+                    <div class="amount_title">投资金额<span class="title_strong">TOP3占比</span></div>
+                </div>
+                <div id="main_part6"></div>
+                <div class="button_under_arrow side_to_page" data-index="4"></div>
+            </div>
 
             <div class="swiper-item swiper-slide5 swiper-slide">
                 <div class="tuotian_logo"></div>
