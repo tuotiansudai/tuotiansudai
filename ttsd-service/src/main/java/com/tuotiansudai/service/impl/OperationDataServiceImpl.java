37988849f6bbd91097643c31a8c30e6bc19bb5b5
zhanglong888 <zhanglong@tuotiansudai.com>
2018-01-11 10:45:50 +0800

    fix bug

--- ttsd-service/src/main/java/com/tuotiansudai/service/impl/OperationDataServiceImpl.java
+++ ttsd-service/src/main/java/com/tuotiansudai/service/impl/OperationDataServiceImpl.java
@@ -103,8 +103,10 @@ public class OperationDataServiceImpl implements OperationDataService {
         operationDataDto.setUsersCount(userMapper.findUsersCount());
 
         List<Integer> sexList = findScaleByGender(new Date());
-        operationDataDto.setFemaleScale(String.valueOf(CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
-        operationDataDto.setMaleScale(String.valueOf(100 - CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
+        if (sexList.size() > 0) {
+            operationDataDto.setFemaleScale(String.valueOf(CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
+            operationDataDto.setMaleScale(String.valueOf(100 - CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
+        }
 
         operationDataDto.setTotalInterest(String.valueOf(findUserSumInterest(new Date())));
         operationDataDto.setAgeDistribution(convertMapToOperationDataAgeDataDto());
@@ -142,8 +144,10 @@ public class OperationDataServiceImpl implements OperationDataService {
         operationDataDto.setInvestCityScaleTop3(convertMapToOperationDataInvestCityDataDto());
         operationDataDto.setInvestAmountScaleTop3(convertMapToOperationDataInvestAmountDataDto());
         List<Integer> sexList = findScaleByGender(new Date());
-        operationDataDto.setFemaleScale(String.valueOf(CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
-        operationDataDto.setMaleScale(String.valueOf(100 - CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
+        if (sexList.size() > 0) {
+            operationDataDto.setFemaleScale(String.valueOf(CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
+            operationDataDto.setMaleScale(String.valueOf(100 - CalculateUtil.calculatePercentage(sexList.get(0), sexList.get(0) + sexList.get(1), 1)));
+        }
 
     }
 
