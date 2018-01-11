d6cf1e6fa77301ebe0a7e73945fbd7ed1a5749b9
zhaozhen <zhaozhen@tuotiansudai.com>
2018-01-11 10:22:04 +0800

    styles

--- ttsd-frontend-manage/resources/static/m-web/js/operational.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/operational.jsx
@@ -234,6 +234,12 @@ commonFun.useAjax({  // 拉取页面数据
     getPartFourPage(data);
 
 }, () => {
+    let data = {"operationDays":922,"usersCount":11522,"totalInterest":"108992411","maleScale":"0.7","femaleScale":"0.3","month":["2015.7","2015.8","2015.9","2015.10","2015.11","2015.12","2016.1","2016.2","2016.3","2016.4","2016.5","2016.6","2016.7","2016.8","2016.9","2016.10","2016.11","2016.12","2017.1","2017.2","2017.3","2017.4","2017.5","2017.6","2017.7","2017.8","2017.9","2017.10","2017.11","2017.12"],"money":["0.00","0.00","0.00","0.00","246.00","828.00","718.00","354.00","139.00","156.00","398.00","513.68","56.11","1066.97","1056.37","2000012.51","48.34","133.50","620411.32","21.00","172.44","100033.00","301018.00","200.00","888017.00","9640025.00","4491098.00","20204064.00","17561894.00","0.00"],"ageDistribution":[{"name":"20岁以下","scale":"2.3"},{"name":"20~30岁","scale":"37.5"},{"name":"30~40岁","scale":"42.0"},{"name":"40~50岁","scale":"15.9"},{"name":"50岁以上","scale":"2.3"}],"investCityScaleTop3":[{"city":"北京","scale":"52.2"},{"city":"南宁","scale":"31.1"},{"city":"成都","scale":"5.6"}],"investAmountScaleTop3":[{"city":"北京","scale":"50"},{"city":"南宁","scale":"30"},{"city":"成都","scale":"20"}],"tradeAmount":"55812680.24"}
+    let dataStr = data.operationDays.toString();
+    getPartOnePage(data,dataStr);
+    getPartTwoPage(data);
+    getPartThreePage(data);
+    getPartFourPage(data);
 
 });
 
@@ -266,14 +272,6 @@ let getPartOnePage = (data,dataStr) => {
     $('.safe_day_wrapper').prepend(dom);
     $('#grand_total_amount').html(formatNumber(data.tradeAmount,2));
     $('#earn_total_amount').html(formatNumber(data.totalInterest,2));
-
-    let myDate = new Date();
-    let currentMonth = myDate.getMonth() + 1;
-    let currentDay = myDate.getDate();
-    currentMonth = currentMonth < 10 ? '0' + currentMonth : currentMonth;
-    currentDay = currentDay < 10 ? '0' + currentDay : currentDay;
-    $('.currentMonth').html(currentMonth);
-    $('.currentDay').html(currentDay);
 };
 
 let getPartTwoPage = (data) => {
