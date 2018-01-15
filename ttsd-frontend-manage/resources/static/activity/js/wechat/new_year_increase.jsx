6cf4f547e0d12f7d864627ecc03872815ef239b9
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2018-01-15 18:02:23 +0800

    click btn to get coupon

--- ttsd-frontend-manage/resources/static/activity/js/wechat/new_year_increase.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/wechat/new_year_increase.jsx
@@ -1,4 +1,5 @@
 require('activityStyle/wechat/new_year_increase.scss');
+let commonFun = require('publicJs/commonFun');
 
 let $newYearIncrease = $('#newYearIncrease'),
     $toGet = $('#toGet'),
@@ -6,12 +7,20 @@ let $newYearIncrease = $('#newYearIncrease'),
 
 //领取加息券
 $toGet.on('click',function() {
-    location.href='/activity/celebration-coupon/draw';
-});
-window.onload = function() {
     let drew = $newYearIncrease.data('drew');
     if(drew) {
         layer.msg('每个用户只能领取一次哦！');
         $toGet.prop('disabled',true);
     }
-}
+    commonFun.useAjax({
+        url:'/activity/celebration-coupon/draw',
+
+
+    },
+        function (draw) {
+            if(draw) {
+                layer.msg('领取成功');
+            }
+        }
+        )
+});
