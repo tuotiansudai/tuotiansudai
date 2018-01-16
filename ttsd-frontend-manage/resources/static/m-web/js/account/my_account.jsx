2cc048883028043a21365e2a3f89ec25a25e716f
zhangzhigang <zhangzhigang@tuotiansudai.com>
2018-01-16 19:11:25 +0800

    complete my-account page on msite

--- ttsd-frontend-manage/resources/static/m-web/js/account/my_account.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/account/my_account.jsx
@@ -2,10 +2,3 @@ require('mWebStyle/account/my_account.scss');
 
 let $myAccount = $('#myAccount'),
     $summaryBox = $('.summary-box',$myAccount);
-
-$summaryBox.on('click',function() {
-    let $this = $(this);
-
-    let key = $this.data('key');
-    location.href = 'overview.ftl?key='+key;
-});
