c30414cde3cbdc829075dce9410ae2e8d72c9582
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2018-01-18 14:38:09 +0800

    add scroll list

--- ttsd-frontend-manage/resources/static/m-web/js/account/investor_invest_list.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/account/investor_invest_list.jsx
@@ -6,18 +6,13 @@ let menuClick = require('mWebJsModule/menuClick');
 let $myInvest = $('#myInvest'),
     $iconMyInvest = $('#iconMyInvest',$myInvest),
     $repayingBtn = $('#repayingBtn',$myInvest),
-    $rasingBtn = $('#rasingBtn',$myInvest),
+    $raisingBtn = $('#raisingBtn',$myInvest),
     $investListBox = $('.invest-list-box',$myInvest);
 
 menuClick({
     pageDom: $myInvest
 });
 
-let myScroll = new IScroll('#wrapperOut', {
-    probeType: 2,
-    mouseWheel: true
-});
-//$('#investList').html(tpl('investListTpl', data));
 $iconMyInvest.on('click',function () {
     location.href='/m'
 })
@@ -31,16 +26,17 @@ function getList(index,status) {
             }
         },
         function (res) {
+            console.log('success')
             console.log(res)
             if(res.success == true){
                 if(res.data.records.length){
-                  //  $investListBox.html(tpl('investListTpl', res.data));
+                   $investListBox.html(tpl('investListTpl', res.data));
                 }else {
                     if(index == 1){
                         let $noListDOM = $('<div class="noList"><div class="img"></div><button>立即投资</button>');
                         $investListBox.append($noListDOM);
                     }else {
-                        console.log('没有数据了')
+                        $investListBox.append('<p>没有更多数据了</p>');
                     }
 
 
@@ -51,8 +47,32 @@ function getList(index,status) {
 }
 let index = 1;
 getList(1,'REPAYING');
+setTimeout(function () {
+       let myScroll = new IScroll('#wrapperOut', {
+           probeType: 2,
+           mouseWheel: true
+       });
+    myScroll.on('scrollEnd', function () {
+
+        index++;
+        //如果滑动到底部，则加载更多数据（距离最底部10px高度）
+        if ((this.y - this.maxScrollY) <= 10) {
+            if($repayingBtn.hasClass('current')){
+                getList(2,'REPAYING')
+            }else if($raisingBtn.hasClass('current')){
+                getList(2,'RAISING')
+            }
+        }
+    });
+},1000)
+
 $repayingBtn.on('click',function () {
+    $investListBox.html('');
     getList(1,'REPAYING')
+})
+$raisingBtn.on('click',function () {
+    $investListBox.html('');
+    getList(1,'RAISING')
 })
 
 $(document).on('click','#toInvest',function () {
