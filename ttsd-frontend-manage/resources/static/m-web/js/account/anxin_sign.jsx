0cfd5c01c2c15614b1e20356ea9f38175a84d7db
zhaozhen <zhaozhen@tuotiansudai.com>
2018-01-25 16:39:08 +0800

    anxinSign

--- ttsd-frontend-manage/resources/static/m-web/js/account/anxin_sign.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/account/anxin_sign.jsx
@@ -1,2 +1,193 @@
 require('mWebStyle/account/anxin_sign.scss');
+require('mWebJsModule/anxin_agreement_pop');
+let commonFun= require('publicJs/commonFun');
+require('publicJs/honeySwitch');
+require('publicStyle/honeySwitch.scss');
+let $anxinElectroSign = $('#anxinElectroSign');
+let $signature = $('#signature');
+let $openMessage = $('#openMessage');
+let $authorization_message = $('#authorization_message');
+let isAnxinUser=$('.bind-data').data('is-anxin-user');
+let switchStatus = $('#switchStatus').data('skip-auth');
 
+commonFun.calculationFun(document,window);
+
+if (!isAnxinUser) {
+    $signature.show();
+}
+else {
+    $openMessage.show();
+}
+
+/*
+* 安心签电子签章服务
+*/
+$('#authentication_identity').on('click',() => {  // 未实名认证则进行跳转
+    location.href = './register/account';
+});
+
+$('.init-checkbox-style',$anxinElectroSign).initCheckbox(function(element) {
+    var $parentBox=$(element).parents('.safety-status-box');
+    //点击我已阅读并同意是否disable按钮
+    $(element).hasClass('on');
+});
+
+// 开启安心签服务
+$('#openSafetySigned').on('click',function() {
+    var $this=$(this);
+    $this.prop('disabled',true);
+    commonFun.useAjax({
+        type:'POST',
+        url:'/anxinSign/createAccount'
+    },function(response) {
+        $this.prop('disabled',false);
+
+        if(!response.success){
+            layer.msg('开启失败');
+        } else {
+            layer.msg('开启成功！');
+            $signature.hide();
+            $openMessage.show();
+        }
+    });
+});
+
+// 点击返回btn
+$('.go-back-container').on('click',() => {
+    history.go(-1);
+});
+
+$('.init-checkbox-style').initCheckbox(function(element) {
+    //点击我已阅读并同意是否disable按钮
+    var isCheck=$(element).hasClass('on');
+    var $btnNormal;
+    if ($('#signature').css('display') == 'block') {
+        $btnNormal=$('#openSafetySigned');
+    }
+    else {
+        $btnNormal=$('#toOpenSMS');
+    }
+
+    if(isCheck) {
+        $btnNormal.prop('disabled',false);
+    }
+    else {
+        $btnNormal.prop('disabled',true);
+    }
+});
+
+$('#openAuthorization').on('click',function() {
+    $('#openMessage').hide();
+    $('#authorization_message').show();
+});
+
+/*
+* 短信授权服务
+*/
+
+let $anxinAuthorization = $('#anxinAuthorization'),
+    $buttonIdentify = $('.button-identify',$anxinAuthorization);
+
+let $toOpenSMS = $('#toOpenSMS');
+
+//获取验证码
+$buttonIdentify.on('click',function (event) {
+    let target = event.target;
+    let isVoice = $(target).data('voice');
+    commonFun.useAjax({
+        type:'POST',
+        url:'/anxinSign/sendCaptcha',
+        data:{
+            isVoice:isVoice
+        }
+    },function(data) {
+        //请求成功开始倒计时
+        if(data.success) {
+            countDownTime();
+        }
+        else {
+            layer.msg('请求失败，请重试或联系客服！');
+        }
+    });
+});
+
+function countDownTime() {
+    let seconds = 60;
+    $('.seconds').html(seconds);
+    $('.button-identify').hide();
+    $('.countDownTime').show();
+    let countDown = setInterval(() => {
+        seconds--;
+        $('.seconds').html(seconds);
+        if (seconds == 0 ) {
+            clearInterval(countDown);
+            $('.button-identify').show();
+            $('.countDownTime').hide();
+        }
+    },1000)
+}
+//验证验证码并开通短信服务
+$('#skipPhoneCode').on('keyup',function() {
+
+    var $skipPhoneCode=$('#skipPhoneCode'),
+        phoneCode=$skipPhoneCode.val();
+
+    if(/^\d{6}$/.test(phoneCode) && $('#readOk1').is(':checked')) {
+        $toOpenSMS.prop('disabled',false);
+    } else {
+        $toOpenSMS.prop('disabled',true);
+    }
+});
+
+$toOpenSMS.on('click',function() {
+    var $this=$(this),
+        $skipPhoneCode=$('#skipPhoneCode'),
+        phoneCode=$skipPhoneCode.val();
+
+    if(!/^\d{6}$/.test(phoneCode)) {
+        $('.error').show();
+        return;
+    }
+
+    $this.prop('disabled',true);
+
+    commonFun.useAjax({
+        type:'POST',
+        url:'/anxinSign/verifyCaptcha',
+        data:{
+            captcha: phoneCode,
+            skipAuth:true
+        }
+    },function(data) {
+        if(data.success) {
+            layer.closeAll();
+            location.href='';
+        }
+        else {
+            $this.prop('disabled',false);
+            $('.error').show();
+        }
+    });
+});
+
+if (switchStatus) {  // 开关为开启状态
+    $('#switch').addClass('switch-on');
+}
+else {
+    $('#switch').addClass('switch-off'); // 开关为关闭状态
+}
+
+$('#goPage_1').on('click',() => {
+    history.go(-1);
+});
+
+$('#goPage_2').on('click',() => {
+    $('#openMessage').hide();
+    $('#authorization_message').hide();
+    $('#signature').show();
+});
+$('#goPage_3').on('click',() => {
+    $('#openMessage').show();
+    $('#authorization_message').hide();
+    $('#signature').hide();
+});
---
+++ ttsd-frontend-manage/resources/static/public/js/honeySwitch.jsx
@@ -0,0 +1,161 @@
+var honeySwitch = {};
+var commonFun= require('publicJs/commonFun');
+honeySwitch.themeColor = "rgb(100, 189, 99)";
+honeySwitch.init = function() {
+	var s = "<span class='slider'></span>";
+	$("[class^=switch]").append(s);
+	$("[class^=switch]").click(function() {
+		if ($(this).hasClass("switch-disabled")) {
+			return;
+		}
+		if ($(this).hasClass("switch-on")) {
+            $('.shade_mine').show();
+            let isOpen = false;
+            commonFun.CommonLayerTip({
+                btn: ['确认','取消'],
+                area:['280px', '160px'],
+                content: $('#freeSuccess'),
+                shade: false  // hack ios shade
+            },() => {
+                commonFun.useAjax({
+                    type:'POST',
+                    url:'/anxinSign/switchSkipAuth',
+                    data:{
+                        "open":isOpen
+                    }
+                },function(data) {
+                    if(data.success) {
+                        layer.closeAll();
+                        $('.shade_mine').hide();
+                        $(this).removeClass("switch-on").addClass("switch-off");
+                        $(".switch-off").css({
+                            'border-color' : '#dfdfdf',
+                            'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+                            'background-color' : 'rgb(255, 255, 255)'
+                        });
+                    }
+                    else {
+                        layer.closeAll();
+                        $('.shade_mine').hide();
+                        layer.msg('请求失败，请重试或联系客服！');
+                    }
+
+                });
+
+            },() => {
+                $('.shade_mine').hide(); // hack ios shade
+            });
+		} else {
+			let isOpen = true;
+            commonFun.useAjax({
+                type:'POST',
+                url:'/anxinSign/switchSkipAuth',
+                data:{
+                    "open":isOpen
+                }
+            },function(data) {
+                if(data.success) {
+                    layer.msg('已开启安心签免短信服务');
+                    $(this).removeClass("switch-off").addClass("switch-on");
+                    if (honeySwitch.themeColor) {
+                        var c = honeySwitch.themeColor;
+                        $(this).css({
+                            'border-color' : c,
+                            'box-shadow' : c + ' 0px 0px 0px 16px inset',
+                            'background-color' : c
+                        });
+                    }
+                    if ($(this).attr('themeColor')) {
+                        var c2 = $(this).attr('themeColor');
+                        $(this).css({
+                            'border-color' : c2,
+                            'box-shadow' : c2 + ' 0px 0px 0px 16px inset',
+                            'background-color' : c2
+                        });
+                    }
+                }
+                else {
+                    layer.msg('请求失败，请重试或联系客服！');
+                }
+
+            });
+		}
+	});
+	window.switchEvent = function(ele, on, off) {
+		$(ele).click(function() {
+			if ($(this).hasClass("switch-disabled")) {
+				return;
+			}
+			if ($(this).hasClass('switch-on')) {
+				if ( typeof on == 'function') {
+					on();
+				}
+			} else {
+				if ( typeof off == 'function') {
+					off();
+				}
+			}
+		});
+	}
+	if (this.themeColor) {
+		var c = this.themeColor;
+		$(".switch-on").css({
+			'border-color' : c,
+			'box-shadow' : c + ' 0px 0px 0px 16px inset',
+			'background-color' : c
+		});
+		$(".switch-off").css({
+			'border-color' : '#dfdfdf',
+			'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+			'background-color' : 'rgb(255, 255, 255)'
+		});
+	}
+	if ($('[themeColor]').length > 0) {
+		$('[themeColor]').each(function() {
+			var c = $(this).attr('themeColor') || honeySwitch.themeColor;
+			if ($(this).hasClass("switch-on")) {
+				$(this).css({
+					'border-color' : c,
+					'box-shadow' : c + ' 0px 0px 0px 16px inset',
+					'background-color' : c
+				});
+			} else {
+				$(".switch-off").css({
+					'border-color' : '#dfdfdf',
+					'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+					'background-color' : 'rgb(255, 255, 255)'
+				});
+			}
+		});
+	}
+};
+honeySwitch.showOn = function(ele) {
+	$(ele).removeClass("switch-off").addClass("switch-on");
+	if(honeySwitch.themeColor){
+		var c = honeySwitch.themeColor;
+		$(ele).css({
+			'border-color' : c,
+			'box-shadow' : c + ' 0px 0px 0px 16px inset',
+			'background-color' : c
+		});
+	}
+	if ($(ele).attr('themeColor')) {
+		var c2 = $(ele).attr('themeColor');
+		$(ele).css({
+			'border-color' : c2,
+			'box-shadow' : c2 + ' 0px 0px 0px 16px inset',
+			'background-color' : c2
+		});
+	}
+}
+honeySwitch.showOff = function(ele) {
+	$(ele).removeClass("switch-on").addClass("switch-off");
+	$(".switch-off").css({
+		'border-color' : '#dfdfdf',
+		'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+		'background-color' : 'rgb(255, 255, 255)'
+	});
+}
+$(function() {
+	honeySwitch.init();
+}); 
