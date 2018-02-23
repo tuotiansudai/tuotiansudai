dcbe9756eb09d6dd1abc7630a9068aa6bd009eff
zhukun5764 <zhukun@tuotiansudai.com>
2018-02-23 13:34:19 +0800

    update login

--- ttsd-frontend-manage/resources/static/activity/js/wechat/start_work.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/wechat/start_work.jsx
@@ -51,14 +51,14 @@ $('.get_it_btn').on('click',function () {
             layer.msg('活动已结束');
         }
         else {
-            $.when(commonFun.isUserLogin())
-                .done(function () {
-                    location.href = '/activity/start-work/draw';
-                })
-                .fail(function () {
-                    ifClickBtn = false;
-                    toLogin();
-            })
+            location.href = '/activity/start-work/draw';
+            // $.when(commonFun.isUserLogin())
+            //     .done(function () {
+            //     })
+            //     .fail(function () {
+            //         ifClickBtn = false;
+            //         toLogin();
+            // })
         }
     }
 
@@ -72,107 +72,107 @@ $('.see_my_redPocket').on('click',function () {
    location.href = '/my-treasure';
 });
 
-//去登录
-function toLogin() {
-    if (sourceKind.params.source == 'app') {
-        location.href = "/login";
-    }else {
-        layer.open({
-            type: 1,
-            title: false,
-            closeBtn: 0,
-            area: ['auto', 'auto'],
-            content: $('#loginTip')
-        });
-    }
-}
-
-
-require.ensure([],function() {
-    let commonFun= require('publicJs/commonFun');
-    let ValidatorObj=require('publicJs/validator');
-    var $loginTipBox=$('#loginTip');
-    let loginInForm = document.getElementById('loginInForm');
-    let errorDom=$(loginInForm).find('.error-box');
-    require("publicStyle/module/login_tip.scss");
-    //刷新验证码
-    $('#imageCaptcha').on('click',function() {
-        commonFun.refreshCaptcha(this, "/login/captcha");
-        loginInForm.captcha.value='';
-    });
-
-    commonFun.refreshCaptcha(globalFun.$('#imageCaptcha'), "/login/captcha", false);
-
-    $('.close-btn',$loginTipBox).on('click',function(event) {
-        event.preventDefault();
-        layer.closeAll();
-    });
-//验证表单
-    let validator = new ValidatorObj.ValidatorForm();
-    validator.add(loginInForm.username, [{
-        strategy: 'isNonEmpty',
-        errorMsg: '用户名不能为空'
-    }]);
-
-    validator.add(loginInForm.password, [{
-        strategy: 'isNonEmpty',
-        errorMsg: '密码不能为空'
-    }, {
-        strategy: 'checkPassword',
-        errorMsg: '密码为6位至20位，不能全是数字'
-    }]);
-    validator.add(loginInForm.captcha, [{
-        strategy: 'isNonEmpty',
-        errorMsg: '验证码不能为空'
-    }]);
-
-    let reInputs=$(loginInForm).find('input:text,input:password');
-
-    Array.prototype.forEach.call(reInputs,function(el) {
-        globalFun.addEventHandler(el,'blur',function() {
-            let errorMsg = validator.start(this);
-            if(errorMsg) {
-                errorDom.text(errorMsg);
-            }
-            else {
-                errorDom.text('');
-            }
-        });
-    });
-
-    loginInForm.onsubmit = function(event) {
-        event.preventDefault();
-        let thisButton=$(event.target).find(':submit')[0];
-        let errorMsg;
-        for(let i=0,len=reInputs.length;i<len;i++) {
-            errorMsg = validator.start(reInputs[i]);
-            if(errorMsg) {
-                errorDom.text(errorMsg);
-                return;
-            }
-        }
-        if (!errorMsg) {
-            globalFun.addClass(thisButton,'loading');
-            //弹框登陆为ajax的form表单提交
-            var dataParam = $(loginInForm).serialize();
-            commonFun.useAjax({
-                url:'/login',
-                type:'POST',
-                data:dataParam
-            },function(data) {
-                if (data.status) {
-                    window.location.reload();
-                } else {
-                    commonFun.refreshCaptcha(globalFun.$('#imageCaptcha'), "/login/captcha");
-                    globalFun.removeClass(thisButton,'loading');
-                    errorDom.text(data.message);
-                }
-            });
-
-        }
-    };
-
-},'login_tip');
+// //去登录
+// function toLogin() {
+//     if (sourceKind.params.source == 'app') {
+//         location.href = "/login";
+//     }else {
+//         layer.open({
+//             type: 1,
+//             title: false,
+//             closeBtn: 0,
+//             area: ['auto', 'auto'],
+//             content: $('#loginTip')
+//         });
+//     }
+// }
+//
+//
+// require.ensure([],function() {
+//     let commonFun= require('publicJs/commonFun');
+//     let ValidatorObj=require('publicJs/validator');
+//     var $loginTipBox=$('#loginTip');
+//     let loginInForm = document.getElementById('loginInForm');
+//     let errorDom=$(loginInForm).find('.error-box');
+//     require("publicStyle/module/login_tip.scss");
+//     //刷新验证码
+//     $('#imageCaptcha').on('click',function() {
+//         commonFun.refreshCaptcha(this, "/login/captcha");
+//         loginInForm.captcha.value='';
+//     });
+//
+//     commonFun.refreshCaptcha(globalFun.$('#imageCaptcha'), "/login/captcha", false);
+//
+//     $('.close-btn',$loginTipBox).on('click',function(event) {
+//         event.preventDefault();
+//         layer.closeAll();
+//     });
+// //验证表单
+//     let validator = new ValidatorObj.ValidatorForm();
+//     validator.add(loginInForm.username, [{
+//         strategy: 'isNonEmpty',
+//         errorMsg: '用户名不能为空'
+//     }]);
+//
+//     validator.add(loginInForm.password, [{
+//         strategy: 'isNonEmpty',
+//         errorMsg: '密码不能为空'
+//     }, {
+//         strategy: 'checkPassword',
+//         errorMsg: '密码为6位至20位，不能全是数字'
+//     }]);
+//     validator.add(loginInForm.captcha, [{
+//         strategy: 'isNonEmpty',
+//         errorMsg: '验证码不能为空'
+//     }]);
+//
+//     let reInputs=$(loginInForm).find('input:text,input:password');
+//
+//     Array.prototype.forEach.call(reInputs,function(el) {
+//         globalFun.addEventHandler(el,'blur',function() {
+//             let errorMsg = validator.start(this);
+//             if(errorMsg) {
+//                 errorDom.text(errorMsg);
+//             }
+//             else {
+//                 errorDom.text('');
+//             }
+//         });
+//     });
+//
+//     loginInForm.onsubmit = function(event) {
+//         event.preventDefault();
+//         let thisButton=$(event.target).find(':submit')[0];
+//         let errorMsg;
+//         for(let i=0,len=reInputs.length;i<len;i++) {
+//             errorMsg = validator.start(reInputs[i]);
+//             if(errorMsg) {
+//                 errorDom.text(errorMsg);
+//                 return;
+//             }
+//         }
+//         if (!errorMsg) {
+//             globalFun.addClass(thisButton,'loading');
+//             //弹框登陆为ajax的form表单提交
+//             var dataParam = $(loginInForm).serialize();
+//             commonFun.useAjax({
+//                 url:'/login',
+//                 type:'POST',
+//                 data:dataParam
+//             },function(data) {
+//                 if (data.status) {
+//                     window.location.reload();
+//                 } else {
+//                     commonFun.refreshCaptcha(globalFun.$('#imageCaptcha'), "/login/captcha");
+//                     globalFun.removeClass(thisButton,'loading');
+//                     errorDom.text(data.message);
+//                 }
+//             });
+//
+//         }
+//     };
+//
+// },'login_tip');
 
 
 
