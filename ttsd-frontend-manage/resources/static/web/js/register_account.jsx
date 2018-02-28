b04070a9f7a9746fe4ef5748cf449c09258291c7
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2018-02-28 20:05:27 +0800

    add info

--- ttsd-frontend-manage/resources/static/web/js/register_account.jsx
+++ ttsd-frontend-manage/resources/static/web/js/register_account.jsx
@@ -88,6 +88,9 @@ registerAccountForm.onsubmit = function (event) {
 if($('#registerSuccess').length){
     let referParam = globalFun.parseURL(location.href);
     let referrer = referParam.params.referrer;
+    if(referrer &&referrer == 'loan'){
+        $('.see_other_project').text('去借款');
+    }
 alert(referrer);
     $('.see_other_project').click(function () {
         if(referrer &&referrer == 'loan'){
