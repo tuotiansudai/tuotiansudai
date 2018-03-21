b89ee83bd4135c29b85bbd774e258d0a9d60b15d
zhukun5764 <zhukun@tuotiansudai.com>
2018-03-21 13:59:16 +0800

    jdbc config add connectionInitSql

--- ttsd-edxask-repository/src/main/java/com/tuotiansudai/ask/repository/mapper/AnswerMapper.java
+++ ttsd-edxask-repository/src/main/java/com/tuotiansudai/ask/repository/mapper/AnswerMapper.java
@@ -10,8 +10,6 @@ import java.util.List;
 @Repository
 public interface AnswerMapper {
 
-    void initCharset();
-
     long create(AnswerModel answerModel);
 
     long update(AnswerModel answerModel);
