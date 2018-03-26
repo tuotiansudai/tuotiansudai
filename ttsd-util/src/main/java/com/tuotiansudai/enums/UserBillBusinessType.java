8098e2c898d3e33afdef58fca37488b4c752ce94
Xiduan Gao <gaoxiduan@gmail.com>
2018-03-26 12:25:53 +0800

    change luxury purchase desc

--- ttsd-util/src/main/java/com/tuotiansudai/enums/UserBillBusinessType.java
+++ ttsd-util/src/main/java/com/tuotiansudai/enums/UserBillBusinessType.java
@@ -37,7 +37,7 @@ public enum UserBillBusinessType {
     CREDIT_LOAN_RECHARGE("信用贷账户充值"),
     CREDIT_LOAN_OUT("信用贷放款"),
     CREDIT_LOAN_REPAY("信用贷还款"),
-    LUXURY_STAGE_PURCHASE("分期首付"),
+    LUXURY_STAGE_PURCHASE("首付+第一期月供"),
     LUXURY_STAGE_REPAY("分期还款"),
     PAYROLL("代发工资"),
     ;
