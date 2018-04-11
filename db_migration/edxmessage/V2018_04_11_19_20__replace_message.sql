1d205fbe78350cb17b819ee823211a36209058af
Xiduan Gao <gaoxiduan@gmail.com>
2018-04-11 20:11:25 +0800

    fixed sql

--- db_migration/edxmessage/V2018_04_11_19_20__replace_message.sql
+++ db_migration/edxmessage/V2018_04_11_19_20__replace_message.sql
@@ -1,6 +1,5 @@
 update message set
 title = replace(title, '预期年化收益', '约定年化收益'),
-app_title = replace(app_title, '预期年化收益', '约定年化收益'),
 template = replace(template, '预期年化收益', '约定年化收益'),
 template_txt = replace(template_txt, '预期年化收益', '约定年化收益');
 
