37341a90ca5c17ee29579ec15cef464ffbf10d40
zhaozhen <zhaozhen@tuotiansudai.com>
2018-04-16 19:32:00 +0800

    styles  wechat pages

--- ttsd-frontend-manage/resources/static/activity/js/wechat/rebate_station_coupons.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/wechat/rebate_station_coupons.jsx
@@ -2,104 +2,39 @@ require('activityStyle/wechat/rebate_station_coupons.scss');
 let commonFun = require('publicJs/commonFun');
 let sourceKind = globalFun.parseURL(location.href);
 commonFun.calculationFun(document,window);
-let Cycle = commonFun.Cycle;
-let timer;
 
-setTimeout(function () {
-    let fontSize = $('html').css('fontSize');
-    let opts2 = {
-        id: "cycle",
-        width: 24 * parseInt(fontSize),
-        height: 24 * parseInt(fontSize),
-        percent: "66.4",
-        border: parseInt(fontSize) / 2,
-        bgColor: "rgba(187,185,176,.7)",
-        barColor: "#e7051c",
-        fillColor: "rgba(251,233,197,.47)"
-    };
-    new Cycle(opts2).init();
-    $('canvas').css('width',12 * parseInt(fontSize) + 'px');
-    $('canvas').css('height',12 * parseInt(fontSize) + 'px');
-    function getPercentLight() {
-        let currentPer = 1000;
-        let percentArr= [100,200,300,400,500,900];
-        for (let i = 0;i < percentArr.length;i++) {
-            let item = percentArr[i];
-            if (currentPer < item) {
-                let index = percentArr.indexOf(item) - 1;
-                // $('.light_line').css('width',16.6 * index + '%');
-                for(let j = 1; j < index + 2;j++) {
-                    let lightCircle = 'percent' + j;
-                    $('.' + lightCircle).addClass('light');
-                }
-                return;
-            }
-            else {
-                if (currentPer > percentArr[percentArr.length - 1]) {
-                    // $('.light_line').css('width','100%');
-                    for(let j = 1; j < 7;j++) {
-                        let lightCircle = 'percent' + j;
-                        $('.' + lightCircle).addClass('light');
-                    }
-                }
-            }
-        }
-    }
 
-    getPercentLight();
-},0);
+let isGet = $('.container').data('get');
+let isSuc = $('.container').data('success');
+let startTime = $('.container').data('startTime');
+let overTime = $('.container').data('overTime');
+let activityTime = new Date(startTime.replace(/-/g, "/")).getTime(); // 活动开始时间
+let activityOverTime = new Date(overTime.replace(/-/g, "/")).getTime();  // 活动结束时间
 
-$('.rules').on('click',() => {
-    $('.flex_rules').show();
-});
+!isGet ?  $('.handle_btn').addClass('get_it_now') :  $('.handle_btn').addClass('invest_btn');
 
-$('.close_rules').on('click',() => {
-    $('.flex_rules').hide();
-});
-
-function countTimePop(str) {
-    $('.pic_wrapper').show();
-    $('.time_over').hide();
-    let end = new Date(str).getTime();
-    let now = new Date().getTime();
-    let leftTime = (end-now)/1000;
-    timerCount();
-    timer = setInterval(() => {
-        timerCount();
-    },1000);
-    function timerCount() {
-        let h,m,s;
-        if (leftTime > 0) {
-            if (leftTime <= 1) {
-                clearInterval(timer);
-                $('.pic_wrapper').hide();
-                $('.time_over').show();
-                return;
-            }
-            h = Math.floor(leftTime/60/60%24);
-            m = Math.floor(leftTime/60%60);
-            s = Math.floor(leftTime%60);
-            h = h < '10' ? '0' + h : h + '';
-            m = m < '10' ? '0' + m : m + '';
-            s = s < '10' ? '0' + s : s + '';
-
-            $('.hour1').html(h.charAt(0));
-            $('.hour2').html(h.charAt(1));
-            $('.minutes1').html(m.charAt(0));
-            $('.minutes2').html(m.charAt(1));
-            $('.seconds1').html(s.charAt(0));
-            $('.seconds2').html(s.charAt(1));
-            --leftTime;
-        }
-        else {
-            clearInterval(timer);
-            $('.pic_wrapper').hide();
-            $('.time_over').show();
-        }
-    }
+if (isSuc) {
+    $('.tip_text').show();
 }
 
-countTimePop('2018-4-16 12:21:30');
+$('.get_it_now').on('click',function () {
+    let currentTime = new Date().getTime();
+    if (currentTime < activityTime) {
+        layer.msg('活动未开始');
+    }
+    else if (currentTime > activityOverTime) {
+        layer.msg('活动已结束');
+    }
+    else {
+        layer.msg('领取成功');
+        //location.href = '/activity/start-work/draw';
+    }
+
+});
+
+$('.invest_btn').on('click',function () {
+    location.href = '/m/loan-list';
+});
 
 
 
