95fdfd317d33378dc6bdd89cdd0e44a8e3b4a364
zhukun5764 <zhukun@tuotiansudai.com>
2018-05-02 21:52:10 +0800

    fix sql

--- db_migration/edxask/V2018_05_02_22_00__again_update_manage_money_matters_to_invest.sql
+++ db_migration/edxask/V2018_05_02_22_00__again_update_manage_money_matters_to_invest.sql
@@ -1,5 +1,6 @@
 BEGIN;
 UPDATE answer set answer = replace(answer, '理财', '投资');
+UPDATE question set question = replace(question, '投资理财', '投资'), addition = replace(addition, '投资理财', '投资');
 UPDATE question set question = replace(question, '理财', '投资'), addition = replace(addition, '理财', '投资');
 COMMIT;
 
