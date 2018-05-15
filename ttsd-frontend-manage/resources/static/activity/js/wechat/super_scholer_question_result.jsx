1dcb0b831dbdec8f838c3fa6f835347260d3b89a
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2018-05-15 18:30:34 +0800

    add shareurl to friend circle

--- ttsd-frontend-manage/resources/static/activity/js/wechat/super_scholer_question_result.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/wechat/super_scholer_question_result.jsx
@@ -53,6 +53,14 @@ wx.ready(function () {
         imgUrl: '${commonStaticServer}/images/icons/share_redPocket.png', // 分享图标
         success: function () {
             // 用户确认分享后执行的回调函数
+            commonFun.useAjax({
+                dataType: 'json',
+                url: '/activity/super-scholar/share/success',
+                type: 'post'
+
+            }, function (response) {
+
+            })
         },
         cancel: function () {
             // 用户取消分享后执行的回调函数
