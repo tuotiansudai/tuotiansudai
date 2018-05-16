9495884fb96a36692c4da9171fcb108c0c732f06
zhukun5764 <zhukun@tuotiansudai.com>
2018-05-16 10:42:10 +0800

    fix share wechat

--- ttsd-frontend-manage/resources/static/activity/js/wechat/super_scholer_question_result.jsx
+++ ttsd-frontend-manage/resources/static/activity/js/wechat/super_scholer_question_result.jsx
@@ -26,5 +26,55 @@ $inviteFriend.on('click',function () {
     location.href = '/activity/super-scholar/view/result?shareType=activityRegister';
 });
 
+var link = webService + '/activity/super-scholar?come=wechat';
+
+if (shareType === 'activityHome'){
+    link = webService + '/activity/super-scholar?come=wechat'
+}else{
+    link = webService + '/activity/super-scholar/share/register?referrerMobile=' + mobile + '&come=wechat'
+}
+
+wx.ready(function () {
+    wx.onMenuShareAppMessage({
+        title: '我在拓天速贷答题赢加薪，邀请你来测一测学霸指数', // 分享标题
+        desc: '你是学霸还是学渣？答题见分晓！', // 分享描述
+        link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: commonStaticService + '/images/2018/super-scholar/icon_red_ware.png', // 分享图标
+        success: function () {
+            commonFun.useAjax({
+                dataType: 'json',
+                url: webService + '/activity/super-scholar/share/success',
+                type: 'post'
+
+            }, function (response) {
+
+            })
+        },
+        cancel: function () {
+        }
+    });
+
+    wx.onMenuShareTimeline({
+        title: '我在拓天速贷答题赢加薪，邀请你来测一测学霸指数', // 分享标题
+        desc: '你是学霸还是学渣？答题见分晓！', // 分享描述
+        link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: commonStaticService + '/images//2018/super-scholar/icon_red_ware.png', // 分享图标
+        success: function () {
+            // 用户确认分享后执行的回调函数
+            commonFun.useAjax({
+                dataType: 'json',
+                url:  webService + '/activity/super-scholar/share/success',
+                type: 'post'
+
+            }, function (response) {
+
+            })
+        },
+        cancel: function () {
+            // 用户取消分享后执行的回调函数
+        }
+    });
+});
+
 
 
