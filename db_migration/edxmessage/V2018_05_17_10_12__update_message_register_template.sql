3694b7c76c4e0f4d73fe46d56e3b49e18ab18334
zhukun5764 <zhukun@tuotiansudai.com>
2018-05-18 12:24:14 +0800

    update register message sql

--- db_migration/edxmessage/V2018_05_17_10_12__update_message_register_template.sql
+++ db_migration/edxmessage/V2018_05_17_10_12__update_message_register_template.sql
@@ -1,4 +1,4 @@
 update message set
 template = '哇，您终于来啦！初次见面，岂能无礼？6888元体验金及1000元投资红包双手奉上，快去投资吧！',
-template_text = '哇，您终于来啦！初次见面，岂能无礼？6888元体验金及1000元投资红包双手奉上，快去投资吧！'
+template_txt = '哇，您终于来啦！初次见面，岂能无礼？6888元体验金及1000元投资红包双手奉上，快去投资吧！'
 where event_type = 'REGISTER_USER_SUCCESS'
