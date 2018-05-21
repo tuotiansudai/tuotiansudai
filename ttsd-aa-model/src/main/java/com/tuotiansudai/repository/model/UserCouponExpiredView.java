b93a0eede9558888f0a131da2cf934c611a9489b
zhukun5764 <zhukun@tuotiansudai.com>
2018-05-21 17:24:32 +0800

    add register sms template

---
+++ ttsd-aa-model/src/main/java/com/tuotiansudai/repository/model/UserCouponExpiredView.java
@@ -0,0 +1,7 @@
+package com.tuotiansudai.repository.model;
+
+/**
+ * Created by zhukun on 18/5/21.
+ */
+public class UserCouponExpiredView {
+}
---
+++ ttsd-client/src/main/java/com/tuotiansudai/dto/sms/SmsRegisterSuccessNotifyDto.java
@@ -0,0 +1,7 @@
+package com.tuotiansudai.dto.sms;
+
+/**
+ * Created by zhukun on 18/5/21.
+ */
+public class SmsRegisterSuccessNotifyDto {
+}
