f7763dd1dbd1bd016dfb0454fd3f0a1208174dbe
Xiduan Gao <gaoxiduan@gmail.com>
2018-06-01 11:38:35 +0800

    update fudian tables

--- db_migration/fudian/V2018_04_25_12_15__create_loan_repay_table.sql
+++ db_migration/fudian/V2018_04_25_12_15__create_loan_repay_table.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `fudian`.`loan_repay` (
   `id`                   BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `request_data`         TEXT,
-  `merchant_no`          VARCHAR(32),
-  `user_name`            VARCHAR(50),
+  `merchant_no`          VARCHAR(50),
+  `user_name`            VARCHAR(32),
   `account_no`           VARCHAR(50),
   `capital`              VARCHAR(20),
   `interest`             VARCHAR(20),
   `loan_fee`             VARCHAR(20),
   `loan_tx_no`           VARCHAR(32),
-  `order_no`             VARCHAR(40),
+  `order_no`             VARCHAR(20),
   `order_date`           VARCHAR(8),
-  `ext_mark`             VARCHAR(512),
+  `ext_mark`             VARCHAR(256),
   `return_url`           VARCHAR(256),
   `notify_url`           VARCHAR(256),
   `return_response_data` TEXT,
@@ -21,6 +21,7 @@ CREATE TABLE `fudian`.`loan_repay` (
   `request_time`         DATETIME,
   `response_time`        DATETIME,
   `query_time`           DATETIME,
+  `status`               VARCHAR(100),
   PRIMARY KEY (`id`),
   KEY `INDEX_LOAN_REPAY_ORDER_NO` (`order_no`)
 )
