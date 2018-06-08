598ca821a447290aea325ba11a145006f171cd0e
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2018-06-08 11:26:13 +0800

    fix style

--- ttsd-frontend-manage/resources/static/m-web/js/bank_callback.jsx
+++ ttsd-frontend-manage/resources/static/m-web/js/bank_callback.jsx
@@ -1,22 +1,6 @@
 require('mWebStyle/bank_callback.scss');
-
+let commonFun= require('publicJs/commonFun');
 if($('.loading-page').length){
-    var arrImg = [];
-    for(let i = 0; i < 26; i++) {
-        arrImg.push(require('../images/gif/gif' +(i+1) + '.png'))
-
-    }
-    var count = 0;
-
-    // var timer = setInterval(function () {
-    //     if(count == 26){
-    //         count = 0;
-    //     }
-    //     count++;
-    //     $('#gifImg').attr('src',require('../images/gif/gif' +count + '.png'));
-    //
-    // },100);
-
     let $countTime = $('#secondsCountDown');
 
     commonFun.countDownLoan({
--- ttsd-web/src/main/webapp/templates/pay_m.ftl
+++ ttsd-web/src/main/webapp/templates/pay_m.ftl
@@ -2,10 +2,9 @@
 
 <@global.main pageCss="${m_css.bank_callback}" pageJavascript="${m_js.bank_callback}" title="正在载入">
 
-<div class="my-account-content personal-profile loading-page">
-    <div class="m-header"><em id="iconRegister" class="icon-left"><i></i></em>正在载入...</div>
+<div class="my-account-content personal-profile">
+    <div class="m-header">正在载入...</div>
     <div class="gif-container">
-        <img id="gifImg" src="" alt="">
     </div>
 
 </div>
