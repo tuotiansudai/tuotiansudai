bbd60ac3bc3a4396a41940a9d4b904d28092b800
zhukun5764 <zhukun@tuotiansudai.com>
2018-06-13 20:59:59 +0800

    delete mq

--- ttsd-mq-client/src/main/java/com/tuotiansudai/mq/client/model/MessageQueue.java
+++ ttsd-mq-client/src/main/java/com/tuotiansudai/mq/client/model/MessageQueue.java
@@ -11,59 +11,43 @@ public enum MessageQueue {
     3. 长度不超过256个字符。
     */
     CouponAssigning("CouponAssigning"),
+    ExperienceAssigning("ExperienceAssigning"),
+
     UserCouponReset("UserCouponReset"),
-    InvestCallback("InvestCallback"),
-    TransferInvestCallback("TransferInvestCallback"),
-    UserRegistered_CompletePointTask("UserRegistered-CompletePointTask"),
     GenerateReferrerRelation("GenerateReferrerRelation"),
     InvestSuccess_ExperienceRepay("InvestSuccess-ExperienceRepay"),
-    InvestSuccess_ActivityAnnualized("InvestSuccess-ActivityAnnualized"),
-    InvestSuccess_Ranking("InvestSuccess-Ranking"),
-    InvestSuccess_WeChatHelp("InvestSuccess-WeChatHelp"),
-    InvestSuccess_ActivityInvest("InvestSuccess-ActivityInvest"),
-    ActivityWeChatCoupon("ActivityWeChatCoupon"),
-    Celebration_Coupon("Celebration-Coupon"),
-    NewYearActivity_Coupon("NewYearActivity-Coupon"),
-    StartWorkActivity_Coupon("StartWorkActivity-Coupon"),
     TurnOnNoPasswordInvest_CompletePointTask("TurnOnNoPasswordInvest-CompletePointTask"),
+
     ExperienceRepayCallback("ExperienceRepayCallback"),
-    RepaySuccess_InvestRepay("RepaySuccess-InvestRepay"),
-    RepaySuccess_CouponRepay("RepaySuccess-CouponRepay"),
-    RepaySuccess_ExtraRateRepay("RepaySuccess-ExtraRateRepay"),
+
     EventMessage("EventMessage"),
     ManualMessage("ManualMessage"),
     PushMessage("PushMessage"),
     EMailMessage("EMailMessage"),
-    RepaySuccessInvestRepayCallback("RepaySuccessInvestRepayCallback"),
-    RepaySuccessCouponRepayCallback("RepaySuccessCouponRepayCallback"),
-    RepaySuccessExtraRateRepayCallback("RepaySuccessExtraRateRepayCallback"),
+
     AuditLog("AuditLog"),
     UserOperateLog("UserOperateLog"),
     TransferReferrerRewardCallback("TransferReferrerRewardCallback"),
     TransferRedEnvelopCallback("TransferRedEnvelopCallback"),
     LoginLog("LoginLog"),
+
     TransferAnxinContract("TransferAnxinContract"),
     QueryAnxinContract("QueryAnxinContract"),
-    LoanOut("LoanOut"),
+
     LoanStartRaising("LoanStartRaising"),
     LoanStopRaising("LoanStopRaising"),
+
     CancelTransferApplication("CancelTransferApplication"),
     CouponSmsAssignNotify("CouponSmsAssignNotify"),
     CouponSmsExpiredNotify("CouponSmsExpiredNotify"),
+
     WeChatBoundNotify("WeChatBoundNotify"),
     WeChatMessageNotify("WeChatMessageNotify"),
-    HuiZuRentRepayNotifyQueue("HuiZuRentRepayNotifyQueue"),
-    HuiZuOpenAutoRepayQueue("HuiZuOpenAutoRepayQueue"), //慧租开通自动还款
-    ExperienceAssigning("ExperienceAssigning"),
+
     SystemBill("SystemBill"),
     AmountTransfer("AmountTransfer"),
-    CreditLoanOutQueue("CreditLoanOutQueue"), //慧租信用贷放款
-    CreditLoanRepayQueue("CreditLoanRepayQueue"), //慧租信用贷还款
-    CreditLoanActivateAccountQueue("CreditLoanActivateAccountQueue"),
-    CreditLoanBill("CreditLoanBill"),
     Payroll("Payroll"),
     CreditLoanBalanceAlert("CreditLoanBalanceAlert"), // 信用贷账户余额不足，短信提醒（延时job）
-    LuxuryStageRepayQueue("LuxuryStageRepayQueue"), // 奢侈品还款
 
     //fudian new queues
     BindBankCard_Success("BindBankCard-Success"),
@@ -94,7 +78,12 @@ public enum MessageQueue {
     LoanFull_GenerateAnXinContract("LoanOutSuccess-GenerateAnXinContract"),
 
     LoanRepay_Success("LoanRepay-Success"),
-    LoanCallback_Success("LoanCallback-Success");
+    LoanCallback_Success("LoanCallback-Success"),
+
+    RepaySuccess_InvestRepay("RepaySuccess-InvestRepay"),
+    RepaySuccess_CouponRepay("RepaySuccess-CouponRepay"),
+    RepaySuccess_ExtraRateRepay("RepaySuccess-ExtraRateRepay"),
+    ;
 
     private final String queueName;
 
