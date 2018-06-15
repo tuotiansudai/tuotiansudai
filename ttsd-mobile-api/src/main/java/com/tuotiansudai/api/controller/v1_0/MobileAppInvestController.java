29e1bc572bda6549eae663a58142102763b37b21
zhukun5764 <zhukun@tuotiansudai.com>
2018-06-15 19:58:07 +0800

    fix bug

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppInvestController.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/controller/v1_0/MobileAppInvestController.java
@@ -1,17 +1,16 @@
 package com.tuotiansudai.api.controller.v1_0;
 
-import com.tuotiansudai.api.dto.v1_0.*;
+import com.tuotiansudai.api.dto.v1_0.BankAsynResponseDto;
+import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
+import com.tuotiansudai.api.dto.v1_0.InvestRequestDto;
 import com.tuotiansudai.api.service.v1_0.MobileAppInvestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 
 @RestController
