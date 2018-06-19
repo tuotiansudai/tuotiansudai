a38395714d2f7621e5fa30917906eb04f75b2dcd
zhukun5764 <zhukun@tuotiansudai.com>
2018-06-19 10:16:05 +0800

    merge fudian master

--- ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBankCardServiceImpl.java
+++ ttsd-mobile-api/src/main/java/com/tuotiansudai/api/service/v1_0/impl/MobileAppBankCardServiceImpl.java
@@ -2,8 +2,8 @@ package com.tuotiansudai.api.service.v1_0.impl;
 
 import com.tuotiansudai.api.dto.v1_0.BankAsynResponseDto;
 import com.tuotiansudai.api.dto.v1_0.BaseResponseDto;
-import com.tuotiansudai.api.dto.v1_0.ReturnMessage;
 import com.tuotiansudai.api.service.v1_0.MobileAppBankCardService;
+import com.tuotiansudai.api.util.CommonUtils;
 import com.tuotiansudai.fudian.message.BankAsyncMessage;
 import com.tuotiansudai.repository.model.Source;
 import com.tuotiansudai.service.BankBindCardService;
@@ -23,22 +23,12 @@ public class MobileAppBankCardServiceImpl implements MobileAppBankCardService {
     @Override
     public BaseResponseDto<BankAsynResponseDto> bindBankCard(String loginName, String ip, String deviceId) {
         BankAsyncMessage bankAsyncMessage = bankBindCardService.bind(loginName, Source.MOBILE, ip, deviceId);
-        if (bankAsyncMessage.isStatus()){
-            BaseResponseDto<BankAsynResponseDto> responseDto = new BaseResponseDto<>(ReturnMessage.SUCCESS);
-            responseDto.setData(new BankAsynResponseDto(bankAsyncMessage.getUrl(), bankAsyncMessage.getData()));
-            return responseDto;
-        }
-        return new BaseResponseDto<>(ReturnMessage.BIND_CARD_FAIL);
+        return CommonUtils.mapToFormData(bankAsyncMessage);
     }
 
     @Override
     public BaseResponseDto<BankAsynResponseDto> unBindBankCard(String loginName, String ip, String deviceId) {
         BankAsyncMessage bankAsyncMessage = bankBindCardService.unbind(loginName, Source.MOBILE, ip, deviceId);
-        if (bankAsyncMessage.isStatus()){
-            BaseResponseDto<BankAsynResponseDto> responseDto = new BaseResponseDto<>(ReturnMessage.SUCCESS);
-            responseDto.setData(new BankAsynResponseDto(bankAsyncMessage.getUrl(), bankAsyncMessage.getData()));
-            return responseDto;
-        }
-        return new BaseResponseDto<>(ReturnMessage.UNBIND_CARD_FAIL);
+        return CommonUtils.mapToFormData(bankAsyncMessage);
     }
 }
