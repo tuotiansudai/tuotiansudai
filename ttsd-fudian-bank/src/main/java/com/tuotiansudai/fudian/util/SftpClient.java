c377532a6ebf9a9cc4c063751f9038012411ea1a
zhukun5764 <zhukun@tuotiansudai.com>
2018-06-21 11:00:16 +0800

    sftp client

--- ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/util/SftpClient.java
+++ ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/util/SftpClient.java
@@ -2,7 +2,6 @@ package com.tuotiansudai.fudian.util;
 
 
 import com.jcraft.jsch.*;
-import com.tuotiansudai.fudian.dto.QueryRechargeDownloadDto;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,7 +10,11 @@ import java.util.Properties;
 
 public class SftpClient {
 
-    private static final int TIME_OUT = 60000; // 超时时间60s
+    private static final int TIME_OUT = 60000;
+    private static final String FTP_HOST = "172.16.88.61";
+    private static final int FTP_PORT = 22;
+    private static final String FTP_USERNAME = "username";
+    private static final String FTP_PASSWORD = "password";
 
     private Session session = null;
 
@@ -19,28 +22,22 @@ public class SftpClient {
 
     public ChannelSftp getChannel() throws JSchException {
 
-        String ftpHost = "172.16.88.61";
-        int port = 22;
-        String ftpUserName = "sftpuser";
-        String ftpPassword = "sftpuser";
-
-
-        JSch jsch = new JSch(); // 创建JSch对象
-        session = jsch.getSession(ftpUserName, ftpHost, port); // 根据用户名，主机ip，端口获取一个Session对象
-        session.setPassword(ftpPassword); // 设置密码
+        JSch jsch = new JSch();
+        session = jsch.getSession(FTP_USERNAME, FTP_HOST, FTP_PORT);
+        session.setPassword(FTP_PASSWORD);
 
         Properties config = new Properties();
         config.put("StrictHostKeyChecking", "no");
-        session.setConfig(config); // 为Session对象设置properties
-        session.setTimeout(TIME_OUT); // 设置timeout时间
-        session.connect(); // 通过Session建立链接
+        session.setConfig(config);
+        session.setTimeout(TIME_OUT);
+        session.connect();
 
-        channel = session.openChannel("sftp"); // 打开SFTP通道
-        channel.connect(); // 建立SFTP通道的连接
+        channel = session.openChannel("sftp");
+        channel.connect();
         return (ChannelSftp) channel;
     }
 
-    public void closeChannel() throws Exception {
+    public void closeChannel(){
         if (channel != null) {
             channel.disconnect();
         }
@@ -49,21 +46,16 @@ public class SftpClient {
         }
     }
 
-
-
     public void download(String path, String name) throws JSchException, SftpException, IOException {
         ChannelSftp sftp = getChannel();
-        sftp.cd(path);
         InputStream inputStream = sftp.get(path + "/" + name);
         ObjectInputStream objectInputStream = null;
         try {
             objectInputStream = new ObjectInputStream(inputStream);
-            QueryRechargeDownloadDto[] dtos = (QueryRechargeDownloadDto[]) objectInputStream.readObject();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            inputStream.close();
-            objectInputStream.close();
+            closeChannel();
         }
     }
 }
