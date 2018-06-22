f5ae405aac8f0809db2a25c0c0ed86116bfd79e9
zhukun5764 <zhukun@tuotiansudai.com>
2018-06-22 15:55:50 +0800

    fix bug

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/ContractController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/ContractController.java
@@ -86,13 +86,14 @@ public class ContractController {
 
     @RequestMapping(value = "/invest/contractNo/{contractNo}", method = RequestMethod.GET)
     public void findContract(@PathVariable String contractNo, HttpServletResponse response) {
-//        String loginName = LoginUserInfo.getLoginName();
-//
-//        if (Strings.isNullOrEmpty(loginName) || !investService.isUserContractNo(loginName, contractNo)) {
-//            return;
-//        }
+
         logger.info(MessageFormat.format("contract download: loginName:{0}", LoginUserInfo.getLoginName()));
 
+        String loginName = LoginUserInfo.getLoginName();
+
+        if (Strings.isNullOrEmpty(loginName) || !investService.isUserContractNo(loginName, contractNo)) {
+            return;
+        }
 
         byte[] pdf = anxinWrapperClient.printAnxinContract(contractNo);
         try {
