8f58d78c786dac4a1b8fd8824c267a7a0baf4141
Xiduan Gao <gaoxiduan@gmail.com>
2018-07-06 17:33:43 +0800

    fixed amount transfer

--- ttsd-amount-mq-consumer/src/main/java/com/tuotiansudai/Application.java
+++ ttsd-amount-mq-consumer/src/main/java/com/tuotiansudai/Application.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication(
         scanBasePackages = {
                 "com.tuotiansudai.mq.consumer.amount",
-                "com.tuotiansudai.repository.mapper"
+                "com.tuotiansudai.repository.mapper",
+                "com.tuotiansudai.rest.client.mapper"
         },
         exclude = {FreeMarkerAutoConfiguration.class})
 @Import({MQConsumerConfig.class, MQProducerConfig.class})
--- ttsd-user-repository/src/main/java/com/tuotiansudai/rest/client/mapper/UserMapper.java
+++ ttsd-user-repository/src/main/java/com/tuotiansudai/rest/client/mapper/UserMapper.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface UserMapper {
     UserModel findByLoginNameOrMobile(String loginNameOrMobile);
 
