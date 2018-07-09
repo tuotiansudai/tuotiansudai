4573aa9a2cd6574786b39c73c35fd67e30bcf601
zhukun5764 <zhukun@tuotiansudai.com>
2018-07-09 20:15:55 +0800

    fix bug

---
+++ ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/config/MybatisFudianConfig.java
@@ -0,0 +1,7 @@
+package com.tuotiansudai.fudian.config;
+
+/**
+ * Created by zhukun on 18/7/9.
+ */
+public class MybatisFudianConfig {
+}
---
+++ ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/config/MybatisUmpConfig.java
@@ -0,0 +1,5 @@
+package com.tuotiansudai.fudian.config;
+
+
+public class MybatisUmpConfig {
+}
