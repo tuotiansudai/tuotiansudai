3a1464536d2bdbab4ae26cdb9edf264305fb061a
zhukun5764 <zhukun@tuotiansudai.com>
2018-07-16 16:49:14 +0800

    delete UmPayServType type

--- ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/ump/UmPayServType.java
+++ ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/ump/UmPayServType.java
@@ -5,49 +5,17 @@ package com.tuotiansudai.fudian.ump;
  */
 public enum UmPayServType {
     /**
-     * 投资
-     */
-    TRANSFER_IN_INVEST("01"),
-
-    /**
-     * 债权购买
-     */
-    TRANSFER_IN_TRANSFER("02"),
-
-    /**
      * 还款
      */
     TRANSFER_IN_REPAY("03"),
-
-    /**
-     * 流标后返款
-     */
-    TRANSFER_OUT_CANCEL_PAYBACK("51"),
-
     /**
      * 平台收费
      */
     TRANSFER_OUT_PLATFORM_FEE("52"),
-
-    /**
-     * 放款
-     */
-    TRANSFER_OUT_LOAN_OUT("53"),
-
     /**
      * 还款后返款
      */
-    TRANSFER_OUT_REPAY_PAYBACK("54"),
-
-    /**
-     * 债权转让的返款
-     */
-    TRANSFER_OUT_TRANSFER("56"),
-
-    /**
-     * 超投返款，债权转让超投返款（撤资后返款）
-     */
-    TRANSFER_OVER_INVEST_PAYBACK("57");
+    TRANSFER_OUT_REPAY_PAYBACK("54");
 
     private final String code;
 
