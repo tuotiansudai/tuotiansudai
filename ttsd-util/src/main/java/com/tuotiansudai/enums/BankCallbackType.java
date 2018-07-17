97e8df72224899e70ca830d59218c2fa4e512f30
zhukun5764 <zhukun@tuotiansudai.com>
2018-07-17 15:37:47 +0800

    fix bug

--- ttsd-util/src/main/java/com/tuotiansudai/enums/BankCallbackType.java
+++ ttsd-util/src/main/java/com/tuotiansudai/enums/BankCallbackType.java
@@ -2,31 +2,28 @@ package com.tuotiansudai.enums;
 
 public enum BankCallbackType {
 
-    REGISTER("实名认证成功", "/register/account", "/register/account", "get"),
-    CARD_BIND("银行卡绑定成功", "/bank-card/bind/source/WEB", "/bank-card/bind/source/M", "post"),
-    CANCEL_CARD_BIND("银行卡解绑成功", "/personal-info", "/personal-info", "get"),
+    REGISTER("实名认证", "/register/account", "/register/account", "get"),
+    CARD_BIND("银行卡绑定", "/bank-card/bind/source/WEB", "/bank-card/bind/source/M", "post"),
+    CANCEL_CARD_BIND("银行卡解绑", "/personal-info", "/personal-info", "get"),
 
-    RECHARGE("充值成功", "/recharge", "/recharge", "get"),
-    WITHDRAW("提现申请成功", "/withdraw", "/withdraw",  "get"),
+    RECHARGE("充值", "/recharge", "/recharge", "get"),
+    WITHDRAW("提现申请", "/withdraw", "/withdraw",  "get"),
 
-    AUTHORIZATION("免密投资开启成功", "/personal-info", "/personal-info", "get"),
+    AUTHORIZATION("开启免密投资", "/personal-info", "/personal-info", "get"),
 
-    PASSWORD_RESET("支付密码重置成功", "/personal-info/reset-bank-password/source/WEB",  "/personal-info/reset-bank-password/source/M", "post"),
-
+    PASSWORD_RESET("重置支付密码", "/personal-info/reset-bank-password/source/WEB",  "/personal-info/reset-bank-password/source/M", "post"),
 
     PHONE_UPDATE("", "", "", "get"),
 
-    LOAN_INVEST("投资成功", "/loan-list", "/loan-list",  "get"),
-    LOAN_FAST_INVEST("投资成功", "/loan-list", "/loan-list",  "get"),
-    LOAN_CREDIT_INVEST("投资成功", "/transfer-list", "/transfer-list",  "get"),
+    LOAN_INVEST("投资", "/loan-list", "/loan-list",  "get"),
+    LOAN_FAST_INVEST("投资", "/loan-list", "/loan-list",  "get"),
+    LOAN_CREDIT_INVEST("投资", "/transfer-list", "/transfer-list",  "get"),
 
-    LOAN_REPAY("还款成功", "/loaner/loan-list", "/loaner/loan-list",  "get"),
+    LOAN_REPAY("还款", "/loaner/loan-list", "/loaner/loan-list",  "get"),
     LOAN_FAST_REPAY("", "", "", "get"),
 
     MERCHANT_TRANSFER("", "", "", "get"),
-
     ;
-
 
     private final String title;
 
