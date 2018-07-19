1e503dc6abbc78e72452cec181a1e799473b58b8
liujiangshan123 <liujiangshan@tuotiansudai.com>
2018-07-19 16:14:52 +0800

    1、删除原来的 message写法

--- ttsd-amount-mq-consumer/src/main/java/com/tuotiansudai/mq/consumer/amount/service/BankSystemBillService.java
+++ ttsd-amount-mq-consumer/src/main/java/com/tuotiansudai/mq/consumer/amount/service/BankSystemBillService.java
@@ -2,34 +2,20 @@ package com.tuotiansudai.mq.consumer.amount.service;
 
 import com.tuotiansudai.enums.BillOperationType;
 import com.tuotiansudai.enums.SystemBillBusinessType;
-import com.tuotiansudai.enums.SystemBillMessageType;
 import com.tuotiansudai.message.BankSystemBillMessage;
-import com.tuotiansudai.message.SystemBillMessage;
 import com.tuotiansudai.repository.mapper.BankSystemBillMapper;
-import com.tuotiansudai.repository.mapper.SystemBillMapper;
 import com.tuotiansudai.repository.model.BankSystemBillModel;
-import com.tuotiansudai.repository.model.SystemBillModel;
-import com.tuotiansudai.repository.model.SystemBillOperationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 @Service
 public class BankSystemBillService {
 
     private static final Logger logger = LoggerFactory.getLogger(BankSystemBillService.class);
-
-    public static Map<SystemBillMessageType,SystemBillOperationType> operationTypeMap=new HashMap<>(2);
-    static {
-        operationTypeMap.put(SystemBillMessageType.TRANSFER_IN,SystemBillOperationType.IN);
-        operationTypeMap.put(SystemBillMessageType.TRANSFER_OUT,SystemBillOperationType.OUT);
-    }
 
 
     @Autowired
@@ -50,6 +36,7 @@ public class BankSystemBillService {
         String detail = operationType.getDescription();
         String bankOrderDate = message.getBankOrderDate();
         long businuessId = message.getBusinessId();
+
         BankSystemBillModel bankSystemBillModel = new BankSystemBillModel(businuessId, bankOrderNo, bankOrderDate, amount, operationType, businessType, detail);
         bankSystemBillMapper.create(bankSystemBillModel);
     }
