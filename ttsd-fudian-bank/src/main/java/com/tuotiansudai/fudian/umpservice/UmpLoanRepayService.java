723affefbd9985bd53965230215053cb113eeb60
zhukun5764 <zhukun@tuotiansudai.com>
2018-08-06 19:37:25 +0800

    fix bug

--- ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/umpservice/UmpLoanRepayService.java
+++ ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/umpservice/UmpLoanRepayService.java
@@ -194,8 +194,8 @@ public class UmpLoanRepayService {
                                     umpInvestRepayDto.getInvestRepayId(),
                                     umpInvestRepayDto.getCorpus(),
                                     umpInvestRepayDto.getInterest(),
-                                    umpInvestRepayDto.getDefaultFee(),
                                     umpInvestRepayDto.getFee(),
+                                    umpInvestRepayDto.getDefaultFee(),
                                     umpInvestRepayDto.getIsNormalRepay()));
 
                             continue;
