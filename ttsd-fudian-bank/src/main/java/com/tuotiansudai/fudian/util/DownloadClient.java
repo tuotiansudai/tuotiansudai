7506558f83cfb401c5004adb11a8a58a40542b79
liujiangshan123 <liujiangshan@tuotiansudai.com>
2018-08-16 16:40:39 +0800

    修改数据

--- ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/util/DownloadClient.java
+++ ttsd-fudian-bank/src/main/java/com/tuotiansudai/fudian/util/DownloadClient.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
+import java.io.*;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -65,15 +65,42 @@ public class DownloadClient {
 
     public List<String> download(ChannelSftp sftp, String path, String name) throws SftpException {
         InputStream inputStream = sftp.get(path + "/" + name);
-        oss.putObject(downloadConfig.getBucketName(), name, inputStream);
+        byte[] bytes = convertToByteOutWithInputClose(inputStream);
+        oss.putObject(downloadConfig.getBucketName(), name, new ByteArrayInputStream(bytes));
         List<String> params = new ArrayList<String>();
-        try (Scanner scanner = new Scanner(inputStream)) {
-            while (scanner.hasNext()) {
-                params.add(scanner.next());
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(bytes)));
+        ) {
+            String content = null;
+            while ((content = br.readLine()) != null) {
+                params.add(content);
             }
-        } catch (Exception e) {
-            logger.error(MessageFormat.format("download fail fileName:{0}, error:{1}", name, e.getMessage()));
+        } catch (IOException e) {
+            logger.error("[DownloadClient] download() occured error",e);
         }
         return params;
+    }
+
+    /**
+     * 缺点:把文件加载到内存中来
+     * @param inputStream
+     * @return
+     */
+    private byte[] convertToByteOutWithInputClose(InputStream inputStream) {
+        byte[] buffer = new byte[1024];
+        try (
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ) {
+            int len;
+            while ((len = inputStream.read(buffer)) > -1) {
+                baos.write(buffer, 0, len);
+            }
+            baos.flush();
+            inputStream.close();
+            return baos.toByteArray();
+        } catch (IOException e) {
+            logger.error("[DownloadClient] convertToByteOutWithInputClose occured error", e);
+        }
+        return buffer;
     }
 }
