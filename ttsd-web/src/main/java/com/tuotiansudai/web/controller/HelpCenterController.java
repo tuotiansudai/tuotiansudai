8e1d9c1d526168f619c465ec37df50cc1612cc9f
liujiangshan123 <liujiangshan@tuotiansudai.com>
2018-08-17 15:29:06 +0800

    1、修改逻辑

--- ttsd-web/src/main/java/com/tuotiansudai/web/controller/HelpCenterController.java
+++ ttsd-web/src/main/java/com/tuotiansudai/web/controller/HelpCenterController.java
@@ -1,5 +1,6 @@
 package com.tuotiansudai.web.controller;
 
+import com.mysql.jdbc.StringUtils;
 import com.tuotiansudai.dto.BaseWrapperDataDto;
 import com.tuotiansudai.dto.OperationDataDto;
 import com.tuotiansudai.service.OperationDataService;
@@ -8,11 +9,15 @@ import com.tuotiansudai.util.RequestIPParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(path = "/help")
@@ -42,20 +47,43 @@ public class HelpCenterController {
     @RequestMapping(path = "/help-content/vote")
     @ResponseBody
     public BaseWrapperDataDto voteHelpCenter(HttpServletRequest request) {
-        BaseWrapperDataDto wrapperDataDto = new BaseWrapperDataDto();
-        wrapperDataDto.setStatus(true);
-        wrapperDataDto.setData(setHelperVoteNumber(request));
-        return wrapperDataDto;
-    }
-
-    private int setHelperVoteNumber(HttpServletRequest request) {
         String contentId = request.getParameter("contentId");
-        if (contentId == null) {
-            return 0;
-        }
+        String isSolution = request.getParameter("isSolution");
         String ip = RequestIPParser.parse(request);
         String browserInfo = RequestIPParser.getOsAndBrowserInfo(request);
-        redisWrapperClient.hset(HELP_CENTER_VOTE_KEY + contentId, ip + browserInfo, "1");
-        return redisWrapperClient.hlen(HELP_CENTER_VOTE_KEY + contentId) + contentId.hashCode() & 127;
+        String redisKey = HELP_CENTER_VOTE_KEY + contentId;
+
+        HashMap<String, Integer> dataMap = new HashMap();
+        BaseWrapperDataDto wrapperDataDto = new BaseWrapperDataDto();
+        wrapperDataDto.setStatus(true);
+
+        if (contentId == null) {
+            dataMap.put("voteNumber", 0);
+            dataMap.put("isSolution", -1);
+            wrapperDataDto.setData(dataMap);
+            return wrapperDataDto;
+        }
+        String redisisSolution = redisWrapperClient.hget(redisKey, ip + browserInfo);
+        if (StringUtils.isNullOrEmpty(isSolution)) {
+            dataMap.put("voteNumber", redisWrapperClient.hlen(redisKey) + contentId.hashCode() & 127);
+            dataMap.put("isSolution", redisisSolution == null ? -1 : Integer.valueOf(redisisSolution));
+            wrapperDataDto.setData(dataMap);
+            return wrapperDataDto;
+        }
+        if (!("1".equals(isSolution) || "0".equals(isSolution))) {
+            wrapperDataDto.setMessage("参数错误");
+            wrapperDataDto.setStatus(false);
+            return wrapperDataDto;
+        }
+        if (!StringUtils.isEmptyOrWhitespaceOnly(redisisSolution)) {
+            wrapperDataDto.setMessage("已经投票");
+            wrapperDataDto.setStatus(false);
+            return wrapperDataDto;
+        }
+        redisWrapperClient.hset(HELP_CENTER_VOTE_KEY + contentId, ip + browserInfo, isSolution);
+        dataMap.put("voteNumber", redisWrapperClient.hlen(redisKey) + contentId.hashCode() & 127);
+        dataMap.put("isSolution", isSolution == null ? -1 : Integer.valueOf(isSolution));
+        wrapperDataDto.setData(dataMap);
+        return wrapperDataDto;
     }
 }
