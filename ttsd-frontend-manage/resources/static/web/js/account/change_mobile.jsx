47c76db6302961a0e6c17047cd61d8ec4f1b6997
ttsd-luhongmin <luhongmin@tuotiansudai.com>
2018-08-23 16:30:39 +0800

    add errortip

--- ttsd-frontend-manage/resources/static/web/js/account/change_mobile.jsx
+++ ttsd-frontend-manage/resources/static/web/js/account/change_mobile.jsx
@@ -5,4 +5,24 @@ let ValidatorObj= require('publicJs/validator');
 
 let $originMobile = $('#originMobile');
 let $newMobile = $('#newMobile');
+let $errorLi = $('.error-li');
+let originMobile = $originMobile.text();
+let $errorBox = $('.error-box');
+let $btnSub = $('.btn-sub');
+$newMobile.on('keyup',function () {
+    $errorLi.hide();
+    $btnSub.attr('disabled',false);
+    let elementInput = $newMobile.val();
+    if(elementInput==originMobile){
+        $errorBox.text('新手机号不能和原手机号一致');
+        $errorLi.show();
+        $btnSub.attr('disabled',true);
+    }else {
+        if(!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(elementInput))){
+            $errorBox.text('手机号格式不正确');
+            $errorLi.show();
+            $btnSub.attr('disabled',true);
+        }
+    }
+})
 
