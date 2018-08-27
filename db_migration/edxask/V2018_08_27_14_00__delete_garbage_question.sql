20bb5bb409db08062be16a35cd9ef5a5f6006339
zhukun5764 <zhukun@tuotiansudai.com>
2018-08-27 18:33:43 +0800

    fix bug

--- db_migration/edxask/V2018_08_27_14_00__delete_garbage_question.sql
+++ db_migration/edxask/V2018_08_27_14_00__delete_garbage_question.sql
@@ -1,1 +1,2 @@
-delete from question WHERE `created_time` BETWEEN '2018-08-25 00:00:00' and now() and status in ('UNAPPROVED', 'REJECTED');
+delete from question where `created_time` BETWEEN '2018-08-25 00:00:00' and now() and login_name in
+(select temp.login_name from (select login_name from question where `created_time` BETWEEN '2018-08-25 00:00:00' and now() and status in('UNAPPROVED', 'REJECTED') group by login_name having count(login_name) > 10) temp)
