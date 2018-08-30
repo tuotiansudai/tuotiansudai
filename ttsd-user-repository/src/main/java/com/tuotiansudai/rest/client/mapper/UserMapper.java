659d635ede57570a8b1ab181201ceecaee625d1c
zhukun5764 <zhukun@tuotiansudai.com>
2018-08-30 18:32:06 +0800

    fix bug

--- ttsd-user-repository/src/main/java/com/tuotiansudai/rest/client/mapper/UserMapper.java
+++ ttsd-user-repository/src/main/java/com/tuotiansudai/rest/client/mapper/UserMapper.java
@@ -3,13 +3,11 @@ package com.tuotiansudai.rest.client.mapper;
 import com.tuotiansudai.dto.BasePaginationDataDto;
 import com.tuotiansudai.repository.model.UserModel;
 import com.tuotiansudai.repository.model.UserRegisterInfo;
-import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Repository
 public interface UserMapper {
     UserModel findByLoginNameOrMobile(String loginNameOrMobile);
 
