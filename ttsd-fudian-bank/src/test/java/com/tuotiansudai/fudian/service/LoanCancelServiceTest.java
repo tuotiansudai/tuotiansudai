fce2d2101f45db6292146294ffcc64f7a672fcc6
liujiangshan123 <liujiangshan@tuotiansudai.com>
2018-08-30 19:30:07 +0800

    loanCancel注解

---
+++ ttsd-fudian-bank/src/test/java/com/tuotiansudai/fudian/service/LoanCancelServiceTest.java
@@ -0,0 +1,17 @@
+package com.tuotiansudai.fudian.service;
+
+/**
+ * Created by qduljs2011 on 2018/8/30.
+ */
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by qduljs2011 on 2018/8/30.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+public class LoanCancelServiceTest {
+}
