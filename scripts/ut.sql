8a68e02baffbfe2035a8d9fbe264f69af1535968
zhukun5764 <zhukun@tuotiansudai.com>
2018-09-07 17:31:56 +0800

    ut test

--- scripts/ut.sql
+++ scripts/ut.sql
@@ -23,8 +23,8 @@ grant select,insert,update,delete,create,drop,alter,index on ump_operations.* to
 grant select,insert,update,delete,create,drop,alter,index on edxactivity.* to sdactivity@'%' identified by 'sdactivity';
 grant select,insert,update,delete,create,drop,alter,index on edxactivity.* to sdactivity@'localhost' identified by 'sdactivity';
 
-grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to sdpoint@'%' identified by 'sdpoint';
-grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to sdpoint@'localhost' identified by 'sdpoint';
+grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to tuotiansd@'%' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to tuotiansd@'localhost' identified by 'tuotiansd';
 
 grant select,insert,update,delete,create,drop,alter,index on edxask.* to sdask@'%' identified by 'sdask';
 grant select,insert,update,delete,create,drop,alter,index on edxask.* to sdask@'localhost' identified by 'sdask';
