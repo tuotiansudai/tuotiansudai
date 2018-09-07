f485100b5e456b440c506f1c9983fbaed43c33c5
zhukun5764 <zhukun@tuotiansudai.com>
2018-09-07 17:13:46 +0800

    ut test

--- scripts/ut.sql
+++ scripts/ut.sql
@@ -9,37 +9,37 @@ CREATE DATABASE `edxask`;
 CREATE DATABASE `anxin_operations`;
 CREATE DATABASE `edxlog`;
 CREATE DATABASE `edxmessage`;
-# CREATE DATABASE `fudian`;
+CREATE DATABASE `fudian`;
 
-grant select,insert,update,delete,create,drop,alter,index on aa.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on aa.* to tuotiansd@'localhost' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on job_worker.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on job_worker.* to tuotiansd@'localhost' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on sms_operations.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on sms_operations.* to tuotiansd@'localhost' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on ump_operations.* to tuotiansd@'%' identified by 'tuotiansd';
-grant select,insert,update,delete,create,drop,alter,index on ump_operations.* to tuotiansd@'localhost' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter,index on aa.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on aa.* to root@'localhost' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on job_worker.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on job_worker.* to root@'localhost' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on sms_operations.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on sms_operations.* to root@'localhost' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on ump_operations.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on ump_operations.* to root@'localhost' identified by 'root';
 
-grant select,insert,update,delete,create,drop,alter,index on edxactivity.* to sdactivity@'%' identified by 'sdactivity';
-grant select,insert,update,delete,create,drop,alter,index on edxactivity.* to sdactivity@'localhost' identified by 'sdactivity';
+grant select,insert,update,delete,create,drop,alter,index on edxactivity.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on edxactivity.* to root@'localhost' identified by 'root';
 
-grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to sdpoint@'%' identified by 'sdpoint';
-grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to sdpoint@'localhost' identified by 'sdpoint';
+grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on edxpoint.* to root@'localhost' identified by 'root';
 
-grant select,insert,update,delete,create,drop,alter,index on edxask.* to sdask@'%' identified by 'sdask';
-grant select,insert,update,delete,create,drop,alter,index on edxask.* to sdask@'localhost' identified by 'sdask';
+grant select,insert,update,delete,create,drop,alter,index on edxask.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on edxask.* to root@'localhost' identified by 'root';
 
-grant select,insert,update,delete,create,drop,alter,index on anxin_operations.* to sdanxin@'%' identified by 'sdanxin';
-grant select,insert,update,delete,create,drop,alter,index on anxin_operations.* to sdanxin@'localhost' identified by 'sdanxin';
+grant select,insert,update,delete,create,drop,alter,index on anxin_operations.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on anxin_operations.* to root@'localhost' identified by 'root';
 
-grant select,insert,update,delete,create,drop,alter,index on edxlog.* to sdlog@'%' identified by 'sdlog';
-grant select,insert,update,delete,create,drop,alter,index on edxlog.* to sdlog@'localhost' identified by 'sdlog';
+grant select,insert,update,delete,create,drop,alter,index on edxlog.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on edxlog.* to root@'localhost' identified by 'root';
 
-grant select,insert,update,delete,create,drop,alter,index on edxmessage.* to sdmessage@'%' identified by 'sdmessage';
-grant select,insert,update,delete,create,drop,alter,index on edxmessage.* to sdmessage@'localhost' identified by 'sdmessage';
-#
-# grant select,insert,update,delete,create,drop,alter,index on fudian.* to tuotiansd@'%' identified by 'tuotiansd';
-# grant select,insert,update,delete,create,drop,alter,index on fudian.* to tuotiansd@'localhost' identified by 'tuotiansd';
+grant select,insert,update,delete,create,drop,alter,index on edxmessage.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on edxmessage.* to root@'localhost' identified by 'root';
+
+grant select,insert,update,delete,create,drop,alter,index on fudian.* to root@'%' identified by 'root';
+grant select,insert,update,delete,create,drop,alter,index on fudian.* to root@'localhost' identified by 'root';
 
 update mysql.user set password=password('root') where user='root';
 
