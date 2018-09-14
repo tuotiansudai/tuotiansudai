d5355b7e0067fbf94f11b731af184a8633f72565
liujiangshan123 <liujiangshan@tuotiansudai.com>
2018-09-14 19:33:56 +0800

    优化

--- db_migration/aa/V2018_09_14_15_43__create_table_loan_mapping_history.sql
+++ db_migration/aa/V2018_09_14_15_43__create_table_loan_mapping_history.sql
@@ -1,14 +1,16 @@
 CREATE TABLE `loan_mapping_history` (
   `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT
   COMMENT '主键',
-  `identity_number`    VARCHAR(18)        NOT NULL
+  `login_name`    VARCHAR(25)        NOT NULL
   COMMENT '身份证号',
   `loan_id`    BIGINT(20) UNSIGNED NOT NULL
   COMMENT '标的id',
   PRIMARY KEY (`id`),
-  INDEX INDEX_LOAN_MAPPING_HISTORY_IDENTITY_NUMBER(`identity_number`),
+  CONSTRAINT `FK_LOAN_MAPPING_HISTORY_LOGIN_NAME_REF_USER_LOGIN_NAME` FOREIGN KEY (`login_name`) REFERENCES `user` (`login_name`),
+  CONSTRAINT `FK_LOAN_MAPPING_HISTORY_LOAN_ID_REF_LOAN_ID` FOREIGN KEY (`loan_id`) REFERENCES `loan` (`id`),
+  INDEX INDEX_LOAN_MAPPING_HISTORY_LOGIN_NAME(`login_name`),
   INDEX INDEX_LOAN_MAPPING_HISTORY_LOAN_ID(`loan_id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COMMENT = '标的身份证号对应表';
+  COMMENT = '标的loginName对应表';
