f9fa659a7e316d3766f3951fdcaf73cf918c7109
zhukun5764 <zhukun@tuotiansudai.com>
2018-10-10 20:53:58 +0800

    fix bug

--- db_migration/aa/V2018_10_10_19_00__update_coupon_repay_expected_interes_by_loan.sql
+++ db_migration/aa/V2018_10_10_19_00__update_coupon_repay_expected_interes_by_loan.sql
@@ -30,7 +30,7 @@ UPDATE user_coupon
           JOIN invest i ON uc.invest_id = i.id
           JOIN loan l ON i.loan_id = l.id
           JOIN coupon c ON c.id = uc.coupon_id
-        WHERE l.id IN ('30497451731632')) temp ON user_coupon.id = temp.id
+        WHERE l.id IN ('112963360205872')) temp ON user_coupon.id = temp.id
 SET user_coupon.expected_interest = floor(temp.amount * temp.rate / 365 * temp.period),
   user_coupon.expected_fee        = floor(temp.amount * temp.rate / 365 * temp.period * temp.fee_rate);
 
