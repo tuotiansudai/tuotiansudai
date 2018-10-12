4ea727565058ab00f2b823ce557203d5843aae15
liujiangshan123 <liujiangshan@tuotiansudai.com>
2018-10-12 10:59:35 +0800

    修改配置文件

--- scripts/config_deploy.py
+++ scripts/config_deploy.py
@@ -13,8 +13,8 @@ def deploy(etcd, env, pay_fake):
         # 读取每个环境特有的配置 envs/${env}.properties, 生成一个 dict 对象 specified_prop
         deploy_prop.append(load_properties("./ttsd-config/src/main/resources/envs/{0}.properties".format(env)))
         # 从redis里读取 三方账号配置
-        _r = redis.StrictRedis(host=('192.168.1.30' if env != 'CI1' else '127.0.0.1'), port=6379, db=2)
-        deploy_prop.append(_r.hgetall('qa_common_account'))
+        #_r = redis.StrictRedis(host=('192.168.1.30' if env != 'CI1' else '127.0.0.1'), port=6379, db=2)
+        #deploy_prop.append(_r.hgetall('qa_common_account'))
 
         flush_etcd(etcd, deploy_prop, pay_fake)
         return
