d443c3d269aa0636557d18c8334d113dc68c4bac
zhukun5764 <zhukun@tuotiansudai.com>
2018-10-15 16:58:08 +0800

    test

--- scripts/prod_properties_deploy.py
+++ scripts/prod_properties_deploy.py
@@ -3,9 +3,11 @@ from scripts import etcd_client
 
 
 def flush_prod_properties(etcd):
+    print '---------------------------------------start'
     file_names = file_name('../prod-properties')
-
+    print file_names
     for file in file_names:
+        print '123123123'.format(etcd.get(file))
         if not etcd.get(file):
             etcd.put(file, 'SUCCESS')
             deploy_prop = load_properties('../prod-properties/{0}'.format(file))
@@ -14,6 +16,9 @@ def flush_prod_properties(etcd):
                 for key, value in props.items():
                     etcd.put(key, value)
                     print 'put {0}={1}'.format(key, value)
+
+    print '---------------------------------------end'
+
 
 
 def file_name(file_dir):
