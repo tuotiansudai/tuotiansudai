6ad66696895843ed0ca997f3cc5733bbdbe32702
zhukun5764 <zhukun@tuotiansudai.com>
2018-12-17 18:06:21 +0800

    add sql update user bill trainsfer interest fee

---
+++ db_migration/aa/V2018_12_17_16_20__update_transfer_interest_fee_user_bill_business_type.sql
@@ -0,0 +1,9 @@
+BEGIN;
+UPDATE user_bill
+  INNER JOIN (SELECT u.id
+              FROM user_bill u
+                JOIN transfer_application t ON u.order_id = t.id
+              WHERE u.business_type = 'INVEST_FEE' AND t.status = 'SUCCESS' AND
+                    t.transfer_time BETWEEN '2018-12-17 00:00:00' AND now()) temp ON user_bill.id = temp.id
+SET business_type = 'TRANSFER_INVEST_FEE';
+COMMIT;
