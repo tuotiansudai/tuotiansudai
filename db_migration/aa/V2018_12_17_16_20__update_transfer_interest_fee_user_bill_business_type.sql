0e9cde8d6306f1c383b0b9b068648c619532d836
zhukun5764 <zhukun@tuotiansudai.com>
2018-12-17 18:18:16 +0800

    fix sql

--- db_migration/aa/V2018_12_17_16_20__update_transfer_interest_fee_user_bill_business_type.sql
+++ db_migration/aa/V2018_12_17_16_20__update_transfer_interest_fee_user_bill_business_type.sql
@@ -4,6 +4,6 @@ UPDATE user_bill
               FROM user_bill u
                 JOIN transfer_application t ON u.order_id = t.id
               WHERE u.business_type = 'INVEST_FEE' AND t.status = 'SUCCESS' AND
-                    t.transfer_time BETWEEN '2018-12-17 00:00:00' AND now()) temp ON user_bill.id = temp.id
+                    t.transfer_time BETWEEN '2018-12-17 00:00:00' AND '2018-12-17 23:00:00') temp ON user_bill.id = temp.id
 SET business_type = 'TRANSFER_INVEST_FEE';
 COMMIT;
