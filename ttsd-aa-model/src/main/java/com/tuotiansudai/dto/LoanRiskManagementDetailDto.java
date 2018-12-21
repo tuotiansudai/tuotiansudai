7c50239225521fae8e71c9a304eff3b6c99a164d
zhukun5764 <zhukun@tuotiansudai.com>
2018-12-21 18:38:24 +0800

    fix bug

--- ttsd-aa-model/src/main/java/com/tuotiansudai/dto/LoanRiskManagementDetailDto.java
+++ ttsd-aa-model/src/main/java/com/tuotiansudai/dto/LoanRiskManagementDetailDto.java
@@ -1,20 +1,36 @@
 package com.tuotiansudai.dto;
 
+import com.google.common.base.Strings;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LoanRiskManagementDetailDto implements Serializable{
 
-    private String id;
+    private long id;
     private String title;
     private boolean checked;
-    private String detail;
+    private List<String> details;
 
-    public String getId() {
-        return id;
+    public LoanRiskManagementDetailDto() {
     }
 
-    public void setId(String id) {
+    public LoanRiskManagementDetailDto(long id, String title, boolean checked, String detail) {
         this.id = id;
+        this.title = title;
+        this.checked = checked;
+        this.details = Strings.isNullOrEmpty(detail) ? new ArrayList<>() : Arrays.asList(detail .split(","));
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -33,11 +49,11 @@ public class LoanRiskManagementDetailDto implements Serializable{
         this.checked = checked;
     }
 
-    public String getDetail() {
-        return detail;
+    public List<String> getDetails() {
+        return details;
     }
 
-    public void setDetail(String detail) {
-        this.detail = detail;
+    public void setDetails(List<String> details) {
+        this.details = details;
     }
 }
